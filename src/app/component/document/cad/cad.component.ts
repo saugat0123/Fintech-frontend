4bf292080ab9002b0150163fbfc88370590177ea
Albert Shrestha <shrestha.albert.as@gmail.com>
2019-02-25 12:22:09 +0545

    doc

---
+++ src/app/component/document/cad/cad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cad',
+  templateUrl: './cad.component.html',
+  styleUrls: ['./cad.component.css']
+})
+export class CadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/component/document/renewal/renewal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-renewal',
+  templateUrl: './renewal.component.html',
+  styleUrls: ['./renewal.component.css']
+})
+export class RenewalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
