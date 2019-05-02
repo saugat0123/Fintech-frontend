c165c859255a4caf22dcf2e83e9e653657465505
Elvin Shrestha <elwyncrestha@gmail.com>
2019-05-02 18:03:06 +0545

    Applicant Personal Details

--- src/app/module/customer-opening/component/customer-base/customer-base.component.ts
+++ src/app/module/customer-opening/component/customer-base/customer-base.component.ts
@@ -18,16 +18,15 @@ export class CustomerBaseComponent implements OnInit {
 
     ngOnInit() {
         this.navigations = new Array<Navigation>(
-            {id: 1, name: 'Existing Account Detail'},
-            {id: 2, name: 'Account Type'},
-            {id: 3, name: 'Applicant Personal Details'},
-            {id: 4, name: 'Employment Details'},
-            {id: 5, name: 'Joint Applicant'},
-            {id: 6, name: 'Beneficiary Details'},
-            {id: 7, name: 'Nominee Details'},
-            {id: 8, name: 'Required Service'},
-            {id: 9, name: 'Declarations'},
-            {id: 10, name: 'Upload'}
+            {id: 1, name: 'Account Details'},
+            {id: 2, name: 'Applicant Personal Details'},
+            {id: 3, name: 'Employment Details'},
+            {id: 4, name: 'Joint Applicant'},
+            {id: 5, name: 'Beneficiary Details'},
+            {id: 6, name: 'Nominee Details'},
+            {id: 7, name: 'Required Service'},
+            {id: 8, name: 'Declarations'},
+            {id: 9, name: 'Upload'}
         );
         this.customerDataService.totalNavsCount = this.navigations.length;
     }
