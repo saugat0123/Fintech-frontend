378c45b8db44b8ae693e478545465b8a52df6165
sthaamula <sthaamulya@gmail.com>
2019-05-31 14:00:03 +0545

    Add: eligible and non-eligible requests.

--- src/app/feature/admin/component/eligibility/non-eligible-requests/non-eligible-requests.component.ts
+++ src/app/feature/admin/component/eligibility/non-eligible-requests/non-eligible-requests.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Applicant} from '../../../modal/applicant';
+import {Pageable} from '../../../../../@core/service/baseservice/common-pageable';
+import {NewRequestService} from '../new-requests/new-request.service';
+import {ToastService} from '../../../../../@core/utils';
+import {PaginationUtils} from '../../../../../@core/utils/PaginationUtils';
+import {Alert, AlertType} from '../../../../../@theme/model/Alert';
 
 @Component({
   selector: 'app-non-eligible-requests',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NonEligibleRequestsComponent implements OnInit {
 
-  constructor() { }
+  spinner = false;
+  applicantList: Array<Applicant> = new Array<Applicant>();
 
-  ngOnInit() {
+  page = 1;
+  search: any = {};
+  pageable: Pageable = new Pageable();
+
+  constructor(private newRequestService: NewRequestService,
+              private toastService: ToastService) { }
+
+  static loadData(other: NonEligibleRequestsComponent) {
+
+    other.spinner = true;
+    other.newRequestService.getAllWithoutSearchObject(other.page, 10).subscribe((response: any) => {
+          other.applicantList = response.detail.content;
+          other.applicantList.forEach((applicant, index) => {
+            if (applicant.eligibilityStatus === 'ELIGIBLE') {
+              other.applicantList.splice(index, 1);
+            }
+          });
+          console.log(other.applicantList);
+          other.pageable = PaginationUtils.getPageable(response.detail);
+
+          other.spinner = false;
+        }, error => {
+
+          console.log(error);
+
+          other.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Data!'));
+          other.spinner = false;
+        }
+    );
   }
 
+  ngOnInit() {
+    NonEligibleRequestsComponent.loadData(this);
+    console.log(this.applicantList);
+  }
+
+  changePage(page: number) {
+    this.page = page;
+
+    NonEligibleRequestsComponent.loadData(this);
+  }
 }
