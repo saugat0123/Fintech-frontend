41342a5276a3f6da4c45963354284564def8300a
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-06-23 08:23:25 +0545

    ahow hide branch as user role selected in user-form and added role type in role

--- src/app/feature/admin/component/user/user-form/user-form.component.ts
+++ src/app/feature/admin/component/user/user-form/user-form.component.ts
@@ -1,21 +1,21 @@
-import {Component, DoCheck, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../../modal/user';
 import {Branch} from '../../../modal/branch';
 import {Role} from '../../../modal/role';
 import {CommonService} from '../../../../../@core/service/baseservice/common-baseservice';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {ModalResponse, ToastService} from '../../../../../@core/utils';
-import {Alert, AlertType} from '../../../../../@theme/model/Alert';
 import {UserService} from '../user.service';
 import {RoleService} from '../../role-permission/role.service';
 import {BranchService} from '../../branch/branch.service';
 import {RoleAccess} from '../../../modal/role-access';
+import {Alert, AlertType} from '../../../../../@theme/model/Alert';
 
 @Component({
     selector: 'app-user-form',
     templateUrl: './user-form.component.html'
 })
-export class UserFormComponent implements OnInit, DoCheck {
+export class UserFormComponent implements OnInit {
 
     @Input()
     model: User;
@@ -23,11 +23,15 @@ export class UserFormComponent implements OnInit, DoCheck {
     submitted = false;
     spinner = false;
     branchList: Array<Branch>;
-    branch = new Branch();
+    branch = new Array<Branch>();
     roleList: Array<Role>;
     role = new Role();
     selectedRole;
     isSpecific = false;
+    isAll = false;
+    tempBranch;
+    finalBranchList = [];
+    branchIdList;
 
     constructor(
         private commonService: CommonService,
@@ -50,26 +54,38 @@ export class UserFormComponent implements OnInit, DoCheck {
         this.roleService.getActiveRoles().subscribe((response: any) => {
             this.roleList = response.detail;
         });
+
+        this.getEdit();
+
     }
 
-    ngDoCheck(): void {
-        if (this.model.id == null) {
-            this.task = 'Add';
-        } else {
-            if (this.model.branch != null) {
-                this.branch = this.model.branch;
-            }
-            if (this.model.role != null) {
-                this.role = this.model.role;
-            }
-            this.task = 'Edit';
-        }
-    }
 
     onSubmit() {
         this.submitted = true;
-        this.model.branch = this.branch;
+        this.finalBranchList = [];
+
+        if (this.selectedRole.roleAccess === RoleAccess[0]) {
+            if (this.branchIdList.length > 0) {
+                for (let i = 0; i < this.branchIdList.length; i++) {
+                    this.tempBranch = new Branch();
+                    this.tempBranch.id = this.branchIdList[i];
+                    this.finalBranchList.push(this.tempBranch);
+
+                }
+            }
+        }
+        if (this.selectedRole.roleAccess === RoleAccess[1]) {
+            const b = {
+                id: this.tempBranch
+            };
+            this.finalBranchList.push(b);
+        }
+        if (this.selectedRole.roleAccess === RoleAccess[2]) {
+            this.finalBranchList = [];
+        }
+        this.model.branch = this.finalBranchList;
         this.model.role = this.role;
+        console.log(this.model);
         this.service.save(this.model).subscribe(() => {
                 this.model = new User();
 
@@ -116,10 +132,12 @@ export class UserFormComponent implements OnInit, DoCheck {
 
     getBranchByRole(id) {
         this.isSpecific = false;
+        this.isAll = false;
         this.roleService.detail(id).subscribe((res: any) => {
             this.selectedRole = res.detail;
             if (this.selectedRole.roleAccess === RoleAccess[2]) {
                 this.isSpecific = false;
+                this.isAll = true;
             }
 
             if (this.selectedRole.roleAccess === RoleAccess[1]) {
@@ -130,7 +148,43 @@ export class UserFormComponent implements OnInit, DoCheck {
                 this.isSpecific = true;
 
             }
+            this.role = res.detail;
         });
+    }
+
+
+    getEdit() {
+        if (this.model.id == null) {
+            this.task = 'Add';
+        } else {
+            if (this.model.branch != null) {
+                this.branch = this.model.branch;
+            }
+            if (this.model.role != null) {
+                this.role = this.model.role;
+            }
+            this.task = 'Edit';
+            this.isAll = false;
+            const tempRoleAccess = this.model.role.roleAccess;
+            this.branchIdList = [];
+            if (tempRoleAccess === RoleAccess[0]) {
+                this.isSpecific = true;
+                for (let i = 0; i < this.model.branch.length; i++) {
+                    this.branchIdList.push(this.model.branch[i].id);
+                }
+            }
+            if (tempRoleAccess === RoleAccess[1]) {
+                this.isSpecific = false;
+                for (let i = 0; i < this.model.branch.length; i++) {
+                    this.tempBranch = this.model.branch[i].id;
+                }
+            }
+
+            if (tempRoleAccess === RoleAccess[2]) {
+                this.isAll = true;
+            }
+
+        }
     }
 
 }
--- src/app/feature/admin/modal/role.ts
+++ src/app/feature/admin/modal/role.ts
@@ -1,10 +1,9 @@
 import {RoleType} from './roleType';
-import {RoleAccess} from './role-access';
 
 export class Role {
     id: number;
     roleName: string;
     status: string;
     roleType: RoleType;
-    roleAccess: RoleAccess;
+    roleAccess: string;
 }
