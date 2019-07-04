72c90d88b6f920f99d57c462f66347fb0a9ae0b4
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-07-04 23:40:23 +0545

    email configuration

--- src/app/feature/admin/component/email-configuration/email-configuration.component.ts
+++ src/app/feature/admin/component/email-configuration/email-configuration.component.ts
@@ -13,7 +13,7 @@ import {Alert, AlertType} from '../../../../@theme/model/Alert';
 export class EmailConfigurationComponent implements OnInit {
 
     emailConfig: FormGroup;
-
+    notice = false;
 
     constructor(
         private router: Router,
@@ -24,12 +24,16 @@ export class EmailConfigurationComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.emailConfigService.getAll().subscribe((res: any) => {
+            this.notice = true;
+        });
         this.emailConfig = this.formBuilder.group({
             username: [undefined, Validators.required],
             password: [undefined, Validators.required],
             domain: [undefined, Validators.required],
             host: [undefined, Validators.required],
             port: [undefined, Validators.required],
+            testMail: [undefined, Validators.required],
             emailType: ['test']
         });
     }
@@ -38,6 +42,17 @@ export class EmailConfigurationComponent implements OnInit {
 
         this.emailConfigService.save(this.emailConfig.value).subscribe((res: any) => {
                 this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully Saved Configuration'));
+                this.emailConfigService.refreshConfiguration().subscribe((re2: any) => {
+                    this.emailConfigService.checkConfiguration(this.emailConfig.value).subscribe((rea1: any) => {
+                        this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully Send Email'));
+
+                    }, err => {
+                        this.toastService.show(new Alert(AlertType.ERROR, err.error.message));
+                    });
+                });
+
+            }, error => {
+                this.toastService.show(new Alert(AlertType.ERROR, error.error.message));
             }
         );
     }
--- src/app/feature/admin/component/email-configuration/email-configuration.service.ts
+++ src/app/feature/admin/component/email-configuration/email-configuration.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {BaseService} from '../../../../@core/BaseService';
+import {ApiUtils} from '../../../../@core/utils/api/ApiUtils';
 
 
 @Injectable({
@@ -13,6 +14,18 @@ export class EmailConfigurationService extends BaseService<any> {
 
     constructor(protected http: HttpClient) {
         super(http);
+    }
+
+    public refreshConfiguration() {
+        const req = ApiUtils.getRequest('actuator/refresh');
+        return this.http.post(req.url, null, {headers: req.header});
+    }
+
+    public checkConfiguration(email: any) {
+        const api = `${EmailConfigurationService.API}/test`;
+        const req = ApiUtils.getRequest(api);
+
+        return this.http.post(req.url, email, {headers: req.header});
     }
 
     protected getApi(): string {
