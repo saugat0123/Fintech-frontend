4e2cb434e9b74c07fffb1d9179656408e32a1cfc
sthaamula <sthaamulya@gmail.com>
2019-07-05 13:02:10 +0545

    update: Pre populate Email config data.

---
+++ src/app/feature/admin/modal/emailConfig.ts
@@ -0,0 +1,11 @@
+export class EmailConfig {
+    domain: string;
+    emailType: string;
+    host: string;
+    id: number;
+    password: string;
+    port: string;
+    testMail: string;
+    username: string;
+    version: number;
+}
