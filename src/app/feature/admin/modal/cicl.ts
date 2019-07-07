401fc151985eff101807f9f05fdb99de6fd4eca5
Elvin Shrestha <elwyncrestha@gmail.com>
2019-07-07 09:55:45 +0545

    Add models

---
+++ src/app/feature/admin/modal/cicl.ts
@@ -0,0 +1,10 @@
+export class Cicl {
+  nameOfFI: string;
+  facility: string;
+  outstandingAmount: number;
+  overdueAmount: number;
+  status: string;
+  createdAt: Date;
+  lastModifiedAt: Date;
+  version: number;
+}
