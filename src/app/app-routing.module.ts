38cd48d1c97c20ae90909e1904a6e91c2da578c7
Bibek Shrestha <rawseek@gmail.com>
2019-07-10 12:44:37 +0545

    Add bar chart based on the branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
-import {ExtraOptions, RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
-import {LoginComponent} from './component/login/login.component';
-import {ForgotPasswordComponent} from './component/forgot-password/forgot-password.component';
-import {ResentForgotPasswordComponent} from './component/resent-forgot-password/resent-forgot-password.component';
-import {LoginBaseComponent} from './component/login-base/login-base.component';
-import {NewPasswordComponent} from './component/new-password/new-password.component';
-import {AuthGuard} from './shared-service/authentication/auth.guard';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './component/login/login.component';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
+import { ResentForgotPasswordComponent } from './component/resent-forgot-password/resent-forgot-password.component';
+import { LoginBaseComponent } from './component/login-base/login-base.component';
+import { NewPasswordComponent } from './component/new-password/new-password.component';
+import { AuthGuard } from './shared-service/authentication/auth.guard';
 
 
 const routes: Routes = [
-    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     {
         path: '', component: LoginBaseComponent, children: [
-            {path: 'login', component: LoginComponent},
-            {path: 'forgotPassword', component: ForgotPasswordComponent},
-            {path: 'resentForgotPassword', component: ResentForgotPasswordComponent},
-            {path: 'newPassword', component: NewPasswordComponent}
+            { path: 'login', component: LoginComponent },
+            { path: 'forgotPassword', component: ForgotPasswordComponent },
+            { path: 'resentForgotPassword', component: ResentForgotPasswordComponent },
+            { path: 'newPassword', component: NewPasswordComponent }
         ]
     },
-    {path: 'home', loadChildren: './feature/feature.module#FeatureModule', canActivate: [AuthGuard]},
-    {path: '**', redirectTo: '/login', pathMatch: 'full'},
+    { path: 'home', loadChildren: './feature/feature.module#FeatureModule', canActivate: [AuthGuard] },
+    { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 const config: ExtraOptions = {
---
+++ src/app/chart/bar-chart/bar-chart.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { BranchService } from '../../feature/admin/component/branch/branch.service';
+import { RoleAccess } from '../../feature/admin/modal/role-access';
+import { Branch } from '../../feature/admin/modal/branch';
+import { BarChartService } from './bar-chart.service';
+import { BarChart } from './bar-chart.model';
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.scss']
+})
+export class BarChartComponent implements OnInit {
+  private data: Array<BarChart>;
+  view: any[] = [800, 500];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  legendTitle = 'Status';
+  showXAxisLabel = true;
+  animations = true;
+  xAxisLabel = 'Loan Types';
+  showYAxisLabel = true;
+  yAxisLabel = 'Loan counts';
+
+  roleAccess: string;
+  accessSpecific: boolean;
+  accessAll: boolean;
+  branches: Branch[] = [];
+  selectedBranch = 'All Branches';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor(private branchService: BranchService, private barChartService: BarChartService) {
+  }
+
+  ngOnInit(): void {
+    this.roleAccess = localStorage.getItem('roleAccess');
+    if (this.roleAccess === RoleAccess.SPECIFIC) {
+      this.accessSpecific = true;
+    } else if (this.roleAccess === RoleAccess.ALL) {
+      this.accessAll = true;
+    }
+    this.branchService.getBranchAccessByCurrentUser().subscribe(
+      (response: any) => {
+        this.branches = response.detail;
+        if (this.roleAccess === RoleAccess.OWN) {
+          this.branches.forEach((branch, index) => {
+            this.selectedBranch = branch.name;
+          });
+        }
+      }
+    );
+  }
+
+  onChange(event) {
+    if (event.target.value === 'All Branches') {
+    } else {
+        const branchId = Number(event.target.value);
+        this.branches.forEach( branch => {
+            if (branch.id === branchId) {
+                this.selectedBranch = branch.name;
+            }
+        });
+        this.barChartService.getBarData(branchId).subscribe((response: any) => {
+            this.data = response.detail;
+        });
+    }
+}
+}
