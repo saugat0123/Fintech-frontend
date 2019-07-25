6929496a7fadbaa5e10327a16a946e5bdd5ed735
Rujan Maharjan <rujnmahrzn@gmail.com>
2019-07-25 12:18:46 +0545

    multiple role maker

--- src/app/feature/admin/component/role-permission/list-role/role-edit/role-edit.component.ts
+++ src/app/feature/admin/component/role-permission/list-role/role-edit/role-edit.component.ts
@@ -4,37 +4,41 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {ModalResponse, ToastService} from '../../../../../../@core/utils';
 import {Alert, AlertType} from '../../../../../../@theme/model/Alert';
 import {RoleService} from '../../role.service';
+import {RoleType} from '../../../../modal/roleType';
 
 @Component({
-  selector: 'app-role-edit',
-  templateUrl: './role-edit.component.html',
-  styleUrls: ['./role-edit.component.scss']
+    selector: 'app-role-edit',
+    templateUrl: './role-edit.component.html',
+    styleUrls: ['./role-edit.component.scss']
 })
 export class RoleEditComponent implements OnInit {
-  @Input()
-  model: Role;
+    @Input()
+    model: Role;
+    roleTypeList = RoleType;
 
-  constructor(private activeModal: NgbActiveModal,
-              private service: RoleService,
-              private toastService: ToastService) { }
+    constructor(private activeModal: NgbActiveModal,
+                private service: RoleService,
+                private toastService: ToastService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onSubmit() {
-    this.service.update(this.model).subscribe(() => {
-          this.activeModal.close(ModalResponse.SUCCESS);
-          this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully Updated Role name!'));
-          this.model = new Role();
-        },
-        (error) => {
-          console.log(error);
-          this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Update Role name!'));
-          this.activeModal.dismiss(ModalResponse.ERROR);
-        });
-  }
-  onClose() {
-    this.activeModal.dismiss(ModalResponse.CANCEL);
-  }
+    onSubmit() {
+        this.service.update(this.model).subscribe(() => {
+                this.activeModal.close(ModalResponse.SUCCESS);
+                this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully Updated Role name!'));
+                this.model = new Role();
+            },
+            (error) => {
+                console.log(error);
+                this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Update Role name!'));
+                this.activeModal.dismiss(ModalResponse.ERROR);
+            });
+    }
+
+    onClose() {
+        this.activeModal.dismiss(ModalResponse.CANCEL);
+    }
 
 }
