254e8f82077db4db62b5427ac66c81c2d6f68042
sthaamula <sthaamulya@gmail.com>
2019-07-31 15:11:37 +0545

    Removed account number from User list.

--- src/app/feature/admin/component/eligibility/new-requests/new-requests.component.ts
+++ src/app/feature/admin/component/eligibility/new-requests/new-requests.component.ts
@@ -8,6 +8,12 @@ import {Pageable} from '../../../../../@core/service/baseservice/common-pageable
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {EligibilityDocumentViewComponent} from '../eligibility-document-view/eligibility-document-view.component';
 import {SubmissionDocument} from '../../../modal/submission-document';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Branch} from '../../../modal/branch';
+import {LoanConfig} from '../../../modal/loan-config';
+import {BranchService} from '../../branch/branch.service';
+import {LoanConfigService} from '../../loan-config/loan-config.service';
+import {DocStatus} from '../../../../loan/model/docStatus';
 
 @Component({
   selector: 'app-new-requests',
@@ -16,15 +22,24 @@ import {SubmissionDocument} from '../../../modal/submission-document';
 })
 export class NewRequestsComponent implements OnInit {
   spinner = false;
+  branchList: Array<Branch> = new Array<Branch>();
+  loanTypeList: Array<LoanConfig> = new Array<LoanConfig>();
   applicantList: Array<Applicant> = new Array<Applicant>();
+  filterForm: FormGroup;
 
   page = 1;
-  search: any = {};
+  search: any = {
+    branchIds: undefined,
+    loanConfigId: undefined
+  };
   pageable: Pageable = new Pageable();
 
   constructor(private newRequestService: NewRequestService,
               private toastService: ToastService,
-              private modalService: NgbModal) { }
+              private formBuilder: FormBuilder,
+              private modalService: NgbModal,
+              private branchService: BranchService,
+              private loanConfigService: LoanConfigService) { }
 
   static loadData(other: NewRequestsComponent) {
 
@@ -45,7 +60,38 @@ export class NewRequestsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.filterForm = this.formBuilder.group({
+      branch: [undefined],
+      loanType: [undefined],
+    });
+
+    this.branchService.getAll().subscribe((response: any) => {
+      this.branchList = response.detail;
+    }, error => {
+      console.error(error);
+      this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Branch!'));
+    });
+
+    this.loanConfigService.getAll().subscribe((response: any) => {
+      this.loanTypeList = response.detail;
+    }, error => {
+      console.error(error);
+      this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Loan Type!'));
+    });
+
     NewRequestsComponent.loadData(this);
+  }
+
+  filterSearch() {
+    this.search.branchIds = this.filterForm.get('branch').value === null ? undefined :
+        this.filterForm.get('branch').value;
+    this.search.loanConfigId = this.filterForm.get('loanType').value === null ? undefined :
+        this.filterForm.get('loanType').value;
+    NewRequestsComponent.loadData(this);
+  }
+
+  clearSearch() {
+    this.search = {};
   }
 
   changePage(page: number) {
