361a87fe8dcf5aa1f9e7dc4e1dfebf15ec754bd2
Elvin Shrestha <elwyncrestha@gmail.com>
2019-07-31 14:59:12 +0545

    AppConstants: Bank Title

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {AppConstant} from '../app/@core/utils/appConstant';
+
 export const environment = {
   production: false,
-  client: 'Mahalaxmi Bikas Bank Ltd.'
+  client: AppConstant.BANKNAME
 };
 
 /*
