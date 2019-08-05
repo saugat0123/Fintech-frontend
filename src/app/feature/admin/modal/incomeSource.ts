7a079ce729e7b7514cbae0981240da1e6c1f3571
dipu.dangol <dangol630@gmail.com>
2019-08-05 10:43:51 +0545

    continued...

---
+++ src/app/feature/admin/modal/incomeSource.ts
@@ -0,0 +1,11 @@
+export enum IncomeSource {
+    SALARY = 'Salary',
+    RENT = 'Rent',
+    REMITTANCE = 'Remittance',
+    VEHICLE = 'Vehicle Income',
+    BUSINESS = 'Business',
+    PROFIT = 'Business Profit',
+    PENSION = 'Pension',
+    CAPITAL_GAIN = 'Capital Gain Income',
+    AGRICULTURE = 'Agriculture'
+}
---
+++ src/app/feature/admin/modal/occupation.ts
@@ -0,0 +1,10 @@
+export enum Occupation {
+    JOB_HOLDER = 'Job Holder',
+    HOUSEWIFE = 'Housewife',
+    FOREIGN_EMPLOYMENT = 'Foreign Employment',
+    BUSINESS = 'Business',
+    PENSIONER = 'Pensioner',
+    FARMER = 'Farmer',
+    MARKETABLE_SECURITIES_TRADER = 'Marketable Securities Trader',
+    Freelancer = 'Freelancer'
+}
