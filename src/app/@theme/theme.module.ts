e23951bc848c5c062f4f088aeb796cc4af5dd470
yunish_stha <yunishshrestha8@gmail.com>
2019-08-07 18:31:46 +0545

    Added password change feature

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -64,6 +64,7 @@ import {SearchResultComponent} from './components/header/header-form/searchResul
 import {MessageModalComponent} from './components/message-modal/message-modal.component';
 import {ProfileComponent} from './components/profile/profile.component';
 import {NotificationComponent} from './components/notification/notification.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 // import {NbSecurityModule} from '@nebular/security';
 
@@ -128,7 +129,8 @@ const COMPONENTS = [
 const ENTRY_COMPONENTS = [
     SearchResultComponent,
     MessageModalComponent,
-    ProfileComponent
+    ProfileComponent,
+    ChangePasswordComponent
 ];
 
 const PIPES = [
@@ -161,7 +163,7 @@ const NB_THEME_PROVIDERS = [
 @NgModule({
     imports: [...BASE_MODULES, ...NB_MODULES, RouterModule],
     exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS, ...PIPES],
-    declarations: [...COMPONENTS, ...PIPES, ProfileComponent],
+    declarations: [...COMPONENTS, ...PIPES, ProfileComponent, ChangePasswordComponent],
     entryComponents: [...ENTRY_COMPONENTS],
 })
 export class ThemeModule {
