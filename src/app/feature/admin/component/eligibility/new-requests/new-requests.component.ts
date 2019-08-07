3f7db1f01fe3583dae7a7d78c57189819186408a
sthaamula <sthaamulya@gmail.com>
2019-08-07 14:11:08 +0545

    UI fixes for New request filter wrt branch and loan config.

--- src/app/feature/admin/component/eligibility/new-requests/new-requests.component.ts
+++ src/app/feature/admin/component/eligibility/new-requests/new-requests.component.ts
@@ -15,93 +15,97 @@ import {BranchService} from '../../branch/branch.service';
 import {LoanConfigService} from '../../loan-config/loan-config.service';
 
 @Component({
-  selector: 'app-new-requests',
-  templateUrl: './new-requests.component.html',
-  styleUrls: ['./new-requests.component.css']
+    selector: 'app-new-requests',
+    templateUrl: './new-requests.component.html',
+    styleUrls: ['./new-requests.component.css']
 })
 export class NewRequestsComponent implements OnInit {
-  spinner = false;
-  branchList: Array<Branch> = new Array<Branch>();
-  loanTypeList: Array<LoanConfig> = new Array<LoanConfig>();
-  applicantList: Array<Applicant> = new Array<Applicant>();
-  filterForm: FormGroup;
+    spinner = false;
+    branchList: Array<Branch> = new Array<Branch>();
+    loanTypeList: Array<LoanConfig> = new Array<LoanConfig>();
+    applicantList: Array<Applicant> = new Array<Applicant>();
+    filterForm: FormGroup;
 
-  page = 1;
-  searchString: string = NewRequestService.resolveSearchString(null, null, null);
-  pageable: Pageable = new Pageable();
+    page = 1;
+    searchString: string = NewRequestService.resolveSearchString(null, null, null);
+    pageable: Pageable = new Pageable();
 
-  constructor(private newRequestService: NewRequestService,
-              private toastService: ToastService,
-              private formBuilder: FormBuilder,
-              private modalService: NgbModal,
-              private branchService: BranchService,
-              private loanConfigService: LoanConfigService) { }
+    constructor(private newRequestService: NewRequestService,
+                private toastService: ToastService,
+                private formBuilder: FormBuilder,
+                private modalService: NgbModal,
+                private branchService: BranchService,
+                private loanConfigService: LoanConfigService) {
+    }
 
-  static loadData(other: NewRequestsComponent) {
+    static loadData(other: NewRequestsComponent) {
 
-    other.spinner = true;
-    other.newRequestService.getAllWithSearchObject(other.page, 10, other.searchString).subscribe((response: any) => {
-          other.applicantList = response.detail.content;
-          other.pageable = PaginationUtils.getPageable(response.detail);
+        other.spinner = true;
+        other.newRequestService.getAllWithSearchObject(other.page, 10, other.searchString).subscribe((response: any) => {
+                other.applicantList = response.detail.content;
+                other.pageable = PaginationUtils.getPageable(response.detail);
 
-          other.spinner = false;
+                other.spinner = false;
+            }, error => {
+
+                console.log(error);
+
+                other.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Data!'));
+                other.spinner = false;
+            }
+        );
+    }
+
+    ngOnInit() {
+        this.buildSearchForm();
+        this.branchService.getAll().subscribe((response: any) => {
+            this.branchList = response.detail;
         }, error => {
+            console.error(error);
+            this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Branch!'));
+        });
 
-          console.log(error);
+        this.loanConfigService.getAll().subscribe((response: any) => {
+            this.loanTypeList = response.detail;
+        }, error => {
+            console.error(error);
+            this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Loan Type!'));
+        });
 
-          other.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Data!'));
-          other.spinner = false;
-        }
-    );
-  }
+        NewRequestsComponent.loadData(this);
+    }
 
-  ngOnInit() {
-    this.filterForm = this.formBuilder.group({
-      branch: [undefined],
-      loanType: [undefined],
-    });
+    buildSearchForm() {
+        this.filterForm = this.formBuilder.group({
+            branch: [undefined],
+            loanType: [undefined],
+        });
+    }
 
-    this.branchService.getAll().subscribe((response: any) => {
-      this.branchList = response.detail;
-    }, error => {
-      console.error(error);
-      this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Branch!'));
-    });
+    filterSearch() {
+        const branchId: number = this.filterForm.get('branch').value === null ? undefined :
+            this.filterForm.get('branch').value;
+        const loanConfigId: number = this.filterForm.get('loanType').value === null ? undefined :
+            this.filterForm.get('loanType').value;
+        this.searchString = NewRequestService.resolveSearchString(null, branchId, loanConfigId);
+        NewRequestsComponent.loadData(this);
+    }
 
-    this.loanConfigService.getAll().subscribe((response: any) => {
-      this.loanTypeList = response.detail;
-    }, error => {
-      console.error(error);
-      this.toastService.show(new Alert(AlertType.ERROR, 'Unable to Load Loan Type!'));
-    });
+    clearSearch() {
+        this.buildSearchForm();
+    }
 
-    NewRequestsComponent.loadData(this);
-  }
+    changePage(page: number) {
+        this.page = page;
 
-  filterSearch() {
-    const branchId: number = this.filterForm.get('branch').value === null ? undefined :
-        this.filterForm.get('branch').value;
-    const loanConfigId: number = this.filterForm.get('loanType').value === null ? undefined :
-        this.filterForm.get('loanType').value;
-    this.searchString = NewRequestService.resolveSearchString(null, branchId, loanConfigId);
-    NewRequestsComponent.loadData(this);
-  }
+        NewRequestsComponent.loadData(this);
+    }
 
-  clearSearch() {
-    this.searchString = '';
-  }
+    viewDocument(document: SubmissionDocument) {
 
-  changePage(page: number) {
-    this.page = page;
-
-    NewRequestsComponent.loadData(this);
-  }
-
-  viewDocument(document: SubmissionDocument) {
-
-    const modalRef = this.modalService.open(EligibilityDocumentViewComponent, {size: 'lg'});
-    modalRef.componentInstance.model = document;
-    ModalUtils.resolve(modalRef.result, NewRequestsComponent.loadData, this);
-  }
+        const modalRef = this.modalService.open(EligibilityDocumentViewComponent, {size: 'lg'});
+        modalRef.componentInstance.model = document;
+        ModalUtils.resolve(modalRef.result, NewRequestsComponent.loadData, this);
+    }
 
 }
