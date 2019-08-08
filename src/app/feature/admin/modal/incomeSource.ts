31f0716ceead9ea44cd2cc94c565254cc4eafaf2
Elvin Shrestha <elwyncrestha@gmail.com>
2019-08-08 16:00:09 +0545

    Add Income Source Dropdowns

--- src/app/feature/admin/modal/incomeSource.ts
+++ src/app/feature/admin/modal/incomeSource.ts
@@ -9,3 +9,23 @@ export enum IncomeSource {
     CAPITAL_GAIN = 'Capital Gain Income',
     AGRICULTURE = 'Agriculture'
 }
+
+export namespace IncomeSource {
+
+    export function values() {
+        return Object.keys(IncomeSource).filter(
+            (type) => isNaN(<any>type) && type !== 'values' && type !== 'enumObject'
+        );
+    }
+
+    export function enumObject() {
+        const enums = [];
+        values().forEach(value => {
+            enums.push({
+                key: value,
+                value: IncomeSource[value]
+            });
+        });
+        return enums;
+    }
+}
