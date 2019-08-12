c088b6b68bf06ed2027e9a0c788b0865b69ecee6
Elvin Shrestha <elwyncrestha@gmail.com>
2019-08-12 17:48:09 +0545

    TSLint Fix

--- src/app/feature/admin/service/entity-info.service.ts
+++ src/app/feature/admin/service/entity-info.service.ts
@@ -1,13 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {BaseService} from '../../../@core/BaseService';
-import {Customer} from '../modal/customer';
 import {EntityInfo} from '../modal/entity-info';
 
 @Injectable({
     providedIn: 'root'
 })
-export class EntityInfoService extends BaseService<EntityInfo>{
+export class EntityInfoService extends BaseService<EntityInfo> {
 
     static API = 'v1/companyInfo';
 
