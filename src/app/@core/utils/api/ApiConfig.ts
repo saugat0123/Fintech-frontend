397891174be89774e7b33bea0857f4affb7dc939
sunilbau <whispersunil@gmail.com>
2019-08-25 14:50:02 +0545

    mega bank related image configuration

--- src/app/@core/utils/api/ApiConfig.ts
+++ src/app/@core/utils/api/ApiConfig.ts
@@ -1,6 +1,8 @@
-
 export class ApiConfig {
-    public static TOKEN = 'http://localhost:8086/oauth/token';
-    public static URL = 'http://localhost:8086';
+  // public static TOKEN = 'http://localhost:8086/oauth/token';
+  // public static URL = 'http://localhost:8086';
+
+  public static TOKEN = 'http://103.98.131.67:8090/oauth/token';
+  public static URL = 'http://103.98.131.67:8090';
 
 }
