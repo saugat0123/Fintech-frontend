a5b8f194bc7c9a1537adc21bc8ed185b2309bae3
SafiShrestha <safishrestha12@gmail.com>
2019-09-18 15:54:33 +0545

    Component replication

--- src/app/feature/loan/component/loan-main-template/security/security-initial-form/security-initial-form.component.ts
+++ src/app/feature/loan/component/loan-main-template/security/security-initial-form/security-initial-form.component.ts
@@ -1,15 +1,182 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {isBoolean} from 'util';
 
 @Component({
-  selector: 'app-security-initial-form',
-  templateUrl: './security-initial-form.component.html',
-  styleUrls: ['./security-initial-form.component.scss']
+    selector: 'app-security-initial-form',
+    templateUrl: './security-initial-form.component.html',
+    styleUrls: ['./security-initial-form.component.scss']
 })
 export class SecurityInitialFormComponent implements OnInit {
+  @Input() title: string;
+  @Output() toCheckedCombined = new EventEmitter();
+    marked = false;
+    securityForm: FormGroup;
+    landSelected = false;
+    apartmentSelected = false;
+    buildingSelected = false;
+    plantSelected = false;
+    underConstructionChecked = false;
+    checkboxSelected = true;
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder) {
+    }
+     check(status) {
+      if (status) {
+        this.toCheckedCombined.next(true);
+      } else {
+        this.toCheckedCombined.next(false);
+      }
+     }
+    ngOnInit() {
+      if (this.title === 'Combined') {
+        this.checkboxSelected = false;
+      }
+        this.securityForm = this.formBuilder.group({
+            valuatorDetails: this.formBuilder.group({
+                valuator: [''],
+                valuatedDate: [''],
+                valuatorRepresentativeName: [''],
+                staffRepresentativeName: [''],
+                chooseAny: [null]
+            }),
+            landDetails: this.formBuilder.array([
+                this.landDetailsFormGroup()
+            ]),
+            buildingDetails: this.formBuilder.array([
+                this.buildingDetailsFormGroup()
+            ]),
+            buildingDetailsBeforeCompletion: this.formBuilder.array([
+                this.buildingDetailsFormGroup()
+            ]),
+            buildingDetailsAfterCompletion: this.formBuilder.array([
+                this.buildingDetailsFormGroup()
+            ]),
+            plantDetails: this.formBuilder.array([
+                this.plantDetailsFormGroup()
+            ])
+        });
+    }
 
-  ngOnInit() {
-  }
+    onChange(event) {
+        const selected = event.target.value;
+        if (selected === 'Land Security') {
+            this.showLand();
+        } else if (selected === 'Apartment Security') {
+            this.showApartment();
+        } else if (selected === 'Plant and Machinery Security') {
+            this.showPlantandMachinery();
+        } else {
+            this.showBoth();
+        }
+    }
 
+    showLand() {
+        this.landSelected = true;
+        this.apartmentSelected = false;
+        this.plantSelected = false;
+    }
+
+    showApartment() {
+        this.apartmentSelected = true;
+        this.landSelected = false;
+        this.plantSelected = false;
+    }
+
+    showBoth() {
+        this.landSelected = true;
+        this.apartmentSelected = true;
+        this.plantSelected = false;
+    }
+
+    showPlantandMachinery() {
+        this.apartmentSelected = false;
+        this.landSelected = false;
+        this.plantSelected = true;
+    }
+
+    landDetailsFormGroup(): FormGroup {
+        return this.formBuilder.group({
+            owner: [''],
+            location: [''],
+            plotNumber: [''],
+            areaFormat: [''],
+            area: [''],
+            marketValue: [''],
+            distressValue: [''],
+            description: [''],
+        });
+    }
+
+    buildingDetailsFormGroup(): FormGroup {
+        return this.formBuilder.group({
+            buildingName: [''],
+            buildingDescription: [''],
+            buildArea: [''],
+            buildRate: [''],
+            totalCost: [''],
+            floorName: [''],
+            valuationArea: [''],
+            ratePerSquareFeet: [''],
+            estimatedCost: [''],
+            waterSupply: [''],
+            sanitation: [''],
+            electrification: [''],
+            buildingTotalCost: [''],
+            buildingFairMarketValue: [''],
+            buildingDistressValue: [''],
+
+        });
+    }
+
+    plantDetailsFormGroup(): FormGroup {
+        return this.formBuilder.group({
+            model: [''],
+            quotation: [''],
+            supplier: [''],
+            downPay: [''],
+            loanExp: ['']
+        });
+    }
+
+    underConstruction(event) {
+
+        if (event.target.checked) {
+            this.underConstructionChecked = true;
+        } else {
+            this.underConstructionChecked = false;
+        }
+    }
+
+    addMoreLand() {
+        (this.securityForm.get('landDetails') as FormArray).push(this.landDetailsFormGroup());
+    }
+
+    removeLandDetails(index: number) {
+        (<FormArray>this.securityForm.get('landDetails')).removeAt(index);
+    }
+
+    addBuilding() {
+        if (this.buildingSelected === false) {
+            this.buildingSelected = true;
+        } else {
+            (this.securityForm.get('buildingDetails') as FormArray).push(this.buildingDetailsFormGroup());
+        }
+    }
+
+    removeBuildingDetails(index: number) {
+        (this.securityForm.get('buildingDetails') as FormArray).removeAt(index);
+    }
+
+    removePlantDetails(index: number) {
+        (this.securityForm.get('plantDetails') as FormArray).removeAt(index);
+    }
+
+    addPlantandMachinery() {
+        (this.securityForm.get('plantDetails') as FormArray).push(this.plantDetailsFormGroup());
+    }
+
+    onSubmit() {
+
+    }
 }
--- src/app/feature/loan/component/loan-main-template/security/security.component.ts
+++ src/app/feature/loan/component/loan-main-template/security/security.component.ts
@@ -2,22 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 
 
-
 @Component({
     selector: 'app-security',
     templateUrl: './security.component.html',
     styleUrls: ['./security.component.css']
 })
 export class SecurityComponent implements OnInit {
-    marked = false;
-    securityForm: FormGroup;
     guarantorsForm: FormGroup;
-    landSelected = false;
-    apartmentSelected = false;
-    buildingSelected = false;
-    plantSelected = false;
-    underConstructionChecked = false;
-
+    checkedCombined = false;
     constructor(
         private formBuilder: FormBuilder
     ) {
@@ -27,70 +19,19 @@ export class SecurityComponent implements OnInit {
         this.guarantorsForm = this.formBuilder.group({
             guarantorsDetails: this.formBuilder.array([
                 this.guarantorsDetailsFormGroup()
-            ]),
-        });
-        this.securityForm = this.formBuilder.group({
-            valuatorDetails: this.formBuilder.array([
-                this.valuatorDetailsFormGroup()
-            ]),
-            landDetails: this.formBuilder.array([
-                this.landDetailsFormGroup()
-            ]),
-            buildingDetails: this.formBuilder.array([
-                this.buildingDetailsFormGroup()
-            ]),
-            buildingDetailsBeforeCompletion: this.formBuilder.array([
-                this.buildingDetailsFormGroup()
-            ]),
-            buildingDetailsAfterCompletion: this.formBuilder.array([
-                this.buildingDetailsFormGroup()
-            ]),
-            plantDetails: this.formBuilder.array([
-                this.plantDetailsFormGroup()
             ])
         });
     }
 
-
-    onChange(event) {
-        const selected = event.target.value;
-        if (selected === 'Land Security') {
-            this.showLand();
-        } else if (selected === 'Apartment Security') {
-            this.showApartment();
-        } else if (selected === 'Plant and Machinery Security') {
-            this.showPlantandMachinery();
-        } else {
-            this.showBoth();
-        }
+    onCheckCombined(status) {
+        this.checkedCombined = status;
     }
 
-    showLand() {
-        this.landSelected = true;
-        this.apartmentSelected = false;
-        this.plantSelected = false;
-    }
-
-    showApartment() {
-        this.apartmentSelected = true;
-        this.landSelected = false;
-        this.plantSelected = false;
-    }
-
-    showBoth() {
-        this.landSelected = true;
-        this.apartmentSelected = true;
-        this.plantSelected = false;
-    }
-
-    showPlantandMachinery() {
-        this.apartmentSelected = false;
-        this.landSelected = false;
-        this.plantSelected = true;
+    print(text) {
+        console.log(text);
     }
 
     onSubmit() {
-
     }
 
     guarantorsDetailsFormGroup(): FormGroup {
@@ -107,105 +48,6 @@ export class SecurityComponent implements OnInit {
         });
     }
 
-    valuatorDetailsFormGroup(): FormGroup {
-        return this.formBuilder.group({
-            valuator: [''],
-            valuatedDate: [''],
-            valuatorRepresentativeName: [''],
-            staffRepresentativeName: [''],
-            chooseAny: [null],
-        });
-    }
-
-    landDetailsFormGroup(): FormGroup {
-        return this.formBuilder.group({
-            owner: [''],
-            location: [''],
-            plotNumber: [''],
-            areaFormat: [''],
-            area: [''],
-            marketValue: [''],
-            distressValue: [''],
-            description: [''],
-        });
-    }
-
-    buildingDetailsFormGroup(): FormGroup {
-        return this.formBuilder.group({
-            buildingName: [''],
-            buildingDescription: [''],
-            buildArea: [''],
-            buildRate: [''],
-            totalCost: [''],
-            floorName: [''],
-            valuationArea: [''],
-            ratePerSquareFeet: [''],
-            estimatedCost: [''],
-            waterSupply: [''],
-            sanitation: [''],
-            electrification: [''],
-            buildingTotalCost: [''],
-            buildingFairMarketValue: [''],
-            buildingDistressValue: [''],
-
-        });
-    }
-
-    plantDetailsFormGroup(): FormGroup {
-        return this.formBuilder.group({
-            model: [''],
-            quotation: [''],
-            supplier: [''],
-            downPay: [''],
-            loanExp: ['']
-        });
-    }
-
-    checkboxSelected(event) {
-        this.marked = event.target.checked;
-        if ( this.marked) {
-            this.addValuatorDetails();
-        //     this.addMoreLand();
-        // } else if (this.apartmentSelected && this.marked) {
-        //     this.addValuatorDetails();
-        //     this.addBuilding();
-        // } else if (this.plantSelected && this.marked) {
-        //     this.addValuatorDetails();
-        //     this.addPlantandMachinery();
-        // } else if (this.plantSelected && this.marked) {
-        //     this.addValuatorDetails();
-        //     this.addPlantandMachinery();
-        // } else if (this.buildingSelected && this.marked) {
-        //     this.addValuatorDetails();
-        //     this.addMoreLand();
-        //     this.addBuilding();
-        } else {
-            this.removeValuatorDetails(0);
-            // this.removeLandDetails(0);
-            // this.removeBuildingDetails(0);
-            // this.removePlantDetails(0);
-        }
-
-    }
-
-    underConstruction(event) {
-
-        if (event.target.checked) {
-            this.underConstructionChecked = true;
-        } else {
-            this.underConstructionChecked = false;
-        }
-    }
-
-    addValuatorDetails() {
-
-        (this.securityForm.get('valuatorDetails') as FormArray).push(this.valuatorDetailsFormGroup());
-    }
-
-    removeValuatorDetails(index: number) {
-        (this.securityForm.get('valuatorDetails') as FormArray).removeAt(index);
-    }
-
     addguarantorsDetails() {
 
         (this.guarantorsForm.get('guarantorsDetails') as FormArray).push(this.guarantorsDetailsFormGroup());
@@ -213,33 +55,5 @@ export class SecurityComponent implements OnInit {
 
     removeguarantorsDetails(index: number) {
         (this.guarantorsForm.get('guarantorsDetails') as FormArray).removeAt(index);
-    }
-
-    addMoreLand() {
-        (this.securityForm.get('landDetails') as FormArray).push(this.landDetailsFormGroup());
-    }
-
-    removeLandDetails(index: number) {
-        (<FormArray>this.securityForm.get('landDetails')).removeAt(index);
-    }
-
-    addBuilding() {
-        if (this.buildingSelected === false) {
-            this.buildingSelected = true;
-        } else {
-            (this.securityForm.get('buildingDetails') as FormArray).push(this.buildingDetailsFormGroup());
-        }
-    }
-
-    removeBuildingDetails(index: number) {
-        (this.securityForm.get('buildingDetails') as FormArray).removeAt(index);
-    }
-
-    removePlantDetails(index: number) {
-        (this.securityForm.get('plantDetails') as FormArray).removeAt(index);
-    }
-
-    addPlantandMachinery() {
-        (this.securityForm.get('plantDetails') as FormArray).push(this.plantDetailsFormGroup());
     }
 }
