3566eabd0f810ba4d0b6348182b8661fae799353
Bibash7 <biswash166@gmail.com>
2019-09-23 17:44:44 +0545

    token intercept

--- src/app/@core/service/authentication/request-interceptor.service.ts
+++ src/app/@core/service/authentication/request-interceptor.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {
+    HttpBackend,
     HttpErrorResponse,
     HttpEvent,
     HttpHandler,
@@ -8,9 +9,10 @@ import {
     HttpResponse
 } from '@angular/common/http';
 import {BehaviorSubject, Observable, throwError} from 'rxjs';
-import {catchError, switchMap, tap} from 'rxjs/operators';
+import {catchError, filter, switchMap, take, tap} from 'rxjs/operators';
 import {UserService} from '../user.service';
 import {ObjectUtil} from '../../utils/ObjectUtil';
+import {ApiConfig} from "../../utils/api/ApiConfig";
 
 @Injectable({
     providedIn: 'root'
@@ -46,13 +48,10 @@ export class RequestInterceptor implements HttpInterceptor {
                   console.log('token not expired no need to perform any action' + event);
                 }
               }), catchError((err): Observable<any> => {
-                if (err instanceof HttpErrorResponse) {
-                  switch (err) {
-
-                  }
-                  console.log(err.message);
-                  console.log('token expired .. attempting refresh');
-                  this.handleHttpResponseError(req, next);
+                if (err instanceof HttpErrorResponse && err.status === 401) {
+                    console.log(err.message);
+                    console.log('token expired .. attempting refresh');
+                     return  this.handleHttpResponseError(req, next);
                 } else {
                   return throwError(err);
                 }
@@ -61,31 +60,72 @@ export class RequestInterceptor implements HttpInterceptor {
         }
     }
 
-    private handleHttpResponseError(request: HttpRequest<any>, next: HttpHandler) {
+   /* private handleHttpResponseError(request: HttpRequest<any>, next: HttpHandler) {
         console.log(this.isTokenRefreshing);
         if (!this.isTokenRefreshing) {
             console.log('token refresh is about to be true');
             this.isTokenRefreshing = true;
+            this.tokenSubject.next(null);
             // call the api to refresh the token
             return this.userService.getNewRefreshToken().subscribe(
                 (tokenResponse: any) => {
                     if (tokenResponse && tokenResponse.access_token) {
-                        this.tokenSubject = tokenResponse;
+                        this.tokenSubject.next(tokenResponse);
+                        this.isTokenRefreshing = false;
                         localStorage.setItem('at', tokenResponse.access_token);
                         localStorage.setItem('rt', tokenResponse.refresh_token);
                         localStorage.setItem('ty', tokenResponse.token_type);
                         localStorage.setItem('et', tokenResponse.expires_in);
-                        this.isTokenRefreshing = false;
                         console.log('token renewed');
                         console.log(localStorage.getItem('at'));
                         return next.handle(RequestInterceptor.attachTokenToRequest(request));
                     }
                 }
-            );
+            ), catchError((err: any ) => {
+                this.isTokenRefreshing = false;
+                return Observable.throw('error');
+            });
         } else {
+            this.isTokenRefreshing = false;
+            return this.tokenSubject.pipe(filter(token => token !== null),
+                take(1),
+                switchMap(token => {
+                    return next.handle(RequestInterceptor.attachTokenToRequest(request));
+                }));
             throwError('token is refreshing');
         }
 
+    }*/
+
+
+    private handleHttpResponseError(request: HttpRequest<any>, next: HttpHandler) {
+        console.log('in the eroor handler' + this.isTokenRefreshing);
+        if (!this.isTokenRefreshing) {
+            this.isTokenRefreshing = true;
+            this.tokenSubject.next(null);
+
+            return this.userService.getNewRefreshToken().pipe(
+                switchMap((tokenResponse: any) => {
+                    this.isTokenRefreshing = false;
+                    this.tokenSubject.next(tokenResponse);
+                    this.isTokenRefreshing = false;
+                    localStorage.setItem('at', tokenResponse.access_token);
+                    localStorage.setItem('rt', tokenResponse.refresh_token);
+                    localStorage.setItem('ty', tokenResponse.token_type);
+                    localStorage.setItem('et', tokenResponse.expires_in);
+                    console.log('token renewed');
+                    console.log(localStorage.getItem('at'));
+                    return next.handle(RequestInterceptor.attachTokenToRequest(request));
+                }));
+
+        } else {
+            return this.tokenSubject.pipe(
+                filter(token => token != null),
+                take(1),
+                switchMap(jwt => {
+                    return next.handle(RequestInterceptor.attachTokenToRequest(request));
+                }));
+        }
     }
 
 }
--- src/app/@core/service/user.service.ts
+++ src/app/@core/service/user.service.ts
@@ -3,6 +3,7 @@ import {ApiUtils} from '../utils/api/ApiUtils';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {ApiConfig} from '../utils/api/ApiConfig';
+import {tap} from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -18,7 +19,7 @@ export class UserService {
         return this.http.get(req.url, {headers: req.header});
     }
 
-    getNewRefreshToken(): Observable<any> {
+  /*  getNewRefreshToken(): Observable<any> {
         console.log('new refresh token method');
         console.log(localStorage.getItem('rt'));
         const data = `grant_type=refresh_token&refresh_token=${localStorage.getItem('rt')}`;
@@ -29,5 +30,22 @@ export class UserService {
                 'Authorization': 'Basic Y3Atc29sdXRpb246Y3Bzb2x1dGlvbjEyMyoj',
             })
         });
-    }
+    }*/
+
+    getNewRefreshToken() {
+        const data = `grant_type=refresh_token&refresh_token=${localStorage.getItem('rt')}`;
+        return this.http.post(`${ApiConfig.TOKEN}`, data, {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic Y3Atc29sdXRpb246Y3Bzb2x1dGlvbjEyMyoj',
+            })
+        }).pipe(tap((tokenResponse: any) => {
+            localStorage.setItem('at', tokenResponse.access_token);
+            localStorage.setItem('rt', tokenResponse.refresh_token);
+            localStorage.setItem('ty', tokenResponse.token_type);
+            localStorage.setItem('et', tokenResponse.expires_in);
+            console.log('token renewed');
+            console.log(localStorage.getItem('at'));
+        }));
+}
 }
