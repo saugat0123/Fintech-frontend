26188411c6ba6c440bd3825e3156898935292f3b
yunish_stha <yunishshrestha8@gmail.com>
2019-10-14 12:37:26 +0545

    remove toaster

--- src/app/@core/service/socket.service.ts
+++ src/app/@core/service/socket.service.ts
@@ -41,7 +41,7 @@ export class SocketService {
   openSocket() {
     this.isCustomSocketOpened = true;
     this.stompClient.subscribe(`/socket-publisher/${this.userId}/${this.userRoleId}`, (message) => {
-      this.toastService.show(new Alert(AlertType.SUCCESS, 'New notification received!!!'));
+      // this.toastService.show(new Alert(AlertType.SUCCESS, 'New notification received!!!'));
       this.notificationService.fetchNotifications();
     });
   }
