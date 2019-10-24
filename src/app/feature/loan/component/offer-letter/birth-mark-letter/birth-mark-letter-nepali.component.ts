6842d57631aa503c5dafa3b3595dc464ef4d7ac2
Elvin Shrestha <elwyncrestha@gmail.com>
2019-10-24 14:53:07 +0545

    Refactor BirthMarkLetter component.

--- src/app/feature/loan/component/offer-letter/birth-mark-letter/birth-mark-letter-nepali.component.ts
+++ src/app/feature/loan/component/offer-letter/birth-mark-letter/birth-mark-letter-nepali.component.ts
@@ -2,9 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {LoanFormService} from '../../loan-form/service/loan-form.service';
 import {LoanDataHolder} from '../../../model/loanData';
-import {Customer} from '../../../../admin/modal/customer';
-import {Applicant} from '../model/applicant';
-import {LoanNepali} from '../model/loanNepali';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {ToastService} from '../../../../../@core/utils';
+import {Alert, AlertType} from '../../../../../@theme/model/Alert';
 
 @Component({
     selector: 'app-birth-mark-letter-nepali',
@@ -12,38 +12,87 @@ import {LoanNepali} from '../model/loanNepali';
     styleUrls: ['./birth-mark-letter-nepali.component.scss']
 })
 export class BirthMarkLetterNepaliComponent implements OnInit {
-    id;
     show = false;
-    applicant: Applicant;
-    applicantList: Array<Applicant> = new Array<Applicant>();
-    list: Array<Customer> = new Array<Customer>();
-    loanNepali: LoanNepali = new LoanNepali();
+    form: FormGroup;
     loanDataHolder: LoanDataHolder = new LoanDataHolder();
 
-    constructor(private activatedRoute: ActivatedRoute,
-                private loanFormService: LoanFormService) {
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private loanFormService: LoanFormService,
+        private formBuilder: FormBuilder,
+        private toastService: ToastService
+    ) {
     }
 
     ngOnInit() {
-        this.id = Number(this.activatedRoute.snapshot.queryParamMap.get('customerId'));
-        this.loanFormService.detail(this.id).subscribe((response: any) => {
+        const customerId = Number(this.activatedRoute.snapshot.queryParamMap.get('customerId'));
+        this.loanFormService.detail(customerId).subscribe((response: any) => {
             this.loanDataHolder = response.detail;
-            this.list.push(response.detail.customerInfo);
-            this.list.forEach((value => {
-                const customer = new Applicant();
-                this.applicantList.push(customer);
-                this.loanNepali.applicants = this.applicantList;
-            }));
-            this.loanNepali.applicants = this.applicantList;
-            console.log(this.applicantList);
         }, error => {
-            const customer = new Applicant();
-            this.applicantList.push(customer);
-            this.loanNepali.applicants = this.applicantList;
+            console.error(error);
+            this.toastService.show(new Alert(AlertType.ERROR, 'Error loading loan information.'));
+        });
+        this.buildForm();
+        this.form.valueChanges.subscribe((value) => {
+            this.form.patchValue(value, {emitEvent: false});
         });
     }
-    data() {
-        this.show = !this.show;
+
+    buildForm(): void {
+        this.form = this.formBuilder.group({
+            date: [undefined],
+            applicantName: [undefined],
+            applicantPermanentAddress: [undefined],
+            applicantPresentAddress: [undefined],
+            applicantAge: [undefined],
+            applicantCitizenshipNumber: [undefined],
+            applicantCitizenshipIssuedPlace: [undefined],
+            applicantCitizenshipIssuedDate: [undefined],
+            applicantMobileNumber: [undefined],
+            loanType: [undefined],
+            applicantRelativeOne: [undefined],
+            applicantRelativeOneRelation: [undefined],
+            applicantRelativeTwo: [undefined],
+            applicantRelativeTwoRelation: [undefined],
+            totalApplicantCount: [undefined],
+            amountLimit: [undefined],
+            amountLimitWord: [undefined],
+            interestRate: [undefined],
+            interestAmount: [undefined],
+            interestWord: [undefined],
+            securityGuarantorFirst: this.formBuilder.group({
+                name: [undefined],
+                district: [undefined],
+                address: [undefined],
+                stockQuantity: [undefined],
+                landArea: [undefined]
+            }),
+            securityGuarantorRemaining: this.formBuilder.array([this.securityGuarantorRemainingFormGroup()]),
+            securityRemaining: this.formBuilder.group({
+                securityRemaining1Amount: [undefined],
+                securityRemaining1Word: [undefined],
+                securityRemaining2Amount: [undefined],
+                securityRemaining2Word: [undefined],
+                securityRemaining3Amount: [undefined],
+                securityRemaining3Word: [undefined]
+            })
+        });
+    }
+
+    securityGuarantorRemainingFormGroup(): FormGroup {
+        return this.formBuilder.group({
+            name: [undefined],
+            amount: [undefined],
+            amountInWords: [undefined]
+        });
+    }
+
+    addGuarantor(): void {
+        (this.form.get('securityGuarantorRemaining') as FormArray).push(this.securityGuarantorRemainingFormGroup());
+    }
+
+    removeGuarantor(deleteIndex: number): void {
+        (this.form.get('securityGuarantorRemaining') as FormArray).removeAt(deleteIndex);
     }
 
 }
--- src/app/feature/loan/component/offer-letter/success-offer-letter/success-offer-letter.component.ts
+++ src/app/feature/loan/component/offer-letter/success-offer-letter/success-offer-letter.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {Applicant} from '../model/applicant';
-import {Customer} from '../../../../admin/modal/customer';
-import {LoanNepali} from '../model/loanNepali';
-import {LoanDataHolder} from '../../../model/loanData';
-import {ActivatedRoute} from '@angular/router';
-import {LoanFormService} from '../../loan-form/service/loan-form.service';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-success-offer-letter',
@@ -12,38 +6,8 @@ import {LoanFormService} from '../../loan-form/service/loan-form.service';
   styleUrls: ['./success-offer-letter.component.scss']
 })
 export class SuccessOfferLetterComponent implements OnInit {
-  id;
-  show = false;
-  applicant: Applicant;
-  applicantList: Array<Applicant> = new Array<Applicant>();
-  list: Array<Customer> = new Array<Customer>();
-  loanNepali: LoanNepali = new LoanNepali();
-  loanDataHolder: LoanDataHolder = new LoanDataHolder();
-
-  constructor(private activatedRoute: ActivatedRoute,
-              private loanFormService: LoanFormService) {
+  ngOnInit(): void {
   }
 
-  ngOnInit() {
-    this.id = Number(this.activatedRoute.snapshot.queryParamMap.get('customerId'));
-    this.loanFormService.detail(this.id).subscribe((response: any) => {
-      this.loanDataHolder = response.detail;
-      this.list.push(response.detail.customerInfo);
-      this.list.forEach((value => {
-        const customer = new Applicant();
-        this.applicantList.push(customer);
-        this.loanNepali.applicants = this.applicantList;
-      }));
-      this.loanNepali.applicants = this.applicantList;
-      console.log(this.applicantList);
-    }, error => {
-      const customer = new Applicant();
-      this.applicantList.push(customer);
-      this.loanNepali.applicants = this.applicantList;
-    });
-  }
-  data() {
-    this.show = !this.show;
-  }
 
 }
