99f2c0a581fff39c951facced318012f76352ae6
sthaamula <sthaamulya@gmail.com>
2019-11-04 15:56:06 +0545

    Added necessary matrices for financial Summary.

--- src/app/feature/loan/component/loan-summary/financial-summary/financial-summary.component.ts
+++ src/app/feature/loan/component/loan-summary/financial-summary/financial-summary.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Financial} from '../../../model/financial';
-import {Observable} from 'rxjs';
+import {FinancialService} from '../../loan-main-template/financial/financial.service';
 
 @Component({
   selector: 'app-financial-summary',
@@ -12,12 +12,43 @@ export class FinancialSummaryComponent implements OnInit {
 
   financialData: Object;
 
-  constructor() { }
+  // Additional Summary Fields---
+  totalDebtValueArray = [];
+  debtServiceCostArray = [];
+
+  constructor(private financialService: FinancialService) { }
 
   ngOnInit() {
     if (this.formData !== undefined) {
       this.financialData = JSON.parse(this.formData.data);
+      this.setTotalDebtValue();
+      this.setDebtServiceCost();
     }
   }
 
+  accountReceivableArray() {
+    return this.financialData['balanceSheetData'].currentAssetsCategory[3].amount;
+  }
+
+  setTotalDebtValue() {
+    this.financialData['fiscalYear'].forEach( (value, index) => {
+      this.totalDebtValueArray.push(Number(this.financialService.fetchValuesForJsonSubCategories(this.financialData['balanceSheetData'].currentLiabilitiesCategory,
+          'Short Term Loan', index)) +
+          Number(this.financialService.fetchValuesForJsonSubCategories(this.financialData['balanceSheetData'].longTermLoanCategory,
+              'Term Loan', index)));
+    });
+  }
+
+  setDebtServiceCost() {
+    this.financialData['fiscalYear'].forEach( (value, index) => {
+      if (index > 0) {
+        this.debtServiceCostArray.push(Number(this.financialData['incomeStatementData'].interestExpenses[index].value +
+            Number(this.financialData['balanceSheetData'].longTermLoan[index-1].value) -
+            Number(this.financialData['balanceSheetData'].longTermLoan[index].value))
+        );
+      } else {
+        this.debtServiceCostArray.push(Number(this.financialData['incomeStatementData'].interestExpenses[index].value));
+      }
+    });
+  }
 }
