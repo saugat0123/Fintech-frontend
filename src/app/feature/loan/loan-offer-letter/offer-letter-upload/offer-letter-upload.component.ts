60a34a166b9bdb5700752b9ec4f928b42ae6c584
Elvin Shrestha <elwyncrestha@gmail.com>
2019-11-05 21:43:03 +0545

    Offer Letter Upload

---
+++ src/app/feature/loan/loan-offer-letter/offer-letter-upload/offer-letter-upload.component.ts
@@ -0,0 +1,75 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CustomerOfferLetter} from '../../model/customer-offer-letter';
+import {NgbActiveModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {DocStatus} from '../../model/docStatus';
+import {Alert, AlertType} from '../../../../@theme/model/Alert';
+import {ApiConfig} from '../../../../@core/utils/api/ApiConfig';
+import {ModalResponse, ToastService} from '../../../../@core/utils';
+import {CustomerOfferLetterService} from '../../service/customer-offer-letter.service';
+
+@Component({
+  selector: 'app-offer-letter-upload',
+  templateUrl: './offer-letter-upload.component.html',
+  styleUrls: ['./offer-letter-upload.component.scss']
+})
+export class OfferLetterUploadComponent implements OnInit {
+  @Input() customerOfferLetter: CustomerOfferLetter;
+  @Input() customerId: number;
+  offerLetterId: number;
+  uploadFile;
+  preview;
+
+  constructor(
+      private ngbModal: NgbActiveModal,
+      private toastService: ToastService,
+      private customerOfferLetterService: CustomerOfferLetterService,
+  ) { }
+
+  ngOnInit() {
+    console.log(this.customerOfferLetter);
+  }
+
+  submitOfferLetter() {
+    const formData: FormData = new FormData();
+
+    formData.append('file', this.uploadFile);
+    formData.append('customerLoanId', this.customerId.toString());
+    formData.append('offerLetterId', this.offerLetterId.toString());
+    if (this.customerId === undefined) {
+      return this.toastService.show(new Alert(AlertType.ERROR, 'Customer Cannot be empty'));
+    }
+    this.customerOfferLetterService.uploadOfferFile(formData).subscribe((response: any) => {
+      this.toastService.show(new Alert(AlertType.SUCCESS, 'OFFER LETTER HAS BEEN UPLOADED'));
+      this.ngbModal.close(ModalResponse.SUCCESS);
+    }, error => {
+      this.toastService.show(new Alert(AlertType.ERROR, error.error.message));
+      this.ngbModal.close(ModalResponse.ERROR);
+    });
+
+  }
+
+  uploadOfferLetter(event) {
+    this.uploadFile = event.target.files[0];
+    this.preview = true;
+  }
+
+  previewClick(file) {
+    alert(file);
+    let fileName = this.uploadFile;
+    if (file !== null) {
+      fileName = ApiConfig.URL + '/' + file;
+
+      const link = document.createElement('a');
+      link.href = fileName;
+      link.target = '_blank';
+      link.click();
+    } else {
+      const downloadUrl = window.URL.createObjectURL(fileName);
+      const link = document.createElement('a');
+      link.href = downloadUrl;
+      link.target = '_blank';
+      link.click();
+    }
+  }
+
+}
