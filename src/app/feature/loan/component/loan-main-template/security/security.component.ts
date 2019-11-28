64cbeb0ceb4d9539a353e4d5e90900cd73efdb84
sthaamula <sthaamulya@gmail.com>
2019-11-28 14:48:59 +0545

    -Security refactored with id and version. -Fixed problem with validation while saving loan.

--- src/app/feature/loan/component/loan-main-template/security/security.component.ts
+++ src/app/feature/loan/component/loan-main-template/security/security.component.ts
@@ -81,6 +81,9 @@ export class SecurityComponent implements OnInit {
     }
 
     onSubmit() {
+      if (this.securityValue !== undefined) {
+        this.securityData = this.securityValue;
+      }
         const mergedForm = {
             initialForm: this.initialSecurity.securityForm.value,
             selectedArray: this.initialSecurity.selectedArray,
