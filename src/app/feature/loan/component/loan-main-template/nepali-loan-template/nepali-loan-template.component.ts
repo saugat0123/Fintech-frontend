9e10839c7bb80e8d3773bd83c68b2e06ad2374b0
SafiShrestha <safishrestha12@gmail.com>
2019-11-29 16:44:48 +0545

    nepali loan template added

--- src/app/feature/loan/component/loan-main-template/nepali-loan-template/nepali-loan-template.component.ts
+++ src/app/feature/loan/component/loan-main-template/nepali-loan-template/nepali-loan-template.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-nepali-loan-template',
@@ -6,10 +7,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./nepali-loan-template.component.scss']
 })
 export class NepaliLoanTemplateComponent implements OnInit {
-
-  constructor() { }
+ nepaliLoan: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.buildForm();
+  }
+  buildForm() {
+    this.nepaliLoan = this.formBuilder.group({
+      relativesDetails: this.formBuilder.array([])
+    });
+    this.addDetails();
+  }
+  addDetails() {
+    const addDetails = this.nepaliLoan.get('relativesDetails') as FormArray;
+    addDetails.push(
+        this.formBuilder.group({
+          relativesName: [undefined],
+          relativesAddress: [undefined],
+          relativesPhone: [undefined],
+          relativesMobileNo: [undefined],
+        })
+    );
+  }
+
+  removeDetails(index: number) {
+    (this.nepaliLoan.get('relativesDetails') as FormArray).removeAt(index);
   }
 
 }
