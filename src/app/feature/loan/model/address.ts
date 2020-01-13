b97cf1bbe5ce1b9664a4fa6b1e692a73c3486c05
buddha.stha04 <buddha.stha04@gmail.com>
2020-01-13 21:55:05 +0545

    check style

--- src/app/feature/loan/model/address.ts
+++ src/app/feature/loan/model/address.ts
@@ -4,7 +4,8 @@ import {MunicipalityVdc} from '../../admin/modal/municipality_VDC';
 export class Address {
     districtList: Array<District>;
     municipalityVdcList: Array<MunicipalityVdc>;
-    constructor(districtList = [], municipalityVdcList = []){
+
+    constructor(districtList = [] , municipalityVdcList = []) {
         this.districtList = districtList;
         this.municipalityVdcList = municipalityVdcList;
     }
