d0ef9239057e1789269f5b6ed813724b276bef2c
noitanec <gnoitanec@gmail.com>
2020-01-22 00:56:26 +0545

    formControlName added

--- src/app/feature/loan/component/loan-main-nepali-template/bike-karja/bike-karja.component.ts
+++ src/app/feature/loan/component/loan-main-nepali-template/bike-karja/bike-karja.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BikeKarjaComponent implements OnInit {
 
+  // date1: string;
+  // proposalAmount: number;
+  // applicantFullName: string;
+  // applicantDob: string;
+  //
+
+
   constructor() { }
 
   ngOnInit() {
