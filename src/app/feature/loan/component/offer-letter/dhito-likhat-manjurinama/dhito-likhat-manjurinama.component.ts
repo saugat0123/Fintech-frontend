4f3e424930381614aae329c2fa43f88783c85c0d
Bibash7 <biswash166@gmail.com>
2020-01-22 12:56:23 +0545

    dhito likhat template

--- src/app/feature/loan/component/offer-letter/dhito-likhat-manjurinama/dhito-likhat-manjurinama.component.ts
+++ src/app/feature/loan/component/offer-letter/dhito-likhat-manjurinama/dhito-likhat-manjurinama.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {LoanDataHolder} from '../../../model/loanData';
 import {CustomerOfferLetter} from '../../../model/customer-offer-letter';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-dhito-likhat-manjurinama',
@@ -13,12 +14,46 @@ export class DhitoLikhatManjurinamaComponent implements OnInit {
     customerOfferLetter: CustomerOfferLetter;
     @Input() customerId: number;
     @Input() offerLetterTypeId: number;
+    dhitoForm: FormGroup;
 
-    constructor() {
+    constructor(private formBuilder: FormBuilder) {
     }
 
     ngOnInit() {
+     this.buildForm();
+    }
+
+    buildForm() {
+        this.dhitoForm = this.formBuilder.group({
+            customerDetail : this.formBuilder.array([]),
+            loanName : [undefined],
+            interestRateLoan : [undefined],
+            loanTenure : [undefined],
+            loanAmount : [undefined],
+            loanAmountInWord : [undefined]
+        });
+        this.addCustomerDetail();
 
     }
 
+    addCustomerDetail() {
+        (this.dhitoForm.get('dhitoForm').get('customerDetail') as FormArray).push(
+            this.formBuilder.group({
+                grandFathername : [undefined],
+                fatherName : [undefined],
+                district : [undefined],
+                municipality : [undefined],
+                wardNo : [undefined],
+                currentDistrict: [undefined],
+                currentMunicipality : [undefined],
+                currentWardNo : [undefined],
+                customerAge : [undefined],
+                customerName : [undefined],
+                citizenshipNo : [undefined],
+                citizenIssuedDate : [undefined],
+                citizenIssuedDistrict: [undefined]
+            })
+        );
+
+    }
 }
