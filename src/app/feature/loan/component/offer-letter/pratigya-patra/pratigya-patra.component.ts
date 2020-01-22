3a98b8a9ae626cf2911747b4505e956623e70150
lucifer403 <sadaib@hotmail.com>
2020-01-22 14:34:19 +0545

    #pratigyapatra template

--- src/app/feature/loan/component/offer-letter/pratigya-patra/pratigya-patra.component.ts
+++ src/app/feature/loan/component/offer-letter/pratigya-patra/pratigya-patra.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {LoanDataHolder} from '../../../model/loanData';
 import {CustomerOfferLetter} from '../../../model/customer-offer-letter';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'app-pratigya-patra',
@@ -8,17 +9,28 @@ import {CustomerOfferLetter} from '../../../model/customer-offer-letter';
     styleUrls: ['./pratigya-patra.component.scss']
 })
 export class PratigyaPatraComponent implements OnInit {
-
     @Input() loanDataHolder: LoanDataHolder;
     customerOfferLetter: CustomerOfferLetter;
     @Input() customerId: number;
     @Input() offerLetterTypeId: number;
-
-    constructor() {
+    offerform: FormGroup;
+    existingOfferLetter = false;
+    spinner = false;
+    initialInfoPrint;
+    constructor(private offerBuilder: FormBuilder) {
     }
 
     ngOnInit() {
 
     }
-
+    buildForm() {
+        this.offerform = this.offerBuilder.group({
+            loanamount: [undefined] ,
+            companyno: [undefined] ,
+            companyname: [undefined] ,
+            loanamount2: [undefined] ,
+            customerName: [undefined] ,
+            interestamount: [undefined] ,
+        });
+    }
 }
