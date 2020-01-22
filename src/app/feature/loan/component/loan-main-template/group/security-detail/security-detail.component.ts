4f41b1fcecc90d25e25bb1de6db772048a0925ad
buddha.stha04 <buddha.stha04@gmail.com>
2020-01-22 13:16:31 +0545

    final push

--- src/app/feature/loan/component/loan-main-template/group/security-detail/security-detail.component.ts
+++ src/app/feature/loan/component/loan-main-template/group/security-detail/security-detail.component.ts
@@ -1,88 +1,86 @@
-import {Component , Input , OnInit} from '@angular/core';
-import {FormArray , FormBuilder , FormGroup} from '@angular/forms';
-import {ObjectUtil} from '../../../../../../@core/utils/ObjectUtil';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 
 
 @Component({
-    selector: 'app-security-detail' ,
-    templateUrl: './security-detail.component.html' ,
-    styleUrls: ['./security-detail.component.scss']
+  selector: 'app-security-detail',
+  templateUrl: './security-detail.component.html',
+  styleUrls: ['./security-detail.component.scss']
 })
 export class SecurityDetailComponent implements OnInit {
-    securityGroup: FormGroup;
-    @Input() data: any;
-    @Input() finalValuatorName;
+  securityGroup: FormGroup;
+  @Input() data: any;
+  @Input() finalValuatorName;
 
-    constructor(
-        private securityBuilder: FormBuilder
-    ) {
+
+  constructor(
+      private securityBuilder: FormBuilder
+  ) {
+  }
+
+  ngOnInit() {
+    this.formBuild();
+    if (this.data !== undefined) {
+      this.data.securityDetail.securityArray.forEach((value) => this.setSecurityDetail(value));
+
+    } else {
+      this.addSecurityDetail();
     }
+  }
 
-    ngOnInit() {
-        this.formBuild();
-        if (this.data !== undefined) {
-            this.data.securityDetail.securityArray.forEach((value) => this.setSecurityDetail(value));
+  formBuild() {
+    this.securityGroup = this.securityBuilder.group({securityArray: this.securityBuilder.array([])});
+  }
 
-        } else {
-            this.addSecurityDetail();
-        }
-    }
+  addSecurityDetail() {
+    const addDetail = this.securityGroup.get('securityArray') as FormArray;
+    addDetail.push(
+        this.securityBuilder.group({
+          details: [undefined],
+          against: [undefined],
+          fmv: [undefined],
+          interestRate: [undefined],
+          dv: [undefined],
+          validatorName: [undefined],
+          valuationDate: [undefined],
+          number: [undefined],
+          couponRate: [undefined],
+          amount: [undefined],
+          expiryDate: [undefined],
+          beneficiary: [undefined],
+          fdAmount: [undefined],
+          value: [undefined],
+          shareValue: [undefined],
+          securityType: [undefined]
+        })
+    );
+  }
 
-    formBuild() {
-        this.securityGroup = this.securityBuilder.group({securityArray: this.securityBuilder.array([])});
-    }
+  setSecurityDetail(value) {
+    const addDetail = this.securityGroup.get('securityArray') as FormArray;
+    addDetail.push(
+        this.securityBuilder.group({
+          details: [value.details],
+          against: [value.against],
+          fmv: [value.fmv],
+          interestRate: [value.interestRate],
+          dv: [value.dv],
+          validatorName: [value.validatorName],
+          valuationDate: [value.valuatedDate],
+          number: [value.number],
+          couponRate: [value.couponRate],
+          amount: [value.amount],
+          expiryDate: [value.expiryDate],
+          beneficiary: [value.beneficiary],
+          fdAmount: [value.fdAmount],
+          value: [value.value],
+          shareValue: [value.shareValue],
+          securityType: [value.securityType]
+        })
+    );
+  }
 
-    addSecurityDetail() {
-        const addDetail = this.securityGroup.get('securityArray') as FormArray;
-        addDetail.push(
-            this.securityBuilder.group({
-                details: [undefined] ,
-                against: [undefined] ,
-                fmv: [undefined] ,
-                interestRate: [undefined] ,
-                dv: [undefined] ,
-                validatorName: [undefined] ,
-                valuationDate: [undefined] ,
-                number: [undefined] ,
-                couponRate: [undefined] ,
-                amount: [undefined] ,
-                expiryDate: [undefined] ,
-                beneficiary: [undefined] ,
-                fdAmount: [undefined] ,
-                value: [undefined] ,
-                shareValue: [undefined] ,
-                securityType: [undefined]
-            })
-        );
-    }
-
-    setSecurityDetail(value) {
-        const addDetail = this.securityGroup.get('securityArray') as FormArray;
-      addDetail.push(
-            this.securityBuilder.group({
-                details: [value.details] ,
-                against: [value.against] ,
-                fmv: [value.fmv] ,
-                interestRate: [value.interestRate] ,
-                dv: [value.dv] ,
-                validatorName: [!ObjectUtil.isEmpty(value.validatorName) &&
-                  !ObjectUtil.isEmpty(value.validatorName.id) ? value.validatorName : undefined ] ,
-                valuationDate: [value.valuatedDate] ,
-                number: [value.number] ,
-                couponRate: [value.couponRate] ,
-                amount: [value.amount] ,
-                expiryDate: [value.expiryDate] ,
-                beneficiary: [value.beneficiary] ,
-                fdAmount: [value.fdAmount] ,
-                value: [value.value] ,
-                shareValue: [value.shareValue] ,
-                securityType: [value.securityType]
-            })
-        );
-    }
-
-    removeSecurityDetail(index) {
-        (this.securityGroup.get('securityArray') as FormArray).removeAt(index);
-    }
-
+  removeSecurityDetail(index) {
+    (this.securityGroup.get('securityArray') as FormArray).removeAt(index);
+  }
 }
