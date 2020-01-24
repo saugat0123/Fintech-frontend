d6d2fe48a289696e02cf2bf6539fe2a99e3e5e36
noitanec <gnoitanec@gmail.com>
2020-01-24 14:11:47 +0545

    applicant-family-info-component added

---
+++ src/app/feature/loan/component/loan-main-nepali-template/applicant-family-info/applicant-family-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-family-info',
+  templateUrl: './applicant-family-info.component.html',
+  styleUrls: ['./applicant-family-info.component.scss']
+})
+export class ApplicantFamilyInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
