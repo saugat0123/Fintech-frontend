b7209cdae41f93961a1e3f9971ff5e0ee58a9244
Bibash7 <biswash166@gmail.com>
2020-02-01 13:02:17 +0545

    karanama offer letter

--- src/app/feature/loan/loan.module.ts
+++ src/app/feature/loan/loan.module.ts
@@ -68,6 +68,7 @@ import {CustomerInfoNepaliComponent} from './component/loan-main-nepali-template
 import { DhitoLikhatPrintComponent } from './component/offer-letter/dhito-likhat-manjurinama/dhito-likhat-print/dhito-likhat-print.component';
 import { ApplicantFamilyInfoComponent } from './component/loan-main-nepali-template/applicant-family-info/applicant-family-info.component';
 import { KarjaTamsukPrintComponent } from './component/offer-letter/karjatamsuk/karja-tamsuk-print/karja-tamsuk-print.component';
+import { KararnamaPrintComponent } from './component/offer-letter/kararnama/kararnama-print/kararnama-print.component';
 
 
 const COMPONENTS = [
@@ -138,7 +139,7 @@ const ENTRY_COMPONENTS = [
 // @ts-ignore
 // @ts-ignore
 @NgModule({
-    declarations: [...COMPONENTS],
+    declarations: [...COMPONENTS, KararnamaPrintComponent],
     imports: [
         ThemeModule,
         CommonModule,
