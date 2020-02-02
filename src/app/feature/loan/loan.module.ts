42eb81366bc73675abf54dfcfb49a746cfffaa07
Rabin maharjan <aloneme0015@gmail.com>
2020-02-02 14:53:44 +0545

    Jamani tamsuk commit

--- src/app/feature/loan/loan.module.ts
+++ src/app/feature/loan/loan.module.ts
@@ -65,6 +65,7 @@ import {PratigyaPatraComponent} from './component/offer-letter/pratigya-patra/pr
 import {LoanMainNepaliTemplateComponent} from './component/loan-main-nepali-template/loan-main-nepali-template.component';
 import {FiscalYearModalComponent} from './component/loan-main-template/financial/fiscal-year-modal/fiscal-year-modal.component';
 import {CustomerInfoNepaliComponent} from './component/loan-main-nepali-template/customer-info-nepali/customer-info-nepali.component';
+import {JamaniTamsukLetterPrintComponent} from './component/offer-letter/jamani-tamsuk/jamani-tamsuk-letter-print/jamani-tamsuk-letter-print.component';
 
 
 const COMPONENTS = [
@@ -130,7 +131,7 @@ const ENTRY_COMPONENTS = [
 // @ts-ignore
 // @ts-ignore
 @NgModule({
-    declarations: [...COMPONENTS],
+    declarations: [...COMPONENTS, JamaniTamsukLetterPrintComponent],
     imports: [
         ThemeModule,
         CommonModule,
