0cd407f4305154142f9904c69b5713ad8da6883a
Rabin maharjan <aloneme0015@gmail.com>
2020-02-02 18:47:20 +0545

    Jamani tamsuk commit

--- src/app/feature/loan/component/offer-letter/jamani-tamsuk/jamani-tamsuk-letter-print/jamani-tamsuk-letter-print.component.ts
+++ src/app/feature/loan/component/offer-letter/jamani-tamsuk/jamani-tamsuk-letter-print/jamani-tamsuk-letter-print.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-jamani-tamsuk-letter-print',
@@ -6,7 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./jamani-tamsuk-letter-print.component.scss']
 })
 export class JamaniTamsukLetterPrintComponent implements OnInit {
-
+  @Input()
+  letter: any;
   constructor() { }
 
   ngOnInit() {
