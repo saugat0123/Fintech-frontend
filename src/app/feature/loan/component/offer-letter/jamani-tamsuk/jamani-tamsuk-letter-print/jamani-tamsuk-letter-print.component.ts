636716c120b5e1d9365ee5ceebf3ed635465eb3c
Rabin maharjan <aloneme0015@gmail.com>
2020-02-02 18:41:20 +0545

    Jamani tamsuk commit

---
+++ src/app/feature/loan/component/offer-letter/jamani-tamsuk/jamani-tamsuk-letter-print/jamani-tamsuk-letter-print.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-jamani-tamsuk-letter-print',
+  templateUrl: './jamani-tamsuk-letter-print.component.html',
+  styleUrls: ['./jamani-tamsuk-letter-print.component.scss']
+})
+export class JamaniTamsukLetterPrintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
