68cc443d3e341d7aa4dff5e628bb1bed561b753d
Bibash7 <biswash166@gmail.com>
2020-02-04 14:01:06 +0545

    Merge branch 'master' of https://gitlab.com/sunilbabu/sbsolution-frontapp into jamaniBasekoTemplete
     Conflicts:
    	src/app/feature/loan/component/loan-main-nepali-template/loan-main-nepali-template.component.ts

--- src/app/feature/admin/service/base-interest.service.ts
+++ src/app/feature/admin/service/base-interest.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {BaseInterest} from '../component/base-interest/BaseInterest';
 import {BaseService} from '../../../@core/BaseService';
+import {Observable} from 'rxjs';
+import {ApiUtils} from '../../../@core/utils/api/ApiUtils';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +20,9 @@ export class BaseInterestService extends BaseService<BaseInterest> {
     return BaseInterestService.API;
   }
 
+ public getActiveBaseRate(): Observable<any> {
+   const req = ApiUtils.getRequest(`${this.getApi()}/active`);
 
+   return this.http.get(req.url , {headers : req.header});
+ }
 }
