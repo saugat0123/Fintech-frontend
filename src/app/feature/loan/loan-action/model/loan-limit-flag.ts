b434994ed1a1fdcd4211b0c88b2c65b15f227bec
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-20 20:03:29 +0545

    Refactor: Added LoanLimitFlag in LoanActionComponent

---
+++ src/app/feature/loan/loan-action/model/loan-limit-flag.ts
@@ -0,0 +1,6 @@
+export class LoanLimitFlag {
+  loanRemarks: string; // comment logs
+  limitExceed: number; // share loan limit exceed
+  lowProposedLimit: boolean; // loan proposed limit
+  lowCreditScore: boolean; // maw credit risk score
+}
