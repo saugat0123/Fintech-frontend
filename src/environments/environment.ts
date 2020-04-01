c75501060d3f1b3d79ed06f2b2d411df7fb77a3e
Rujan Maharjan <rujnmahrzn@gmail.com>
2020-04-01 09:23:54 +0545

    sb img

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  client: 'Mahalaxmi Bikas Bank Ltd',
+  client: 'Sb solutions Ltd',
   GOOGLE_MAP_API_KEY: 'AIzaSyDKERYllGf5BfVR_c_45nqHVkbHoPgXPDA',
   LOCAL_STORAGE_KEY: 'QP\'`0tWfyBni^(*rv0gB].ck$s@z(/',
   LOCAL_STORAGE_NAME: 'SBSolutionsLAS'
