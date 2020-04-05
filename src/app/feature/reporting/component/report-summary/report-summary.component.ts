851f7c06c087f43a21fa7335c02271582a9750ec
Elvin Shrestha <elwyncrestha@gmail.com>
2020-04-05 22:23:52 +0545

    Feature: Added functional reporting info summary.

--- src/app/feature/reporting/component/report-summary/report-summary.component.ts
+++ src/app/feature/reporting/component/report-summary/report-summary.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ReportingInfoService} from '../../../../@core/service/reporting-info.service';
+import {ReportingInfo} from '../../model/reporting-info';
+import {ReportingInfoLevel} from '../../model/reporting-info-level';
 
 @Component({
   selector: 'app-report-summary',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-summary.component.scss']
 })
 export class ReportSummaryComponent implements OnInit {
+  @Input() public reportingInfoLevels: Array<ReportingInfoLevel>;
+  public reportingInfoSummary: Array<ReportingInfo> = new Array<ReportingInfo>();
+  private savedReportTagsId: Set<number> = new Set<number>();
 
-  constructor() { }
+  constructor(
+      private reportingInfoService: ReportingInfoService,
+  ) {
+  }
 
   ngOnInit() {
+    this.reportingInfoService.getAllWithSearch({}).toPromise().then((response: any) => {
+      const reportingInfoList: Array<ReportingInfo> = response.detail;
+      this.reportingInfoLevels.forEach(v => this.savedReportTagsId.add(v.id));
+      reportingInfoList.forEach(v => {
+        const summaryReportingInfo = new ReportingInfo();
+        summaryReportingInfo.reportingInfoLevels = new Array<ReportingInfoLevel>();
+        this.checkReportingInfoLevels(v.name, v.reportingInfoLevels, summaryReportingInfo);
+        if (summaryReportingInfo.reportingInfoLevels.length > 0) {
+          this.reportingInfoSummary.push(summaryReportingInfo);
+        }
+      });
+    });
+  }
+
+  private checkReportingInfoLevels(reportName: string, reportingInfoLevels: Array<ReportingInfoLevel>,
+                                   summaryReportingInfo: ReportingInfo) {
+    if (reportingInfoLevels && reportingInfoLevels.length > 0) {
+      reportingInfoLevels.forEach(v => {
+        if (this.savedReportTagsId.has(v.id)) {
+          summaryReportingInfo.name = reportName;
+          summaryReportingInfo.reportingInfoLevels.push(v);
+        }
+        this.checkReportingInfoLevels(reportName, v.reportingInfoLevels, summaryReportingInfo);
+      });
+    }
   }
 
 }
