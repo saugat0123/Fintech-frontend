e2756dac5d4db931d4ca9f1540da771edf362912
Elvin Shrestha <elwyncrestha@gmail.com>
2020-04-12 18:55:20 +0545

    Refactor: Refactored insurance component

--- src/app/feature/loan/component/loan-main-template/insurance/insurance.component.ts
+++ src/app/feature/loan/component/loan-main-template/insurance/insurance.component.ts
@@ -33,22 +33,19 @@ export class InsuranceComponent implements OnInit {
 
     buildForm() {
         this.form = this.formBuilder.group({
-            insuranceCompany: [this.insurance.company === undefined ? undefined : this.insurance.company],
-            insuredAmount: [this.insurance.insuredAmount === undefined ? undefined : this.insurance.insuredAmount],
-            premiumAmount: [this.insurance.premiumAmount === undefined ? undefined : this.insurance.premiumAmount],
-            issuedDate: [this.insurance.issuedDate === undefined ? undefined : this.insurance.issuedDate],
-            expiryDate: [this.insurance.expiryDate === undefined ? undefined : this.insurance.expiryDate],
-            insurancePolicyType: [this.insurance.policyType === undefined ? undefined : this.insurance.policyType],
+            id: [ObjectUtil.setUndefinedIfNull(this.insurance.id)],
+            version: [ObjectUtil.setUndefinedIfNull(this.insurance.version)],
+            company: [ObjectUtil.setUndefinedIfNull(this.insurance.company)],
+            insuredAmount: [ObjectUtil.setUndefinedIfNull(this.insurance.insuredAmount)],
+            premiumAmount: [ObjectUtil.setUndefinedIfNull(this.insurance.premiumAmount)],
+            issuedDate: [this.insurance.issuedDate === undefined ? undefined : new Date(this.insurance.issuedDate)],
+            expiryDate: [this.insurance.expiryDate === undefined ? undefined : new Date(this.insurance.expiryDate)],
+            policyType: [ObjectUtil.setUndefinedIfNull(this.insurance.policyType)],
         });
     }
 
     submit() {
-        this.insurance.company = this.form.get('insuranceCompany').value;
-        this.insurance.insuredAmount = this.form.get('insuredAmount').value;
-        this.insurance.premiumAmount = this.form.get('premiumAmount').value;
-        this.insurance.issuedDate = this.form.get('issuedDate').value;
-        this.insurance.expiryDate = this.form.get('expiryDate').value;
-        this.insurance.policyType = this.form.get('insurancePolicyType').value;
+        this.insurance = this.form.value as Insurance;
     }
 
     returnIssuedDate() {
