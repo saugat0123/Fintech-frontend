913b8a7f431fd89931a1f18c8f507a4f4bbc5874
SujanDhakal111 <sdhakal361@gmail.com>
2020-08-20 07:13:00 +0545

    kyc form update continue

--- src/app/feature/customer/component/individual-customer-form/kyc-form/kyc-form.component.ts
+++ src/app/feature/customer/component/individual-customer-form/kyc-form/kyc-form.component.ts
@@ -49,6 +49,8 @@ export class KycFormComponent implements OnInit {
     this.id = this.route.snapshot.params.id;
     this.customerService.detail(this.id).subscribe((res: any) => {
       this.customer = res.detail;
+      this.kycRelative.push(res.detail.customerRelatives);
+      console.log(res.detail.customerRelatives);
       this.buildForm();
       this.createRelativesArray();
       console.log(this.customer);
@@ -56,26 +58,6 @@ export class KycFormComponent implements OnInit {
   }
   buildForm() {
     this.kycInfo =  this.formBuilder.group({
-      customerName: [this.customer.customerName === undefined ? undefined : this.customer.customerName],
-      province: [this.customer.province === null ? undefined : this.customer.province],
-      district: [this.customer.district === null ? undefined : this.customer.district],
-      municipalities: [this.customer.municipalities === null ? undefined : this.customer.municipalities],
-      street: [this.customer.street === null ? undefined : this.customer.street],
-      wardNumber: [this.customer.wardNumber === null ? undefined : this.customer.wardNumber],
-      contactNumber: [this.customer.contactNumber === undefined ? undefined : this.customer.contactNumber],
-      email: [this.customer.email === undefined ? undefined : this.customer.email],
-      initialRelationDate: [this.customer.initialRelationDate === undefined ? undefined :
-          new Date(this.customer.initialRelationDate)],
-      citizenshipNumber: [this.customer.citizenshipNumber === undefined ? undefined : this.customer.citizenshipNumber
-        , Validators.required],
-      citizenshipIssuedPlace: [this.customer.citizenshipIssuedPlace === undefined ? undefined : this.customer.citizenshipIssuedPlace,
-        Validators.required],
-      citizenshipIssuedDate: [ObjectUtil.isEmpty(this.customer.citizenshipIssuedDate) ? undefined :
-          new Date(this.customer.citizenshipIssuedDate)],
-      dob: [ObjectUtil.isEmpty(this.customer.dob) ? undefined :
-          new Date(this.customer.dob)],
-      occupation: [this.customer.occupation === undefined ? undefined : this.customer.occupation],
-      incomeSource: [this.customer.incomeSource === undefined ? undefined : this.customer.incomeSource],
       customerRelatives: this.formBuilder.array([])
     });
 
@@ -83,36 +65,13 @@ export class KycFormComponent implements OnInit {
 
 
   onSubmit() {
-    // this.data = this.kycInfo.value;
-    // this.customerService.save(this.data).subscribe((res: any) => {
-    //   console.log(res.detail);
-    //   this.customer = res.detail;
-    //   this.toastService.show(new Alert(AlertType.SUCCESS, 'SUCCESSFULLY UPDATED '));
-    // }, error => {
-    //   this.toastService.show(new Alert(AlertType.ERROR, error.error.message));
-    // });
-    this.customer.id = (this.customer.citizenshipIssuedPlace ===
-        this.kycInfo.get('citizenshipIssuedPlace').value) ? this.customer.id : undefined;
-    this.customer.customerName = this.kycInfo.get('customerName').value;
-    this.customer.province = this.kycInfo.get('province').value;
-    this.customer.district = this.kycInfo.get('district').value;
-    this.customer.municipalities = this.kycInfo.get('municipalities').value;
-    this.customer.street = this.kycInfo.get('street').value;
-    this.customer.wardNumber = this.kycInfo.get('wardNumber').value;
-    this.customer.contactNumber = this.kycInfo.get('contactNumber').value;
-    this.customer.email = this.kycInfo.get('email').value;
-    this.customer.dob = this.kycInfo.get('dob').value;
-    this.customer.initialRelationDate = this.kycInfo.get('initialRelationDate').value;
-    this.customer.citizenshipNumber = this.kycInfo.get('citizenshipNumber').value;
-    this.customer.citizenshipIssuedPlace = this.kycInfo.get('citizenshipIssuedPlace').value;
-    this.customer.citizenshipIssuedDate = this.kycInfo.get('citizenshipIssuedDate').value;
-    this.customer.occupation = this.kycInfo.get('occupation').value;
-    this.customer.incomeSource = this.kycInfo.get('incomeSource').value;
-    const rawFromValue = this.kycInfo.getRawValue();
-    this.customer.customerRelatives = rawFromValue.customerRelatives;
+    this.customer.customerRelatives.push(this.kycInfo.get('customerRelatives').value);
+    console.log(this.customer.customerRelatives);
 
     this.customerService.save(this.customer).subscribe(res => {
       console.log(res.detail);
+      this.kycRelative.push(res.detail);
+      console.log(this.kycRelative);
       this.toastService.show(new Alert(AlertType.SUCCESS, 'SUCCESSFULLY UPDATED '));
       this.buildForm();
     }, error => {
