34b30a8a6a36cd176730c5c9a067ee85feffdc01
buddha.stha04 <buddha.stha04@gmail.com>
2020-08-30 22:33:00 +0545

    general Doc Upload

---
+++ src/app/feature/customer/service/customer-general-document.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BaseService} from '../../../@core/BaseService';
+import {Observable} from 'rxjs';
+import {ApiUtils} from '../../../@core/utils/api/ApiUtils';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerGeneralDocumentService extends  BaseService<Object> {
+  static API = 'v1/customer-general-document';
+
+  constructor(protected http: HttpClient)  {
+    super(http);
+
+  }
+  public uploadDoc(formData: FormData): Observable<any> {
+    const req = ApiUtils.getRequestWithFileSupport(`${this.getApi()}/upload-document`);
+    return this.http.post(req.url, formData, {headers: req.header});
+  }
+  protected getApi(): string {
+    return CustomerGeneralDocumentService.API;
+  }
+}
