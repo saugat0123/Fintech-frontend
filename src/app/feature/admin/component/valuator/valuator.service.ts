d713e305625e742a8ea49b33c4bcbb73b85b2ade
buddha.stha04 <buddha.stha04@gmail.com>
2020-09-09 23:15:16 +0545

    valuator service import optimized

--- src/app/feature/admin/component/valuator/valuator.service.ts
+++ src/app/feature/admin/component/valuator/valuator.service.ts
@@ -2,8 +2,6 @@ import {BaseService} from '../../../../@core/BaseService';
 import {Valuator} from '../../modal/valuator';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {ApiUtils} from '../../../../@core/utils/api/ApiUtils';
 
 @Injectable({providedIn: 'root'})
 export class ValuatorService extends BaseService<Valuator> {
