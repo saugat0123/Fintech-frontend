c33080f4112d603d6902bc0ade0fdae662df03d4
Amulya Stha <sthaamulya@gmail.com>
2020-09-18 12:48:06 +0545

    crg questions configuration finalization.

---
+++ src/app/feature/credit-risk-grading/model/CrgQuestion.ts
@@ -0,0 +1,15 @@
+import {Status} from '../../../@core/Status';
+import {LoanCategory} from '../../loan/model/loan-category';
+import {CrgAnswer} from './CrgAnswer';
+
+export class CrgQuestion {
+    id: number;
+    description: string;
+    maximumPoints: number;
+    appearanceOrder: number;
+    status: Status;
+    version: number;
+    crgGroupId: number;
+    loanApprovalType: LoanCategory;
+    answers: Array<CrgAnswer>;
+}
