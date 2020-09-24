283286cf427db69fc6676463f286cda7404a86f4
Rujan Maharjan <rujnmahrzn@gmail.com>
2020-09-24 15:48:54 +0545

    added crg and crg alpha log

--- src/app/feature/customer-activity/component/update-view/update-view.component.ts
+++ src/app/feature/customer-activity/component/update-view/update-view.component.ts
@@ -2,6 +2,11 @@ import {Component, Input, OnInit} from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {CustomerInfoData} from '../../../loan/model/customerInfoData';
 import {ObjectUtil} from '../../../../@core/utils/ObjectUtil';
+import {CustomerType} from '../../../customer/model/customerType';
+import {Alert, AlertType} from '../../../../@theme/model/Alert';
+import {CompanyInfo} from '../../../admin/modal/company-info';
+import {CompanyInfoService} from '../../../admin/service/company-info.service';
+import {ToastService} from '../../../../@core/utils';
 
 @Component({
   selector: 'app-update-view',
@@ -24,7 +29,11 @@ export class UpdateViewComponent implements OnInit {
     priority: null
   };
 
-  constructor(private modalService: NgbModal
+  companyInfo = new CompanyInfo();
+
+  constructor(private modalService: NgbModal,
+              private companyInfoService: CompanyInfoService,
+              private toastService: ToastService,
   ) {
   }
 
@@ -47,6 +56,16 @@ export class UpdateViewComponent implements OnInit {
 
   close() {
     this.modalService.dismissAll();
+  }
+
+  checkCustomerType() {
+    if (CustomerType[this.profile.customerType] === CustomerType.COMPANY) {
+      this.companyInfoService.detail(this.profile.associateId).subscribe((res: any) => {
+        this.companyInfo = res.detail;
+      }, error => {
+        this.toastService.show(new Alert(AlertType.ERROR, 'Failed to load company information!'));
+      });
+    }
   }
 
 }
