f9424e19075090d448f1bf8b46bcb2a2337411df
sunilbau <whispersunil@gmail.com>
2020-09-29 18:26:42 +0545

    life insurance company added on insurance list

--- src/app/feature/loan/model/insuranceList.ts
+++ src/app/feature/loan/model/insuranceList.ts
@@ -80,5 +80,82 @@ export class InsuranceList {
       id: 'General Insurance Company Nepal Limited',
       name: 'General Insurance Company Nepal Limited',
     },
+    {
+      id: 'American Life Insurance Company Limited (MetLife)',
+      name: 'American Life Insurance Company Limited (MetLife)',
+    },
+    {
+      id: 'Asian Life Insurance Company Limited',
+      name: 'Asian Life Insurance Company Limited',
+    },
+    {
+      id: 'Citizen Life Insurance Company Limited',
+      name: 'Citizen Life Insurance Company Limited',
+    },
+    {
+      id: 'Gurans Life Insurance Company Limited',
+      name: 'Gurans Life Insurance Company Limited',
+    },
+    {
+      id: 'I.M.E. Life Insurance Company Limited',
+      name: 'I.M.E. Life Insurance Company Limited',
+    },
+    {
+      id: 'Jyoti Life Insurance Company Limited',
+      name: 'Jyoti Life Insurance Company Limited',
+    },
+    {
+      id: 'Life Insurance Corporation (Nepal) Limited',
+      name: 'Life Insurance Corporation (Nepal) Limited',
+    },
+    {
+      id: 'Mahalaxmi Life Insurance Company Limited',
+      name: 'Mahalaxmi Life Insurance Company Limited',
+    },
+    {
+      id: 'National Life Insurance Company Limited',
+      name: 'National Life Insurance Company Limited',
+    },
+    {
+      id: 'Nepal Life Insurance Company Limited',
+      name: 'Nepal Life Insurance Company Limited',
+    },
+    {
+      id: 'Prabhu Life Insurance Company Limited',
+      name: 'Prabhu Life Insurance Company Limited',
+    },
+    {
+      id: 'Prime Life Insurance Company Limited',
+      name: 'Prime Life Insurance Company Limited',
+    },
+    {
+      id: 'Rastriya Beema Sansthan',
+      name: 'Rastriya Beema Sansthan',
+    },
+    {
+      id: 'Reliable Nepal Life Insurance Limited',
+      name: 'Reliable Nepal Life Insurance Limited',
+    },
+    {
+      id: 'Reliance Life Insurance Limited',
+      name: 'Reliance Life Insurance Limited',
+    },
+    {
+      id: 'Sanima Life Insurance Company Limited',
+      name: 'Sanima Life Insurance Company Limited',
+    },
+    {
+      id: 'Sun Nepal Life Insurance Company Limited',
+      name: 'Sun Nepal Life Insurance Company Limited',
+    },
+    {
+      id: 'Surya Life Insurance Company Limited',
+      name: 'Surya Life Insurance Company Limited',
+    },
+    {
+      id: 'Union Life Insurance Company Limited',
+      name: 'Union Life Insurance Company Limited',
+    },
+
   ];
 }
