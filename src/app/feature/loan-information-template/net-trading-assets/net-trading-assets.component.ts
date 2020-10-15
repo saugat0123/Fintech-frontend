e11f181d937da5d1d28288d3a4834a2934684c78
Amulya Stha <sthaamulya@gmail.com>
2020-10-16 01:29:36 +0545

    Added NTA data field for fiscal year

--- src/app/feature/loan-information-template/net-trading-assets/net-trading-assets.component.ts
+++ src/app/feature/loan-information-template/net-trading-assets/net-trading-assets.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {NetTradingAssets} from '../../admin/modal/NetTradingAssets';
+import {ObjectUtil} from '../../../@core/utils/ObjectUtil';
+import {FormArray, FormBuilder} from '@angular/forms';
+import {FiscalYearService} from '../../admin/service/fiscal-year.service';
+import {FiscalYear} from '../../admin/modal/FiscalYear';
 
 @Component({
-  selector: 'app-net-trading-assets',
-  templateUrl: './net-trading-assets.component.html',
-  styleUrls: ['./net-trading-assets.component.scss']
+    selector: 'app-net-trading-assets',
+    templateUrl: './net-trading-assets.component.html',
+    styleUrls: ['./net-trading-assets.component.scss']
 })
 export class NetTradingAssetsComponent implements OnInit {
+    @Input() netTradingAssetsData: NetTradingAssets;
+    @Input() fromProfile: boolean;
+    @Output() netTradingAssetsEventEmitter = new EventEmitter();
 
-  constructor() { }
+    netTradingAssetSubmitData: NetTradingAssets;
+    netTradingAssetsFormArray: FormArray;
+    fiscalYearArray = new Array<FiscalYear>();
 
-  ngOnInit() {
-  }
+    constructor(protected formBuilder: FormBuilder,
+                protected fiscalYearService: FiscalYearService) {
+    }
+
+    ngOnInit() {
+        this.buildNtaForm();
+        this.getFiscalYears();
+
+    }
+
+    getFiscalYears() {
+        this.fiscalYearService.getAll().subscribe( response => {
+            this.fiscalYearArray = response.detail as Array<FiscalYear>;
+            console.log(this.fiscalYearArray);
+        }, error => {
+            console.log(error);
+        });
+    }
+
+    buildNtaForm() {
+        this.netTradingAssetsFormArray = this.formBuilder.array([]);
+    }
+
+    get netTradingAssetsFormArrayControl() {
+        return this.netTradingAssetsFormArray.controls;
+    }
+
+    onSubmit() {
+        if (!ObjectUtil.isEmpty(this.netTradingAssetsData)) {
+            this.netTradingAssetSubmitData = this.netTradingAssetsData;
+        }
+        this.netTradingAssetSubmitData.data = JSON.stringify(this.netTradingAssetsFormArray.value);
+        this.netTradingAssetsEventEmitter.emit(this.netTradingAssetSubmitData);
+    }
 
 }
