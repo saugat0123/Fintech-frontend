63a72b46253a696da5290506cad2e9f7e3473ca6
Amulya Stha <sthaamulya@gmail.com>
2020-10-26 23:59:31 +0545

    NTA summary component cleanup

--- src/app/feature/loan/component/loan-summary/loan-summary.module.ts
+++ src/app/feature/loan/component/loan-summary/loan-summary.module.ts
@@ -15,7 +15,6 @@ import {NgxPrintModule} from 'ngx-print';
 import {CoreModule} from '../../../../@core/core.module';
 import {CompanyInfoSummaryComponent} from './company-info-summary/company-info-summary.component';
 import {IncomeFromAccountSummeryComponent} from './income-from-account-summery/income-from-account-summery.component';
-import {NetTradingAssetsSummaryComponent} from './net-trading-assets-summary/net-trading-assets-summary.component';
 import {LoanInformationTemplateModule} from '../../../loan-information-template/loan-information-template.module';
 import {ProposalJustificationSummaryComponent} from './proposal-justification-summary/proposal-justification-summary.component';
 import { BusinessBackgroundComponent } from './company-info-summary/business-background/business-background.component';
@@ -34,7 +33,6 @@ const COMPONENTS = [
     LoanSummaryComponent,
     CompanyInfoSummaryComponent,
     IncomeFromAccountSummeryComponent,
-    NetTradingAssetsSummaryComponent,
     ProposalJustificationSummaryComponent,
     FinancialRiskSummaryComponent,
     BusinessBackgroundComponent
