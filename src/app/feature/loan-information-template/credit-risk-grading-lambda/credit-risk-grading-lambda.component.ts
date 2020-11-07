fc6ec569128b5c9bcb7cf94f104a1fa678f7bb9e
Amulya Stha <sthaamulya@gmail.com>
2020-11-07 13:03:12 +0545

    Added credit risk lambda component

---
+++ src/app/feature/loan-information-template/credit-risk-grading-lambda/credit-risk-grading-lambda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-risk-grading-lambda',
+  templateUrl: './credit-risk-grading-lambda.component.html',
+  styleUrls: ['./credit-risk-grading-lambda.component.scss']
+})
+export class CreditRiskGradingLambdaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
