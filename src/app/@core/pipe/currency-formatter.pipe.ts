66ce3284aeafce03a7c1ea0e9a9a1e83fb040047
Bibash7 <biswash166@gmail.com>
2020-12-01 14:07:24 +0545

    Merge branch 'frontend-master-v2' of https://gitlab.com/sunilbabu/sbsolution-frontapp into customerTemplateSummaryField
     Conflicts:
    	src/app/feature/loan/component/loan-summary/loan-summary.component.html

--- src/app/@core/pipe/currency-formatter.pipe.ts
+++ src/app/@core/pipe/currency-formatter.pipe.ts
@@ -7,7 +7,7 @@ export class CurrencyFormatterPipe implements PipeTransform {
 
     transform(value: number): string {
 
-        value = Math.round(value);
+      /*  value = Math.round(value);
         const result = value.toString().split('.');
         let lastThree = result[0].substring(result[0].length - 3);
         const otherNumbers = result[0].substring(0, result[0].length - 3);
@@ -18,9 +18,9 @@ export class CurrencyFormatterPipe implements PipeTransform {
 
         if (result.length > 1) {
             output += '.' + result[1];
-        }
+        }*/
 
-        return output;
+        return Number(value).toLocaleString();
 
     }
 
