356fb15a588634db4000ec4b213689d854f5be55
lucifer403403 <np01cp4a170169@islingtoncollege.edu.np>
2020-12-07 20:29:30 +0545

    Transfer doc component

--- src/app/@core/service/user.service.ts
+++ src/app/@core/service/user.service.ts
@@ -2,18 +2,84 @@ import {HttpClient} from '@angular/common/http';
 import {ApiUtils} from '../utils/api/ApiUtils';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
+import {User} from "../../feature/admin/modal/user";
+import {BaseService} from "../BaseService";
 
-@Injectable({
-    providedIn: 'root'
-})
-export class UserService {
-    static API = 'v1/user/authenticated';
+@Injectable({providedIn: 'root'})
+export class UserService extends BaseService<User> {
+    static API = 'v1/user';
 
-    constructor(private http: HttpClient) {
+    constructor(readonly http: HttpClient) {
+        super(http);
     }
 
-    public getLoggedInUser(): Observable<Object> {
-        const req = ApiUtils.getRequest(UserService.API);
+    protected getApi(): string {
+        return UserService.API;
+    }
+
+    public getUserListByRoleId(id): Observable<any> {
+        const req = ApiUtils.getRequestWithFileSupport(`${UserService.API}/${id}/users`);
+        return this.http.get(req.url, {headers: req.header});
+    }
+
+    public getUserListByRoleCad(): Observable<any> {
+        const req = ApiUtils.getRequestWithFileSupport(`${UserService.API}/user-cad`);
+        return this.http.get(req.url, {headers: req.header});
+    }
+
+    public getUsersByRole(roles: any): Observable<any> {
+        const req = ApiUtils.getRequest(`${UserService.API}/listByRole`);
+
+        return this.http.post(req.url, roles, {headers: req.header});
+    }
+
+    public getRoles(): Observable<any> {
+        const req = ApiUtils.getRequest(`${UserService.API}/listRole`);
+
+        return this.http.get(req.url, {headers: req.header});
+    }
+
+    public uploadFile(formData: FormData): Observable<any> {
+        const req = ApiUtils.getRequestWithFileSupport(`${this.getApi()}/uploadFile`);
+        console.log(formData.get('file'));
+        return this.http.post(req.url, formData, {headers: req.header});
+    }
+
+    public getLoggedInUser(): Observable<any> {
+        const req = ApiUtils.getRequest(`${UserService.API}/authenticated`);
+
+        return this.http.get(req.url, {headers: req.header});
+    }
+
+    public postDismiss(user: any): Observable<any> {
+        const req = ApiUtils.getRequest(`${UserService.API}/dismiss`);
+
+        return this.http.post(req.url, user, {headers: req.header});
+    }
+
+    public getUserListForTransfer(id): Observable<any> {
+        const req = ApiUtils.getRequest(`${UserService.API}/get-all-doc-transfer/${id}`);
+
+        return this.http.get(req.url, {headers: req.header});
+    }
+
+    public updateUserPassword(obj: Object): Observable<any> {
+        const req = ApiUtils.getRequest(`${UserService.API}/changePassword`);
+        return this.http.post(req.url, obj, {headers: req.header});
+    }
+
+    public getAuthenticatedUserBranches(): Observable<any> {
+        const req = ApiUtils.getRequest(`${this.getApi()}/branch/all`);
+        return this.http.get(req.url, {headers: req.header});
+    }
+
+    public getUserForChat(): Observable<any> {
+        const req = ApiUtils.getRequest(`${this.getApi()}/chat`);
+        return this.http.get(req.url, {headers: req.header});
+    }
+
+    public getUserListByRoleIdAndBranchIdForDocumentAction(id, bId): Observable<any> {
+        const req = ApiUtils.getRequestWithFileSupport(`${UserService.API}/${id}/users/branch/${bId}`);
         return this.http.get(req.url, {headers: req.header});
     }
 }
