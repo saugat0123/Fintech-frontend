4632803fd898f233d4600ea81938dda52f9bf313
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-12-10 12:03:06 +0545

    Allow only two decimal number

--- src/app/@core/directive/decimal-number.directive.ts
+++ src/app/@core/directive/decimal-number.directive.ts
@@ -10,7 +10,6 @@ export class DecimalNumberDirective {
   constructor(private el: ElementRef) {}
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
-    console.log(this.el.nativeElement.value);
     // Allow Backspace, tab, end, and home keys
     if (this.specialKeys.indexOf(event.key) !== -1) {
       return;
