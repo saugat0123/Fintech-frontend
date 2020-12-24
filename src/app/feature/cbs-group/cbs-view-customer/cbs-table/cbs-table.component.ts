fc48bcf9b27596983be659bb6d79339e30fa0158
rujnmahrzn <rujnmahrzn@gmail.com>
2020-12-24 08:14:46 +0545

    cbs approved limit and outstanding principal calcualtion

--- src/app/feature/cbs-group/cbs-view-customer/cbs-table/cbs-table.component.ts
+++ src/app/feature/cbs-group/cbs-view-customer/cbs-table/cbs-table.component.ts
@@ -13,6 +13,8 @@ export class CbsTableComponent implements OnInit {
     @Input()
     customerInfo: CustomerInfoData;
     list: Array<CbsGroup> = new Array<CbsGroup>();
+    totalApprovedLimit = 0;
+    totalOutstandingLimit = 0;
 
     constructor(private cbsService: CbsGroupService) {
     }
@@ -21,8 +23,24 @@ export class CbsTableComponent implements OnInit {
         if (!ObjectUtil.isEmpty(this.customerInfo)) {
             this.cbsService.getAllByOblId(this.customerInfo.obligor).subscribe((res: any) => {
                 this.list = res.detail;
+                this.list.forEach(l => {
+                    this.totalApprovedLimit = this.totalApprovedLimit + this.isNumber(l.jsonDataMap.ApprovedLimit);
+                    this.totalOutstandingLimit = this.totalApprovedLimit + this.isNumber(l.jsonDataMap.OSPrincipal);
+                });
             });
         }
+    }
+
+    public isNumber(value) {
+        if (ObjectUtil.isEmpty(value)) {
+            return 0;
+        }
+        if (Number.isNaN(value)) {
+            return 0;
+        } else {
+            return value;
+        }
+
     }
 
 }
