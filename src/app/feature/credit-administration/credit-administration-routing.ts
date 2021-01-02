82fe2ea65dda7a0172882db408f6f8ec89aa5fa6
rujnmahrzn <rujnmahrzn@gmail.com>
2021-01-02 16:04:48 +0545

    continued cad

--- src/app/feature/credit-administration/credit-administration-routing.ts
+++ src/app/feature/credit-administration/credit-administration-routing.ts
@@ -2,10 +2,11 @@ import {Routes} from '@angular/router';
 import {LoanListComponent} from './component/loan-list/loan-list.component';
 import {OfferLetterListComponent} from './component/offer-letter-list/offer-letter-list.component';
 import {CadOfferLetterProfileComponent} from './cad-offerletter-profile/cad-offerletter-profile.component';
+import {LegalAndDisbursementComponent} from './cad-work-flow/cad-work-flow-base/legal-and-disbursement/legal-and-disbursement.component';
 
 export const routes: Routes = [
     {path: 'offer-pending', component: OfferLetterListComponent},
-    {path: 'offer-letter-profile', component: CadOfferLetterProfileComponent},
+    {path: 'offer-letter-profile', component: LegalAndDisbursementComponent},
 
     {path: '', component: LoanListComponent},
 ];
