9172ac318ee73419b8611b27644a437b9f978d91
Bishow Thapa <bishowthapa33@gmail.com>
2021-01-05 12:16:07 +0545

    added component 1. assignment of receivable 2. deed hyop of machinery 3. hyop of stock

--- src/app/feature/credit-administration/cad-document/mega/mega.module.ts
+++ src/app/feature/credit-administration/cad-document/mega/mega.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoanDeedCompanyComponent } from './loan-deed-company/loan-deed-company.component';
+import { AssignmentOfReceivableComponent } from './assignment-of-receivable/assignment-of-receivable.component';
+import { DeedHypoOfMachineryComponent } from './deed-hypo-of-machinery/deed-hypo-of-machinery.component';
+import { HyopOfStockComponent } from './hyop-of-stock/hyop-of-stock.component';
 
 
 
 @NgModule({
-  declarations: [LoanDeedCompanyComponent],
+  declarations: [LoanDeedCompanyComponent, AssignmentOfReceivableComponent, DeedHypoOfMachineryComponent, HyopOfStockComponent],
   imports: [
     CommonModule
   ]
