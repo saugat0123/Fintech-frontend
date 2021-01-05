a5225b0222e87860c8e0cdb533c1a083fc61f899
Ritesh_Shrestha <shrestharitesh10024@gmail.com>
2021-01-05 17:13:49 +0545

    Added nine components in mega cad documents.

--- src/app/feature/credit-administration/cad-document/mega/mega.module.ts
+++ src/app/feature/credit-administration/cad-document/mega/mega.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoanDeedCompanyComponent } from './loan-deed-company/loan-deed-company.component';
+import { PersonalGuaranteeCompanyComponent } from './personal-guarantee-company/personal-guarantee-company.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { PersonalGuaranteePersonToPersonComponent } from './personal-guarantee-person-to-person/personal-guarantee-person-to-person.component';
+import { PersonalGuaraneeJointBorrowerComponent } from './personal-guaranee-joint-borrower/personal-guaranee-joint-borrower.component';
+import { ManjurinamaForCompanyComponent } from './manjurinama-for-company/manjurinama-for-company.component';
+import { TrustReceiptNepaliLimitComponent } from './trust-receipt-nepali-limit/trust-receipt-nepali-limit.component';
+import { PromissoryNoteSingleBorrowerComponent } from './promissory-note-single-borrower/promissory-note-single-borrower.component';
+import { AssignmentOfReceivableComponent } from './assignment-of-receivable/assignment-of-receivable.component';
+import { DeedHypoOfMachineryComponent } from './deed-hypo-of-machinery/deed-hypo-of-machinery.component';
+import { HypoOfStockComponent } from './hypo-of-stock/hypo-of-stock.component';
 
 
 
 @NgModule({
-  declarations: [LoanDeedCompanyComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoanDeedCompanyComponent, PersonalGuaranteeCompanyComponent, PersonalGuaranteePersonToPersonComponent, PersonalGuaraneeJointBorrowerComponent, ManjurinamaForCompanyComponent, TrustReceiptNepaliLimitComponent, PromissoryNoteSingleBorrowerComponent, AssignmentOfReceivableComponent, DeedHypoOfMachineryComponent, HypoOfStockComponent],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ]
 })
 export class MegaModule { }
