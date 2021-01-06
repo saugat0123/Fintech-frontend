bed6b50068cef36bbbaaa2f8ac0b389b2337ab49
rujnmahrzn <rujnmahrzn@gmail.com>
2021-01-06 15:20:37 +0545

    null value check

--- src/app/@core/utils/replace.pipe.ts
+++ src/app/@core/utils/replace.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {ObjectUtil} from './ObjectUtil';
 
 @Pipe({
   name: 'replace'
@@ -7,6 +8,9 @@ export class ReplacePipe implements PipeTransform {
 
   transform(value: string, regexValue: string, replaceValue: string): any {
     const regex = new RegExp(regexValue, 'g');
+    if(ObjectUtil.isEmpty(value)){
+      return value;
+    }
     return value.replace(regex, replaceValue);
   }
 
