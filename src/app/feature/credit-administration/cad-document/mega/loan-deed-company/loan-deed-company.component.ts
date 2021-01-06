f21dffba0c9b801e0e66a7a9a0a58d9f2090fb9b
= <=>
2021-01-06 12:46:36 +0545

    loan deed company added loan deed multiple added loan deed single added promissory note company added promissory note joint added

--- src/app/feature/credit-administration/cad-document/mega/loan-deed-company/loan-deed-company.component.ts
+++ src/app/feature/credit-administration/cad-document/mega/loan-deed-company/loan-deed-company.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {LoanDataHolder} from '../../../../loan/model/loanData';
+import {CustomerOfferLetter} from '../../../../loan/model/customer-offer-letter';
+import {CustomerOfferLetterService} from '../../../../loan/service/customer-offer-letter.service';
+import {ToastService} from '../../../../../@core/utils';
+import {Router} from '@angular/router';
+import {Alert, AlertType} from '../../../../../@theme/model/Alert';
+import {DocStatus} from '../../../../loan/model/docStatus';
+import {OfferLetter} from '../../../../admin/modal/offerLetter';
+import {CustomerOfferLetterPath} from '../../../../loan/model/customer-offer-letter-path';
 
 @Component({
   selector: 'app-loan-deed-company',
@@ -7,9 +17,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanDeedCompanyComponent implements OnInit {
 
-  constructor() { }
+  loandeedcompany: FormGroup;
 
-  ngOnInit() {
+
+  constructor(private formBuilder: FormBuilder,
+  ) {
   }
 
+  ngOnInit() {
+    console.log(this.loandeedcompany);
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.loandeedcompany = this.formBuilder.group({
+      temporaryProvince: [undefined],
+      permanentDistrict: [undefined],
+      permanentMunicipalityVdc: [undefined],
+      permanentWardNo: [undefined],
+      registrarRegistrationOffice: [undefined],
+      registrationNo: [undefined],
+      registrationDate: [undefined],
+      grandParents: [undefined],
+      parents: [undefined],
+      temporaryDistrict: [undefined],
+      temporaryMunicipalityVDC: [undefined],
+      temporaryWardNo: [undefined],
+      temporaryAddress: [undefined],
+      age: [undefined],
+      relation: [undefined],
+      citizenshipNo: [undefined],
+      issueDate: [undefined],
+      issueDistrict: [undefined],
+      date2: [undefined],
+      date3: [undefined],
+      loan: [undefined],
+      purpose: [undefined],
+      annualRate: [undefined],
+      onePerson: [undefined],
+      relation2: [undefined],
+      rate: [undefined],
+      sNo: [undefined],
+      landOwnerName: [undefined],
+      amount: [undefined],
+      amountInWords: [undefined],
+      district4: [undefined],
+      timeDuration: [undefined],
+      municipalityVdc5: [undefined],
+      wardNo5: [undefined],
+      seatNo: [undefined],
+      kNo: [undefined],
+      area: [undefined],
+      rNoDate: [undefined],
+      rohbarBankEmployeeName: [undefined],
+      nameOfAuthorizedPerson: [undefined],
+      guarantorName: [undefined],
+      guarantorName2: [undefined],
+      year: [undefined],
+      month: [undefined],
+      day: [undefined],
+      time: [undefined],
+      districtOfWitness: [undefined],
+      municipalityVdcOfWitness: [undefined],
+      wardNoOfWitness: [undefined],
+      ageOfWitness: [undefined],
+      relationOfWitness: [undefined],
+      districtOfWitness2: [undefined],
+      municipalityVdcOfWitness2: [undefined],
+      wardNoOfWitness2: [undefined],
+      ageOfWitness2: [undefined],
+      relationOfWitness2: [undefined]
+    });
+  }
+
+
+  submit(): void {
+    console.log(this.loandeedcompany.value);
+  }
 }
--- src/app/feature/credit-administration/cad-document/mega/promissory-note-joint/promissory-note-joint.component.ts
+++ src/app/feature/credit-administration/cad-document/mega/promissory-note-joint/promissory-note-joint.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-promissory-note-joint',
@@ -7,9 +8,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromissoryNoteJointComponent implements OnInit {
 
-  constructor() { }
+  promissorynotejoint: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder,
+             ) {
   }
 
+  ngOnInit() {
+    console.log(this.promissorynotejoint);
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.promissorynotejoint = this.formBuilder.group({
+      temporaryProvince: [undefined],
+      permanentDistrict: [undefined],
+      permanentMunicipalityVdc: [undefined],
+      permanentWardNo: [undefined],
+      registrarRegistrationOffice: [undefined],
+      registrationNo: [undefined],
+      registrationDate: [undefined],
+      grandParents: [undefined],
+      parents: [undefined],
+      temporaryDistrict: [undefined],
+      temporaryMunicipalityVDC: [undefined],
+      temporaryWardNo: [undefined],
+      temporaryAddress: [undefined],
+      age: [undefined],
+      relation: [undefined],
+      citizenshipNo: [undefined],
+      issueDate: [undefined],
+      issueDistrict: [undefined],
+      date2: [undefined],
+      date3: [undefined],
+      loan: [undefined],
+      purpose: [undefined],
+      annualRate: [undefined],
+      onePerson: [undefined],
+      relation2: [undefined],
+      rate: [undefined],
+      sNo: [undefined],
+      landOwnerName: [undefined],
+      amount: [undefined],
+      amountInWords: [undefined],
+      district4: [undefined],
+      timeDuration: [undefined],
+      municipalityVdc5: [undefined],
+      wardNo5: [undefined],
+      seatNo: [undefined],
+      kNo: [undefined],
+      area: [undefined],
+      rNoDate: [undefined],
+      rohbarBankEmployeeName: [undefined],
+      nameOfAuthorizedPerson: [undefined],
+      guarantorName: [undefined],
+      guarantorName2: [undefined],
+      year: [undefined],
+      month: [undefined],
+      day: [undefined],
+      time: [undefined],
+      districtOfWitness: [undefined],
+      municipalityVdcOfWitness: [undefined],
+      wardNoOfWitness: [undefined],
+      ageOfWitness: [undefined],
+      relationOfWitness: [undefined],
+      districtOfWitness2: [undefined],
+      municipalityVdcOfWitness2: [undefined],
+      wardNoOfWitness2: [undefined],
+      ageOfWitness2: [undefined],
+      relationOfWitness2: [undefined]
+    });
+  }
+
+  submit(): void {
+    console.log(this.promissorynotejoint.value);
+  }
 }
