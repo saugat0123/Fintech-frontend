1af5dda94c50fad4cbd679a971d0706e7de39de1
rujnmahrzn <rujnmahrzn@gmail.com>
2021-01-08 13:55:46 +0545

    cad stage button validation

--- src/app/feature/credit-administration/model/customerApprovedLoanCadDocumentation.ts
+++ src/app/feature/credit-administration/model/customerApprovedLoanCadDocumentation.ts
@@ -1,17 +1,17 @@
 import {BaseEntity} from '../../../@core/model/base-entity';
 import {CustomerInfoData} from '../../loan/model/customerInfoData';
 import {LoanDataHolder} from '../../loan/model/loanData';
-import {Stage} from '../../loan/model/stage';
 import {CadFile} from './CadFile';
 import {OfferDocument} from './OfferDocument';
 import {CadDocStatus} from './CadDocStatus';
 import {Exposure} from './Exposure';
+import {CadStage} from './cadStage';
 
 export class CustomerApprovedLoanCadDocumentation extends BaseEntity {
 
     loanHolder: CustomerInfoData;
     assignedLoan: Array<LoanDataHolder>;
-    cadCurrentStage: any;
+    cadCurrentStage: CadStage;
     cadFileList: Array<CadFile>;
     offerDocumentList: Array<OfferDocument>;
     cadStageList: string;
