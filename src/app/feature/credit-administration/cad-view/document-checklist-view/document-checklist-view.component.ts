aa86890947c6d49655e5cd39df018854dafc9134
rujnmahrzn <rujnmahrzn@gmail.com>
2021-01-10 12:52:54 +0545

    cad document view

--- src/app/feature/credit-administration/cad-view/document-checklist-view/document-checklist-view.component.ts
+++ src/app/feature/credit-administration/cad-view/document-checklist-view/document-checklist-view.component.ts
@@ -1,5 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CustomerApprovedLoanCadDocumentation} from '../../model/customerApprovedLoanCadDocumentation';
+import {LoanDataHolder} from '../../../loan/model/loanData';
+import {CreditAdministrationService} from '../../service/credit-administration.service';
+import {ToastService} from '../../../../@core/utils';
+import {NbDialogService} from '@nebular/theme';
+import {RouterUtilsService} from '../../utils/router-utils.service';
+import {ObjectUtil} from '../../../../@core/utils/ObjectUtil';
+import {Alert, AlertType} from '../../../../@theme/model/Alert';
+import {ApiConfig} from '../../../../@core/utils/api/ApiConfig';
+import {CadChecklistDocTemplateModalComponent} from '../../cad-offerletter-profile/cad-checklist-doc-template-modal/cad-checklist-doc-template-modal.component';
 
 @Component({
   selector: 'app-document-checklist-view',
@@ -7,11 +16,76 @@ import {CustomerApprovedLoanCadDocumentation} from '../../model/customerApproved
   styleUrls: ['./document-checklist-view.component.scss']
 })
 export class DocumentChecklistViewComponent implements OnInit {
-  @Input()
-  cadOfferLetterApprovedDoc: CustomerApprovedLoanCadDocumentation;
-  constructor() { }
+  @Input() cadData: CustomerApprovedLoanCadDocumentation;
+  customerLoanList: Array<LoanDataHolder>;
+  customerCadFile = [];
 
-  ngOnInit() {
+  uploadFile;
+
+  constructor(private creditAdministrationService: CreditAdministrationService,
+              private toastService: ToastService,
+              private nbDialogService: NbDialogService ,
+              private routerUtilsService: RouterUtilsService) {
   }
 
+  ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.cadData)) {
+      this.customerLoanList = this.cadData.assignedLoan;
+
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        this.customerLoanList.forEach(loanList => {
+          if (singleCadFile.customerLoanId === loanList.id) {
+            loanList.loan.approvedDocument.forEach(doc => {
+              if (doc.id === singleCadFile.cadDocument.id) {
+                doc.checked = true;
+                doc.url = singleCadFile.path;
+              }
+            });
+          }
+        });
+      });
+    }
+  }
+
+  uploadOfferLetter(event) {
+    this.uploadFile = event.target.files[0];
+  }
+
+  save(loanHolderId, customerLoanId, documentId, documentName) {
+    const formData: FormData = new FormData();
+    formData.append('file', this.uploadFile);
+    formData.append('customerInfoId', loanHolderId);
+    formData.append('loanID', customerLoanId);
+    formData.append('documentId', documentId);
+    formData.append('customerApprovedDocId', this.cadData.id.toString());
+
+    formData.append('documentName', documentName);
+
+    this.creditAdministrationService.uploadCreditCheckList(formData).subscribe((res: any) => {
+          this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved ' + documentName));
+          this.routerUtilsService.reloadCadProfileRoute(this.cadData.id);
+        }, error => {
+          this.toastService.show(new Alert(AlertType.ERROR, error));
+        }
+
+    );
+
+  }
+
+  previewDoc(url: string, name: string) {
+    const link = document.createElement('a');
+    link.target = '_blank';
+    link.href = `${ApiConfig.URL}/${url}`;
+    link.setAttribute('visibility', 'hidden');
+    link.click();
+  }
+
+  populateCadTemplate(documentId, loanId) {
+    this.nbDialogService.open(CadChecklistDocTemplateModalComponent, { context: {
+        documentId: documentId,
+        cadData: this.cadData,
+        customerLoanId: loanId
+      }
+    });
+  }
 }
