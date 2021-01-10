bd40a6fe7df381f811fcf0bf46bb3b12406333f7
Bibash7 <biswash166@gmail.com>
2021-01-10 17:11:27 +0545

    refactoring fee commission form

--- src/app/feature/credit-administration/cad-work-flow/cad-work-flow-base/legal-and-disbursement/fees-commission/fees-commission.component.ts
+++ src/app/feature/credit-administration/cad-work-flow/cad-work-flow-base/legal-and-disbursement/fees-commission/fees-commission.component.ts
@@ -1,12 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CustomerApprovedLoanCadDocumentation} from '../../../../model/customerApprovedLoanCadDocumentation';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Form, FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CreditAdministrationService} from '../../../../service/credit-administration.service';
 import {RouterUtilsService} from '../../../../utils/router-utils.service';
 import {ToastService} from '../../../../../../@core/utils';
 import {Alert, AlertType} from '../../../../../../@theme/model/Alert';
 import {ObjectUtil} from '../../../../../../@core/utils/ObjectUtil';
-import {Exposure} from '../../../../model/Exposure';
 
 @Component({
   selector: 'app-fees-commission',
@@ -27,12 +26,24 @@ export class FeesCommissionComponent implements OnInit {
               private service: CreditAdministrationService,
               private toastService: ToastService) { }
 
+  get feeAmountDetails() {
+    return this.feeCommissionFormGroup.get('feeAmountDetails') as FormArray;
+  }
+
+  // @ts-ignore
+   loanFeeDetail(i: number) {
+    return this.feeAmountDetails.at(i).get('loanFeeDetails') as FormArray;
+  }
+
   ngOnInit() {
     this.feeCommissionFormGroup = this.formBuilder.group({
-      feeAmountDetails : this.formBuilder.array([])
+      feeAmountDetails: this.formBuilder.array([])
     });
     this.addFeeAmountDetails();
-    this.setFeeAmountDetails();
+    /*  this.setFeeAmountDetails();*/
+
+    console.log(this.feeCommissionFormGroup);
+    console.log(this.feeCommissionFormGroup.value);
   }
 
   setFeeAmountDetails() {
@@ -41,26 +52,42 @@ export class FeesCommissionComponent implements OnInit {
        data = JSON.parse(this.cadData.feesAndCommission);
       this.feeAmountDetails.patchValue(data.feeAmountDetails);
     }
-    }
+  }
 
-  addFeeAmountDetails () {
+  addFeeAmountDetails() {
     this.cadData.assignedLoan.forEach(value => {
       this.feeAmountDetails.push(this.formBuilder.group({
         loanName: [value.loan.name],
-        feeType: [undefined , Validators.required],
-        feePercent: [0 , Validators.required],
-        feeAmount: [0 , Validators.required],
+        loanId: [value.loan.id],
+        loanFeeDetails: this.formBuilder.array([this.loanFeeDetails()])
       }));
     });
   }
+  /** @param i -: index of parent form array **/
+  addLoanFeeDetails(i: number) {
+    this.loanFeeDetail(i).push(this.loanFeeDetails());
+  }
 
-  get feeAmountDetails() {
-    return this.feeCommissionFormGroup.get('feeAmountDetails') as FormArray;
+  /** @param i -: index of parent form array *
+   * @param j -: index of child form array
+   */
+  removeLoanFeeDetails(i: number , j: number) {
+    this.loanFeeDetail(i).removeAt(j);
+  }
+
+  loanFeeDetails() {
+    return this.formBuilder.group({
+      feeType: [undefined, Validators.required],
+      feePercent: [0, Validators.required],
+      feeAmount: [0, Validators.required],
+    });
   }
 
   get totalFeeAmount() {
     let t = 0;
-    this.feeAmountDetails.controls.forEach(value => t += Number(value.get('feeAmount').value));
+    this.feeAmountDetails.controls.forEach(f => {
+    (f.get('loanFeeDetails') as FormArray).controls
+        .forEach(l =>  t += Number(l.get('feeAmount').value)); });
     return t;
   }
 
