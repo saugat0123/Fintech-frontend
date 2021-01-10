a91ab8b8d939598e32ecbb9bf718f1aa2fb2d2be
rujnmahrzn <rujnmahrzn@gmail.com>
2021-01-10 13:03:09 +0545

    cad document view d/w

--- src/app/feature/credit-administration/cad-view/document-checklist-view/document-checklist-view.component.ts
+++ src/app/feature/credit-administration/cad-view/document-checklist-view/document-checklist-view.component.ts
@@ -7,85 +7,79 @@ import {NbDialogService} from '@nebular/theme';
 import {RouterUtilsService} from '../../utils/router-utils.service';
 import {ObjectUtil} from '../../../../@core/utils/ObjectUtil';
 import {Alert, AlertType} from '../../../../@theme/model/Alert';
-import {ApiConfig} from '../../../../@core/utils/api/ApiConfig';
 import {CadChecklistDocTemplateModalComponent} from '../../cad-offerletter-profile/cad-checklist-doc-template-modal/cad-checklist-doc-template-modal.component';
+import {CommonService} from '../../../../@core/service/common.service';
 
 @Component({
-  selector: 'app-document-checklist-view',
-  templateUrl: './document-checklist-view.component.html',
-  styleUrls: ['./document-checklist-view.component.scss']
+    selector: 'app-document-checklist-view',
+    templateUrl: './document-checklist-view.component.html',
+    styleUrls: ['./document-checklist-view.component.scss']
 })
 export class DocumentChecklistViewComponent implements OnInit {
-  @Input() cadData: CustomerApprovedLoanCadDocumentation;
-  customerLoanList: Array<LoanDataHolder>;
-  customerCadFile = [];
+    @Input() cadData: CustomerApprovedLoanCadDocumentation;
+    customerLoanList: Array<LoanDataHolder>;
+    customerCadFile = [];
 
-  uploadFile;
+    uploadFile;
 
-  constructor(private creditAdministrationService: CreditAdministrationService,
-              private toastService: ToastService,
-              private nbDialogService: NbDialogService ,
-              private routerUtilsService: RouterUtilsService) {
-  }
-
-  ngOnInit() {
-    if (!ObjectUtil.isEmpty(this.cadData)) {
-      this.customerLoanList = this.cadData.assignedLoan;
-
-      this.cadData.cadFileList.forEach(singleCadFile => {
-        this.customerLoanList.forEach(loanList => {
-          if (singleCadFile.customerLoanId === loanList.id) {
-            loanList.loan.approvedDocument.forEach(doc => {
-              if (doc.id === singleCadFile.cadDocument.id) {
-                doc.checked = true;
-                doc.url = singleCadFile.path;
-              }
-            });
-          }
-        });
-      });
+    constructor(private creditAdministrationService: CreditAdministrationService,
+                private toastService: ToastService,
+                private nbDialogService: NbDialogService,
+                private routerUtilsService: RouterUtilsService,
+                public commonService: CommonService) {
     }
-  }
 
-  uploadOfferLetter(event) {
-    this.uploadFile = event.target.files[0];
-  }
+    ngOnInit() {
+        if (!ObjectUtil.isEmpty(this.cadData)) {
+            this.customerLoanList = this.cadData.assignedLoan;
 
-  save(loanHolderId, customerLoanId, documentId, documentName) {
-    const formData: FormData = new FormData();
-    formData.append('file', this.uploadFile);
-    formData.append('customerInfoId', loanHolderId);
-    formData.append('loanID', customerLoanId);
-    formData.append('documentId', documentId);
-    formData.append('customerApprovedDocId', this.cadData.id.toString());
-
-    formData.append('documentName', documentName);
-
-    this.creditAdministrationService.uploadCreditCheckList(formData).subscribe((res: any) => {
-          this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved ' + documentName));
-          this.routerUtilsService.reloadCadProfileRoute(this.cadData.id);
-        }, error => {
-          this.toastService.show(new Alert(AlertType.ERROR, error));
+            this.cadData.cadFileList.forEach(singleCadFile => {
+                this.customerLoanList.forEach(loanList => {
+                    if (singleCadFile.customerLoanId === loanList.id) {
+                        loanList.loan.approvedDocument.forEach(doc => {
+                            if (doc.id === singleCadFile.cadDocument.id) {
+                                doc.checked = true;
+                                doc.url = singleCadFile.path;
+                            }
+                        });
+                    }
+                });
+            });
         }
+    }
 
-    );
+    uploadOfferLetter(event) {
+        this.uploadFile = event.target.files[0];
+    }
 
-  }
+    save(loanHolderId, customerLoanId, documentId, documentName) {
+        const formData: FormData = new FormData();
+        formData.append('file', this.uploadFile);
+        formData.append('customerInfoId', loanHolderId);
+        formData.append('loanID', customerLoanId);
+        formData.append('documentId', documentId);
+        formData.append('customerApprovedDocId', this.cadData.id.toString());
 
-  previewDoc(url: string, name: string) {
-    const link = document.createElement('a');
-    link.target = '_blank';
-    link.href = `${ApiConfig.URL}/${url}`;
-    link.setAttribute('visibility', 'hidden');
-    link.click();
-  }
+        formData.append('documentName', documentName);
 
-  populateCadTemplate(documentId, loanId) {
-    this.nbDialogService.open(CadChecklistDocTemplateModalComponent, { context: {
-        documentId: documentId,
-        cadData: this.cadData,
-        customerLoanId: loanId
-      }
-    });
-  }
+        this.creditAdministrationService.uploadCreditCheckList(formData).subscribe((res: any) => {
+                this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved ' + documentName));
+                this.routerUtilsService.reloadCadProfileRoute(this.cadData.id);
+            }, error => {
+                this.toastService.show(new Alert(AlertType.ERROR, error));
+            }
+        );
+
+    }
+
+
+    populateCadTemplate(documentId, loanId) {
+        this.nbDialogService.open(CadChecklistDocTemplateModalComponent, {
+            context: {
+                documentId: documentId,
+                cadData: this.cadData,
+                customerLoanId: loanId
+            }
+        });
+    }
 }
