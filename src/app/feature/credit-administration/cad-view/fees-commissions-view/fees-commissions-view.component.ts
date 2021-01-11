ec19125b4073f83fa59e99fe24f7fe97762ca547
rujnmahrzn <rujnmahrzn@gmail.com>
2021-01-11 17:05:17 +0545

    view financial

--- src/app/feature/credit-administration/cad-view/fees-commissions-view/fees-commissions-view.component.ts
+++ src/app/feature/credit-administration/cad-view/fees-commissions-view/fees-commissions-view.component.ts
@@ -1,32 +1,37 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CustomerApprovedLoanCadDocumentation} from '../../model/customerApprovedLoanCadDocumentation';
-import {FormArray} from '@angular/forms';
+import {ObjectUtil} from '../../../../@core/utils/ObjectUtil';
 
 @Component({
-  selector: 'app-fees-commissions-view',
-  templateUrl: './fees-commissions-view.component.html',
-  styleUrls: ['./fees-commissions-view.component.scss']
+    selector: 'app-fees-commissions-view',
+    templateUrl: './fees-commissions-view.component.html',
+    styleUrls: ['./fees-commissions-view.component.scss']
 })
 export class FeesCommissionsViewComponent implements OnInit {
-  @Input()
-  cadOfferLetterApprovedDoc: CustomerApprovedLoanCadDocumentation;
+    @Input()
+    cadOfferLetterApprovedDoc: CustomerApprovedLoanCadDocumentation;
 
-  feeCommission;
+    feeCommission;
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  ngOnInit() {
-    console.log(this.cadOfferLetterApprovedDoc.feesAndCommission);
-    this.feeCommission = JSON.parse(this.cadOfferLetterApprovedDoc.feesAndCommission);
-    console.log(this.feeCommission);
-  }
+    ngOnInit() {
+        if (!ObjectUtil.isEmpty(this.feeCommission)) {
+            console.log(this.cadOfferLetterApprovedDoc.feesAndCommission);
+            this.feeCommission = JSON.parse(this.cadOfferLetterApprovedDoc.feesAndCommission);
+            console.log(this.feeCommission);
+        }
+    }
 
-  get totalFeeAmount() {
-    let t = 0;
-    this.feeCommission.feeAmountDetails.forEach(f => {
-      (f.loanFeeDetails.forEach(l =>  t += Number(l.feeAmount))); });
-    return t;
-  }
+    get totalFeeAmount() {
+        let t = 0;
+        if (!ObjectUtil.isEmpty(this.feeCommission)) {
+            this.feeCommission.feeAmountDetails.forEach(f => {
+                (f.loanFeeDetails.forEach(l => t += Number(l.feeAmount)));
+            });
+        }
+        return t;
+    }
 
 }
