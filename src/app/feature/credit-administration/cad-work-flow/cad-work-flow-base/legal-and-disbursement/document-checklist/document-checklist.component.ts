0f590c79df7322902411a24aa99da31892f0a992
buddha.stha04 <buddha.stha04@gmail.com>
2021-01-11 16:24:50 +0545

    added amount remarks and date with  popup action

--- src/app/feature/credit-administration/cad-work-flow/cad-work-flow-base/legal-and-disbursement/document-checklist/document-checklist.component.ts
+++ src/app/feature/credit-administration/cad-work-flow/cad-work-flow-base/legal-and-disbursement/document-checklist/document-checklist.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {CustomerApprovedLoanCadDocumentation} from '../../../../model/customerApprovedLoanCadDocumentation';
 import {ObjectUtil} from '../../../../../../@core/utils/ObjectUtil';
 import {LoanDataHolder} from '../../../../../loan/model/loanData';
@@ -9,6 +9,10 @@ import {Alert, AlertType} from '../../../../../../@theme/model/Alert';
 import {RouterUtilsService} from '../../../../utils/router-utils.service';
 import {NbDialogService} from '@nebular/theme';
 import {CadChecklistDocTemplateModalComponent} from '../../../../cad-offerletter-profile/cad-checklist-doc-template-modal/cad-checklist-doc-template-modal.component';
+import {CadFile} from '../../../../model/CadFile';
+import {Document} from '../../../../../admin/modal/document';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {RemarksEnum} from '../../../../../admin/modal/remarksEnum';
 
 @Component({
     selector: 'app-document-checklist',
@@ -19,13 +23,19 @@ export class DocumentChecklistComponent implements OnInit {
     @Input() cadData: CustomerApprovedLoanCadDocumentation;
     customerLoanList: Array<LoanDataHolder>;
     customerCadFile = [];
-
     uploadFile;
+    saveEditParam = {
+        loanId: null,
+        documentId: null,
+        amount: null,
+        remarks: null};
+    remarkOption = RemarksEnum.enumObject();
 
     constructor(private creditAdministrationService: CreditAdministrationService,
                 private toastService: ToastService,
                 private nbDialogService: NbDialogService ,
-                private routerUtilsService: RouterUtilsService) {
+                private routerUtilsService: RouterUtilsService,
+                private modelService: NgbModal) {
     }
 
     ngOnInit() {
@@ -39,6 +49,9 @@ export class DocumentChecklistComponent implements OnInit {
                             if (doc.id === singleCadFile.cadDocument.id) {
                                 doc.checked = true;
                                 doc.url = singleCadFile.path;
+                                doc.amount = singleCadFile.amount;
+                                doc.remarks = singleCadFile.remarks;
+                                doc.uploadedDate =  singleCadFile.uploadedDate;
                             }
                         });
                     }
@@ -87,5 +100,59 @@ export class DocumentChecklistComponent implements OnInit {
                 customerLoanId: loanId
             }
         });
+    }
+
+    saveText(customerLoanId, documentId, amount, remarks) {
+        console.log(amount);
+        let flag = true;
+        if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+            this.cadData.cadFileList.forEach(singleCadFile => {
+                if (singleCadFile.customerLoanId === customerLoanId && singleCadFile.cadDocument.id === documentId) {
+                    flag = false;
+                    singleCadFile.amount = amount;
+                    singleCadFile.remarks = remarks;
+                }
+            });
+            if (flag) {
+                const cadFile = new CadFile();
+                const document = new Document();
+                cadFile.amount = amount;
+                cadFile.remarks = remarks;
+                document.id = documentId;
+                cadFile.cadDocument = document;
+                cadFile.customerLoanId = customerLoanId;
+                this.cadData.cadFileList.push(cadFile);
+            }
+        } else {
+            const cadFile = new CadFile();
+            const document = new Document();
+            cadFile.amount = amount;
+            cadFile.remarks = remarks;
+            document.id = documentId;
+            cadFile.cadDocument = document;
+            cadFile.customerLoanId = customerLoanId;
+            this.cadData.cadFileList.push(cadFile);
+        }
+
+        this.creditAdministrationService.saveCadDocumentBulk(this.cadData).subscribe(() => {
+            this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved Data'));
+            this.close();
+            this.routerUtilsService.reloadCadProfileRoute(this.cadData.id);
+        }, error => {
+            console.error(error);
+            this.toastService.show(new Alert(AlertType.ERROR, 'Failed to save Data'));
+        });
+    }
+
+    openSaveEditForm(template, loanId, docId, amount, remarks) {
+        this.saveEditParam.loanId = loanId;
+        this.saveEditParam.documentId = docId;
+        this.saveEditParam.amount = amount;
+        this.saveEditParam.remarks = remarks;
+
+        this.modelService.open(template);
+    }
+    close() {
+        this.modelService.dismissAll();
     }
 }
