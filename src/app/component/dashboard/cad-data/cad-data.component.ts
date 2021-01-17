1cfd045a27aaa5f57c7d411b161712921f2c3764
Bibash7 <biswash166@gmail.com>
2021-01-17 09:52:29 +0545

    adding pending count in cad document

--- src/app/component/dashboard/cad-data/cad-data.component.ts
+++ src/app/component/dashboard/cad-data/cad-data.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {RouteConst} from '../../../feature/credit-administration/model/RouteConst';
+import {LocalStorageUtil} from '../../../@core/utils/local-storage-util';
+import {RoleType} from '../../../feature/admin/modal/roleType';
 
 @Component({
   selector: 'app-cad-data',
@@ -10,10 +12,18 @@ export class CadDataComponent implements OnInit {
   @Input() customerApproveCountDto;
 
   offerRouteConst = RouteConst;
+  legal = true;
+  disbursement = true;
+  offer = true;
 
   constructor() { }
 
   ngOnInit() {
+    const local = LocalStorageUtil.getStorage();
+    if (local.roleType === RoleType.CAD_LEGAL) {
+      this.disbursement = false;
+      this.offer = false;
+    }
   }
 
 }
