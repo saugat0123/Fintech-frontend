4442aebc5a04809794a3c8535b603608e0f98e6d
buddha.stha04 <buddha.stha04@gmail.com>
2021-01-24 19:35:33 +0545

    housing offer letter edit

--- src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/retail-housing-loan/retail-housing-loan.component.ts
+++ src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/retail-housing-loan/retail-housing-loan.component.ts
@@ -12,6 +12,7 @@ import {NbDialogRef} from '@nebular/theme';
 import {CadOfferLetterModalComponent} from '../../../cad-offerletter-profile/cad-offer-letter-modal/cad-offer-letter-modal.component';
 import {RouterUtilsService} from '../../../utils/router-utils.service';
 import {Editor} from '../../../../../@core/utils/constants/editor';
+import {NepaliEditor} from '../../../../../@core/utils/constants/nepaliEditor';
 
 @Component({
     selector: 'app-retail-housing-loan',
@@ -31,7 +32,7 @@ export class RetailHousingLoanComponent implements OnInit {
     housingFinanceSelected: boolean;
     mortgageFinanceSelected: boolean;
     mortgageOverdraftSelected: boolean;
-    ckeConfig = Editor.CK_CONFIG;
+    ckeConfig = NepaliEditor.CK_CONFIG;
     note = '<ul><li><span style="font-family:Preeti">C0fL tyf JolQmutsf] ;DklQ v\'nfpg] lnvt -</span><span>Net Worth Statement<span style="font-family:Preeti">_ kmf]6f] tyf ;Dks{ 7]ufgf ;lxt k]z ug\'kg]{5 .</span></li>' +
         '<li><span style="font-family:Preeti">tcGo a}+sx?;+u u/]sf] sf/f]jf/ af/] lnlvt ?kdf v\'nfpg\'kg]{ -</span><span>Multiple Banking Declaration<span style="font-family:Preeti">_ k]z ug\'{kg]{5 .</span></li> ' +
         '<li><span style="font-family:Preeti">tpNn]lvt k|:tfljt crn ;DklQsf] k"0f{ d\'Nofªsg k|ltj]bg -</span><span>Complete Valuation Report<span style="font-family:Preeti">_ k]z ePkZrft dfq shf{ e\'Qmfg ul/g]5 .</span></li> </ul>';
@@ -84,7 +85,7 @@ export class RetailHousingLoanComponent implements OnInit {
             previousOwner: undefined,
             landNumberPreviousOwnerShow: true,
             tableShow: true,
-
+            address: undefined,
         });
     }
 
