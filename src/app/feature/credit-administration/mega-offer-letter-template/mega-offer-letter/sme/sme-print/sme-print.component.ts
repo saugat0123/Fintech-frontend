e6396e47a3c9fb26d217b772afa631dd574aea9a
mohammadhussain257 <mohammadhusen257@gmail.com>
2021-01-28 17:18:55 +0545

    sme template print completed and some content modification

--- src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/sme/sme-print/sme-print.component.ts
+++ src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/sme/sme-print/sme-print.component.ts
@@ -9,9 +9,41 @@ import {MegaOfferLetterConst} from '../../../../mega-offer-letter-const';
 export class SmePrintComponent implements OnInit {
   @Input() letter: any;
   offerLetterConst =  MegaOfferLetterConst;
+  overdraft = false;
+  demandLoan = false;
+  fixedTermLoan = false;
+  hirePurchase = false;
+  letterOfCredit = false;
+  trustReceipt = false;
+  cashCredit = false;
+  shortTermLoan = false;
+  bankGuarantee = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.letter.loanTypeSelectedArray.forEach(loanType => {
+      switch (loanType) {
+        case 'Overdraft': this.overdraft = true;
+        break;
+        case 'DemandLoan': this.demandLoan = true;
+        break;
+        case 'FixedTermLoan': this.fixedTermLoan = true;
+        break;
+        case 'HirePurchase': this.hirePurchase = true;
+          break;
+        case 'LetterOfCredit': this.letterOfCredit = true;
+          break;
+        case 'TrustReceipt': this.trustReceipt = true;
+          break;
+        case 'CashCredit': this.cashCredit = true;
+          break;
+        case 'ShortTermLoan': this.shortTermLoan = true;
+          break;
+        case 'BankGuarantee': this.bankGuarantee = true;
+          break;
+      }
+    });
   }
 
 }
