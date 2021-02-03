67a4bf45a53835566539d1302cb7deedd01fddeb
97798 <saugan@sbsolutionsnepal.com>
2021-02-03 16:16:59 +0545

    Added span after input and print&save and print button in loan deed single and trust receipt nepali

--- src/app/@core/utils/api/ApiConfig.ts
+++ src/app/@core/utils/api/ApiConfig.ts
@@ -1,13 +1,13 @@
 export class ApiConfig {
-  public static TOKEN = 'http://localhost:8086/oauth/token';
-  public static URL = 'http://localhost:8086';
+  /*public static TOKEN = 'http://localhost:8086/oauth/token';
+  public static URL = 'http://localhost:8086';*/
   //
   // public static TOKEN = 'http://3.17.141.198:8080/LAS/oauth/token';
   // public static URL = 'http://3.17.141.198:8080/LAS';
 
   // SBSolutions las v2
- /* public static TOKEN = 'http://150.107.205.62:8080/las-v2/oauth/token';
-  public static URL = 'http://150.107.205.62:8080/las-v2';*/
+  public static TOKEN = 'http://202.166.201.235:8080/cad/oauth/token';
+  public static URL = 'http://202.166.201.235:8080/cad';
 
   // United Finance server info
   // public static TOKEN = 'http://192.168.1.217:8080/oauth/token';
