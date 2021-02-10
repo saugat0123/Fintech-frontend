37efce839b1b2e08617bfa8d1e8d709156fd3f7c
mohammadhussain257 <mohammadhusen257@gmail.com>
2021-02-10 19:03:36 +0545

    add: five pillar component and design

---
+++ src/app/feature/micro-loan/form-component/micro-synopsis/micro-synopsis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-micro-synopsis',
+  templateUrl: './micro-synopsis.component.html',
+  styleUrls: ['./micro-synopsis.component.scss']
+})
+export class MicroSynopsisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/feature/micro-loan/micro-loan.module.ts
+++ src/app/feature/micro-loan/micro-loan.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MicroCompanyFormComponentComponent } from './form-component/micro-company-form-component/micro-company-form-component.component';
 import { MicroIndividualFormComponent } from './form-component/micro-individual-form/micro-individual-form.component';
 import {ThemeModule} from '../../@theme/theme.module';
+import { MicroSynopsisComponent } from './form-component/micro-synopsis/micro-synopsis.component';
 
 
 
 @NgModule({
-  declarations: [MicroCompanyFormComponentComponent, MicroIndividualFormComponent],
+  declarations: [MicroCompanyFormComponentComponent, MicroIndividualFormComponent, MicroSynopsisComponent],
   exports: [
     MicroCompanyFormComponentComponent,
-    MicroIndividualFormComponent
+    MicroIndividualFormComponent,
+    MicroSynopsisComponent,
   ],
   imports: [
     CommonModule,
