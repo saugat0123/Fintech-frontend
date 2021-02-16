4068a201663b2ace47e1111d76edd88f90ed73ff
Karma Lama <karma@sbsolutionsnepal.com>
2021-02-16 20:51:11 +0545

    Added some new components for micro-loan for micro-loan-summary.

---
+++ src/app/feature/loan/component/micro-loan-summary/micro-individual/micro-individual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-micro-individual',
+  templateUrl: './micro-individual.component.html',
+  styleUrls: ['./micro-individual.component.scss']
+})
+export class MicroIndividualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/feature/loan/component/micro-loan-summary/micro-institution/micro-institution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-micro-institution',
+  templateUrl: './micro-institution.component.html',
+  styleUrls: ['./micro-institution.component.scss']
+})
+export class MicroInstitutionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
