cf516a7ef038775b13be5f5394eeee08cbfb901f
Bibash7 <biswash166@gmail.com>
2021-02-22 00:20:59 +0545

    micro detail view base

--- src/app/feature/micro-loan/micro-loan.module.ts
+++ src/app/feature/micro-loan/micro-loan.module.ts
@@ -8,6 +8,16 @@ import { MarketingActivitiesComponent } from './template/marketing-activities/ma
 import { BaselRiskExposureComponent } from './template/basel-risk-exposure/basel-risk-exposure.component';
 import { MicroSynopsisComponent } from './form-component/micro-synopsis/micro-synopsis.component';
 import { MicroSummaryComponent } from './summary/micro-summary/micro-summary.component';
+import {NgxPrintModule} from 'ngx-print';
+import { MicroLoanDetailViewBaseComponent } from './detail-view/micro-loan-detail-view-base/micro-loan-detail-view-base.component';
+import { MicroIndividualViewComponent } from './detail-view/micro-loan-detail-view-base/micro-individual-view/micro-individual-view.component';
+import {CoreModule} from '../../@core/core.module';
+import { MicroCompanyInfoViewComponent } from './detail-view/micro-loan-detail-view-base/micro-company-info-view/micro-company-info-view.component';
+import { MicroProposalViewComponent } from './detail-view/micro-loan-detail-view-base/micro-proposal-view/micro-proposal-view.component';
+import {LoanInformationViewModule} from '../loan-information-view/loan-information-view.module';
+import {LoanSummaryModule} from '../loan/component/loan-summary/loan-summary.module';
+import {ReportingModule} from '../reporting/reporting.module';
+import {LoanInformationTemplateModule} from '../loan-information-template/loan-information-template.module';
 
 
 
@@ -19,7 +29,11 @@ import { MicroSummaryComponent } from './summary/micro-summary/micro-summary.com
       MarketingActivitiesComponent,
       BaselRiskExposureComponent,
       MicroSynopsisComponent,
-      MicroSummaryComponent],
+      MicroSummaryComponent,
+      MicroLoanDetailViewBaseComponent,
+      MicroIndividualViewComponent,
+      MicroCompanyInfoViewComponent,
+      MicroProposalViewComponent],
     exports: [
         MicroCompanyFormComponentComponent,
         MicroIndividualFormComponent,
@@ -27,10 +41,17 @@ import { MicroSummaryComponent } from './summary/micro-summary/micro-summary.com
         MarketingActivitiesComponent,
         BorrowerLoanPortfolioComponent,
         MicroSynopsisComponent,
+        MicroLoanDetailViewBaseComponent,
     ],
-  imports: [
-    CommonModule,
-    ThemeModule
-  ]
+    imports: [
+        CommonModule,
+        ThemeModule,
+        NgxPrintModule,
+        CoreModule,
+        LoanInformationViewModule,
+        LoanSummaryModule,
+        ReportingModule,
+        LoanInformationTemplateModule
+    ]
 })
 export class MicroLoanModule { }
--- src/app/feature/micro-loan/template/marketing-activities/marketing-activities.component.ts
+++ src/app/feature/micro-loan/template/marketing-activities/marketing-activities.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {FiscalYear} from '../../../admin/modal/FiscalYear';
+import {Alert, AlertType} from '../../../../@theme/model/Alert';
+import {FiscalYearService} from '../../../admin/service/fiscal-year.service';
+import {ToastService} from '../../../../@core/utils';
 
 @Component({
   selector: 'app-marketing-activities',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketingActivitiesComponent implements OnInit {
 
-  constructor() { }
+  marketingActivityForm: FormGroup;
+  spinner = false;
+  fiscalYearArray = new Array<FiscalYear>();
+
+  constructor(private formBuilder: FormBuilder,
+              protected fiscalYearService: FiscalYearService,
+              private toastService: ToastService) {
+  }
 
   ngOnInit() {
+    this.getFiscalYears();
+  }
+
+  buildForm() {
+    this.marketingActivityForm = this.formBuilder.group({});
+  }
+
+  getFiscalYears() {
+    this.spinner = true;
+    this.fiscalYearService.getAll().subscribe(response => {
+      this.fiscalYearArray = response.detail;
+      /*this.verifyDataWithFiscalYear(this.fiscalYearArray);*/
+      this.spinner = false;
+    }, error => {
+      console.log(error);
+      this.spinner = false;
+      this.toastService.show(new Alert(AlertType.ERROR, 'Unable to load Fiscal Year!'));
+    });
+  }
+
+  onChangeFiscalYear(selectedFiscalYearObj) {
+    this.spinner = true;
+   /* this.netTradingAssetsFormArray.controls.forEach(singleControl => {
+      if (Number(selectedFiscalYearObj.id) === Number(singleControl.value.id)) {
+        this.selectedIndex = this.netTradingAssetsFormArray.controls.indexOf(singleControl);
+      }
+    });*/
+    this.spinner = false;
   }
 
 }
