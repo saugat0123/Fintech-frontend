bc422f6f19a9acc94ae9c84f507af27472e17ee1
mohammadhussain257 <mohammadhusen257@gmail.com>
2021-02-22 20:51:03 +0545

    code refactor

--- src/app/feature/loan/component/micro-loan-summary/micro-institution/micro-institution.component.ts
+++ src/app/feature/loan/component/micro-loan-summary/micro-institution/micro-institution.component.ts
@@ -4,31 +4,31 @@ import {CompanyInfo} from '../../../../admin/modal/company-info';
 import {BusinessType} from '../../../../admin/modal/businessType';
 import {CompanyJsonData} from '../../../../admin/modal/CompanyJsonData';
 import {ObjectUtil} from '../../../../../@core/utils/ObjectUtil';
-import {Proprietors} from '../../../../admin/modal/proprietors';
 
 @Component({
-  selector: 'app-micro-institution',
-  templateUrl: './micro-institution.component.html',
-  styleUrls: ['./micro-institution.component.scss']
+    selector: 'app-micro-institution',
+    templateUrl: './micro-institution.component.html',
+    styleUrls: ['./micro-institution.component.scss']
 })
 export class MicroInstitutionComponent implements OnInit {
 
-  @Input() companyInfo: CompanyInfo;
-  @Input() loanDataHolder: LoanDataHolder;
-  businessType = BusinessType;
-  companyJsonData: CompanyJsonData = new CompanyJsonData();
-  contact = [];
-  additionalInfoJsonData;
-  companyLocationData;
+    @Input() companyInfo: CompanyInfo;
+    @Input() loanDataHolder: LoanDataHolder;
+    businessType = BusinessType;
+    companyJsonData: CompanyJsonData = new CompanyJsonData();
+    contact = [];
+    additionalInfoJsonData;
+    companyLocationData;
 
-  constructor() { }
-
-  ngOnInit() {
-    if (!ObjectUtil.isEmpty(this.companyJsonData)) {
-      this.companyJsonData = JSON.parse(this.companyInfo.companyJsonData);
-      this.companyLocationData = JSON.parse(this.companyInfo.companyLocations.address);
-      this.contact = JSON.parse(this.companyInfo.contactPersons);
+    constructor() {
     }
-  }
+
+    ngOnInit() {
+        if (!ObjectUtil.isEmpty(this.companyJsonData)) {
+            this.companyJsonData = JSON.parse(this.companyInfo.companyJsonData);
+            this.companyLocationData = JSON.parse(this.companyInfo.companyLocations.address);
+            this.contact = JSON.parse(this.companyInfo.contactPersons);
+        }
+    }
 
 }
