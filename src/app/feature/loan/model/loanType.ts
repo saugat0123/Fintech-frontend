c3df94bfb79666fe9d7b44667d94e3b17433147d
Ritesh_Shrestha <shrestharitesh10024@gmail.com>
2021-02-22 11:09:03 +0545

    SRDB #19 Added Re-initiate option for rejected loans.

--- src/app/feature/loan/model/loanType.ts
+++ src/app/feature/loan/model/loanType.ts
@@ -5,6 +5,7 @@ export enum LoanType {
     ENHANCED_LOAN = 'Enhanced Loan',
     PARTIAL_SETTLEMENT_LOAN = 'Partial Settlement Loan',
     FULL_SETTLEMENT_LOAN = 'Full Settlement Loan',
+    RE_INITIATE_LOAN = 'Reinitiate Loan',
 }
 
 export namespace LoanType {
