79f0c664f6376184d7bd7df4ed56ddef472c078c
sunilbau <whispersunil@gmail.com>
2021-02-23 12:30:13 +0545

    SRDB #21 remove loan flag check for send forward, remove drop down placeholder

--- src/app/feature/loan/loan-action/loan-action.component.ts
+++ src/app/feature/loan/loan-action/loan-action.component.ts
@@ -128,14 +128,14 @@ export class LoanActionComponent implements OnInit, OnChanges {
                 };
                 break;
             case 'approve':
-                if (this.loanFlags && this.loanFlags.length > 0) {
-                    this.loanFlags.sort((a, b) => a.order - b.order);
-                    this.toastService.show(new Alert(AlertType.INFO, this.loanFlags[0].description));
-                    if (!((this.loanFlags[0].flag === LoanFlag[LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY]) ||
-                        (this.loanFlags[0].flag === LoanFlag[LoanFlag.COMPANY_VAT_PAN_EXPIRY]))) {
-                        return;
-                    }
-                }
+                // if (this.loanFlags && this.loanFlags.length > 0) {
+                //     this.loanFlags.sort((a, b) => a.order - b.order);
+                //     this.toastService.show(new Alert(AlertType.INFO, this.loanFlags[0].description));
+                //     if (!((this.loanFlags[0].flag === LoanFlag[LoanFlag.EMPTY_COMPANY_VAT_PAN_EXPIRY]) ||
+                //         (this.loanFlags[0].flag === LoanFlag[LoanFlag.COMPANY_VAT_PAN_EXPIRY]))) {
+                //         return;
+                //     }
+                // }
                 context = {
                     popUpTitle: 'Approve',
                     isForward: false,
