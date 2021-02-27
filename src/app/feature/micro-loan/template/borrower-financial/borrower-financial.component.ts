41deaf31d46fd77e23ba982c4d97e3fc7e630d03
Bibash7 <biswash166@gmail.com>
2021-02-27 22:27:42 +0545

    #430 micro borrower financial

---
+++ src/app/feature/micro-loan/template/borrower-financial/borrower-financial.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {MicroBorrowerFinancial} from '../../../loan/model/micro-borrower-financial';
+
+@Component({
+  selector: 'app-borrower-financial',
+  templateUrl: './borrower-financial.component.html',
+  styleUrls: ['./borrower-financial.component.scss']
+})
+export class BorrowerFinancialComponent implements OnInit {
+  @Input() fromProfile;
+  @Input() microBorrowerFinancial: MicroBorrowerFinancial;
+  @Output() dataEmitter = new EventEmitter();
+  dataForEdit;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    this.dataEmitter.emit(new MicroBorrowerFinancial());
+  }
+
+}
