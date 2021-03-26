ca934991907705b3dab5388bae46ca0153b77397
sudil <sudilshrestha123>
2021-03-26 12:46:58 +0545

    custom-input used

--- src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/custom-input/custom-input.component.ts
+++ src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/custom-input/custom-input.component.ts
@@ -1,79 +1,71 @@
-import {Component, EventEmitter, forwardRef, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {ControlValueAccessor, FormBuilder, FormControl, FormControlDirective, FormGroup, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {Component, EventEmitter, forwardRef, Input, OnInit, Output} from '@angular/core';
+import {ControlValueAccessor, FormGroup, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {EngToNepaliNumberPipe} from '../../../../../@core/pipe/eng-to-nepali-number.pipe';
 import {NepaliCurrencyWordPipe} from '../../../../../@core/pipe/nepali-currency-word.pipe';
-import {CurrencyFormatterPipe} from '../../../../../@core/pipe/currency-formatter.pipe';
+import {NepaliToEngNumberPipe} from '../../../../../@core/pipe/nepali-to-eng-number.pipe';
 
 @Component({
-  selector: 'app-custom-input',
-  styleUrls: ['./custom-input.component.scss'],
-  templateUrl: './custom-input.component.html',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => CustomInputComponent),
-      multi: true
-    }
-  ]
+    selector: 'app-custom-input',
+    styleUrls: ['./custom-input.component.scss'],
+    templateUrl: './custom-input.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => CustomInputComponent),
+            multi: true
+        }
+    ]
 })
 export class CustomInputComponent implements ControlValueAccessor, OnInit {
-  @ViewChild(FormControlDirective, {static: true})
-  formControlDirective: FormControlDirective;
-  @Input()
-  formControl: FormControl;
+    @Input() name: string;
+    @Output() eventEmit = new EventEmitter();
 
-  @Input()
-  formControlName: string;
-  @Input() name: string;
-  @Output() Amount = new EventEmitter();
+    form: FormGroup;
 
-  form: FormGroup;
+    constructor(
+        private engToNepNumberPipe: EngToNepaliNumberPipe,
+        private nepaliCurrencyWordPipe: NepaliCurrencyWordPipe,
+        private nepToEngNumberPipe: NepaliToEngNumberPipe
+    ) {
+    }
 
-  constructor(
-      private engToNepNumberPipe: EngToNepaliNumberPipe,
-      private nepaliCurrencyWordPipe: NepaliCurrencyWordPipe,
-      private currencyFormatPipe: CurrencyFormatterPipe,
-      private formBuilder: FormBuilder
-  ) {
-  }
+    set value(val) {
+        this.val = val;
+        this.nepVal = this.nepaliCurrencyWordPipe.transform(this.val);
+        this.onChange(val);
+        this.onTouch(val);
+    }
 
-  set value(val) {
-    this.val = val;
-    this.nepVal = this.nepaliCurrencyWordPipe.transform(this.val);
-    this.onChange(val);
-    this.onTouch(val);
-  }
-  val: '';
-  engVal = this.val;
-  nepVal = '';
+    val: '';
+    engVal = this.val;
+    nepVal;
 
-  onChange: any = () => {};
-  onTouch: any = () => {};
+    onChange: any = () => {};
+    onTouch: any = () => {};
 
-  writeValue(value: any) {
-    this.value = value;
-    console.log('this is writevalue');
-  }
+    writeValue(value: any) {
+        this.value = value;
+    }
 
-  registerOnChange(fn: any) {
-    this.onChange = fn;
-    console.log('this is register on change');
-  }
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
 
-  registerOnTouched(fn: any) {
-    this.onTouch = fn;
-    console.log('this is touched');
-  }
+    registerOnTouched(fn: any) {
+        this.onTouch = fn;
+    }
 
-  valueChange(value: any) {
-    // this.form.get(name).patchValue(this.external);
-  }
-  changeAmount(val) {
-    this.nepVal = this.nepaliCurrencyWordPipe.transform(this.val);
-    this.Amount.emit(this.nepVal);
-  }
+    changeAmount(val) {
+        const val1 = this.nepToEngNumberPipe.transform(val);
+        const nepVal = this.nepaliCurrencyWordPipe.transform(val1);
+        const op = {
+            val: val,
+            nepVal: nepVal
+        };
+        this.eventEmit.emit(op);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 }
 
