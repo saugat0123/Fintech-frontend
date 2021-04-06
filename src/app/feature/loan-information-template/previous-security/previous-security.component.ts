9c166f89cbacf6edf006ea033f2e832e36883717
moktana68 <moktana68@gmail.com>
2021-04-06 10:23:09 +0545

    Added previous security and comments-summary components at git

---
+++ src/app/feature/loan-information-template/previous-security/previous-security.component.ts
@@ -0,0 +1,60 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {PreviousSecurity} from '../../admin/modal/previousSecurity';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ObjectUtil} from '../../../@core/utils/ObjectUtil';
+import {Comments} from '../../admin/modal/comments';
+
+@Component({
+  selector: 'app-previous-security',
+  templateUrl: './previous-security.component.html',
+  styleUrls: ['./previous-security.component.scss']
+})
+export class PreviousSecurityComponent implements OnInit {
+  @Input() securityDataResponse: PreviousSecurity;
+  @Input() securityData: any;
+  @Input() fromProfile;
+  @Output() securityDataEmitter = new EventEmitter();
+  @Input() commentData;
+  ckeConfig;
+  submitted = false;
+  previousSecurityFormGroup: FormGroup;
+  securityDataObject = new Comments();
+
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.buildForm();
+    const commentsForEdit = JSON.parse(this.commentData);
+    const comment = JSON.parse(commentsForEdit.data);
+    if ( !ObjectUtil.isEmpty(this.securityData)) {
+        const securityForEdit = JSON.parse(this.securityData);
+        this.setFormData(securityForEdit.data);
+    }
+    console.log(this.commentData);
+  }
+  buildForm() {
+    this.previousSecurityFormGroup = this.formBuilder.group({
+      securityDetails: [undefined],
+      previousComments: [undefined],
+      auditorComments: [undefined],
+    });
+
+  }
+
+  public setFormData(formData): void {
+    if (!ObjectUtil.isEmpty(formData)) {
+      const parseData = JSON.parse(formData);
+      this.previousSecurityFormGroup.patchValue(parseData);
+    }
+  }
+
+  submitForm() {
+    this.submitted = true;
+    if (!ObjectUtil.isEmpty(this.securityDataResponse)) {
+      this.securityDataObject = this.securityDataResponse;
+    }
+    this.securityDataObject.data = JSON.stringify(this.previousSecurityFormGroup.value);
+    this.securityDataEmitter.emit(this.securityDataObject);
+  }
+}
