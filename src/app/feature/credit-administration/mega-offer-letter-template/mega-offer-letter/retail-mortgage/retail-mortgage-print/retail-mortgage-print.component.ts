2b6dd334eaac5e4de5f5c0f3fc241ff4580b92b9
Sulav Shrestha <sulav@sbsolutionsnepal.com>
2021-04-07 14:15:47 +0545

    Convert neplai number to it's corresponding word and get the data from config model.

--- src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/retail-mortgage/retail-mortgage-print/retail-mortgage-print.component.ts
+++ src/app/feature/credit-administration/mega-offer-letter-template/mega-offer-letter/retail-mortgage/retail-mortgage-print/retail-mortgage-print.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {MegaOfferLetterConst} from '../../../../mega-offer-letter-const';
+import {ObjectUtil} from "../../../../../../@core/utils/ObjectUtil";
+import {CustomerApprovedLoanCadDocumentation} from '../../../../model/customerApprovedLoanCadDocumentation';
 
 @Component({
     selector: 'app-retail-mortgage-print',
@@ -7,13 +9,18 @@ import {MegaOfferLetterConst} from '../../../../mega-offer-letter-const';
     styleUrls: ['./retail-mortgage-print.component.scss']
 })
 export class RetailMortgagePrintComponent implements OnInit {
+    @Input() cadOfferLetterApprovedDoc: CustomerApprovedLoanCadDocumentation;
     @Input() form: any;
     offerLetterConst = MegaOfferLetterConst;
+    loanHolderInfo;
 
     constructor() {
     }
 
     ngOnInit() {
+        if (!ObjectUtil.isEmpty(this.cadOfferLetterApprovedDoc.loanHolder)) {
+            this.loanHolderInfo = JSON.parse(this.cadOfferLetterApprovedDoc.loanHolder.nepData);
+        }
     }
 
 }
