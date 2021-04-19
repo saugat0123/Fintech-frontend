b15200f3596a7e9395a4a4ab88271a1a7516dd85
Sulav Shrestha <connect@sulav.name.np>
2021-04-19 12:40:26 +0545

    added total land security values in summary view

--- src/app/feature/loan/component/loan-summary/security-summary/security-summary.component.ts
+++ src/app/feature/loan/component/loan-summary/security-summary/security-summary.component.ts
@@ -7,130 +7,153 @@ import {OwnershipTransfer} from '../../../model/ownershipTransfer';
 
 
 @Component({
-    selector: 'app-security-summary',
-    templateUrl: './security-summary.component.html',
-    styleUrls: ['./security-summary.component.scss']
+  selector: 'app-security-summary',
+  templateUrl: './security-summary.component.html',
+  styleUrls: ['./security-summary.component.scss']
 })
 export class SecuritySummaryComponent implements OnInit {
-    @Input() formData: Object;
-    @Input() shareSecurity;
-    landSelected = false;
-    apartmentSelected = false;
-    plantSelected = false;
-    vehicleSelected = false;
-    depositSelected = false;
-    shareSelected = false;
-    isPresentGuarantor = false;
-    totalAmount = 0;
-    shareTotalValue = 0;
-    totalConsideredValue = 0;
-    buildingSelected = false;
-    hypothecation = false;
-    securityOther = false;
-    corporate = false;
-    personal = false;
-    loanSharePercent: NepseMaster = new NepseMaster();
-    landBuilding = false;
-    showTitle = false;
-    insurancePolicySelected = false;
-    client = environment.client;
-    clientName = Clients;
-    ownerShipTransfer = OwnershipTransfer;
-    assignment = false;
-    otherDetail: any;
-    assignments = false;
-    leaseAssignment: any;
+  @Input() formData: Object;
+  @Input() shareSecurity;
+  landSelected = false;
+  apartmentSelected = false;
+  plantSelected = false;
+  vehicleSelected = false;
+  depositSelected = false;
+  shareSelected = false;
+  isPresentGuarantor = false;
+  totalAmount = 0;
+  shareTotalValue = 0;
+  totalConsideredValue = 0;
+  buildingSelected = false;
+  hypothecation = false;
+  securityOther = false;
+  corporate = false;
+  personal = false;
+  loanSharePercent: NepseMaster = new NepseMaster();
+  landBuilding = false;
+  showTitle = false;
+  insurancePolicySelected = false;
+  client = environment.client;
+  clientName = Clients;
+  ownerShipTransfer = OwnershipTransfer;
+  assignment = false;
+  otherDetail: any;
+  assignments = false;
+  leaseAssignment: any;
+  totalLandSecurityDistressValue = 0;
+  totalLandSecurityMarketValue = 0;
+  totalLandSecurityConsideredValue = 0;
 
-    constructor() {
+  constructor() {
+  }
+
+  ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.formData)) {
+      (this.formData['selectedArray'] as Array<any>).forEach(selectedValue => {
+        switch (selectedValue) {
+          case 'LandSecurity' :
+            this.showTitle = true;
+            this.landSelected = true;
+            break;
+          case 'VehicleSecurity' :
+            this.showTitle = true;
+            this.vehicleSelected = true;
+            break;
+          case 'ApartmentSecurity' :
+            this.showTitle = true;
+            this.apartmentSelected = true;
+            break;
+          case 'Land and Building Security' :
+            this.showTitle = true;
+            this.landBuilding = true;
+            break;
+          case 'PlantSecurity' :
+            this.showTitle = true;
+            this.plantSelected = true;
+            break;
+          case 'FixedDeposit':
+            this.showTitle = true;
+            this.depositSelected = true;
+            break;
+          case 'ShareSecurity':
+            this.showTitle = true;
+            this.shareSelected = true;
+            break;
+          case 'HypothecationOfStock':
+            this.showTitle = true;
+            this.hypothecation = true;
+            break;
+          case 'OtherSecurity':
+            this.showTitle = true;
+            this.securityOther = true;
+            break;
+          case 'LeaseAssignment':
+            this.showTitle = true;
+            this.assignments = true;
+            break;
+          case 'CorporateGuarantee':
+            this.showTitle = true;
+            this.corporate = true;
+            break;
+          case 'PersonalGuarantee':
+            this.showTitle = true;
+            this.personal = true;
+            break;
+          case 'InsurancePolicySecurity':
+            this.showTitle = true;
+            this.insurancePolicySelected = true;
+            break;
+          case 'AssignmentOfReceivables':
+            this.showTitle = true;
+            this.assignment = true;
+        }
+      });
     }
 
-    ngOnInit() {
-        if (!ObjectUtil.isEmpty(this.formData)) {
-            (this.formData['selectedArray'] as Array<any>).forEach(selectedValue => {
-                switch (selectedValue) {
-                    case 'LandSecurity' :
-                        this.showTitle = true;
-                        this.landSelected = true;
-                        break;
-                    case 'VehicleSecurity' :
-                        this.showTitle = true;
-                        this.vehicleSelected = true;
-                        break;
-                    case 'ApartmentSecurity' :
-                        this.showTitle = true;
-                        this.apartmentSelected = true;
-                        break;
-                    case 'Land and Building Security' :
-                        this.showTitle = true;
-                        this.landBuilding = true;
-                        break;
-                    case 'PlantSecurity' :
-                        this.showTitle = true;
-                        this.plantSelected = true;
-                        break;
-                    case 'FixedDeposit':
-                        this.showTitle = true;
-                        this.depositSelected = true;
-                        break;
-                    case 'ShareSecurity':
-                        this.showTitle = true;
-                        this.shareSelected = true;
-                        break;
-                    case 'HypothecationOfStock':
-                        this.showTitle = true;
-                        this.hypothecation = true;
-                        break;
-                    case 'OtherSecurity':
-                        this.showTitle = true;
-                        this.securityOther = true;
-                        break;
-                    case 'LeaseAssignment':
-                        this.showTitle = true;
-                        this.assignments = true;
-                        break;
-                    case 'CorporateGuarantee':
-                        this.showTitle = true;
-                        this.corporate = true;
-                        break;
-                    case 'PersonalGuarantee':
-                        this.showTitle = true;
-                        this.personal = true;
-                        break;
-                    case 'InsurancePolicySecurity':
-                        this.showTitle = true;
-                        this.insurancePolicySelected = true;
-                        break;
-                    case 'AssignmentOfReceivables':
-                        this.showTitle = true;
-                        this.assignment = true;
-                }
-            });
-        }
-
-        if (this.depositSelected) {
-            this.calculateTotal();
-        }
-        if (this.shareSelected) {
-            this.calculateShareTotals();
-            this.loanSharePercent = this.shareSecurity['loanShareRate'];
-        }
-        if (this.formData['guarantorsForm']['guarantorsDetails'].length !== 0) {
-            this.isPresentGuarantor = true;
-        }
+    if (this.depositSelected) {
+      this.calculateTotal();
     }
-
-    calculateTotal() {
-        const depositList = this.formData['initialForm']['fixedDepositDetails'];
-        depositList.forEach(deposit => {
-            this.totalAmount += deposit.amount;
-        });
+    if (this.shareSelected) {
+      this.calculateShareTotals();
+      this.loanSharePercent = this.shareSecurity['loanShareRate'];
     }
-
-    private calculateShareTotals() {
-        const shareList = this.shareSecurity['shareSecurityDetails'];
-        shareList.forEach(share => {
-            this.shareTotalValue += share.total;
-            this.totalConsideredValue += share.consideredValue;
-        });
+    if (this.landSelected) {
+      this.calcLandSecuritiesTotal();
     }
+    if (this.formData['guarantorsForm']['guarantorsDetails'].length !== 0) {
+      this.isPresentGuarantor = true;
+    }
+  }
+
+  calculateTotal() {
+    const depositList = this.formData['initialForm']['fixedDepositDetails'];
+    depositList.forEach(deposit => {
+      this.totalAmount += deposit.amount;
+    });
+  }
+
+  private calculateShareTotals() {
+    const shareList = this.shareSecurity['shareSecurityDetails'];
+    shareList.forEach(share => {
+      this.shareTotalValue += share.total;
+      this.totalConsideredValue += share.consideredValue;
+    });
+  }
+
+  calcLandSecuritiesTotal() {
+    this.totalLandSecurityDistressValue = 0;
+    this.totalLandSecurityMarketValue = 0;
+    this.totalLandSecurityConsideredValue = 0;
+    this.formData['initialForm']['landDetails'].forEach(sec => {
+      if (sec['revaluationData'] !== null && sec['revaluationData']['isReValuated']) {
+        this.totalLandSecurityDistressValue += Number(sec['revaluationData']['reValuatedDv']);
+        this.totalLandSecurityMarketValue += Number(sec['revaluationData']['reValuatedFmv']);
+        this.totalLandSecurityConsideredValue += Number(sec['revaluationData']['reValuatedConsideredValue']);
+      } else {
+        this.totalLandSecurityDistressValue += Number(sec['distressValue']);
+        this.totalLandSecurityMarketValue += Number(sec['marketValue']);
+        this.totalLandSecurityConsideredValue += Number(sec['landConsideredValue']);
+      }
+    });
+  }
 }
