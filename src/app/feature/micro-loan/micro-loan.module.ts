a6ee8ea09bf3056ca4485934ea5e9dea66c16e32
Bibash77 <biswash166@gmail.com>
2021-05-18 14:43:52 +0545

    micro loan list temp fix

--- src/app/feature/micro-loan/micro-loan.module.ts
+++ src/app/feature/micro-loan/micro-loan.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MicroCompanyFormComponentComponent } from './form-component/micro-company-form-component/micro-company-form-component.component';
-import { MicroIndividualFormComponent } from './form-component/micro-individual-form/micro-individual-form.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MicroCompanyFormComponentComponent} from './form-component/micro-company-form-component/micro-company-form-component.component';
+import {MicroIndividualFormComponent} from './form-component/micro-individual-form/micro-individual-form.component';
 import {ThemeModule} from '../../@theme/theme.module';
-import { BorrowerLoanPortfolioComponent } from './template/borrower-loan-portofolio/borrower-loan-portofolio.component';
-import { MarketingActivitiesComponent } from './template/marketing-activities/marketing-activities.component';
-import { BaselRiskExposureComponent } from './template/basel-risk-exposure/basel-risk-exposure.component';
-import { MicroSynopsisComponent } from './form-component/micro-synopsis/micro-synopsis.component';
-import { MicroSummaryComponent } from './summary/micro-summary/micro-summary.component';
+import {BorrowerLoanPortfolioComponent} from './template/borrower-loan-portofolio/borrower-loan-portofolio.component';
+import {MarketingActivitiesComponent} from './template/marketing-activities/marketing-activities.component';
+import {BaselRiskExposureComponent} from './template/basel-risk-exposure/basel-risk-exposure.component';
+import {MicroSynopsisComponent} from './form-component/micro-synopsis/micro-synopsis.component';
+import {MicroSummaryComponent} from './summary/micro-summary/micro-summary.component';
 import {NgxPrintModule} from 'ngx-print';
-import { MicroLoanDetailViewBaseComponent } from './detail-view/micro-loan-detail-view-base/micro-loan-detail-view-base.component';
-import { MicroIndividualViewComponent } from './detail-view/micro-loan-detail-view-base/micro-individual-view/micro-individual-view.component';
+import {MicroLoanDetailViewBaseComponent} from './detail-view/micro-loan-detail-view-base/micro-loan-detail-view-base.component';
+import {MicroIndividualViewComponent} from './detail-view/micro-loan-detail-view-base/micro-individual-view/micro-individual-view.component';
 import {CoreModule} from '../../@core/core.module';
-import { MicroCompanyInfoViewComponent } from './detail-view/micro-loan-detail-view-base/micro-company-info-view/micro-company-info-view.component';
-import { MicroProposalViewComponent } from './detail-view/micro-loan-detail-view-base/micro-proposal-view/micro-proposal-view.component';
+import {MicroCompanyInfoViewComponent} from './detail-view/micro-loan-detail-view-base/micro-company-info-view/micro-company-info-view.component';
+import {MicroProposalViewComponent} from './detail-view/micro-loan-detail-view-base/micro-proposal-view/micro-proposal-view.component';
 import {LoanInformationViewModule} from '../loan-information-view/loan-information-view.module';
 import {LoanSummaryModule} from '../loan/component/loan-summary/loan-summary.module';
 import {ReportingModule} from '../reporting/reporting.module';
@@ -21,9 +21,10 @@ import {LoanInformationTemplateModule} from '../loan-information-template/loan-i
 import {MicroBaselRiskExposureViewComponent} from './detail-view/micro-loan-detail-view-base/micro-basel-risk-exposure-view/micro-basel-risk-exposure-view.component';
 import {MicroSynopsisCreditworthinessViewComponent} from './detail-view/micro-loan-detail-view-base/micro-synopsis-creditworthiness-view/micro-synopsis-creditworthiness.component';
 import {BorrowerPortfolioViewComponent} from './detail-view/micro-loan-detail-view-base/borrower-portfolio-summary-view/borrower-portfolio-view.component';
-import { BorrowerFinancialComponent } from './template/borrower-financial/borrower-financial.component';
-import { MarketingActivitiesViewComponent } from './detail-view/micro-loan-detail-view-base/marketing-activities-view/marketing-activities-view.component';
-
+import {BorrowerFinancialComponent} from './template/borrower-financial/borrower-financial.component';
+import {MarketingActivitiesViewComponent} from './detail-view/micro-loan-detail-view-base/marketing-activities-view/marketing-activities-view.component';
+import {MicroChecklistComponent} from './detail-view/micro-loan-detail-view-base/micro-checklist/micro-checklist.component';
+import {NepaliCalendarModule} from '../nepali-calendar/nepali-calendar.module';
 
 
 @NgModule({
@@ -43,6 +44,7 @@ import { MarketingActivitiesViewComponent } from './detail-view/micro-loan-detai
       BorrowerPortfolioViewComponent,
       MicroProposalViewComponent,
       BorrowerFinancialComponent,
+      MicroChecklistComponent,
       MarketingActivitiesViewComponent],
     exports: [
         MicroCompanyFormComponentComponent,
@@ -63,6 +65,7 @@ import { MarketingActivitiesViewComponent } from './detail-view/micro-loan-detai
         LoanSummaryModule,
         ReportingModule,
         LoanInformationTemplateModule,
+        NepaliCalendarModule,
     ]
 })
 export class MicroLoanModule { }
