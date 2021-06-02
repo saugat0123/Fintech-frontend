7f570962ac49bbfe1a5404e528a358fe429efac8
amarshrestha <amar@sbsolutionsnepal.com>
2021-06-02 12:33:26 +0545

    created templates six templates( cross-guarantee-bond, guarantee-bond-personal,hypothecation-of-goods-and-receivables-b,mortgage-deed, offer-letter-corporate,offer-letter-personal)

---
+++ src/app/feature/credit-administration/cad-document-template/progressive/progressive-offer-letter/guarantee-bond-personal/guarantee-bond-personal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guarantee-bond-personal',
+  templateUrl: './guarantee-bond-personal.component.html',
+  styleUrls: ['./guarantee-bond-personal.component.scss']
+})
+export class GuaranteeBondPersonalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/feature/credit-administration/cad-document-template/progressive/progressive-offer-letter/mortgage-deed/mortgage-deed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hypothecation-of-goods-and-receivables-a',
+  templateUrl: './mortgage-deed.component.html',
+  styleUrls: ['./mortgage-deed.component.scss']
+})
+export class MortgageDeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
