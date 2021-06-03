abce933381382afedea1af66c45b8b41b114d487
amarshrestha <amar@sbsolutionsnepal.com>
2021-06-03 12:31:18 +0545

    created templates( hypothecation-of-goods-and-receivables-a)

---
+++ src/app/feature/credit-administration/cad-document-template/progressive/progressive-offer-letter/hypothecation-of-goods-and-receivables-a/hypothecation-of-goods-and-receivables-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hypothecation-of-goods-and-receivables-a',
+  templateUrl: './hypothecation-of-goods-and-receivables-a.component.html',
+  styleUrls: ['./hypothecation-of-goods-and-receivables-a.component.scss']
+})
+export class HypothecationOfGoodsAndReceivablesAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
