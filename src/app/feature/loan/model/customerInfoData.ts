3cdef109e883644ba5bbafcc7c0858d28421c209
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-06-03 14:29:47 +0545

    additional security functionality

--- src/app/feature/loan/model/customerInfoData.ts
+++ src/app/feature/loan/model/customerInfoData.ts
@@ -24,6 +24,7 @@ import {ReportingInfoLevel} from '../../reporting/model/reporting-info-level';
 import {Comments} from '../../admin/modal/comments';
 import {PreviousSecurity} from '../../admin/modal/previousSecurity';
 import {MicroCrgParams} from './MicroCrgParams';
+import {AdditionalSecurity} from './additional-security';
 
 export class CustomerInfoData {
     id: number;
@@ -75,4 +76,5 @@ export class CustomerInfoData {
     comments: Comments;
     previousSecurity: PreviousSecurity;
     data: string;
+    additionalSecurity: Array<AdditionalSecurity>;
 }
