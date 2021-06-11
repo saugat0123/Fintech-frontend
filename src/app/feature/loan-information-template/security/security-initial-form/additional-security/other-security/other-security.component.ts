2bc781583884a0db84d8d05263a2a609ca840403
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-06-11 17:44:09 +0545

    build other security form and component

--- src/app/feature/loan-information-template/security/security-initial-form/additional-security/other-security/other-security.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/additional-security/other-security/other-security.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Editor} from '../../../../../../@core/utils/constants/editor';
 
 @Component({
   selector: 'app-other-security',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./other-security.component.scss']
 })
 export class OtherSecurityComponent implements OnInit {
+  otherSecurityForm: FormGroup;
+  ckeConfig = Editor.CK_CONFIG;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  private buildForm(): FormGroup {
+    return this.otherSecurityForm = this.formBuilder.group({
+      otherSecurity: this.formBuilder.array([this.buildOtherSecurityFormGroup()]),
+    });
+  }
+
+  private buildOtherSecurityFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      otherDetail: [undefined],
+    });
+  }
 }
