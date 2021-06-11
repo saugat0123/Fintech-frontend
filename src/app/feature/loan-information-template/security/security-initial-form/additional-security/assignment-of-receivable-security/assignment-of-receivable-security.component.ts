9fffbe7ec5ad9fdce047bf4886170c2686705a2b
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-06-11 17:33:12 +0545

    build assignemnt of receivable security form and component

--- src/app/feature/loan-information-template/security/security-initial-form/additional-security/assignment-of-receivable-security/assignment-of-receivable-security.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/additional-security/assignment-of-receivable-security/assignment-of-receivable-security.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Editor} from '../../../../../../@core/utils/constants/editor';
 
 @Component({
   selector: 'app-assignment-of-receivable-security',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment-of-receivable-security.component.scss']
 })
 export class AssignmentOfReceivableSecurityComponent implements OnInit {
+  assignmentOfReceivableForm: FormGroup;
+  ckeConfig = Editor.CK_CONFIG;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm(): FormGroup {
+    return this.assignmentOfReceivableForm = this.formBuilder.group({
+      assignmentOfReceivables: this.formBuilder.array([this.buildAssignmentOfReceivableForm()])
+    });
+  }
+
+  private buildAssignmentOfReceivableForm(): FormGroup {
+    return this.formBuilder.group({
+      amount: [undefined],
+      otherDetail: [undefined],
+    });
   }
 
 }
