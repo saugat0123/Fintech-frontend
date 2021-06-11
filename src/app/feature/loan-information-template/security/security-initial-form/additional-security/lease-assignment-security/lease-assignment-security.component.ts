e805f2231527cf178f343ffee2dce6b09da334dd
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-06-11 17:39:01 +0545

    build lease assignment security form and component

--- src/app/feature/loan-information-template/security/security-initial-form/additional-security/lease-assignment-security/lease-assignment-security.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/additional-security/lease-assignment-security/lease-assignment-security.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Editor} from '../../../../../../@core/utils/constants/editor';
 
 @Component({
   selector: 'app-lease-assignment-security',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lease-assignment-security.component.scss']
 })
 export class LeaseAssignmentSecurityComponent implements OnInit {
+  leaseAssignmentForm: FormGroup;
+  ckeConfig = Editor.CK_CONFIG;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm(): FormGroup {
+    return this.leaseAssignmentForm = this.formBuilder.group({
+      leaseAssignment: this.formBuilder.array([this.buildLeaseAssignmentFormGroup()]),
+    });
+  }
+
+  private buildLeaseAssignmentFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      otherDetail: [undefined],
+    });
   }
 
 }
