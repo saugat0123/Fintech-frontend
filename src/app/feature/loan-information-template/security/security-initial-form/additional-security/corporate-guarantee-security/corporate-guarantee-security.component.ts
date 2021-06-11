10b59ba79484b9d3638f855bf48e6439712b0a30
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-06-11 16:50:20 +0545

    build corporate guarantee security form and component

--- src/app/feature/loan-information-template/security/security-initial-form/additional-security/corporate-guarantee-security/corporate-guarantee-security.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/additional-security/corporate-guarantee-security/corporate-guarantee-security.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Editor} from '../../../../../../@core/utils/constants/editor';
 
 @Component({
   selector: 'app-corporate-guarantee-security',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./corporate-guarantee-security.component.scss']
 })
 export class CorporateGuaranteeSecurityComponent implements OnInit {
-
-  constructor() { }
+  corporateGuaranteeForm: FormGroup;
+  ckeConfig = Editor.CK_CONFIG;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  private buildForm(): FormGroup {
+    return this.corporateGuaranteeForm = this.formBuilder.group({
+      corporateGuarantee: this.formBuilder.array([this.corporateDetailsFormGroup()])
+    });
+  }
+
+  private corporateDetailsFormGroup(): FormGroup {
+    return this.formBuilder.group({
+          name: [undefined, Validators.required],
+          address: [undefined],
+          keyPerson: [undefined],
+          email: [undefined],
+          phoneNumber: [undefined],
+          otherDetail: [undefined],
+        }
+    );
+  }
+
+  public removeCorporate(index: number): void {
+    (<FormArray>this.corporateGuaranteeForm.get('corporateGuarantee')).removeAt(index);
+
+  }
+
+  public addCorporateGuarantee(): void {
+    (this.corporateGuaranteeForm.get('corporateGuarantee') as FormArray).push(this.corporateDetailsFormGroup());
+  }
 }
