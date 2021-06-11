68b986c955d5cba7ee5d4764158b6c9504238554
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-06-11 17:10:08 +0545

    build personal guarantee security form and component

--- src/app/feature/loan-information-template/security/security-initial-form/additional-security/personal-guarantee-security/personal-guarantee-security.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/additional-security/personal-guarantee-security/personal-guarantee-security.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Editor} from '../../../../../../@core/utils/constants/editor';
 
 @Component({
   selector: 'app-personal-guarantee-security',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-guarantee-security.component.scss']
 })
 export class PersonalGuaranteeSecurityComponent implements OnInit {
-
-  constructor() { }
+  personalGuaranteeForm: FormGroup;
+  ckeConfig = Editor.CK_CONFIG;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  private buildForm(): FormGroup {
+    return this.personalGuaranteeForm = this.formBuilder.group({
+      personalGuarantee: this.formBuilder.array([this.personalDetailsFormGroup()])
+    });
+  }
+  private personalDetailsFormGroup(): FormGroup {
+    return this.formBuilder.group({
+          name: [undefined, Validators.required],
+          address: [undefined],
+          email: [undefined],
+          phoneNumber: [undefined],
+          owner: [undefined],
+          otherDetail: [undefined],
+        }
+    );
+  }
+
+  public removePersonalGuaranteeForm(index: number): void {
+    (<FormArray>this.personalGuaranteeForm.get('personalGuarantee')).removeAt(index);
+  }
+
+  public addPersonalGuaranteeForm(): void {
+    (this.personalGuaranteeForm.get('personalGuarantee') as FormArray).push(this.personalDetailsFormGroup());
+  }
 }
