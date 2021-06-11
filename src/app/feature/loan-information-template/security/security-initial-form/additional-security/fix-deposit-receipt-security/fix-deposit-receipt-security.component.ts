b28a85329aa655e209cdd7e66ed57de5e648fb35
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-06-11 15:33:11 +0545

    build fixed deposit receipt security form and component

--- src/app/feature/loan-information-template/security/security-initial-form/additional-security/fix-deposit-receipt-security/fix-deposit-receipt-security.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/additional-security/fix-deposit-receipt-security/fix-deposit-receipt-security.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CalendarType} from '../../../../../../@core/model/calendar-type';
 
 @Component({
   selector: 'app-fix-deposit-receipt-security',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fix-deposit-receipt-security.component.scss']
 })
 export class FixDepositReceiptSecurityComponent implements OnInit {
+  @Input() calendarType: CalendarType;
+  fixedDepositForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm(): FormGroup {
+    return this.fixedDepositForm = this.formBuilder.group({
+      fixedDepositReceipt: this.formBuilder.array([this.fixedDepositFormGroup()])
+    });
+  }
+
+  private fixedDepositFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      receiptNumber: [''],
+      amount: [''],
+      expiryDate: undefined,
+      couponRate: [''],
+      beneficiary: [''],
+      remarks: [''],
+      accountHolderName: undefined,
+      accountNumber: [undefined, Validators.required],
+      tenureStartDate: undefined
+    });
+  }
+
+  public addFixedDepositForm(): void {
+    (this.fixedDepositForm.get('fixedDepositReceipt') as FormArray).push(this.fixedDepositFormGroup());
+  }
+
+  public removeFixedDepositForm(index: number): void {
+    (this.fixedDepositForm.get('fixedDepositReceipt') as FormArray).removeAt(index);
   }
 
 }
