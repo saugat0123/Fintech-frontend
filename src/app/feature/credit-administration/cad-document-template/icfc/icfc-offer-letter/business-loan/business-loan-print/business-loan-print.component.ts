a8288f594c6f9f62bd12c6fd5d13a6d4e15ed211
aayushlalshrestha <mail2ayus@yahoo.com>
2021-06-25 22:33:58 +0545

    dropdown functionality in business-loan-print component ICFC template and some code changes in other file.

--- src/app/feature/credit-administration/cad-document-template/icfc/icfc-offer-letter/business-loan/business-loan-print/business-loan-print.component.ts
+++ src/app/feature/credit-administration/cad-document-template/icfc/icfc-offer-letter/business-loan/business-loan-print/business-loan-print.component.ts
@@ -10,10 +10,33 @@ export class BusinessLoanPrintComponent implements OnInit {
   @Input() letter;
   @Input() nepaliData;
   offerLetterConst = IcfcOfferLetterConst;
-
+  businessOverdraftLoanQSelected;
+  businessOverdraftLoanMSelected;
+  businessTermLoanSelected;
+  overdraftLoanSelected;
+  termLoanSelected;
   constructor() { }
 
   ngOnInit() {
+    this.letter.interestTypeSelectedArray.forEach(value => {
+      switch (value) {
+        case 'Business Overdraft Loan (Quarterly)': this.businessOverdraftLoanQSelected = true;
+          break;
+        case 'Business Overdraft Loan (Monthly)': this.businessOverdraftLoanMSelected = true;
+          break;
+        case 'Business Term Loan': this.businessTermLoanSelected = true;
+          break;
+      }
+    });
+
+    this.letter.loanTermSelectedArray.forEach(value => {
+      switch (value) {
+        case 'Overdraft Loan': this.overdraftLoanSelected = true;
+          break;
+        case 'Term Loan': this.termLoanSelected = true;
+          break;
+      }
+    });
   }
 
 }
