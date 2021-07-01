d87ac74a7292632e0107692615756e43bc3095c4
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-07-01 12:56:56 +0545

    integrate download all feature on detail view

--- src/app/feature/loan-information-view/all-document-view/all-document-view.component.ts
+++ src/app/feature/loan-information-view/all-document-view/all-document-view.component.ts
@@ -8,6 +8,10 @@ import {ObjectUtil} from '../../../@core/utils/ObjectUtil';
 import {ProductUtils} from '../../admin/service/product-mode.service';
 import {LocalStorageUtil} from '../../../@core/utils/local-storage-util';
 import {AffiliateId} from '../../../@core/utils/constants/affiliateId';
+import * as JSZip from 'jszip';
+import * as JSZipUtils from 'jszip-utils/lib/index.js';
+import {saveAs as importedSaveAs} from 'file-saver';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-all-document-view',
@@ -23,6 +27,7 @@ export class AllDocumentViewComponent implements OnInit {
   showCadDoc;
   productUtils: ProductUtils = LocalStorageUtil.getStorage().productUtil;
   affiliatedId;
+  allDocumentPaths = [];
 
   constructor(private dmsLoanService: DmsLoanService,
               private toastService: ToastService,
@@ -87,6 +92,62 @@ export class AllDocumentViewComponent implements OnInit {
     link.download = name;
     link.setAttribute('visibility', 'hidden');
     link.click();
+  }
+
+  // method to get all the paths which is require to zipping all files
+  public getDocPath(): void {
+    const docPaths = [];
+    const loanDocument = this.loanDataHolder.customerDocument;
+    for (const doc of loanDocument) {
+      docPaths.push(doc.documentPath);
+    }
+    const generalDocument = this.loanDataHolder.loanHolder.customerGeneralDocuments;
+    for (const doc of generalDocument) {
+      docPaths.push(doc.docPath);
+    }
+    const guarantorDocument = this.taggedGuarantorWithDoc;
+    for (const doc of guarantorDocument) {
+      docPaths.push(doc.docPath);
+    }
+    const insuranceDocument = this.insuranceWithDoc;
+    for (const doc of insuranceDocument) {
+      docPaths.push(doc.policyDocumentPath);
+    }
+    this.downloadAll(docPaths);
+  }
+
+  // method to make all files as a .zip file
+  private downloadAll(documentUrls: string[]): void {
+    const zip = new JSZip();
+    let count = 0;
+    const zipFilename = 'AllDocument.zip';
+    const urls = [];
+    if (documentUrls.length > 0) {
+      documentUrls.map(d => {
+        d = ApiConfig.URL + '/' + d;
+        urls.push(d);
+      });
+
+      urls.forEach((url: string) => {
+        const pathToZipFrom = new URL(url).pathname;
+        // loading a file and add it in a zip file
+        JSZipUtils.getBinaryContent(url, (err, data) => {
+          if (err) {
+            throw err; // or handle the error
+          }
+          zip.file(pathToZipFrom, data, {binary: true});
+          count++;
+          if (count === urls.length) {
+            zip.generateAsync({type: 'blob'}).then(content => {
+              importedSaveAs(content, zipFilename);
+            });
+          }
+        });
+      });
+      this.toastService.show(new Alert(AlertType.SUCCESS, 'Files has been downloaded!'));
+    } else {
+      this.toastService.show(new Alert(AlertType.ERROR, 'No file found!!!'));
+    }
   }
 
 }
