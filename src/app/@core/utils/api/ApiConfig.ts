fa32cfc9cd70627175778d895991efe17f3271f6
Ritesh_Shrestha <shrestharitesh10024@gmail.com>
2021-07-15 02:03:24 +0545

    simple changes in api config.

--- src/app/@core/utils/api/ApiConfig.ts
+++ src/app/@core/utils/api/ApiConfig.ts
@@ -1,7 +1,7 @@
 export class ApiConfig {
   public static TOKEN = 'http://localhost:8086/oauth/token';
   public static URL = 'http://localhost:8086';
-
+  //
   // public static TOKEN = 'http://3.17.141.198:8080/LAS/oauth/token';
   // public static URL = 'http://3.17.141.198:8080/LAS';
 
