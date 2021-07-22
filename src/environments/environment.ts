40ac87abc7e7493271954b93ef9a87627c84eb69
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2021-07-22 13:17:04 +0545

    Control flag using environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,6 +13,8 @@ import {Clients} from './Clients';
  command to build client related product:
  ng build --configuration= 'client name'
 
+ // SummaryType = GENERAL except SRDB bank
+
  active client name in current repo: 'srdb' , 'tinau' , 'mega'
  */
 export const environment = {
