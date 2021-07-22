c01e10e572ad238506005615ac38b6f2d9d1dc2a
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2021-07-22 12:49:49 +0545

    Control flag using environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -26,6 +26,8 @@ export const environment = {
   disableCrgAlpha: true,
   disableCrgLambda: true,
   disableApprovalSheet: false,
+  RISK_INITIAL_ROLE_SME: 'RISK OFFICER ( SME )',
+  RISK_INITIAL_ROLE_CORPORATE: 'RISK OFFICER ( CORPORATE )',
   microLoan: false,
   SBS_GROUP: false,
   MEGA_GROUP: false,
