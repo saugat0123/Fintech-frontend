13ae5a6ad93d24c591d5bc939df08f8437f76706
Nikkso <86937561+Nikkso@users.noreply.github.com>
2021-07-23 11:41:19 +0545

    click on checkbox to hide the temporary details (same as permanent address)

--- src/app/feature/customer/component/customer-form/individual-form/customer-form.component.ts
+++ src/app/feature/customer/component/customer-form/individual-form/customer-form.component.ts
@@ -64,6 +64,7 @@ export class CustomerFormComponent implements OnInit, DoCheck {
 
     calendarType = 'AD';
     microCustomer = false;
+    samePerAddress = false;
     microEnabled: boolean = env.microLoan;
 
     basicInfo: FormGroup;
@@ -207,7 +208,7 @@ export class CustomerFormComponent implements OnInit, DoCheck {
         this.commonLocation.getMunicipalityVDCByDistrict(district).subscribe(
             (response: any) => {
                 this.temporaryMunicipalitiesList = response.detail;
-                this.temporaryMunicipalitiesList.sort((a,b) => a.name.localeCompare(b.name));
+                this.temporaryMunicipalitiesList.sort((a, b) => a.name.localeCompare(b.name));
                 this.temporaryMunicipalitiesList.forEach(municipality => {
                     if (!ObjectUtil.isEmpty(this.customer.temporaryMunicipalities) &&
                         municipality.id === this.customer.temporaryMunicipalities.id) {
@@ -624,21 +625,39 @@ export class CustomerFormComponent implements OnInit, DoCheck {
         }
 
     }
-    sameAsPermanent() {
+    sameAsPermanent(value) {
         // if (ObjectUtil.isEmpty(this.basicInfo.get('municipalities').value)) {
         //     this.toastService.show(new Alert(AlertType.WARNING, 'Please fill Permanent Address Completely'));
         //     return true;
         // }
-        this.basicInfo.get('temporaryProvince').patchValue(this.basicInfo.get('province').value);
-        this.customer.temporaryDistrict = this.basicInfo.get('district').value;
-        this.getTemporaryDistricts(this.basicInfo.get('temporaryProvince').value);
-        this.customer.temporaryMunicipalities = this.basicInfo.get('municipalities').value;
-        this.getTemporaryMunicipalities(this.basicInfo.get('temporaryMunicipalities').value);
-        this.basicInfo.controls.temporaryAddressLine1.patchValue(this.basicInfo.get('permanentAddressLine1').value);
-        this.basicInfo.controls.temporaryAddressLine2.patchValue(this.basicInfo.get('permanentAddressLine2').value);
-        this.basicInfo.controls.temporaryWardNumber.setValue(this.basicInfo.get('wardNumber').value);
-
+        if (value) {
+            this.basicInfo.get('temporaryProvince').patchValue(this.basicInfo.get('province').value);
+            this.customer.temporaryDistrict = this.basicInfo.get('district').value;
+            this.getTemporaryDistricts(this.basicInfo.get('temporaryProvince').value);
+            this.customer.temporaryMunicipalities = this.basicInfo.get('municipalities').value;
+            this.getTemporaryMunicipalities(this.basicInfo.get('temporaryMunicipalities').value);
+            this.basicInfo.controls.temporaryAddressLine1.patchValue(this.basicInfo.get('permanentAddressLine1').value);
+            this.basicInfo.controls.temporaryAddressLine2.patchValue(this.basicInfo.get('permanentAddressLine2').value);
+            this.basicInfo.controls.temporaryWardNumber.setValue(this.basicInfo.get('wardNumber').value);
+            this.samePerAddress = value;
+        } else {
+            this.resetValue();
+            this.samePerAddress = value;
+        }
+        console.log('Button event triggered ! ', value);
     }
+
+            resetValue()  {
+                this.basicInfo.get('temporaryAddressLine1').patchValue(null);
+                this.basicInfo.get('temporaryAddressLine2').patchValue(null);
+                this.basicInfo.get('temporaryProvince').patchValue(null);
+                this.basicInfo.get('temporaryDistrict').patchValue(null);
+                this.basicInfo.get('temporaryMunicipalities').patchValue(null);
+                this.basicInfo.get('temporaryWardNumber').patchValue(null);
+    }
+
+
+
 
     /** @Param validate --- true for add validation and false for remove validation
      * @Param controlNames --- list of formControlName**/
