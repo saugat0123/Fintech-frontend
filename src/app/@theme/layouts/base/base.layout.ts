8201e81e189626295d8085c14e7d1ff7b9c2aa80
aayushlalshrestha <mail2ayus@yahoo.com>
2021-08-04 17:41:50 +0545

    some changes in code

--- src/app/@theme/layouts/base/base.layout.ts
+++ src/app/@theme/layouts/base/base.layout.ts
@@ -23,8 +23,17 @@ import {StateService} from '../../../@core/utils';
 
             <nb-layout-column class="main-content">
                 <ng-content select="router-outlet"></ng-content>
+                <div class="chat">
+                    <div class="d-flex flex-row-reverse">
+                        <div class="p-2">
+                            
+                        </div>
+                    </div>
+                </div>
+
             </nb-layout-column>
             <nb-layout-footer fixed>
+                
                 <app-footer></app-footer>
             </nb-layout-footer>
         </nb-layout>
