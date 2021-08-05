c2432a51c391b4e1c8cdc53a84cfc4b00fcf9ed5
Nikkso <86937561+Nikkso@users.noreply.github.com>
2021-08-05 10:20:36 +0545

    rechecked the changes

--- src/app/feature/loan-information-template/guarantor/guarantor.component.ts
+++ src/app/feature/loan-information-template/guarantor/guarantor.component.ts
@@ -100,7 +100,6 @@ export class GuarantorComponent implements OnInit {
                 }
                 formArray.push(this.addGuarantorDetails(v));
                 this.sameAsCurrentChecked = v.checkedSameAsCurrent;
-                // console.log(v.checkedSameAsCurrent, 'Current Value');
             });
         }
     }
@@ -325,7 +324,6 @@ export class GuarantorComponent implements OnInit {
     }
 
     sameAsAbove(i, event) {
-        console.log('Event value: ', event);
         if (event === true) {
             console.log('Value of temporaryAddress', this.form.get('guarantorDetails'));
             this.form.get(['guarantorDetails', i]).patchValue({checkedSameAsCurrent: true});
@@ -352,8 +350,6 @@ export class GuarantorComponent implements OnInit {
             this.toastService.show(new Alert(AlertType.WARNING, 'Please fill Permanent Address Completely'));
             return;
         }
-
-        console.log('Button event triggered ! ', i);
     }
     resetValue(index)  {
         this.form.get(['guarantorDetails', index, 'provinceTemporary']).patchValue(null);
