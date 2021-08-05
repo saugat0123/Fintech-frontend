e7f9a2be1ce99727f66f27862fd562ffde614059
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-08-05 16:42:29 +0545

    remove default committee from signature list

--- src/app/feature/loan/component/gamma-loan-summary/gamma-loan-summary.component.ts
+++ src/app/feature/loan/component/gamma-loan-summary/gamma-loan-summary.component.ts
@@ -641,18 +641,16 @@ export class GammaLoanSummaryComponent implements OnInit {
         }
         const signatureList = new Array<LoanStage>();
         const addedStages = new Map<number, number>(); // KEY = loan stage from user id, value = array index
-        stages.forEach((loanStage, index) => {
-                if (loanStage.docAction.toString() !== DocAction.value(DocAction.TRANSFER)) {
-                    if (!loanStage.fromUser.name.toLowerCase().includes('default') ) {
-                        if (addedStages.has(loanStage.fromUser.id)) {
-                            signatureList[addedStages.get(loanStage.fromUser.id)] = loanStage;
-                        } else {
-                            signatureList.push(loanStage);
-                            addedStages.set(loanStage.fromUser.id, index);
-                        }
-                    }
-                }
-        });
+      stages.forEach((loanStage, index) => {
+        if (loanStage.docAction.toString() !== DocAction.value(DocAction.TRANSFER)) {
+          if (addedStages.has(loanStage.fromUser.id)) {
+            signatureList[addedStages.get(loanStage.fromUser.id)] = loanStage;
+          } else {
+            signatureList.push(loanStage);
+            addedStages.set(loanStage.fromUser.id, index);
+          }
+        }
+      });
 
         return signatureList;
     }
