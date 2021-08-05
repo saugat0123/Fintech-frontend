311ba06566768209da7437bc2cdf66d24bc28944
bibash <biswsh166@gmail.com>
2021-08-05 18:06:43 +0545

    micro customer direct

--- src/app/feature/micro-loan/detail-view/micro-loan-detail-view-base/micro-loan-detail-view-base.component.ts
+++ src/app/feature/micro-loan/detail-view/micro-loan-detail-view-base/micro-loan-detail-view-base.component.ts
@@ -6,43 +6,44 @@ import {Clients} from '../../../../../environments/Clients';
 import {ObjectUtil} from '../../../../@core/utils/ObjectUtil';
 
 @Component({
-  selector: 'app-micro-loan-detail-view-base',
-  templateUrl: './micro-loan-detail-view-base.component.html',
-  styleUrls: ['./micro-loan-detail-view-base.component.scss']
+    selector: 'app-micro-loan-detail-view-base',
+    templateUrl: './micro-loan-detail-view-base.component.html',
+    styleUrls: ['./micro-loan-detail-view-base.component.scss']
 })
 export class MicroLoanDetailViewBaseComponent implements OnInit {
-  @Input() loanDataHolder: LoanDataHolder;
-  @Input() loanHolder: CustomerInfoData;
-  @Input() calendarType;
-  @Input() loanId;
-  @Input() customerAllLoanList;
-  @Input() fiscalYearArray;
-  client = environment.client;
-  clientName = Clients;
-  securityId: number;
-  commentsSummary = false;
-  dataFromComments;
-  previousSecuritySummary = false;
-  dataFromPreviousSecurity;
+    @Input() loanDataHolder: LoanDataHolder;
+    @Input() loanHolder: CustomerInfoData;
+    @Input() calendarType;
+    @Input() loanId;
+    @Input() customerAllLoanList;
+    @Input() fiscalYearArray;
+    client = environment.client;
+    clientName = Clients;
+    securityId: number;
+    commentsSummary = false;
+    dataFromComments;
+    previousSecuritySummary = false;
+    dataFromPreviousSecurity;
 
-  constructor() { }
-
-  ngOnInit() {
-    console.log(this.loanHolder);
-    if (!ObjectUtil.isEmpty(this.loanHolder.security)) {
-      this.securityId = this.loanHolder.security.id;
-    }
-    // Setting Comments data--
-    if (!ObjectUtil.isEmpty(this.loanDataHolder.loanHolder.data)) {
-      this.dataFromComments = JSON.parse(this.loanDataHolder.loanHolder.data);
-      this.commentsSummary = true;
+    constructor() {
     }
 
-    // Setting Previous Security Data
-    if (!ObjectUtil.isEmpty(this.loanDataHolder.loanHolder.data)) {
-      this.dataFromPreviousSecurity = JSON.parse(this.loanDataHolder.loanHolder.data);
-      this.previousSecuritySummary = true;
+    ngOnInit() {
+        console.log(this.loanHolder);
+        if (!ObjectUtil.isEmpty(this.loanHolder.security)) {
+            this.securityId = this.loanHolder.security.id;
+        }
+        // Setting Comments data--
+        if (!ObjectUtil.isEmpty(this.loanDataHolder.loanHolder.data)) {
+            this.dataFromComments = JSON.parse(this.loanDataHolder.loanHolder.data);
+            this.commentsSummary = true;
+        }
+
+        // Setting Previous Security Data
+        if (!ObjectUtil.isEmpty(this.loanDataHolder.loanHolder.data)) {
+            this.dataFromPreviousSecurity = JSON.parse(this.loanDataHolder.loanHolder.data);
+            this.previousSecuritySummary = true;
+        }
     }
-  }
 
 }
