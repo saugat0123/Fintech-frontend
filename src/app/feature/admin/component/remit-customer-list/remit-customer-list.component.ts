7932ff00136a66392fe9172981f6d33998c32e16
Revert <Sujesh@sbsolutionsnepal.com>
2021-08-10 22:35:21 +0545

    Dummy

--- src/app/feature/admin/component/remit-customer-list/remit-customer-list.component.ts
+++ src/app/feature/admin/component/remit-customer-list/remit-customer-list.component.ts
@@ -36,7 +36,27 @@ export class RemitCustomerListComponent implements OnInit {
         {id: 2, customerName: 'Test 2', phoneNo: '12345', email: 'test2@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '12345'},
         {id: 3, customerName: 'Test 3', phoneNo: '12345', email: 'test3@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '12345'},
         {id: 4, customerName: 'Test 4', phoneNo: '12345', email: 'test4@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '12345'},
-        {id: 5, customerName: 'Test 5', phoneNo: '12345', email: 'test5@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '12345'}
+        {id: 5, customerName: 'Test 5', phoneNo: '12345', email: 'test5@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '12345'},
+        {id: 6, customerName: 'Test 6', phoneNo: '12345', email: 'test6@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '2222'},
+        {id: 7, customerName: 'Test 7', phoneNo: '12345', email: 'test7@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '333'},
+        {id: 8, customerName: 'Test 8', phoneNo: '12345', email: 'test8@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '444'},
+        {id: 9, customerName: 'Test 9', phoneNo: '12345', email: 'test9@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '555'},
+        {id: 10, customerName: 'Test 10', phoneNo: '12345', email: 'test10@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '666'},
+        {id: 11, customerName: 'Test 11', phoneNo: '12345', email: 'test11@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '777'},
+        {id: 12, customerName: 'Test 12', phoneNo: '12345', email: 'test12@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '888'},
+        {id: 13, customerName: 'Test 13', phoneNo: '12345', email: 'test13@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '999'},
+        {id: 14, customerName: 'Test 14', phoneNo: '12345', email: 'test14@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '100'},
+        {id: 15, customerName: 'Test 15', phoneNo: '12345', email: 'test15@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '101'},
+        {id: 16, customerName: 'Test 16', phoneNo: '12345', email: 'test15@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '102'},
+        {id: 17, customerName: 'Test 17', phoneNo: '12345', email: 'test15@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '103'},
+        {id: 18, customerName: 'Test 18', phoneNo: '12345', email: 'test18@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '104'},
+        {id: 19, customerName: 'Test 19', phoneNo: '12345', email: 'test19@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '105'},
+        {id: 20, customerName: 'Test 20', phoneNo: '12345', email: 'test20@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '106'},
+        {id: 21, customerName: 'Test 21', phoneNo: '12345', email: 'test21@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '107'},
+        {id: 22, customerName: 'Test 22', phoneNo: '12345', email: 'test22@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '108'},
+        {id: 23, customerName: 'Test 23', phoneNo: '12345', email: 'test23@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '109'},
+        {id: 24, customerName: 'Test 23', phoneNo: '12345', email: 'test23@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '110'},
+        {id: 25, customerName: 'Test 23', phoneNo: '12345', email: 'test23@gmail.com', loanAmount: '2345', address: 'Test', citizenshipNumber: '111'}
     ];
 
     ngOnInit(): void {
