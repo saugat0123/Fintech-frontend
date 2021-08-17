92c2ea693f8670c4fadd48dc40c695f21023aa79
MANSURTHAPA <mansurk184@gmail.com>
2021-08-17 12:46:54 +0545

    removed pannum function

--- src/app/feature/customer/component/customer-form/company-form/company-form.component.ts
+++ src/app/feature/customer/component/customer-form/company-form/company-form.component.ts
@@ -1092,15 +1092,6 @@ export class CompanyFormComponent implements OnInit {
         }
     }
 
-    checkPanNumberNumber(regNumber: String) {
-        this.companyInfoService.getCompanyInfoWithPanNumber(regNumber).subscribe((res) => {
-            if (regNumber.toLowerCase() === res.detail.panNumber.toLowerCase()) {
-                this.toastService.show(new Alert(AlertType.WARNING, 'This customer already exists. Please input a unique value or choose the customer from catalogue section'));
-            }
-        }, error => {
-            console.error(error);
-        });
-    }
     checkRegistrationNumber(regNumber: String) {
         this.companyInfoService.getCompanyInfoWithRegistrationNumber(regNumber).subscribe((res) => {
             if (regNumber.toLowerCase() === res.detail.registrationNumber.toLowerCase()) {
