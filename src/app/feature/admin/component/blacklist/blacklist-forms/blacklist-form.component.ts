133685c25d7422db992cf3da5d6fe773b79612ec
Oshama Nmaberdar <oshamanamberdargujjar@gmail.com>
2021-08-18 10:01:36 +0545

    upload button in blacklist added features solved

--- src/app/feature/admin/component/blacklist/blacklist-forms/blacklist-form.component.ts
+++ src/app/feature/admin/component/blacklist/blacklist-forms/blacklist-form.component.ts
@@ -4,76 +4,84 @@ import {BlackList} from '../../../modal/BlackList';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {ModalResponse, ToastService} from '../../../../../@core/utils';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {Alert, AlertType} from '../../../../../@theme/model/Alert';
 import {BlacklistService} from '../blacklist.service';
+import {Alert, AlertType} from '../../../../../@theme/model/Alert';
 
 @Component({
-  selector: 'app-blacklist-forms',
-  templateUrl: './blacklist-form.component.html',
-  styleUrls: ['./blacklist-form.component.scss']
+    selector: 'app-blacklist-forms',
+    templateUrl: './blacklist-form.component.html',
+    styleUrls: ['./blacklist-form.component.scss']
 })
 export class BlacklistFormsComponent implements OnInit {
 
-  @Input() action: Action = Action.ADD;
-  @Input() model: BlackList;
-  types = [
-    {id: 'c', name: 'Citizenship. No'},
-    {id: 'p', name: 'PAN.No'}];
-  spinner = false;
-  private submitted: boolean;
-  formdata: FormData = new FormData();
-  form: FormGroup;
-  blacklistForm: FormGroup;
-  isCitizen = true;
-  constructor(
-      private activeModal: NgbActiveModal,
-      private toastService: ToastService,
-      private blackListService: BlacklistService,
-      private formBuilder: FormBuilder
-  ) {
-  }
+    @Input() action: Action = Action.ADD;
+    @Input() model: BlackList;
+    types = [
+        {id: 'c', name: 'Citizenship. No'},
+        {id: 'p', name: 'PAN.No'}];
+    spinner = false;
+    isTrue = false;
+    private submitted: boolean;
+    formdata: FormData = new FormData();
+    form: FormGroup;
+    blacklistForm: FormGroup;
+    isCitizen = true;
+    search: any = {};
 
-  ngOnInit() {
-    this.buildForm();
-    this.buildModalForm();
-  }
-
-  buildForm() {
-    this.form = this.formBuilder.group({
-      file: [undefined]
-    });
-  }
-
-  buildModalForm() {
-    this.blacklistForm = this.formBuilder.group({
-      name: [undefined],
-      ref: [undefined],
-      docType: [undefined]
-    });
-  }
-
-  onClose() {
-    this.activeModal.dismiss(ModalResponse.CANCEL);
-  }
-
-  onSubmit() {
-    this.submitted = true;
-    this.blackListService.save(this.blacklistForm.value).subscribe((res) => {
-      this.activeModal.close(ModalResponse.SUCCESS);
-      this.toastService.show(new Alert(AlertType.SUCCESS, 'sucessfully saved'));
-    }, error => {
-      this.activeModal.dismiss(error);
-      this.toastService.show(new Alert(AlertType.ERROR, error.error.message));
-    });
-  }
-
-  show(data) {
-    console.log(data);
-    if (data.id === 'p') {
-      this.isCitizen = false;
-    } else {
-      this.isCitizen = true;
+    constructor(
+        private activeModal: NgbActiveModal,
+        private toastService: ToastService,
+        private blackListService: BlacklistService,
+        private formBuilder: FormBuilder
+    ) {
     }
-  }
+
+    ngOnInit() {
+        this.buildForm();
+        this.buildModalForm();
+    }
+
+    buildForm() {
+        this.form = this.formBuilder.group({
+            file: [undefined]
+        });
+    }
+
+    buildModalForm() {
+        this.blacklistForm = this.formBuilder.group({
+            name: [undefined],
+            ref: [undefined],
+            docType: [undefined]
+        });
+    }
+
+    onClose() {
+        this.activeModal.dismiss(ModalResponse.CANCEL);
+    }
+
+    onSubmit() {
+        this.blackListService.checkBlacklistByRef(this.blacklistForm.get('ref').value).subscribe((data) => {
+            if (data.detail) {
+                this.toastService.show(new Alert(AlertType.ERROR, 'User Already Exist'));
+                return;
+            }
+            this.blackListService.save(this.blacklistForm.value).subscribe((res) => {
+                this.activeModal.close(ModalResponse.SUCCESS);
+                this.toastService.show(new Alert(AlertType.SUCCESS, 'sucessfully saved'));
+            }, error => {
+                this.activeModal.dismiss(error);
+                this.toastService.show(new Alert(AlertType.ERROR, error.error.message));
+            });
+        });
+    }
+
+    show(data) {
+        console.log(data);
+        if (data.id === 'p') {
+            this.isCitizen = false;
+        } else {
+            this.isCitizen = true;
+        }
+    }
 
 }
