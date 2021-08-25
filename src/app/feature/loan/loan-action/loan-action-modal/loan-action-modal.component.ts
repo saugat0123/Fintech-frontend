d413cb1ce9a918ccfdd201baba491dbb5714ec5a
sunil babu shrestha <whispersunil@gmail.com>
2021-08-25 08:20:42 +0000

    Merge branch 'sol-tag' into 'credix_d-icfc-master'
    Sol tag
    
    See merge request sunilbabu/sbsolution-frontapp!1582
    
    (cherry picked from commit ec9cd9d1f56cf5858ea6da64113d8ebf01b42307)
    
    bf738081 remove sol tag when there is no sol tagged
    f94f476a Merge branch 'credix_d-icfc-master' of...
    8a1517d5 Merge branch 'credix_d-icfc-master' of...
    1fdb925f show hide sol in combined loan case
    5685c87c Merge branch 'credix_d-icfc-master' of...

--- src/app/feature/loan/loan-action/loan-action-modal/loan-action-modal.component.ts
+++ src/app/feature/loan/loan-action/loan-action-modal/loan-action-modal.component.ts
@@ -134,6 +134,11 @@ export class LoanActionModalComponent implements OnInit {
                     this.isNoUserSelectedSol = true;
                     return;
                 }
+            } else {
+                this.formAction.patchValue({
+                    solUser: null,
+                    isSol: false
+                });
             }
         }
 
