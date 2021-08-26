7e83b9b90ea86919f36665451f9e757b821e9f30
Rujan Maharjan <rujnmahrzn@gmail.com>
2021-08-26 14:23:52 +0545

    Remove chat module
    Signed-off-by: Rujan Maharjan <rujnmahrzn@gmail.com>

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -4,52 +4,52 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {
-    NbAccordionModule,
-    NbActionsModule,
-    NbAlertModule,
-    NbBadgeModule,
-    NbButtonModule,
-    NbCalendarKitModule,
-    NbCalendarModule,
-    NbCalendarRangeModule,
-    NbCardModule,
-    NbChatModule,
-    NbCheckboxModule,
-    NbContextMenuModule,
-    NbDatepickerModule,
-    NbDialogModule,
-    NbIconModule,
-    NbInputModule,
-    NbLayoutModule,
-    NbListModule,
-    NbMenuModule,
-    NbPopoverModule,
-    NbProgressBarModule,
-    NbRadioModule,
-    NbRouteTabsetModule,
-    NbSearchModule,
-    NbSelectModule,
-    NbSidebarModule,
-    NbSpinnerModule,
-    NbStepperModule,
-    NbTabsetModule,
-    NbThemeModule,
-    NbToastrModule,
-    NbToggleModule,
-    NbTooltipModule,
-    NbUserModule,
-    NbWindowModule
+  NbAccordionModule,
+  NbActionsModule,
+  NbAlertModule,
+  NbBadgeModule,
+  NbButtonModule,
+  NbCalendarKitModule,
+  NbCalendarModule,
+  NbCalendarRangeModule,
+  NbCardModule,
+  NbChatModule,
+  NbCheckboxModule,
+  NbContextMenuModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbIconModule,
+  NbInputModule,
+  NbLayoutModule,
+  NbListModule,
+  NbMenuModule,
+  NbPopoverModule,
+  NbProgressBarModule,
+  NbRadioModule,
+  NbRouteTabsetModule,
+  NbSearchModule,
+  NbSelectModule,
+  NbSidebarModule,
+  NbSpinnerModule,
+  NbStepperModule,
+  NbTabsetModule,
+  NbThemeModule,
+  NbToastrModule,
+  NbToggleModule,
+  NbTooltipModule,
+  NbUserModule,
+  NbWindowModule
 } from '@nebular/theme';
 
 import {
-    AlertComponent,
-    BreadcrumbComponent,
-    FooterComponent,
-    HeaderComponent,
-    PagingComponent,
-    SpinnerComponent,
-    UpdateModalComponent,
-    ValidationErrorComponent
+  AlertComponent,
+  BreadcrumbComponent,
+  FooterComponent,
+  HeaderComponent,
+  PagingComponent,
+  SpinnerComponent,
+  UpdateModalComponent,
+  ValidationErrorComponent
 } from './components';
 
 import {CapitalizePipe, EvaIconsPipe, NumberWithCommasPipe, PluralPipe, RoundPipe, TimingPipe} from './pipes';
@@ -67,126 +67,125 @@ import {ChangePasswordComponent} from './components/change-password/change-passw
 import {CKEditorModule} from 'ng2-ckeditor';
 import {NgxSpinnerModule} from 'ngx-spinner';
 import {NbEvaIconsModule} from '@nebular/eva-icons';
-import { OverlaySpinnerComponent } from './components/overlay-spinner/overlay-spinner.component';
-import { DeleteModalComponent } from './components/delete-modal/delete-modal.component';
+import {OverlaySpinnerComponent} from './components/overlay-spinner/overlay-spinner.component';
+import {DeleteModalComponent} from './components/delete-modal/delete-modal.component';
 import {ChartModule} from '../chart/chart.module';
-import {ChatModule} from '../chat/chat.module';
-import { JsonParsePipe } from './pipes/json-parse.pipe';
-import { RolesSwitchComponent } from './components/roles-switch/roles-switch.component';
+import {JsonParsePipe} from './pipes/json-parse.pipe';
+import {RolesSwitchComponent} from './components/roles-switch/roles-switch.component';
 
 // import {NbSecurityModule} from '@nebular/security';
 
 const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule, CKEditorModule, ChartModule];
 
 const NB_MODULES = [
-    NbCardModule,
-    NbLayoutModule,
-    NbTabsetModule,
-    NbRouteTabsetModule,
-    NbMenuModule,
-    NbUserModule,
-    NbActionsModule,
-    NbSearchModule,
-    NbSidebarModule,
-    NbCheckboxModule,
-    NbPopoverModule,
-    NbContextMenuModule,
-    NgbModule,
-    // NbSecurityModule, // *nbIsGranted directive,
-    NbProgressBarModule,
-    NbCalendarModule,
-    NbCalendarRangeModule,
-    NbStepperModule,
-    NbButtonModule,
-    NbListModule,
-    NbToastrModule,
-    NbInputModule,
-    NbAccordionModule,
-    NbDatepickerModule,
-    NbDialogModule,
-    NbWindowModule,
-    NbAlertModule,
-    NbSpinnerModule,
-    NbRadioModule,
-    NbSelectModule,
-    NbChatModule,
-    NbTooltipModule,
-    NbCalendarKitModule,
-    NbIconModule,
-    NbToggleModule,
-    NbEvaIconsModule,
-    NbBadgeModule
+  NbCardModule,
+  NbLayoutModule,
+  NbTabsetModule,
+  NbRouteTabsetModule,
+  NbMenuModule,
+  NbUserModule,
+  NbActionsModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbCheckboxModule,
+  NbPopoverModule,
+  NbContextMenuModule,
+  NgbModule,
+  // NbSecurityModule, // *nbIsGranted directive,
+  NbProgressBarModule,
+  NbCalendarModule,
+  NbCalendarRangeModule,
+  NbStepperModule,
+  NbButtonModule,
+  NbListModule,
+  NbToastrModule,
+  NbInputModule,
+  NbAccordionModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbWindowModule,
+  NbAlertModule,
+  NbSpinnerModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbChatModule,
+  NbTooltipModule,
+  NbCalendarKitModule,
+  NbIconModule,
+  NbToggleModule,
+  NbEvaIconsModule,
+  NbBadgeModule
 
 ];
 
 const COMPONENTS = [
-    HeaderComponent,
-    FooterComponent,
-    SearchResultComponent,
-    BaseLayout,
-    IconCardComponent,
-    AlertComponent,
-    BreadcrumbComponent,
-    SpinnerComponent,
-    UpdateModalComponent,
-    PagingComponent,
-    ValidationErrorComponent,
-    MessageModalComponent,
-    NotificationComponent,
-    OverlaySpinnerComponent,
-    DeleteModalComponent,
+  HeaderComponent,
+  FooterComponent,
+  SearchResultComponent,
+  BaseLayout,
+  IconCardComponent,
+  AlertComponent,
+  BreadcrumbComponent,
+  SpinnerComponent,
+  UpdateModalComponent,
+  PagingComponent,
+  ValidationErrorComponent,
+  MessageModalComponent,
+  NotificationComponent,
+  OverlaySpinnerComponent,
+  DeleteModalComponent,
 
 ];
 
 const ENTRY_COMPONENTS = [
-    SearchResultComponent,
-    MessageModalComponent,
-    ProfileComponent,
-    ChangePasswordComponent,
-    UpdateModalComponent,
-    DeleteModalComponent,
+  SearchResultComponent,
+  MessageModalComponent,
+  ProfileComponent,
+  ChangePasswordComponent,
+  UpdateModalComponent,
+  DeleteModalComponent,
 
 ];
 
 const PIPES = [
-    CapitalizePipe,
-    PluralPipe,
-    RoundPipe,
-    TimingPipe,
-    NumberWithCommasPipe,
-    EvaIconsPipe,
-    JsonParsePipe
+  CapitalizePipe,
+  PluralPipe,
+  RoundPipe,
+  TimingPipe,
+  NumberWithCommasPipe,
+  EvaIconsPipe,
+  JsonParsePipe
 ];
 
 const NB_THEME_PROVIDERS = [
-    ...NbThemeModule.forRoot(
-        {
-            name: 'default',
-        },
-        [DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME],
-    ).providers,
-    ...NbSidebarModule.forRoot().providers,
-    ...NbMenuModule.forRoot().providers,
-    ...NbDatepickerModule.forRoot().providers,
-    ...NbDialogModule.forRoot().providers,
-    ...NbWindowModule.forRoot().providers,
-    ...NbToastrModule.forRoot().providers
-    // ...NbChatModule.forRoot({
-    //     messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
-    // }).providers,
+  ...NbThemeModule.forRoot(
+      {
+        name: 'default',
+      },
+      [DEFAULT_THEME, COSMIC_THEME, CORPORATE_THEME],
+  ).providers,
+  ...NbSidebarModule.forRoot().providers,
+  ...NbMenuModule.forRoot().providers,
+  ...NbDatepickerModule.forRoot().providers,
+  ...NbDialogModule.forRoot().providers,
+  ...NbWindowModule.forRoot().providers,
+  ...NbToastrModule.forRoot().providers
+  // ...NbChatModule.forRoot({
+  //     messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
+  // }).providers,
 ];
 
 @NgModule({
-    imports: [...BASE_MODULES, ...NB_MODULES, RouterModule, NgxSpinnerModule, ChatModule],
-    exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS, ...PIPES, NgxSpinnerModule],
-    declarations: [...COMPONENTS, ...PIPES, ProfileComponent, ChangePasswordComponent, RolesSwitchComponent],
-    entryComponents: [...ENTRY_COMPONENTS],
+  imports: [...BASE_MODULES, ...NB_MODULES, RouterModule, NgxSpinnerModule],
+  exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS, ...PIPES, NgxSpinnerModule],
+  declarations: [...COMPONENTS, ...PIPES, ProfileComponent, ChangePasswordComponent, RolesSwitchComponent],
+  entryComponents: [...ENTRY_COMPONENTS],
 })
 export class ThemeModule {
-    static forRoot(): ModuleWithProviders {
-        return <ModuleWithProviders>{
-            ngModule: ThemeModule,
-            providers: [...NB_THEME_PROVIDERS],
-        };
-    }
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders>{
+      ngModule: ThemeModule,
+      providers: [...NB_THEME_PROVIDERS],
+    };
+  }
 }
