af2bad97189cd9afddbf8fc475aa164765f71b5e
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-08-27 11:33:27 +0545

    clean up unused code

--- src/app/@core/utils/constants/default-loan-template.ts
+++ src/app/@core/utils/constants/default-loan-template.ts
@@ -1,20 +1,5 @@
 export class DefaultLoanTemplate {
     DEFAULT_TEMPLATE = [
-        // {
-        //     active: false,
-        //     name: 'Credit Risk Grading - Alpha',
-        //     templateUrl: null
-        // },
-        // {
-        //     active: false,
-        //     name: 'Credit Risk Grading - Lambda',
-        //     templateUrl: null
-        // },
-        // {
-        //     active: false,
-        //     name: 'Credit Risk Grading - Micro',
-        //     templateUrl: null
-        // },
         {
             active: false,
             name: 'Credit Risk Grading - Gamma',
