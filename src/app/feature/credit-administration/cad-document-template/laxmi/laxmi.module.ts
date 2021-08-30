d1c6d7b351bd6725e6d033c1a9b7c0419826bf26
Sulav Shrestha <connect@sulav.name.np>
2021-08-30 22:35:39 +0545

    complete offer letter

--- src/app/feature/credit-administration/cad-document-template/laxmi/laxmi.module.ts
+++ src/app/feature/credit-administration/cad-document-template/laxmi/laxmi.module.ts
@@ -8,10 +8,11 @@ import { OfferLetterComponent } from './laxmi-offer-letter/offer-letter/offer-le
 import {ReactiveFormsModule} from '@angular/forms';
 import {NgxPrintModule} from 'ngx-print';
 import { LetterOfCommitmentPrintComponent } from './laxmi-offer-letter/letter-of-commitment/letter-of-commitment-print/letter-of-commitment-print.component';
+import { OfferLetterPrintComponent } from './laxmi-offer-letter/offer-letter/offer-letter-print/offer-letter-print.component';
 
 
 @NgModule({
-    declarations: [LaxmiOfferLetterComponent, LetterOfCommitmentComponent, PersonalGuaranteeComponent, OfferLetterComponent, LetterOfCommitmentPrintComponent],
+    declarations: [LaxmiOfferLetterComponent, LetterOfCommitmentComponent, PersonalGuaranteeComponent, OfferLetterComponent, LetterOfCommitmentPrintComponent, OfferLetterPrintComponent],
     imports: [
         CommonModule,
         NbCardModule,
