e6cac38384b7c51b1145125403061cdffdd30ad3
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2021-09-02 16:33:53 +0545

    handle null file extension

--- src/app/feature/loan-information-template/security/security-initial-form/fix-asset-collateral/fix-asset-collateral.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/fix-asset-collateral/fix-asset-collateral.component.ts
@@ -49,6 +49,7 @@ export class FixAssetCollateralComponent implements OnInit {
     modelBody: string;
     isSiteVisitPresent: boolean;
     security_id_for_delete: string;
+    fileType = '.jpg';
 
     constructor(private formBuilder: FormBuilder,
                 private http: HttpClient,
@@ -318,7 +319,12 @@ export class FixAssetCollateralComponent implements OnInit {
     }
 
     viewDocument(url: string, name: string, fileType: string) {
-        const viewDocName = name.concat('.').concat(fileType);
+        let viewDocName;
+        if (fileType === null) {
+            viewDocName = name.concat(this.fileType);
+        } else {
+            viewDocName = name.concat('.').concat(fileType);
+        }
         const link = document.createElement('a');
         link.target = '_blank';
         link.href = `${ApiConfig.URL}/${url}${viewDocName}?${Math.floor(Math.random() * 100) + 1}`;
