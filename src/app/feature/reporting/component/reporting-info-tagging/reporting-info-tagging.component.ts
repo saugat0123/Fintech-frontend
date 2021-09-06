cd5c4a093f432c527de2babc3b031fa30367f6df
sushanprajapati9808 <sushan@chipster.in>
2021-09-06 15:38:36 +0545

    for reporting info

--- src/app/feature/reporting/component/reporting-info-tagging/reporting-info-tagging.component.ts
+++ src/app/feature/reporting/component/reporting-info-tagging/reporting-info-tagging.component.ts
@@ -7,7 +7,6 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {ToastService} from '../../../../@core/utils';
 import {ObjectUtil} from '../../../../@core/utils/ObjectUtil';
 import {ReportingInfoTaggingFormComponent} from '../reporting-info-tagging-form/reporting-info-tagging-form.component';
-import {NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-reporting-info-tagging',
@@ -34,8 +33,7 @@ export class ReportingInfoTaggingComponent implements OnInit {
   constructor(
       private reportingInfoService: ReportingInfoService,
       private toastService: ToastService,
-      private formBuilder: FormBuilder,
-      private overlay: NgxSpinnerService
+      private formBuilder: FormBuilder
   ) {
   }
 
