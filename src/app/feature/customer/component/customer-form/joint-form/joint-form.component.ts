a3c93f907d9257c4106f99342b9195f65e43a3b1
sunil babu shrestha <sunil@sbsolutionsnepal.com>
2021-09-14 14:41:11 +0545

    spinner while saving customer

--- src/app/feature/customer/component/customer-form/joint-form/joint-form.component.ts
+++ src/app/feature/customer/component/customer-form/joint-form/joint-form.component.ts
@@ -312,6 +312,7 @@ this.dynamic = false;
     }
 
     onSubmit(value) {
+        this.spinner = true;
         this.submitted = true;
         const tempId = this.basicJointInfo.get('jointCustomerInfo')['controls'][0].get('citizenshipNumber').value;
         this.blackListService.checkBlacklistByRef(tempId).subscribe((response: any) => {
