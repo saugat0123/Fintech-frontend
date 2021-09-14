0c51cd1ae7f2addcb19b299c0375da283f2e18db
sunil babu shrestha <sunil@sbsolutionsnepal.com>
2021-09-14 16:14:54 +0545

    spinner while saving customer

--- src/app/feature/customer/component/customer-form/company-form/company-form.component.ts
+++ src/app/feature/customer/component/customer-form/company-form/company-form.component.ts
@@ -857,7 +857,6 @@ export class CompanyFormComponent implements OnInit {
     }
 
     onSubmit() {
-        this.spinner = true;
         this.submitted = true;
         this.marketScenarioComponent.onSubmit();
         this.companyOtherDetailComponent.onSubmit();
