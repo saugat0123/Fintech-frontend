a515231d9a31c2a9f86d7ed4f79255f65cc0709b
Himal Rai <himal@sbsolutionsnepal.com>
2021-09-22 12:48:25 +0545

    decimal num refactor

--- src/app/@core/directive/decimal-number.directive.ts
+++ src/app/@core/directive/decimal-number.directive.ts
@@ -16,7 +16,6 @@ export class DecimalNumberDirective {
     }
     let current: string = this.el.nativeElement.value;
     let next: string = current.concat(event.key);
-
     if (next && !String(next).match(this.regex)) {
       event.preventDefault();
     }
