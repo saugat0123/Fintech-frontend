cd11dc477b20538870f8c2e4e3920d5c836d5c82
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-09-23 11:19:48 +0545

    begin to segregate initial and proposed security

--- src/app/feature/loan-information-template/loan-information-template.module.ts
+++ src/app/feature/loan-information-template/loan-information-template.module.ts
@@ -49,6 +49,7 @@ import { CrgMicroComponent } from './crg-micro/crg-micro.component';
 import { SecurityTableComponent } from './security/security-initial-form/security-table/security-table.component';
 import { CreateDocumentComponent } from './security/security-initial-form/create-document/create-document.component';
 import { ObtainedDocumentComponent } from './obtained-document/obtained-document.component';
+import { SecurityApprovedFormComponent } from './security/security-approved-form/security-approved-form.component';
 
 const COMPONENTS = [
     SiteVisitComponent,
@@ -89,7 +90,8 @@ const COMPONENTS = [
 @NgModule({
   declarations: [...COMPONENTS, CadDocumentUploadComponent, MicroProposalComponent, CommentsComponent, PreviousSecurityComponent,
       CrgMicroComponent,
-      ObtainedDocumentComponent],
+      ObtainedDocumentComponent,
+      SecurityApprovedFormComponent],
     exports: [...COMPONENTS, MicroProposalComponent, CommentsComponent, CrgMicroComponent, ObtainedDocumentComponent],
     entryComponents: [...COMPONENTS],
     imports: [
