d97ef141e2e33996975c44e7ea05cbbb841b9df6
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-10-01 10:50:42 +0545

    loan deed company template mapping left

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-loan-deed-company/nabil-loan-deed-company.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-loan-deed-company/nabil-loan-deed-company.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Editor} from '../../../../../@core/utils/constants/editor';
+import {CustomerApprovedLoanCadDocumentation} from '../../../model/customerApprovedLoanCadDocumentation';
+import {NabilDocumentChecklist} from '../../../../admin/modal/nabil-document-checklist.enum';
+import {ObjectUtil} from '../../../../../@core/utils/ObjectUtil';
 
 @Component({
   selector: 'app-nabil-loan-deed-company',
@@ -8,6 +11,12 @@ import {Editor} from '../../../../../@core/utils/constants/editor';
   styleUrls: ['./nabil-loan-deed-company.component.scss']
 })
 export class NabilLoanDeedCompanyComponent implements OnInit {
+  @Input() cadData: CustomerApprovedLoanCadDocumentation;
+  @Input() documentId: number;
+  @Input() customerLoanId: number;
+  individualData;
+  initialInfoPrint;
+  offerLetterConst = NabilDocumentChecklist;
   form: FormGroup;
   ckEditorConfig = Editor.CK_CONFIG;
 
@@ -18,6 +27,25 @@ export class NabilLoanDeedCompanyComponent implements OnInit {
 
   ngOnInit() {
     this.buildForm();
+    console.log('cad data', this.cadData);
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      this.cadData.cadFileList.forEach(individualCadFile => {
+        console.log(individualCadFile);
+        if (individualCadFile.customerLoanId === this.customerLoanId && individualCadFile.cadDocument.id === this.documentId) {
+          const initialInfo = JSON.parse(individualCadFile.initialInformation);
+          this.initialInfoPrint = initialInfo;
+        }
+      });
+    }
+    this.setLoanDeedCompanyFormData(this.cadData);
+  }
+
+  private setLoanDeedCompanyFormData(cadData): void {
+    if (!ObjectUtil.isEmpty(cadData)) {
+      const data = JSON.parse(cadData.loanHolder.nepData);
+      console.log(data);
+      this.form.get('nameOfBranch').patchValue(data.branch.ct);
+    }
   }
 
   private buildForm(): FormGroup {
