bf34871dabe88698dbc2d7f11e7c3c620f18f405
suCpu <maharjannimesh11@gmail.com>
2021-10-08 17:10:43 +0545

    letter-vehicle-individual done

--- src/app/feature/credit-administration/cad-document-template/nabil/letter-vehicle-individual/letter-vehicle-individual.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/letter-vehicle-individual/letter-vehicle-individual.component.ts
@@ -58,12 +58,9 @@ export class LetterVehicleIndividualComponent implements OnInit {
         if (individualCadFile.customerLoanId === this.customerLoanId && individualCadFile.cadDocument.id === this.documentId) {
           const initialInfo = JSON.parse(individualCadFile.initialInformation);
           this.initialInfoPrint = initialInfo;
+          this.letterVehicleIndividual.patchValue(initialInfo);
         }
       });
-    }
-    if (!ObjectUtil.isEmpty(this.cadData.loanHolder.nepData)) {
-      this.individualData = JSON.parse(this.cadData.loanHolder.nepData);
-      this.clientType = this.cadData.loanHolder['customerSubType'];
     }
   }
 
@@ -84,21 +81,51 @@ export class LetterVehicleIndividualComponent implements OnInit {
       chasisNumber: [undefined],
     });
   }
-
+  fillform() {
+    this.letterVehicleIndividual.patchValue(
+        {
+          date: this.individualData.date.ct ?
+              this.individualData.date.ct : '',
+          nameOfBranchLocated: this.individualData.nameOfBranchLocated.ct ?
+              this.individualData.nameOfBranchLocated.ct : '',
+          nameOfGrandFather: this.individualData.nameOfGrandFather.ct ?
+              this.individualData.nameOfGrandFather.ct : '',
+          name: this.individualData.name.ct ?
+              this.individualData.name.ct : '',
+          district: this.individualData.district.ct ?
+              this.individualData.district.ct : '',
+          vdcMuncipality: this.individualData.vdcMuncipality.ct ?
+              this.individualData.vdcMunicipality.ct : '',
+          wardNo: this.individualData.wardNo.ct ?
+              this.individualData.wardNo.ct : '',
+          nameOfBorrower: this.individualData.nameOfBorrower.ct ?
+              this.individualData.nameOfBorrower.ct : '',
+          sanctionLetterIssuedDate: this.individualData.sanctionLetterIssuedDate.ct ?
+              this.individualData.sanctionLetterIssuedDate.ct : '',
+          modelTypeOfVehicle: this.individualData.modelTypeOfVehicle.ct ?
+              this.individualData.modelTypeOfVehicle.ct : '',
+          vehicleRegistrationNumber: this.individualData.vehicleRegistrationNumber.ct ?
+              this.individualData.vehicleRegistrationNumber.ct : '',
+          engineNumber: this.individualData.engineNumber.ct ?
+              this.individualData.engineNumber.ct : '',
+          chasisNumber: this.individualData.chasisNumber.ct ?
+              this.individualData.chasisNumber.ct : '',
+        }
+    );
+  }
   submit() {
     let flag = true;
     if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
-      this.cadData.cadFileList.forEach(individualCadFile => {
-        if (individualCadFile.customerLoanId === this.customerLoanId && individualCadFile.cadDocument.id === this.documentId) {
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
           flag = false;
-          individualCadFile.initialInformation = JSON.stringify(this.letterVehicleIndividual.value);
+          singleCadFile.initialInformation = JSON.stringify(this.letterVehicleIndividual.value);
         }
       });
       if (flag) {
         const cadFile = new CadFile();
         const document = new Document();
         cadFile.initialInformation = JSON.stringify(this.letterVehicleIndividual.value);
-        this.initialInfoPrint = cadFile.initialInformation;
         document.id = this.documentId;
         cadFile.cadDocument = document;
         cadFile.customerLoanId = this.customerLoanId;
@@ -108,7 +135,6 @@ export class LetterVehicleIndividualComponent implements OnInit {
       const cadFile = new CadFile();
       const document = new Document();
       cadFile.initialInformation = JSON.stringify(this.letterVehicleIndividual.value);
-      this.initialInfoPrint = cadFile.initialInformation;
       document.id = this.documentId;
       cadFile.cadDocument = document;
       cadFile.customerLoanId = this.customerLoanId;
@@ -124,6 +150,5 @@ export class LetterVehicleIndividualComponent implements OnInit {
       this.toastService.show(new Alert(AlertType.ERROR, 'Failed to save '));
       this.dialogRef.close();
     });
-    console.log(this.letterVehicleIndividual.value);
   }
 }
