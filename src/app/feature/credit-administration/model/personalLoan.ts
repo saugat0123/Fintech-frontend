cedba5a486daa648d69f993286b6839bd83571e1
aayushlalshrestha <mail2ayus@yahoo.com>
2021-11-08 16:07:39 +0545

    added personal loan and corporate field list  in cad offer letter configuration and nep proposed amount form component. Small changes in offer letter personal. Added personalLOan.ts file.

---
+++ src/app/feature/credit-administration/model/personalLoan.ts
@@ -0,0 +1,12 @@
+export class PersonalLoan {
+    branchName: string;
+    branchDistrict: string;
+    branchMunVdc: string;
+    branchWardNo: string;
+    loanType: string;
+    interestRate: string;
+    serviceFee: string;
+    tenureOfLoanInMonths: string;
+    tenureOfLoanInYears: string;
+    installmentAmount: string;
+}
