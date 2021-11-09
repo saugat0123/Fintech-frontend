3338edef8cd3966f4f61ea5e8ecf6a72e6b46cc8
ayonij2006 <ayonij2006@gmail.com>
2021-11-09 17:58:31 +0545

    Added function to generate random hashcode.

--- build/post-build.js
+++ build/post-build.js
@@ -19,6 +19,17 @@ let mainBundleFile = '';
 // RegExp to find main.bundle.js, even if it doesn't include a hash in it's name (dev build)
 let mainBundleRegexp = /^main[-]([a-z0-9]*).([a-z0-9]*).js$/;
 
+function makeId(length) {
+    let result           = '';
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
 // read the dist folder files and find the one we're looking for
 readDir(path.join(__dirname, '/../dist/sb-frontend/'))
     .then(files => {
@@ -29,8 +40,12 @@ readDir(path.join(__dirname, '/../dist/sb-frontend/'))
             let matchHash = mainBundleFile.match(mainBundleRegexp);
             console.log('matchhash: ', matchHash);
             // if it has a hash in it's name, mark it down
-            if (matchHash.length > 1 && !!matchHash[1]) {
+            if (matchHash.length > 1 && !!matchHash[2]) {
                 mainHash = matchHash[2];
+            }
+            const generatedHash = makeId(20);
+            if (generatedHash) {
+                mainHash = generatedHash;
             }
         }
         console.log('main bundle file: ', mainBundleFile);
@@ -55,7 +70,7 @@ readDir(path.join(__dirname, '/../dist/sb-frontend/'))
                 console.log('mainFilepath', mainFilepath);
                 const replacedFile = mainFileData.replace('{{POST_BUILD_ENTERS_HASH_HERE}}', mainHash);
                 return writeFile(mainFilepath, replacedFile);
-        });
+            });
     }).catch(err => {
         console.log('Error with post build:', err);
-});
+    });
