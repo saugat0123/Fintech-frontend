1f264c6d1802da781d8f147dcfad16f844aba04d
sujesh07 <sujesh@sbsolutionsnepal.com>
2021-11-10 16:47:10 +0545

    Refactor api config

--- src/app/@core/utils/api/ApiConfig.ts
+++ src/app/@core/utils/api/ApiConfig.ts
@@ -2,9 +2,6 @@ export class ApiConfig {
     public static TOKEN = 'http://localhost:8086/oauth/token';
     public static URL = 'http://localhost:8086';
 
-    // Laxmi Micro Finance
-    public static MICRO_URL = 'http://172.16.16.52:8086';
-
     // Remit App UAT
     public static REMIT_WEB = 'http://202.166.201.232/api';
 
