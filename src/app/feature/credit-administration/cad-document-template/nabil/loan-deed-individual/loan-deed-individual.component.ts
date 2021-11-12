04c281968808f4e3cc078a23285e113b0583678f
sushanprajapati <sushan.prajapati@sbsolutions.com>
2021-11-12 11:18:14 +0545

    legal docs update

--- src/app/feature/credit-administration/cad-document-template/nabil/loan-deed-individual/loan-deed-individual.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/loan-deed-individual/loan-deed-individual.component.ts
@@ -139,6 +139,7 @@ export class LoanDeedIndividualComponent implements OnInit {
     let approvedDate: any;
     this.docName = this.cadData.offerDocumentList ? this.cadData.offerDocumentList[0].docName : '';
     console.log('Loan Deed Data', this.offerDocumentDetails);
+    console.log('Loan Deed Document Name', this.docName);
     if (!ObjectUtil.isEmpty(this.offerDocumentDetails) && !ObjectUtil.isEmpty(this.offerDocumentDetails.dateOfApproval)) {
       // tslint:disable-next-line:max-line-length
       // approvedDate = this.offerDocumentDetails.dateOfApproval && this.offerDocumentDetails.dateOfApproval.en.eDate ? this.offerDocumentDetails.dateOfApproval.en.eDate : this.offerDocumentDetails.dateOfApproval && this.offerDocumentDetails.dateOfApproval.en ? this.offerDocumentDetails.dateOfApproval.en : '';
