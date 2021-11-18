0a5a55094b49924f99273e7778369e958dc235fd
sbintern <sbsolutionstest@gmail.com>
2021-11-18 17:39:41 +0545

    template of class-a sanction-letter.component (NABIL)

---
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/class-a-sanction-letter/class-a-sanction-letter-print/class-a-sanction-letter-print.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class-a-sanction-letter-print',
+  templateUrl: './class-a-sanction-letter-print.component.html',
+  styleUrls: ['./class-a-sanction-letter-print.component.scss']
+})
+export class ClassASanctionLetterPrintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/class-a-sanction-letter/class-a-sanction-letter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class-a-sanction-letter',
+  templateUrl: './class-a-sanction-letter.component.html',
+  styleUrls: ['./class-a-sanction-letter.component.scss']
+})
+export class ClassASanctionLetterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
