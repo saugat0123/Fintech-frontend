83da550a9043bafc5a898afb642289d36669839e
Binita Simbu <grishulim67@gmail.com>
2021-11-19 17:25:39 +0545

    loan deed institutional template

--- src/app/feature/credit-administration/cad-document-template/laxmi/laxmi-offer-letter/loan-deed-institutional/loan-deed-institutional.component.ts
+++ src/app/feature/credit-administration/cad-document-template/laxmi/laxmi-offer-letter/loan-deed-institutional/loan-deed-institutional.component.ts
@@ -11,6 +11,10 @@ import {EngToNepaliNumberPipe} from '../../../../../../@core/pipe/eng-to-nepali-
 import {NepaliCurrencyWordPipe} from '../../../../../../@core/pipe/nepali-currency-word.pipe';
 import {NepaliToEngNumberPipe} from '../../../../../../@core/pipe/nepali-to-eng-number.pipe';
 import {NepaliNumberPipe} from '../../../../../../@core/pipe/nepali-number.pipe';
+import {ObjectUtil} from '../../../../../../@core/utils/ObjectUtil';
+import {CadFile} from '../../../../model/CadFile';
+import {Document} from '../../../../../admin/modal/document';
+import {Alert, AlertType} from '../../../../../../@theme/model/Alert';
 
 @Component({
   selector: 'app-loan-deed-institutional',
@@ -44,9 +48,92 @@ export class LoanDeedInstitutionalComponent implements OnInit {
 
   ngOnInit() {
     this.buildForm();
+    this.amount = this.cadData.assignedLoan[0].proposal.proposedLimit;
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
+          this.initialInfoPrint = singleCadFile.initialInformation;
+          if (!ObjectUtil.isEmpty( JSON.parse(singleCadFile.initialInformation).rupees)) {
+            this.amount = JSON.parse(singleCadFile.initialInformation).rupees;
+          }
+        }
+      });
+    }
+    if (!ObjectUtil.isEmpty(this.cadData.loanHolder.nepData)) {
+      this.nepaliData = JSON.parse(this.cadData.loanHolder.nepData);
+    }
+    if (!ObjectUtil.isEmpty(this.initialInfoPrint)) {
+      this.form.patchValue(JSON.parse(this.initialInfoPrint));
+      this.form.patchValue({
+        amount: this.nepaliCurrencyWordPipe.transform(this.nepaliToEnglishPipe.transform(this.amount))
+      });
+    } else {
+      this.fillForm();
+    }
   }
-
+  fillForm() {
+    if (!ObjectUtil.isEmpty(this.nepaliData)) {
+      const customerTempAddress =
+          this.nepaliData.temporaryMunicipality + ' वडा नं. ' +
+          this.nepaliData.temporaryWard + ', ' +
+          this.nepaliData.temporaryDistrict;
+      this.form.patchValue({
+        wadNo: this.nepaliNumber.transform(this.cadData.assignedLoan[0].branch.wardNumber, 'preeti'),
+        grandFatherName: this.nepaliData.grandFatherName,
+        fatherName: this.nepaliData.fatherName ? this.nepaliData.fatherName : this.nepaliData.fatherInLawName,
+        customerDistrict: this.nepaliData.permanentDistrict,
+        customerMetropolitan: this.nepaliData.permanentMunicipality,
+        customerWardNo: this.nepaliData.permanentWard,
+        customerTemporaryAddress: customerTempAddress ? customerTempAddress : '',
+        customerAge: this.nepaliData.age,
+        customerCitizenshipNo: this.nepaliNumber.transform(this.cadData.assignedLoan[0].customerInfo.citizenshipNumber, 'preeti'),
+        citizenshipIssueDistrict: this.nepaliData.citizenshipIssueDistrict,
+        citizenshipIssueDate: this.nepaliData.citizenshipIssueDate,
+        customerName: this.nepaliData.name,
+        rupees: this.nepaliNumber.transform(this.amount, 'preeti'),
+        amount: this.nepaliCurrencyWordPipe.transform(this.amount)
+      });
+    }
+  }
   submit() {
+    this.spinner = true;
+    let flag = true;
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
+          flag = false;
+          singleCadFile.initialInformation = JSON.stringify(this.form.value);
+        }
+      });
+      if (flag) {
+        const cadFile = new CadFile();
+        const document = new Document();
+        cadFile.initialInformation = JSON.stringify(this.form.value);
+        document.id = this.documentId;
+        cadFile.cadDocument = document;
+        cadFile.customerLoanId = this.customerLoanId;
+        this.cadData.cadFileList.push(cadFile);
+      }
+    } else {
+      const cadFile = new CadFile();
+      const document = new Document();
+      cadFile.initialInformation = JSON.stringify(this.form.value);
+      document.id = this.documentId;
+      cadFile.cadDocument = document;
+      cadFile.customerLoanId = this.customerLoanId;
+      this.cadData.cadFileList.push(cadFile);
+    }
+    this.administrationService.saveCadDocumentBulk(this.cadData).subscribe(() => {
+      this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved '));
+      this.dialogRef.close();
+      this.spinner = false;
+      this.routerUtilsService.reloadCadProfileRoute(this.cadData.id);
+    }, error => {
+      console.error(error);
+      this.spinner = false;
+      this.toastService.show(new Alert(AlertType.ERROR, 'Failed to save '));
+      this.dialogRef.close();
+    });
   }
 
   private buildForm() {
@@ -78,8 +165,8 @@ export class LoanDeedInstitutionalComponent implements OnInit {
       citizenshipIssueDate: [undefined],
       approvedDate: [undefined],
       customeName: [undefined],
-      loanAmount: [undefined],
-      loanAmountInWord: [undefined],
+      rupees: [undefined],
+      amount: [undefined],
       year: [undefined],
       month: [undefined],
       day: [undefined],
