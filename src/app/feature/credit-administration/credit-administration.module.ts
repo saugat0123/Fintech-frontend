3723a8e81060e6440062aed91bee6cb3dc3c2d5e
sushanprajapati <sushan.prajapati@sbsolutions.com>
2021-11-23 15:17:51 +0545

    personal overdraft without collateral edit update and mortage loan few fixes

--- src/app/feature/credit-administration/credit-administration.module.ts
+++ src/app/feature/credit-administration/credit-administration.module.ts
@@ -95,7 +95,7 @@ import { UdhyamsilKarjaSubsidyTemplateDataComponent } from './cad-view/template-
 import { UdhyamsilKarjaSubsidyTemplateEditComponent } from './cad-view/template-data/nabil-sme-template-data/udhyamsil-karja-subsidy/udhyamsil-karja-subsidy-template-edit/udhyamsil-karja-subsidy-template-edit.component';
 import {InterestSubsidySanctionLetterComponent} from './cad-document-template/nabil/nabil-sme/interest-subsidy-sanction-letter/interest-subsidy-sanction-letter.component';
 import {InterestSubsidySanctionLetterPrintComponent} from './cad-document-template/nabil/nabil-sme/interest-subsidy-sanction-letter/interest-subsidy-sanction-letter-print/interest-subsidy-sanction-letter-print.component';
-import { PersonalOverdraftWithoutCollateralTemplateEditComponent } from './cad-view/template-data/auto-loan-commercial-template-data/personal-overdraft-without-collateral-template-edit/personal-overdraft-without-collateral-template-edit.component';
+import { PersonalOverdraftWithoutCollateralTemplateEditComponent } from './cad-view/template-data/personal-overdraft-without-collateral-template-edit/personal-overdraft-without-collateral-template-edit.component';
 
 @NgModule({
     declarations: [UnassignedLoanComponent,
