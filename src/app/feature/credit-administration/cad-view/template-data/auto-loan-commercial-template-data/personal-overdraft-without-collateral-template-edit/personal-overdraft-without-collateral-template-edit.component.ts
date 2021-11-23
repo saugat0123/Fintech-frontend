6f6b7ddbc79866b50403b69e438457e30f43f183
sushanprajapati <sushan.prajapati@sbsolutions.com>
2021-11-23 11:18:43 +0545

    personal overdraft without collateral edit

---
+++ src/app/feature/credit-administration/cad-view/template-data/auto-loan-commercial-template-data/personal-overdraft-without-collateral-template-edit/personal-overdraft-without-collateral-template-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-overdraft-without-collateral-template-edit',
+  templateUrl: './personal-overdraft-without-collateral-template-edit.component.html',
+  styleUrls: ['./personal-overdraft-without-collateral-template-edit.component.scss']
+})
+export class PersonalOverdraftWithoutCollateralTemplateEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
