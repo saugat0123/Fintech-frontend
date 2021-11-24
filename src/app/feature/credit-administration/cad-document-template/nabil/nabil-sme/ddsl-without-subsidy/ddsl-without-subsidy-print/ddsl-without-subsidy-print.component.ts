c4148f281a2835bb533dd33b8acc2ed8f17ac280
Ramesh Kumar Bhandari <ramesh152200@gmail.com>
2021-11-24 16:32:06 +0545

    Design Edit Form For DDSL Without Subsidy Template-Edit Form and Other Some changes in Html template

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/ddsl-without-subsidy/ddsl-without-subsidy-print/ddsl-without-subsidy-print.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/ddsl-without-subsidy/ddsl-without-subsidy-print/ddsl-without-subsidy-print.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NabilOfferLetterConst} from '../../../../../nabil-offer-letter-const';
+import {CustomerApprovedLoanCadDocumentation} from '../../../../../model/customerApprovedLoanCadDocumentation';
 
 @Component({
   selector: 'app-ddsl-without-subsidy-print',
@@ -7,7 +8,13 @@ import {NabilOfferLetterConst} from '../../../../../nabil-offer-letter-const';
   styleUrls: ['./ddsl-without-subsidy-print.component.scss']
 })
 export class DdslWithoutSubsidyPrintComponent implements OnInit {
-  @Input() ddslData;
+  @Input() ddslData: any;
+  @Input() cadOfferLetterApprovedDoc: CustomerApprovedLoanCadDocumentation;
+  @Input() preview = false;
+  @Input() security: any;
+  @Input() renewal: any;
+  @Input() offerData;
+  @Input() loanLimit;
   offerLetterConst = NabilOfferLetterConst;
   constructor() { }
 
--- src/app/feature/credit-administration/component/offer-letter/offer-letter-list/offer-letter-list.component.ts
+++ src/app/feature/credit-administration/component/offer-letter/offer-letter-list/offer-letter-list.component.ts
@@ -30,6 +30,7 @@ import {RetailMortageLoanTemplateDataEditComponent} from '../../../cad-view/temp
 import {AutoLoanTemplateEditComponent} from '../../../cad-view/template-data/auto-loan-template-edit/auto-loan-template-edit.component';
 import {UdhyamsilKarjaSubsidyTemplateEditComponent} from '../../../cad-view/template-data/nabil-sme-template-data/udhyamsil-karja-subsidy/udhyamsil-karja-subsidy-template-edit/udhyamsil-karja-subsidy-template-edit.component';
 import {PersonalOverdraftWithoutCollateralTemplateEditComponent} from "../../../cad-view/template-data/personal-overdraft-without-collateral-template-edit/personal-overdraft-without-collateral-template-edit.component";
+import {DdslWithoutSubsidyTemplateEditComponent} from '../../../cad-view/template-data/nabil-sme-template-data/ddsl-without-subsidy/ddsl-without-subsidy-template-edit/ddsl-without-subsidy-template-edit.component';
 
 @Component({
   selector: 'app-offer-letter-list',
@@ -281,6 +282,16 @@ export class OfferLetterListComponent implements OnInit {
                   });
                 } else if (this.docName === 'Personal overdraft without collateral') {
                   this.dialogService.open(PersonalOverdraftWithoutCollateralTemplateEditComponent, {
+                    context: {
+                      customerApprovedDoc: this.cadOfferLetterApprovedDoc,
+                      offerDocumentList: this.offerDocumentList,
+                      initialInformation: JSON.parse(offerLetter.initialInformation)
+                    },
+                    hasBackdrop: false,
+                    dialogClass: 'model-full',
+                  });
+                } else if (this.docName === 'DDSL Without Subsidy') {
+                  this.dialogService.open(DdslWithoutSubsidyTemplateEditComponent, {
                     context: {
                       customerApprovedDoc: this.cadOfferLetterApprovedDoc,
                       offerDocumentList: this.offerDocumentList,
