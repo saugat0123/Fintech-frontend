33841973a8ab646b87319ab7ddb42aa448ee2c48
Rujan Maharjan <rujnmahrzn@gmail.com>
2021-11-28 13:58:17 +0000

    Merge branch 'document-download' into 'credix_d-icfc-master'
    Download all document
    
    See merge request sunilbabu/sbsolution-frontapp!2453
    
    (cherry picked from commit 8de7ec4a3554245ba119e19d0570ef06e88cbecc)
    
    0d0a1dda Download all document

--- src/app/feature/loan/component/gamma-loan-summary/gamma-loan-summary.component.ts
+++ src/app/feature/loan/component/gamma-loan-summary/gamma-loan-summary.component.ts
@@ -677,8 +677,10 @@ export class GammaLoanSummaryComponent implements OnInit {
         docPaths.push(doc.policyDocumentPath);
       }
       const siteVisitDocument = this.siteVisitDocuments;
-      for (const doc of siteVisitDocument) {
-        docPaths.push(doc.docPath.concat(doc.docName).concat('.jpg'));
+      if (!ObjectUtil.isEmpty(siteVisitDocument)) {
+        for (const doc of siteVisitDocument) {
+          docPaths.push(doc.docPath.concat(doc.docName).concat('.jpg'));
+        }
       }
     } else {
       docPaths.push(this.loanDataHolder.zipPath);
