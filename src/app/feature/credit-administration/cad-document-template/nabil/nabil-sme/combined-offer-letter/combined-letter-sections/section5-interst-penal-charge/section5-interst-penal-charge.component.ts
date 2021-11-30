3f0192e3061fa1eb09b914663cf4917c2efcfda0
Nimesh Maharjan <maharjannimesh11@gmail.com>
2021-11-30 15:18:40 +0545

    section 4, 5

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section5-interst-penal-charge/section5-interst-penal-charge.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section5-interst-penal-charge/section5-interst-penal-charge.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-section5-interst-penal-charge',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section5-interst-penal-charge.component.scss']
 })
 export class Section5InterstPenalChargeComponent implements OnInit {
-
+  section5: FormGroup;
   constructor() { }
 
   ngOnInit() {
