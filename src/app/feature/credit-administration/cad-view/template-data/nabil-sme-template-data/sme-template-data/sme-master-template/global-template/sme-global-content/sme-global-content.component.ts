d4ff83ba56caa9db40e16f54a7961660348943fe
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2021-11-30 17:48:45 +0545

    setting up global template form design

--- src/app/feature/credit-administration/cad-view/template-data/nabil-sme-template-data/sme-template-data/sme-master-template/global-template/sme-global-content/sme-global-content.component.ts
+++ src/app/feature/credit-administration/cad-view/template-data/nabil-sme-template-data/sme-template-data/sme-master-template/global-template/sme-global-content/sme-global-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-sme-global-content',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sme-global-content.component.scss']
 })
 export class SmeGlobalContentComponent implements OnInit {
+  globalForm: FormGroup;
+  loanOptions = [
+      {value: 'New Plain Renewal'},
+      {value: 'Plain Renewal'},
+      {value: 'Renewal with Enhancement or Additional Loan'},
+      {value: 'Additional Loan'}
+  ];
+  yesNoOptions = [
+    {value: 'Yes'},
+    {value: 'No'}
+  ];
+  mortgageType = [
+    {value: 'New'},
+    {value: 'Existing'},
+    {value: 'Enhancement'}
+  ];
+  hypothecationType = [
+    {value: 'New'},
+    {value: 'Existing'}
+  ];
+  chargeType = [
+    {value: 'For All Loan'},
+    {value: 'For Specific Loan Only'},
+  ];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm(): FormGroup {
+    return this.globalForm = this.formBuilder.group({});
   }
 
 }
