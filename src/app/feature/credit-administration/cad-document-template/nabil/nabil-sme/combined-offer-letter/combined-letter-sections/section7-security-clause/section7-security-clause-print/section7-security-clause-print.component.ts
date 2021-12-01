9f811df3ab1e3892bfa7a8e2b551063f22ad9054
sushanprajapati <sushan.prajapati@sbsolutions.com>
2021-12-01 11:13:32 +0545

    section 3, 7, 10 print

---
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section7-security-clause/section7-security-clause-print/section7-security-clause-print.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section7-security-clause-print',
+  templateUrl: './section7-security-clause-print.component.html',
+  styleUrls: ['./section7-security-clause-print.component.scss']
+})
+export class Section7SecurityClausePrintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
