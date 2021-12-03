98d842ae1fcac20f7498f831469d7d6882d6d936
Ramesh Kumar Bhandari <ramesh152200@gmail.com>
2021-12-03 14:53:05 +0545

    split section2-loanType in different components

---
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section2-loan-type/term-loan-to-or-for/term-loan-to-or-for-print/term-loan-to-or-for-print.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-term-loan-to-or-for-print',
+  templateUrl: './term-loan-to-or-for-print.component.html',
+  styleUrls: ['./term-loan-to-or-for-print.component.scss']
+})
+export class TermLoanToOrForPrintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
