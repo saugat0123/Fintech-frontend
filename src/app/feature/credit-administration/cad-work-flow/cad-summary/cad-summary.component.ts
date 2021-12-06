d63c949f61a8c77cb6aa5a20c0f7493fd39e1850
Sonamxrest <xresthasonam@gmail.com>
2021-12-06 15:07:50 +0545

    Added Print In Post Approval For Micro Finance

--- src/app/feature/credit-administration/cad-work-flow/cad-summary/cad-summary.component.ts
+++ src/app/feature/credit-administration/cad-work-flow/cad-summary/cad-summary.component.ts
@@ -6,7 +6,6 @@ import {ActivatedRoute} from '@angular/router';
 import {CreditAdministrationService} from '../../service/credit-administration.service';
 import {CommonService} from '../../../../@core/service/common.service';
 import * as CryptoJS from 'crypto-js';
-import {CadDocStatus} from '../../model/CadDocStatus';
 
 @Component({
     selector: 'app-cad-summary',
@@ -18,6 +17,7 @@ export class CadSummaryComponent implements OnInit {
     customerInfoData: CustomerInfoData;
     cadDocumentId;
     spinner = false;
+    loaded = false;
     currentUserLocalStorage = LocalStorageUtil.getStorage().userId;
     toggleArray: { toggled: boolean }[] = [];
     checkListLiteVersion = LocalStorageUtil.getStorage().productUtil.CHECK_LIST_LITE_VERSION;
@@ -27,32 +27,30 @@ export class CadSummaryComponent implements OnInit {
                 public commonService: CommonService,
     ) {
     }
-    approvedLoan = false;
-    ngOnInit() {
-        console.log(this.cadOfferLetterApprovedDoc );
-        this.cadDocumentId = Number(this.decryptUrl(this.activatedRoute.snapshot.params.id));
-       this.loadData();
-        console.log(this.cadOfferLetterApprovedDoc );
-        if (this.cadOfferLetterApprovedDoc.docStatus === CadDocStatus.DISBURSEMENT_APPROVED) {
-            this.approvedLoan = true;
-        }
 
-    }
-     loadData() {
-        this.spinner = true;
-        this.service.detail(this.cadDocumentId).subscribe((res: any) => {
-            console.log('this is respionse', res.detail);
-            this.cadOfferLetterApprovedDoc = res.detail;
-            this.customerInfoData = this.cadOfferLetterApprovedDoc.loanHolder;
-            this.spinner = false;
-            this.customerInfoData = this.cadOfferLetterApprovedDoc.loanHolder;
-            this.cadOfferLetterApprovedDoc.assignedLoan.forEach(() => this.toggleArray.push({toggled: false}));
+    static loadData(other: CadSummaryComponent) {
+        other.spinner = true;
+        other.service.detail(other.cadDocumentId).subscribe((res: any) => {
+            other.cadOfferLetterApprovedDoc = res.detail;
+            other.customerInfoData = other.cadOfferLetterApprovedDoc.loanHolder;
+            other.spinner = false;
+            other.loaded = true;
+            other.customerInfoData = other.cadOfferLetterApprovedDoc.loanHolder;
+            other.cadOfferLetterApprovedDoc.assignedLoan.forEach(() => other.toggleArray.push({toggled: false}));
 
         }, error => {
             console.log(error);
-            this.spinner = false;
+            other.spinner = false;
         });
     }
+
+    ngOnInit() {
+        this.cadDocumentId = Number(this.decryptUrl(this.activatedRoute.snapshot.params.id));
+        CadSummaryComponent.loadData(this);
+
+
+    }
+
     decryptUrl(id) {
         return new CryptoJS.AES.decrypt(id, 'id').toString(CryptoJS.enc.Utf8);
     }
