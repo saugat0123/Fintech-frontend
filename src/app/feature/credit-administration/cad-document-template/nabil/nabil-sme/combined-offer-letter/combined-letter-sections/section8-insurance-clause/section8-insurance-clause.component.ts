f6df675d17460b9ae6323924663cd5d71c89a5af
Binita Simbu <grishulim67@gmail.com>
2021-12-12 09:34:58 +0545

    applied condition in section 8

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section8-insurance-clause/section8-insurance-clause.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section8-insurance-clause/section8-insurance-clause.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ObjectUtil} from '../../../../../../../../@core/utils/ObjectUtil';
+import {CustomerApprovedLoanCadDocumentation} from '../../../../../../model/customerApprovedLoanCadDocumentation';
 
 @Component({
   selector: 'app-section8-insurance-clause',
@@ -6,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section8-insurance-clause.component.scss']
 })
 export class Section8InsuranceClauseComponent implements OnInit {
+  @Input() cadOfferLetterApprovedDoc: CustomerApprovedLoanCadDocumentation;
+  tempData;
+  trustReceiptVisible;
+  form;
+  insuranceVisible = false;
   constructor() { }
 
   ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.cadOfferLetterApprovedDoc)) {
+      this.tempData = JSON.parse(this.cadOfferLetterApprovedDoc.offerDocumentList[0].initialInformation);
+    }
+    const trustReceipt = this.tempData.importLoanTrust;
+    const securities = this.tempData.securities;
+    if (securities.primarySecurity.filter(s => s.insuranceRequired === true).length > 0
+        || securities.secondarySecurity.filter(s => s.insuranceRequired === true).length > 0
+    ) {
+      this.insuranceVisible = true;
+    }
+    if (trustReceipt.selected) {
+      this.trustReceiptVisible = true;
+    }
   }
 
 }
