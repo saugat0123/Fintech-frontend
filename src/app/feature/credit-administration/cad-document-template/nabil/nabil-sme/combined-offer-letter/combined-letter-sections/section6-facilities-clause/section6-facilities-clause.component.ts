d3ab12b97a22bcae99c656d2df619825926dda4f
MiliRisal <milirisal11@gmail.com>
2021-12-13 11:38:48 +0545

    Section 6 1st condition added

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section6-facilities-clause/section6-facilities-clause.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section6-facilities-clause/section6-facilities-clause.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {ObjectUtil} from '../../../../../../../../@core/utils/ObjectUtil';
+import {LoanNameConstant} from '../../../../../../cad-view/template-data/nabil-sme-template-data/sme-costant/loan-name-constant';
+import {CustomerApprovedLoanCadDocumentation} from '../../../../../../model/customerApprovedLoanCadDocumentation';
+import {LoanDataHolder} from '../../../../../../../loan/model/loanData';
 
 @Component({
   selector: 'app-section6-facilities-clause',
@@ -8,12 +11,15 @@ import {ObjectUtil} from '../../../../../../../../@core/utils/ObjectUtil';
   styleUrls: ['./section6-facilities-clause.component.scss']
 })
 export class Section6FacilitiesClauseComponent implements OnInit {
-  @Input() cadOfferLetterApprovedDoc;
+  @Input() cadOfferLetterApprovedDoc: CustomerApprovedLoanCadDocumentation;
   @Input() freeText;
   form: FormGroup;
   tempData;
   tenureOfLoan;
   freeInformation: any;
+  loanData = [];
+  isCustomerAcceptance = false;
+  isIrrevocableLetter = false;
   constructor(
       private formBuilder: FormBuilder,
   ) { }
@@ -24,6 +30,8 @@ export class Section6FacilitiesClauseComponent implements OnInit {
       this.freeInformation = JSON.parse(this.cadOfferLetterApprovedDoc.offerDocumentList[0].supportedInformation);
     }
     this.fillForm();
+    this.getLoanName();
+    this.checkLoanName();
   }
   buildForm() {
     this.form = this.formBuilder.group({
@@ -33,6 +41,23 @@ export class Section6FacilitiesClauseComponent implements OnInit {
   fillForm() {
     this.form.patchValue({
       tenureOfLoan: this.freeInformation ? this.freeInformation.section6.tenureOfLoan : ''
+    });
+  }
+  private checkLoanName(): void {
+    if (this.loanData.length > 0) {
+      this.loanData.forEach(v => {
+        if (v === LoanNameConstant.IRREVOCABLE_LETTER_OF_CREDIT_FACILITY) {
+          this.isIrrevocableLetter = true;
+        } if (v === LoanNameConstant.CUSTOMER_ACCEPTANCE_FOR_TIME_LETTER_OF_CREDIT) {
+          this.isCustomerAcceptance = true;
+        }
+      });
+    }
+  }
+  getLoanName() {
+    this.cadOfferLetterApprovedDoc.assignedLoan.forEach(val => {
+      const loanName = val.loan.name;
+      this.loanData.push(loanName);
     });
   }
 
