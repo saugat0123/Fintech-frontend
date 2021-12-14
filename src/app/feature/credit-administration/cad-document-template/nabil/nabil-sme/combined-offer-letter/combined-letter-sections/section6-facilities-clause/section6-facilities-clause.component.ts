95913adc7f212c2aa88d87b4ad833b0db5c291a0
MiliRisal <milirisal11@gmail.com>
2021-12-14 16:57:14 +0545

    section 6 facilities condition completed

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section6-facilities-clause/section6-facilities-clause.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section6-facilities-clause/section6-facilities-clause.component.ts
@@ -20,6 +20,14 @@ export class Section6FacilitiesClauseComponent implements OnInit {
   loanData = [];
   isCustomerAcceptance = false;
   isIrrevocableLetter = false;
+  isAutoLoan = false;
+  isTermLoan = false;
+  isExcept = false;
+  isBankGuarantee = false;
+  autoLoanOrTermLoan = false;
+  isTermLoanForVehicle;
+  isTermOrAuto = false;
+  isOneOffFacility = false;
   constructor(
       private formBuilder: FormBuilder,
   ) { }
@@ -28,10 +36,12 @@ export class Section6FacilitiesClauseComponent implements OnInit {
     this.buildForm();
     if (!ObjectUtil.isEmpty(this.cadOfferLetterApprovedDoc)) {
       this.freeInformation = JSON.parse(this.cadOfferLetterApprovedDoc.offerDocumentList[0].supportedInformation);
+      this.tempData = JSON.parse(this.cadOfferLetterApprovedDoc.offerDocumentList[0].initialInformation);
     }
     this.fillForm();
     this.getLoanName();
     this.checkLoanName();
+    this.isApplicable();
   }
   buildForm() {
     this.form = this.formBuilder.group({
@@ -44,12 +54,25 @@ export class Section6FacilitiesClauseComponent implements OnInit {
     });
   }
   private checkLoanName(): void {
+    const tempD = this.tempData;
+    if (!ObjectUtil.isEmpty(tempD)) {
+      this.isOneOffFacility = this.checkOneOffFacility(tempD);
+    }
     if (this.loanData.length > 0) {
       this.loanData.forEach(v => {
         if (v === LoanNameConstant.IRREVOCABLE_LETTER_OF_CREDIT_FACILITY) {
           this.isIrrevocableLetter = true;
         } if (v === LoanNameConstant.CUSTOMER_ACCEPTANCE_FOR_TIME_LETTER_OF_CREDIT) {
           this.isCustomerAcceptance = true;
+        } if (v === LoanNameConstant.AUTO_LOAN || v === LoanNameConstant.TERM_LOAN_TO_FOR_PURCHASE_OF_VEHICLE) {
+          this.isTermOrAuto = true;
+        }
+        if (v === LoanNameConstant.AUTO_LOAN) {
+          this.isAutoLoan = true;
+        } if (v === LoanNameConstant.TERM_LOAN_TO_FOR_PURCHASE_OF_VEHICLE && !ObjectUtil.isEmpty(this.tempData.termLoanForm)) {
+          this.isTermLoanForVehicle = this.tempData.termLoanForm.termLoanFor;
+        } if (v === LoanNameConstant.BANK_GUARANTEE) {
+          this.isBankGuarantee = true;
         }
       });
     }
@@ -59,6 +82,38 @@ export class Section6FacilitiesClauseComponent implements OnInit {
       const loanName = val.loan.name;
       this.loanData.push(loanName);
     });
+  }
+
+  checkOneOffFacility(tempD) {
+    if ((!ObjectUtil.isEmpty(tempD.letterOfCreditForm) && tempD.letterOfCreditForm.loanOption === 'ONE_OFF_BASIS') ||
+        (!ObjectUtil.isEmpty(tempD.timeLetterCreditForm) && tempD.timeLetterCreditForm.loanOption === 'ONE_OFF_BASIS') ||
+        (!ObjectUtil.isEmpty(tempD.importLoanTrust) && tempD.importLoanTrust.loanOption === 'ONE_OFF_BASIS') ||
+        (!ObjectUtil.isEmpty(tempD.revolvingShortTermLoan) && tempD.revolvingShortTermLoan.loanOption === 'ONE_OFF_BASIS')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isApplicable() {
+    const temp = this.tempData;
+    if ((!ObjectUtil.isEmpty(temp.letterOfCreditForm) && temp.letterOfCreditForm.loanOption !== 'ONE_OFF_BASIS'
+        || !ObjectUtil.isEmpty(temp.timeLetterCreditForm) && temp.timeLetterCreditForm !== 'ONE_OFF_BASIS'
+        || !ObjectUtil.isEmpty(temp.importBillsDiscountForm) && temp.importBillsDiscountForm !== 'ONE_OFF_BASIS'
+        || !ObjectUtil.isEmpty(temp.importLoanTrust) && temp.importLoanTrust !== 'ONE_OFF_BASIS'
+        || !ObjectUtil.isEmpty(temp.revolvingShortTermLoan) && temp.revolvingShortTermLoan !== 'ONE_OFF_BASIS'
+        || !ObjectUtil.isEmpty(temp.demandLoanForm) && temp.demandLoanForm
+        || !ObjectUtil.isEmpty(temp.preExportForm) && temp.preExportForm
+        || !ObjectUtil.isEmpty(temp.documentaryBillPurchase) && temp.documentaryBillPurchase
+        || !ObjectUtil.isEmpty(temp.overdraftLoanForm) && temp.overdraftLoanForm
+        || !ObjectUtil.isEmpty(temp.equityMortgaged) && temp.equityMortgaged
+        || !ObjectUtil.isEmpty(temp.overdraftFixedForm) && temp.overdraftFixedForm
+        || !ObjectUtil.isEmpty(temp.overDraftFacilityForm) && temp.overDraftFacilityForm
+        || !ObjectUtil.isEmpty(temp.bridgeGapLoan) && temp.bridgeGapLoan
+        || !ObjectUtil.isEmpty(temp.mortgageEquityTermForm) && temp.mortgageEquityTermForm
+        || !ObjectUtil.isEmpty(temp.bankGuarantee) && temp.bankGuarantee
+        || !ObjectUtil.isEmpty(temp.billPurchaseForm) && temp.billPurchaseForm)) {
+      this.isExcept = true;
+    }
   }
 
 }
