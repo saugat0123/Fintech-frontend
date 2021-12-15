c03d8126c8553fbee6b512f661ee802d644d3aae
Ramesh Kumar Bhandari <ramesh152200@gmail.com>
2021-12-15 21:58:54 +0545

    applied conditions and mapping in section2-loan-type's print components

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section2-loan-type/mortgage-equity-term-loan/mortgage-equity-term-loan-print/mortgage-equity-term-loan-print.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/combined-offer-letter/combined-letter-sections/section2-loan-type/mortgage-equity-term-loan/mortgage-equity-term-loan-print/mortgage-equity-term-loan-print.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {NepaliCurrencyWordPipe} from '../../../../../../../../../../@core/pipe/nepali-currency-word.pipe';
+import {ObjectUtil} from '../../../../../../../../../../@core/utils/ObjectUtil';
+import {CurrencyFormatterPipe} from '../../../../../../../../../../@core/pipe/currency-formatter.pipe';
+import {EngToNepaliNumberPipe} from '../../../../../../../../../../@core/pipe/eng-to-nepali-number.pipe';
 
 @Component({
   selector: 'app-mortgage-equity-term-loan-print',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mortgage-equity-term-loan-print.component.scss']
 })
 export class MortgageEquityTermLoanPrintComponent implements OnInit {
+  @Input() letterData;
+  @Input() customerApprovedDoc;
+  @Input() freeText;
+  tempData;
+  loanAmount;
+  loanAmountInWord;
+  mortgageEquity: any = {};
+  termLoanForMortgageEquityTerm; mortgageTypeMortgageEquityTerm; complementaryOtherMortgageEquityTerm = false;
+  emiPaymentTypeMortgageEquityTerm; interestSubAgMortgageEquityTerm; paymentTermMortgageEquityTerm;
+  loanOptionMortgageEquityTerm; drawingPowerMortgageEquityTerm; termLoanTypeMortgageEquityTerm;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private engToNepWord: NepaliCurrencyWordPipe,
+              private currencyFormatPipe: CurrencyFormatterPipe,
+              private engToNepNumberPipe: EngToNepaliNumberPipe,
+  ) { }
 
   ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.customerApprovedDoc)) {
+      this.tempData = JSON.parse(this.customerApprovedDoc.offerDocumentList[0].initialInformation);
+      const totalLoanAmount = this.customerApprovedDoc.assignedLoan[0].proposal.proposedLimit;
+      this.loanAmount = this.engToNepNumberPipe.transform(this.currencyFormatPipe.transform(totalLoanAmount));
+      this.loanAmountInWord = this.engToNepWord.transform(totalLoanAmount);
+    }
+    if (!ObjectUtil.isEmpty(this.tempData.mortgageEquityTermForm)) {
+      this.termLoanForMortgageEquityTerm = this.tempData.mortgageEquityTermForm.termLoanFor;
+      this.mortgageTypeMortgageEquityTerm = this.tempData.mortgageEquityTermForm.mortgageType;
+      this.termLoanTypeMortgageEquityTerm = this.tempData.mortgageEquityTermForm.termLoanType;
+      this.emiPaymentTypeMortgageEquityTerm = this.tempData.mortgageEquityTermForm.emiPaymentType;
+      this.interestSubAgMortgageEquityTerm = this.tempData.mortgageEquityTermForm.subsidyOrAgricultureLoan;
+      this.paymentTermMortgageEquityTerm = this.tempData.mortgageEquityTermForm.paymentTerms;
+      this.drawingPowerMortgageEquityTerm = this.tempData.mortgageEquityTermForm.drawingPowerBasis;
+      this.loanOptionMortgageEquityTerm = this.tempData.smeGlobalForm.loanOption;
+      if (this.tempData.mortgageEquityTermForm.complementaryOther === true) {
+        this.complementaryOtherMortgageEquityTerm = true;
+      }
+    }
   }
 
 }
