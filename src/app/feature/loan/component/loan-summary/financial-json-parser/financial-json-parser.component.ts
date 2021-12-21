3c5deffb69f3267c484ca6b710cbe1220f9a6d0b
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2021-12-21 15:32:11 +0545

    Parse schemal and columns

--- src/app/feature/loan/component/loan-summary/financial-json-parser/financial-json-parser.component.ts
+++ src/app/feature/loan/component/loan-summary/financial-json-parser/financial-json-parser.component.ts
@@ -7,6 +7,52 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FinancialJsonParserComponent implements OnInit {
 
+    financialJson = {
+        'schema': [
+            'ASSETS',
+            'Fixed Assets',
+            ' Land \u0026 Building',
+            ' Plant \u0026 Machinery',
+            ' Others'
+        ],
+        'column': [
+            'FY 2011/12 Audited',
+            'FY 2012/13 Audited',
+            'FY 2013/14 Audited',
+            'FY 2014/15 Audited'
+        ],
+        'data': [
+            [
+                12.0,
+                0.0,
+                14.0,
+                0.0
+            ],
+            [
+                89.0,
+                0.0,
+                90.0,
+                0.0
+            ],
+            [
+                45.0,
+                0.0,
+                0.0,
+                12.0
+            ], [
+                0.0,
+                0.0,
+                0.0,
+                0.0
+            ], [
+                0.0,
+                0.0,
+                156.0,
+                0.0
+            ]
+        ]
+    };
+
     ngOnInit() {
     }
 
