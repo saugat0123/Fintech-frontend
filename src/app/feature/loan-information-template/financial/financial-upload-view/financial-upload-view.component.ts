e06b00cabf1e5be5127d4a947f4d60921d1e1064
Sonamxrest <xresthasonam@gmail.com>
2021-12-28 10:48:08 +0545

    Small Bug Fixes

--- src/app/feature/loan-information-template/financial/financial-upload-view/financial-upload-view.component.ts
+++ src/app/feature/loan-information-template/financial/financial-upload-view/financial-upload-view.component.ts
@@ -1,8 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {CustomerInfoService} from '../../../customer/service/customer-info.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {CustomerInfoData} from '../../../loan/model/customerInfoData';
+import {ToastService} from '../../../../@core/utils';
+import {Alert, AlertType} from '../../../../@theme/model/Alert';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-financial-upload-view',
@@ -13,25 +16,23 @@ export class FinancialUploadViewComponent implements OnInit {
 
     @Input() customerInfo: CustomerInfoData;
     @Input() customerInfoId;
+    @Output() customer = new EventEmitter();
     form: FormGroup;
     uploadFile;
     fg = new FormData();
     keysbyindex;
     financialArray = [];
+    spinner = false;
+    financialData;
 
     constructor(
         private activeRoutes: ActivatedRoute,
         private customerInfoService: CustomerInfoService,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private toast: ToastService,
+        private modalService: NgbModal
     ) {
     }
-
-    financialData;
-    balanceSheet;
-    executiveSummary;
-    plAccount;
-    commonSizePl;
-    detailCf;
 
     ngOnInit() {
         this.buildForm();
@@ -40,11 +41,6 @@ export class FinancialUploadViewComponent implements OnInit {
         this.keysbyindex.forEach((title) => {
             this.financialArray.push(JSON.parse(this.financialData[title]));
         });
-        // this.balanceSheet = JSON.parse(this.financialData.balanceSheetData);
-        // this.executiveSummary = JSON.parse(this.financialData.executiveSummaryData);
-        // this.plAccount = JSON.parse(this.financialData.plAccountData);
-        // this.commonSizePl = JSON.parse(this.financialData.commonSizePlData);
-        // this.detailCf = JSON.parse(this.financialData.cashFlowData);
     }
 
     buildForm() {
@@ -55,18 +51,22 @@ export class FinancialUploadViewComponent implements OnInit {
 
     upload(event) {
         this.uploadFile = event.target.files[0];
-        console.log('this is the file', event.target.files[0]);
         this.fg.append('file', this.uploadFile);
         this.fg.append('customerId', this.customerInfoId);
     }
 
     submitData() {
-        this.customerInfoService.uploadFinancialExcel(this.fg).subscribe(res => {
-            console.log('this is response', res);
+        this.spinner = true;
+        this.customerInfoService.uploadFinancialExcel(this.fg).subscribe((res: any) => {
+            this.customerInfo = res.detail;
+            this.toast.show(new Alert(AlertType.SUCCESS, 'Saved Financial Data'));
+            this.spinner = false;
+            this.customer.emit(res.detail);
+            this.modalService.dismissAll();
+        }, err => {
+            this.spinner = false;
+            this.toast.show(new Alert(AlertType.ERROR, 'Error While Saving Financial Data'));
         });
     }
 
-    onChangeTab(event) {
-        console.log('this is event', event);
-    }
 }
