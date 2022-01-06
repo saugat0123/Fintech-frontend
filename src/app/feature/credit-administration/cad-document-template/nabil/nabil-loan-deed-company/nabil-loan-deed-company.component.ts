0d8160ee039dc9ef11dca7b937e2c784645bc7b0
MiliRisal <milirisal11@gmail.com>
2022-01-06 11:07:51 +0545

    Loan deed company patch value in progress

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-loan-deed-company/nabil-loan-deed-company.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-loan-deed-company/nabil-loan-deed-company.component.ts
@@ -45,27 +45,57 @@ export class NabilLoanDeedCompanyComponent implements OnInit {
       const data = JSON.parse(cadData.loanHolder.nepData);
       console.log(data);
       this.form.get('nameOfBranch').patchValue(data.branch.ct);
+      this.form.get('actName').patchValue(data.actName.ct);
+      this.form.get('actYearInFigure').patchValue(data.actYear.ct);
+      this.form.get('authorizedBody').patchValue(data.authorizedBodyName.ct);
+      this.form.get('headSectionDepartment').patchValue(data.registeredWith.ct);
+      this.form.get('registrationDate').patchValue(data.registrationDate.np);
+      this.form.get('registrationNo').patchValue(data.registrationNo.ct);
     }
   }
 
   private buildForm(): FormGroup {
     return this.form = this.formBuilder.group({
       nameOfBranch: [undefined],
+      actName: [undefined],
       actYearInFigure: [undefined],
+      authorizedBody: [undefined],
       headSectionDepartment: [undefined],
       registrationDate: [undefined],
       registrationNo: [undefined],
       companyName: [undefined],
       nameOfAuthorizePerson: [undefined],
+      nameOfGrandSon: [undefined],
+      nameOfGrandDaughter: [undefined],
+      nameOfDaughterInLaw: [undefined],
+      nameOfSon: [undefined],
+      nameOfDaughter: [undefined],
+      nameOfWife: [undefined],
+      districtOfAuthPerson: [undefined],
+      vdcMunicipalityOfAuth: [undefined],
+      municipality: [undefined],
+      VDC: [undefined],
+      gaupalika: [undefined],
+      wardNo: [undefined],
+      ageOfAuthPerson: [undefined],
+      nameOfAuthPerson: [undefined],
+      purposeOfLoan: [undefined],
       sanctionLetterIssueDate: [undefined],
       givenAuthorityPerson: [undefined],
       totalLoanAmountInFigure: [undefined],
       totalLoanAmountInWord: [undefined],
+      districtOfWitness: [undefined],
+      municipalityVDC: [undefined],
+      wardNoOfWitness: [undefined],
+      ageOfWitness: [undefined],
+      nameOfWitness: [undefined],
+      nameOfBank: [undefined],
       yearInFigure: [undefined],
       year: [undefined],
       month: [undefined],
       days: [undefined],
       freeText: [undefined],
+      collateral: [undefined],
     });
   }
 
