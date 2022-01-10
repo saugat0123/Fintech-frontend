ec646eb99b76b3d3f4384fe956e77132784f7835
sushanprajapati <sushan.prajapati@sbsolutions.com>
2022-01-10 15:17:13 +0545

    letter of hypothecation component creation

---
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/legal-documents/letter-of-hypothecation-proprietorship/letter-of-hypothecation-proprietorship.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-letter-of-hypothecation-proprietorship',
+  templateUrl: './letter-of-hypothecation-proprietorship.component.html',
+  styleUrls: ['./letter-of-hypothecation-proprietorship.component.scss']
+})
+export class LetterOfHypothecationProprietorshipComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
