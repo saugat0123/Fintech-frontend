49f64777891aedc88cc2e98d16c3bced49811bf6
Nimesh Maharjan <maharjannimesh11@gmail.com>
2022-01-12 14:36:53 +0545

    form and add text area

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/legal-documents/supplementary-aggrement-proprietorship/supplementary-aggrement-proprietorship.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/legal-documents/supplementary-aggrement-proprietorship/supplementary-aggrement-proprietorship.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { NabilDocumentChecklist } from '../../../../../../admin/modal/nabil-document-checklist.enum';
 import {CustomerApprovedLoanCadDocumentation} from "../../../../../model/customerApprovedLoanCadDocumentation";
 
 @Component({
@@ -7,13 +9,47 @@ import {CustomerApprovedLoanCadDocumentation} from "../../../../../model/custome
   styleUrls: ['./supplementary-aggrement-proprietorship.component.scss']
 })
 export class SupplementaryAggrementProprietorshipComponent implements OnInit {
+  supplementaryAgreementProprietorship: FormGroup;
   @Input() cadData: CustomerApprovedLoanCadDocumentation;
   @Input() documentId: number;
   @Input() customerLoanId: number;
-
-  constructor() { }
+  offerLetterConst = NabilDocumentChecklist;
+  freeTextVal
+  constructor(private formBuilder: FormBuilder,) { }
 
   ngOnInit() {
+    this.buildForm();
+    console.log('cadData', this.cadData);
+    
   }
-
+  buildForm() {
+    this.supplementaryAgreementProprietorship = this.formBuilder.group({
+      date: [undefined],
+      bankAddress: [undefined],
+      firmName: [undefined],
+      dateOfHypothecation: [undefined],
+      sanctionLetterIssuedDate: [undefined],
+      loanAmountInFigure: [undefined],
+      loanAmountInWords: [undefined],
+      dateOfTrustReceipt: [undefined],
+      letterOfCreditNo: [undefined],
+      letterOfCreditIssuedDate: [undefined],
+      totalLoanAmountInFigure: [undefined],
+      witnessDistrict: [undefined],
+      witnessMunicipality: [undefined],
+      witnessWardNumber: [undefined],
+      witnessAge: [undefined],
+      witnessName: [undefined],
+      textAreas: this.formBuilder.array([]),
+    });
+  }
+  addTextArea() {
+    (this.supplementaryAgreementProprietorship.get('textAreas') as FormArray).push(
+        this.formBuilder.group({
+          additionalGuarantorDetails: [undefined]
+    }));
+  }
+  removeAtIndex(i: number) {
+    (this.supplementaryAgreementProprietorship.get('textAreas') as FormArray).removeAt(i);
+  }
 }
