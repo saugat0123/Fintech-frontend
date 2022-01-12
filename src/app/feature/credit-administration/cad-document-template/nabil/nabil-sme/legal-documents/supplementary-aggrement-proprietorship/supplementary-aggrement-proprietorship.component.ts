643d1a4c40880778999ddb42f8820b099894151c
Nimesh Maharjan <maharjannimesh11@gmail.com>
2022-01-12 15:38:11 +0545

    form and add text area

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/legal-documents/supplementary-aggrement-proprietorship/supplementary-aggrement-proprietorship.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/legal-documents/supplementary-aggrement-proprietorship/supplementary-aggrement-proprietorship.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { NabilDocumentChecklist } from '../../../../../../admin/modal/nabil-document-checklist.enum';
-import {CustomerApprovedLoanCadDocumentation} from "../../../../../model/customerApprovedLoanCadDocumentation";
+import {CustomerApprovedLoanCadDocumentation} from '../../../../../model/customerApprovedLoanCadDocumentation';
+import {ObjectUtil} from '../../../../../../../@core/utils/ObjectUtil';
 
 @Component({
   selector: 'app-supplementary-aggrement-proprietorship',
@@ -14,13 +15,25 @@ export class SupplementaryAggrementProprietorshipComponent implements OnInit {
   @Input() documentId: number;
   @Input() customerLoanId: number;
   offerLetterConst = NabilDocumentChecklist;
-  freeTextVal
-  constructor(private formBuilder: FormBuilder,) { }
+  freeTextVal;
+  initialInfoPrint;
+  isInstitutional = false;
+  nepData;
+  constructor(private formBuilder: FormBuilder,
+              ) { }
 
   ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      if (this.cadData.loanHolder.customerType === 'INSTITUTION') {
+        this.isInstitutional = true;
+      }
+    }
+    if (!ObjectUtil.isEmpty(this.cadData.loanHolder.nepData)) {
+      this.nepData = JSON.parse(this.cadData.loanHolder.nepData);
+    }
     this.buildForm();
+    this.fillForm();
     console.log('cadData', this.cadData);
-    
   }
   buildForm() {
     this.supplementaryAgreementProprietorship = this.formBuilder.group({
@@ -41,6 +54,25 @@ export class SupplementaryAggrementProprietorshipComponent implements OnInit {
       witnessAge: [undefined],
       witnessName: [undefined],
       textAreas: this.formBuilder.array([]),
+    });
+  }
+  fillForm() {
+    if (!ObjectUtil.isEmpty(this.cadData.offerDocumentList[0].supportedInformation)) {
+      this.freeTextVal = JSON.parse(this.cadData.offerDocumentList[0].supportedInformation);
+      // tslint:disable-next-line:max-line-length
+      if (!ObjectUtil.isEmpty(this.freeTextVal.section10) && !ObjectUtil.isEmpty(this.freeTextVal.section10[0].additionalGuarantorDetails)) {
+        for (let val = 0; val < this.freeTextVal.section10.length - 1; val++) {
+          this.addTextArea();
+        }
+        for (let val = 0; val < this.freeTextVal.section10.length; val++) {
+          // tslint:disable-next-line:max-line-length
+          this.supplementaryAgreementProprietorship.get(['textAreas', val, 'additionalGuarantorDetails']).patchValue(this.freeTextVal.section10[val].additionalGuarantorDetails);
+        }
+      }
+    }
+    this.supplementaryAgreementProprietorship.patchValue({
+      firmName: this.cadData.assignedLoan[0].companyInfo.companyName ? this.cadData.assignedLoan[0].companyInfo.companyName : '',
+      bankAddress: this.nepData.branch.ct ? this.nepData.branch.ct : '',
     });
   }
   addTextArea() {
