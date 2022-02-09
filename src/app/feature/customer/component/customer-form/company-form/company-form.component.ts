98b9a35dd176690cfe388139e73828c0f26af06b
Nimesh Maharjan <maharjannimesh11@gmail.com>
2022-02-09 14:45:03 +0545

    removal of conditional BS date

--- src/app/feature/customer/component/customer-form/company-form/company-form.component.ts
+++ src/app/feature/customer/component/customer-form/company-form/company-form.component.ts
@@ -307,11 +307,11 @@ export class CompanyFormComponent implements OnInit {
                 [(ObjectUtil.isEmpty(this.companyInfo)
                     || ObjectUtil.isEmpty(this.companyInfo.registrationNumber)) ? undefined :
                     this.companyInfo.registrationNumber, [Validators.required]],
-            companyPAN:
-                [(ObjectUtil.isEmpty(this.companyInfo)
-                    || ObjectUtil.isEmpty(this.companyInfo.panNumber)) ? undefined :
-                    this.companyInfo.panNumber, [Validators.required, WhiteSpaceValidation.cannotContainSpace,
-                    Validators.maxLength(9), Validators.minLength(9)]],
+                    companyPAN:
+                        [(ObjectUtil.isEmpty(this.companyInfo)
+                            || ObjectUtil.isEmpty(this.companyInfo.panNumber)) ? undefined :
+                            this.companyInfo.panNumber, [Validators.required, WhiteSpaceValidation.cannotContainSpace,
+                            Validators.maxLength(9), Validators.minLength(9)]],
             companyEstablishmentDate:
                 [(ObjectUtil.isEmpty(this.companyInfo)
                     || ObjectUtil.isEmpty(this.companyInfo.establishmentDate)) ? undefined :
@@ -1111,8 +1111,12 @@ export class CompanyFormComponent implements OnInit {
 
     checkPanNumberNumber(regNumber: String) {
         this.companyInfoService.getCompanyInfoWithPanNumber(regNumber).subscribe((res) => {
-            if (regNumber.toLowerCase() === res.detail.panNumber.toLowerCase()) {
-                this.toastService.show(new Alert(AlertType.WARNING, 'This customer already exists. Please input a unique value or choose the customer from catalogue section'));
+            console.log(res.detail);
+            
+            if (!ObjectUtil.isEmpty(res.detail)) {
+                if (regNumber.toLowerCase() === res.detail.panNumber.toLowerCase()) {
+                    this.toastService.show(new Alert(AlertType.WARNING, 'This customer already exists. Please input a unique value or choose the customer from catalogue section'));
+                }
             }
         }, error => {
             console.error(error);
