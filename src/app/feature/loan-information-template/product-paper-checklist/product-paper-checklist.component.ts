fbdaad29530d64276d9bb00339833488af070b80
Sonamxrest <xresthasonam@gmail.com>
2022-02-21 17:41:27 +0545

    Yes To All

--- src/app/feature/loan-information-template/product-paper-checklist/product-paper-checklist.component.ts
+++ src/app/feature/loan-information-template/product-paper-checklist/product-paper-checklist.component.ts
@@ -10,12 +10,11 @@ import {LoanConfig} from '../../admin/modal/loan-config';
   templateUrl: './product-paper-checklist.component.html',
   styleUrls: ['./product-paper-checklist.component.scss']
 })
-export class ProductPaperChecklistComponent implements OnInit{
+export class ProductPaperChecklistComponent implements OnInit {
 
-  constructor(
-      private formBuilder: FormBuilder,
-      private loanService: LoanConfigService
-  ) { }
+  constructor(private formBuilder: FormBuilder) {
+  }
+
   @Input() loan: LoanConfig;
   @Input() fromLoan;
   @Input() loanDataHolder: LoanDataHolder;
@@ -23,13 +22,17 @@ export class ProductPaperChecklistComponent implements OnInit{
   paperForm: FormGroup;
   yesNo = ['Yes', 'No', 'Na'];
   formData;
+
   ngOnInit() {
-    this.buildForm();
     if (!ObjectUtil.isEmpty(this.loanDataHolder.paperProductChecklist)) {
       this.formData = JSON.parse(this.loanDataHolder.paperProductChecklist);
+    }
+    if (this.fromLoan) {
+      this.buildForm();
       this.paperForm.patchValue(this.formData);
     }
   }
+
   buildForm() {
     this.paperForm = this.formBuilder.group({
       ciclDate: [undefined],
@@ -64,8 +67,113 @@ export class ProductPaperChecklistComponent implements OnInit{
       swapFee: [undefined],
     });
   }
+
   save() {
     this.checkList.emit(this.paperForm.value);
+  }
+
+  yesToAll() {
+    this.paperForm.patchValue({
+      ciclDate: 'Yes',
+      internal: 'Yes',
+      negative: 'Yes',
+      reference: 'Yes',
+      statements: 'Yes',
+      lalPurja: 'Yes',
+      bluePrint: 'Yes',
+      boundary: 'Yes',
+      bcc: 'Yes',
+      bca: 'Yes',
+      fac: 'Yes',
+      salaryCertificate: 'Yes',
+      pension: 'Yes',
+      rental: 'Yes',
+      professional: 'Yes',
+      businessIncome: 'Yes',
+      agricultureIncome: 'Yes',
+      loanAmount: 'Yes',
+      creditChain: 'Yes',
+      loanTenure: 'Yes',
+      loanAmountMax: 'Yes',
+      ltv: 'Yes',
+      groupExposure: 'Yes',
+      ageOfBorrower: 'Yes',
+      ageOfCoBorrower: 'Yes',
+      dbr: 'Yes',
+      interestRate: 'Yes',
+      loanFee: 'Yes',
+      commitmentFee: 'Yes',
+      swapFee: 'Yes',
+    });
+  }
+
+  noToAll() {
+    this.paperForm.patchValue({
+      ciclDate: 'No',
+      internal: 'No',
+      negative: 'No',
+      reference: 'No',
+      statements: 'No',
+      lalPurja: 'No',
+      bluePrint: 'No',
+      boundary: 'No',
+      bcc: 'No',
+      bca: 'No',
+      fac: 'No',
+      salaryCertificate: 'No',
+      pension: 'No',
+      rental: 'No',
+      professional: 'No',
+      businessIncome: 'No',
+      agricultureIncome: 'No',
+      loanAmount: 'No',
+      creditChain: 'No',
+      loanTenure: 'No',
+      loanAmountMax: 'No',
+      ltv: 'No',
+      groupExposure: 'No',
+      ageOfBorrower: 'No',
+      ageOfCoBorrower: 'No',
+      dbr: 'No',
+      interestRate: 'No',
+      loanFee: 'No',
+      commitmentFee: 'No',
+      swapFee: 'No',
+    });
+  }
+  naToAll() {
+    this.paperForm.patchValue({
+      ciclDate: 'Na',
+      internal: 'Na',
+      negative: 'Na',
+      reference: 'Na',
+      statements: 'Na',
+      lalPurja: 'Na',
+      bluePrint: 'Na',
+      boundary: 'Na',
+      bcc: 'Na',
+      bca: 'Na',
+      fac: 'Na',
+      salaryCertificate: 'Na',
+      pension: 'Na',
+      rental: 'Na',
+      professional: 'Na',
+      businessIncome: 'Na',
+      agricultureIncome: 'Na',
+      loanAmount: 'Na',
+      creditChain: 'Na',
+      loanTenure: 'Na',
+      loanAmountMax: 'Na',
+      ltv: 'Na',
+      groupExposure: 'Na',
+      ageOfBorrower: 'Na',
+      ageOfCoBorrower: 'Na',
+      dbr: 'Na',
+      interestRate: 'Na',
+      loanFee: 'Na',
+      commitmentFee: 'Na',
+      swapFee: 'Na',
+    });
   }
 }
 
