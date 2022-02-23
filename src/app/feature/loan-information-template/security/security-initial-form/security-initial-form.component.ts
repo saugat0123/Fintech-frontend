15780ea912358ded45b7e4dbacc58e43d5b2aec2
Binita0 <binita.ghimire@sbsloutionsnepal.com>
2022-02-23 17:16:18 +0545

    fdr issue solved and made share security editable

--- src/app/feature/loan-information-template/security/security-initial-form/security-initial-form.component.ts
+++ src/app/feature/loan-information-template/security/security-initial-form/security-initial-form.component.ts
@@ -180,16 +180,16 @@ export class SecurityInitialFormComponent implements OnInit {
         this.buildForm();
         this.branchList();
         this.checkLoanTags();
-        // this.nepsePriceInfoService.getActiveNepsePriceInfoData().subscribe((response) => {
-        //     this.nepsePriceInfo = response.detail;
-        //     console.log('response.detail', response.detail);
-        //     this.shareSecurityForm.get('sharePriceDate').patchValue(this.nepsePriceInfo && this.nepsePriceInfo.sharePriceDate ?
-        //         this.datePipe.transform(this.nepsePriceInfo.sharePriceDate, 'yyyy-MM-dd') : undefined);
-        //     this.shareSecurityForm.get('avgDaysForPrice').patchValue(this.nepsePriceInfo && this.nepsePriceInfo.avgDaysForPrice
-        //         ? this.nepsePriceInfo.avgDaysForPrice : undefined);
-        // }, error => {
-        //     console.error(error);
-        // });
+        this.nepsePriceInfoService.getActiveNepsePriceInfoData().subscribe((response) => {
+            this.nepsePriceInfo = response.detail;
+            console.log('response.detail', response.detail);
+            this.shareSecurityForm.get('sharePriceDate').patchValue(this.nepsePriceInfo && this.nepsePriceInfo.sharePriceDate ?
+                this.datePipe.transform(this.nepsePriceInfo.sharePriceDate, 'yyyy-MM-dd') : undefined);
+            this.shareSecurityForm.get('avgDaysForPrice').patchValue(this.nepsePriceInfo && this.nepsePriceInfo.avgDaysForPrice
+                ? this.nepsePriceInfo.avgDaysForPrice : undefined);
+        }, error => {
+            console.error(error);
+        });
         this.pushNewSecurityType();
         if (this.formData !== undefined) {
             this.formDataForEdit = this.formData['initialForm'];
