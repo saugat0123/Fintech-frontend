44182a9711c76ba98298bcd19adab72bef049612
Sonamxrest <xresthasonam@gmail.com>
2022-03-04 12:54:46 +0545

    Guarantor View New Cchanges

--- src/app/feature/loan-information-view/guarantor-view/guarantor-view.component.ts
+++ src/app/feature/loan-information-view/guarantor-view/guarantor-view.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Guarantor} from '../../loan/model/guarantor';
 import {Occupation} from '../../admin/modal/occupation';
+import {ObjectUtil} from '../../../@core/utils/ObjectUtil';
 
 @Component({
   selector: 'app-guarantor-view',
@@ -11,9 +11,30 @@ export class GuarantorViewComponent implements OnInit {
   @Input() guarantorData;
   @Input() individual;
   Occupation = Occupation;
+  newGuarantor = [];
+  cgid;
   constructor() { }
 
   ngOnInit() {
+    console.log('guarantor', this.guarantorData);
+    this.guarantorData.forEach((g, i) => {
+      const innerGuarantor = [];
+      if(this.cgid !== g.id) {
+        innerGuarantor.push(g);
+      }
+      if (!ObjectUtil.isEmpty(this.guarantorData[i + 1])) {
+        this.cgid = this.guarantorData[i + 1].id;
+        innerGuarantor.push(this.guarantorData[i + 1]);
+      }
+      if(innerGuarantor.length > 0) {
+        this.newGuarantor.push(innerGuarantor);
+      }
+    });
+    console.log(this.newGuarantor);
   }
-
+  calculateAge(dob) {
+    const difference = Math.abs(Date.now() - new Date(dob).getTime());
+    const age = Math.floor((difference / (1000 * 3600 * 24)) / 365);
+    return age;
+  }
 }
