391063b1b2aab400950c6f98ea2174da1ef9ea49
Sonamxrest <xresthasonam@gmail.com>
2022-03-04 16:54:33 +0545

    Added New Common Component For Joint Customer View

--- src/app/feature/loan/component/loan-summary/loan-summary.module.ts
+++ src/app/feature/loan/component/loan-summary/loan-summary.module.ts
@@ -44,7 +44,6 @@ import {NetWorthModule} from '../../../newt-worth/net-worth-module';
 import {DbrModule} from '../../../dbr/dbr.module';
 import { CollateralSiteVisitComponent } from './collateral-site-visit/collateral-site-visit.component';
 import {LoanInformationDetailViewModule} from '../../../loan-information-detail-view/loan-information-detail-view.module';
-import {JointInformationModule} from '../../../joint-customer-view/joint-information/joint-information.module';
 
 
 const COMPONENTS = [
