b2d0e5e2025888efba3e6b5c3233a43ebbe3c3f1
Binita0 <binita.ghimire@sbsloutionsnepal.com>
2022-03-18 09:59:28 +0545

    added new template group credit facility report

---
+++ src/app/feature/loan/component/loan-summary/group-credit-facility-report/group-credit-facility-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-credit-facility-report',
+  templateUrl: './group-credit-facility-report.component.html',
+  styleUrls: ['./group-credit-facility-report.component.scss']
+})
+export class GroupCreditFacilityReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
