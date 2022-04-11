5fb110129dfcad53d47cffa121baceff1d941f46
Binita0 <binita.ghimire@sbsloutionsnepal.com>
2022-04-11 11:17:22 +0545

    patching values in above 1o millions

--- src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-background-of-customer/above-background-of-customer.component.ts
+++ src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-background-of-customer/above-background-of-customer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LoanDataHolder} from '../../../../../model/loanData';
+import {ObjectUtil} from '../../../../../../../@core/utils/ObjectUtil';
 
 @Component({
   selector: 'app-above-background-of-customer',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./above-background-of-customer.component.scss']
 })
 export class AboveBackgroundOfCustomerComponent implements OnInit {
-
+  @Input() loanDataHolder: LoanDataHolder;
+  tempData;
+  legalStatusData;
+  companyJson;
+  teamList = [];
   constructor() { }
 
   ngOnInit() {
+    console.log('this.tempData', this.loanDataHolder);
+    if (!ObjectUtil.isEmpty(this.loanDataHolder)) {
+      this.tempData = this.loanDataHolder.companyInfo;
+      if (!ObjectUtil.isEmpty(this.tempData)) {
+        this.legalStatusData = this.tempData.legalStatus;
+        this.companyJson = JSON.parse(this.tempData.companyJsonData);
+      }
+      if (!ObjectUtil.isEmpty(this.companyJson)) {
+        this.teamList = this.companyJson.managementTeamList;
+      }
+    }
   }
 
 }
--- src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-swot-analysis/above-swot-analysis.component.ts
+++ src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-swot-analysis/above-swot-analysis.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LoanDataHolder} from '../../../../../model/loanData';
+import {ObjectUtil} from '../../../../../../../@core/utils/ObjectUtil';
 
 @Component({
   selector: 'app-above-swot-analysis',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./above-swot-analysis.component.scss']
 })
 export class AboveSwotAnalysisComponent implements OnInit {
-
+  @Input() loanDataHolder: LoanDataHolder;
+  tempData;
+  swotData;
   constructor() { }
 
   ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.loanDataHolder)) {
+      this.tempData = JSON.parse(this.loanDataHolder.companyInfo.companyJsonData);
+    }
+    if (!ObjectUtil.isEmpty(this.tempData)) {
+      this.swotData = this.tempData.swot;
+    }
   }
 
 }
