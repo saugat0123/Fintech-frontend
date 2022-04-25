d085f757355af32c7b96e657daf7b3ba71d7bd31
AayushShrestha14 <mail2ayus@yahoo.com>
2022-04-25 14:31:49 +0545

    Retail CA point no 8

--- src/app/feature/loan-information-detail-view/retail-purpose-and-justification/retail-purpose-and-justification.component.ts
+++ src/app/feature/loan-information-detail-view/retail-purpose-and-justification/retail-purpose-and-justification.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Proposal} from '../../admin/modal/proposal';
 
 @Component({
   selector: 'app-retail-purpose-and-justification',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retail-purpose-and-justification.component.scss']
 })
 export class RetailPurposeAndJustificationComponent implements OnInit {
-
+  proposalData: Proposal = new Proposal();
+  proposalAllData: any;
   constructor() { }
 
   ngOnInit() {
+    this.proposalAllData = JSON.parse(this.proposalData.data);
+    console.log('proposalAllData', this.proposalAllData);
   }
 
 }
