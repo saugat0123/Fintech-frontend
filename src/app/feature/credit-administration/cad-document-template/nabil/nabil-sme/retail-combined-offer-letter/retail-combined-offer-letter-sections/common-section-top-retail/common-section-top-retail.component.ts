eb96744b3e23faf155f00ee2d9c2b092fb0ae88b
MiliRisal <milirisal11@gmail.com>
2022-04-26 11:26:01 +0545

    Common section top retail data patch completed

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/retail-combined-offer-letter/retail-combined-offer-letter-sections/common-section-top-retail/common-section-top-retail.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/retail-combined-offer-letter/retail-combined-offer-letter-sections/common-section-top-retail/common-section-top-retail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {ObjectUtil} from '../../../../../../../../@core/utils/ObjectUtil';
 
 @Component({
   selector: 'app-common-section-top-retail',
@@ -7,13 +8,23 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   styleUrls: ['./common-section-top-retail.component.scss']
 })
 export class CommonSectionTopRetailComponent implements OnInit {
+  @Input() cadData;
   form: FormGroup;
+  tempData;
+  assignedData;
+  sanctionDate;
+  loanHolderInfo;
   constructor(
       private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
     this.buildForm();
+    if (!ObjectUtil.isEmpty(this.cadData.offerDocumentList)) {
+      this.tempData = JSON.parse(this.cadData.offerDocumentList[0].initialInformation);
+      this.loanHolderInfo = JSON.parse(this.cadData.loanHolder.nepData);
+    }
+    this.fillform();
   }
   buildForm() {
     return this.form = this.formBuilder.group({
@@ -23,5 +34,26 @@ export class CommonSectionTopRetailComponent implements OnInit {
       addressOfBorrower: [undefined],
     });
   }
-
+  fillform() {
+    let refNum;
+    if (!ObjectUtil.isEmpty(this.cadData.assignedLoan)) {
+      this.assignedData = this.cadData.assignedLoan[0];
+      refNum = this.assignedData.refNo;
+    }
+    let sanctionLetterDate;
+    if (!ObjectUtil.isEmpty(this.tempData.retailGlobalForm)) {
+      this.sanctionDate = this.tempData.retailGlobalForm.sanctionLetterDateNepali.nDate ?
+      this.tempData.retailGlobalForm.sanctionLetterDateNepali.nDate : '';
+      sanctionLetterDate = this.sanctionDate ? this.sanctionDate : '';
+    }
+    const address = this.loanHolderInfo.permanentMunicipality.ct + '-' +
+        this.loanHolderInfo.permanentWard.ct + ', ' + this.loanHolderInfo.permanentDistrict.ct + ', ' +
+        this.loanHolderInfo.permanentProvince.ct + ' प्रदेश ';
+    this.form.patchValue({
+      referenceNumber: refNum ? refNum : '',
+      sanctionLetterDate: sanctionLetterDate ? sanctionLetterDate : '',
+      nameOfBorrower : this.loanHolderInfo.name ? this.loanHolderInfo.name.np : '',
+      addressOfBorrower : address ? address : '',
+    });
+  }
 }
