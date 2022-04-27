ab42496ef25ea4471519de5331f57bf5ec3b002c
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2022-04-27 10:51:53 +0545

    Comments

--- src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-ten-million.component.ts
+++ src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-ten-million.component.ts
@@ -2,9 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {LoanDataHolder} from '../../../../model/loanData';
 
 @Component({
-  selector: "app-above-ten-million",
-  templateUrl: "./above-ten-million.component.html",
-  styleUrls: ["./above-ten-million.component.scss"],
+  selector: 'app-above-ten-million',
+  templateUrl: './above-ten-million.component.html',
+  styleUrls: ['./above-ten-million.component.scss'],
 })
 export class AboveTenMillionComponent implements OnInit {
   @Input() loanDataHolder: LoanDataHolder;
