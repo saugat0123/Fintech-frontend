3499395bc1b1a39fb9218ae32906efef2e401a9a
sushanprajapati <sushan.prajapati@sbsolutionsnepal.com>
2022-04-28 10:34:43 +0545

    group exposure with ccbl (part 15)

---
+++ src/app/feature/customer/component/customer-profile/company-profile/group-exposure-with-ccbl/group-exposure-with-ccbl.component.ts
@@ -0,0 +1,86 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CustomerInfoData} from '../../../../../loan/model/customerInfoData';
+import {MGroup} from '../../../../model/mGroup';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Editor} from '../../../../../../@core/utils/constants/editor';
+import {ToastService} from '../../../../../../@core/utils';
+import {MGroupService} from '../../../../service/mgroup.service';
+import {ObjectUtil} from '../../../../../../@core/utils/ObjectUtil';
+import {Alert, AlertType} from '../../../../../../@theme/model/Alert';
+import {CalendarType} from '../../../../../../@core/model/calendar-type';
+
+@Component({
+  selector: 'app-group-exposure-with-ccbl',
+  templateUrl: './group-exposure-with-ccbl.component.html',
+  styleUrls: ['./group-exposure-with-ccbl.component.scss']
+})
+export class GroupExposureWithCcblComponent implements OnInit {
+  @Input() customerInfo: CustomerInfoData;
+  @Output() responseData: EventEmitter<MGroup> = new EventEmitter();
+  calendarType: CalendarType;
+
+  form: FormGroup;
+
+  submitted = false;
+
+  ckeConfig = Editor.CK_CONFIG;
+  mGroupInfo: MGroup = new MGroup();
+  spinner = false;
+
+  constructor(private formBuilder: FormBuilder,
+              private toastService: ToastService,
+              private mGroupService: MGroupService) {
+  }
+
+  get formControls() {
+    return this.form.controls;
+  }
+
+  ngOnInit() {
+    this.buildForm();
+    if (!ObjectUtil.isEmpty(this.customerInfo.mgroupInfo)) {
+      this.mGroupInfo = this.customerInfo.mgroupInfo;
+      this.form.patchValue(this.mGroupInfo);
+    }
+  }
+
+  buildForm() {
+    this.form = this.formBuilder.group({
+      groupExposureDateType: [undefined],
+      groupExposureDate: [undefined],
+      detailInformation: [undefined],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    this.spinner = true;
+    this.mGroupInfo = this.setMGroupValue();
+    this.mGroupService.save(this.mGroupInfo).subscribe((response) => {
+      this.responseData.emit(response.detail);
+      this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved Group Exposure'));
+      this.spinner = false;
+    }, error => {
+      console.log(error);
+
+      this.toastService.show(new Alert(AlertType.ERROR, 'Error while saving: ' + error));
+      this.spinner = false;
+    });
+  }
+
+  setMGroupValue() {
+    let mGroup = new MGroup();
+    if (!ObjectUtil.isEmpty(this.customerInfo.mgroupInfo)) {
+      mGroup = this.customerInfo.mgroupInfo;
+    }
+    mGroup.customerInfoId = this.customerInfo.id;
+    mGroup.groupExposureDateType = this.formControls.groupExposureDateType.value;
+    mGroup.detailInformation = this.formControls.detailInformation.value;
+    mGroup.groupExposureDate = this.formControls.groupExposureDate.value;
+    return mGroup;
+  }
+
+}
