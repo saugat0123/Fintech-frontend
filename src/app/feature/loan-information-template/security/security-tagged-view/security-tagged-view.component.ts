1b3cde49b153a8527474729d249e1f24f65899c6
Sonamxrest <xresthasonam@gmail.com>
2022-05-03 18:01:49 +0545

    New Changes

--- src/app/feature/loan-information-template/security/security-tagged-view/security-tagged-view.component.ts
+++ src/app/feature/loan-information-template/security/security-tagged-view/security-tagged-view.component.ts
@@ -12,26 +12,29 @@ export class SecurityTaggedViewComponent implements OnInit {
   hasAuto = false;
   constructor() { }
   ngOnInit() {
-  this.checkSecurity();
+  // this.checkSecurity();
+    this.hasLandBuilding = this.checkIndividualSecurity(this.loanDataHolder.landBuildings);
+    this.hasAuto = this.checkIndividualSecurity(this.loanDataHolder.autos);
   }
 
-  checkSecurity() {
-    const selectedArray = JSON.parse(this.loanDataHolder.selectedArray);
-    selectedArray.forEach((d) => {
-    switch (d) {
-      case 'Land and Building Security': {
-        this.hasLandBuilding = this.checkIndividualSecurity(this.loanDataHolder.landBuildings);
-      }
-      break;
-      case 'VehicleSecurity': {
-        this.hasAuto = this.checkIndividualSecurity(this.loanDataHolder.autos);
-      }
-      break;
-      default: return;
-
-    }
-    });
-  }
+  // checkSecurity() {
+  //   console.log('this is loan holder', this.loanDataHolder);
+  //   const selectedArray = JSON.parse(this.loanDataHolder.selectedArray);
+  //   selectedArray.forEach((d) => {
+  //   switch (d) {
+  //     case 'Land and Building Security': {
+  //       this.hasLandBuilding = this.checkIndividualSecurity(this.loanDataHolder.landBuildings);
+  //     }
+  //     break;
+  //     case 'VehicleSecurity': {
+  //       this.hasAuto = this.checkIndividualSecurity(this.loanDataHolder.autos);
+  //     }
+  //     break;
+  //     default: return;
+  //
+  //   }
+  //   });
+  // }
   checkIndividualSecurity(array: Array<any>) {
     return array.length > 0;
   }
