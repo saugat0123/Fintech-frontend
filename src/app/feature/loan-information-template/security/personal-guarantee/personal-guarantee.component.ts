793e2a1005a6635f61a716bafea1744ac2a43941
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2022-05-06 17:28:52 +0545

    segregate component for personal guarantee

--- src/app/feature/loan-information-template/security/personal-guarantee/personal-guarantee.component.ts
+++ src/app/feature/loan-information-template/security/personal-guarantee/personal-guarantee.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Editor} from '../../../../@core/utils/constants/editor';
 
 @Component({
-  selector: 'app-personal-guarantee',
-  templateUrl: './personal-guarantee.component.html',
-  styleUrls: ['./personal-guarantee.component.scss']
+    selector: 'app-personal-guarantee',
+    templateUrl: './personal-guarantee.component.html',
+    styleUrls: ['./personal-guarantee.component.scss']
 })
 export class PersonalGuaranteeComponent implements OnInit {
+    personalGuaranteeForm: FormGroup;
+    submitted = false;
+    ckeConfig;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.buildForm();
+    }
+
+    private buildForm(): FormGroup {
+        return this.personalGuaranteeForm = this.formBuilder.group({
+            personalGuarantee: this.formBuilder.array([])
+        });
+    }
+
+    configEditor() {
+        this.ckeConfig = Editor.CK_CONFIG;
+    }
+
+    public addPersonalGuarantee(): void {
+        (this.personalGuaranteeForm.get('personalGuarantee') as FormArray).push(this.personalDetailsFormGroup());
+    }
+
+
+    public removePersonal(index: number) {
+        (<FormArray>this.personalGuaranteeForm.get('personalGuarantee')).removeAt(index);
+    }
+
+    private personalDetailsFormGroup(): FormGroup {
+        return this.formBuilder.group({
+                name: [undefined, Validators.required],
+                address: [undefined],
+                email: [undefined],
+                phoneNumber: [undefined],
+                owner: [undefined],
+                otherDetail: [undefined]
+            }
+        );
+    }
 
 }
