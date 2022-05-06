5e4a7854ce9abf1ce362567ecc9ac564a7d2823f
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2022-05-06 15:57:40 +0545

    segregate component for assignment of receivables

--- src/app/feature/loan-information-template/security/assignment-of-receivable/assignment-of-receivable.component.ts
+++ src/app/feature/loan-information-template/security/assignment-of-receivable/assignment-of-receivable.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Editor} from '../../../../@core/utils/constants/editor';
 
 @Component({
   selector: 'app-assignment-of-receivable',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment-of-receivable.component.scss']
 })
 export class AssignmentOfReceivableComponent implements OnInit {
+  assignmentForm: FormGroup;
+  submitted = false;
+  ckeConfig;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.configEditor();
+    this.buildForm();
+  }
+
+  configEditor() {
+    this.ckeConfig = Editor.CK_CONFIG;
+  }
+
+  private buildForm(): FormGroup {
+    return this.assignmentForm = this.formBuilder.group({
+      assignmentOfReceivables: this.formBuilder.array([])
+    });
+  }
+
+  public addAssignment(): void {
+    (this.assignmentForm.get('assignmentOfReceivables') as FormArray).push(this.assignmentDetailsFormGroup());
+  }
+
+  public removeAssignment(index: number): void {
+    (<FormArray>this.assignmentForm.get('assignmentOfReceivables')).removeAt(index);
+  }
+
+  public assignmentDetailsFormGroup(): FormGroup {
+    return this.formBuilder.group({
+          amount: [undefined, Validators.required],
+          otherDetail: [undefined]
+        }
+    );
   }
 
 }
