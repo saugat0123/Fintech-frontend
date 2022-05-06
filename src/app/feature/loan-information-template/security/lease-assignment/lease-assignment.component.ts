49aae7b963cde5c24c093d1162dab248bd45c1fe
Mohammad Hussain Sheikh <mohammadhusen257@gmail.com>
2022-05-06 17:11:45 +0545

    segregate component for leas assignment

--- src/app/feature/loan-information-template/security/lease-assignment/lease-assignment.component.ts
+++ src/app/feature/loan-information-template/security/lease-assignment/lease-assignment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {Editor} from '../../../../@core/utils/constants/editor';
 
 @Component({
   selector: 'app-lease-assignment',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lease-assignment.component.scss']
 })
 export class LeaseAssignmentComponent implements OnInit {
+  leaseAssignmentForm: FormGroup;
+  ckeConfig;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+    this.configEditor();
+  }
+
+  configEditor() {
+    this.ckeConfig = Editor.CK_CONFIG;
+  }
+
+  private buildForm(): FormGroup {
+    return this.leaseAssignmentForm = this.formBuilder.group({
+      leaseAssignment: this.formBuilder.array([]),
+    });
+  }
+
+  private assignmentsDetailsFormGroup(): FormGroup {
+    return this.formBuilder.group({
+          otherDetail: [undefined],
+          ownerKycApplicableData: [undefined],
+        }
+    );
+  }
+
+  public addAssignments(): void {
+    (this.leaseAssignmentForm.get('leaseAssignment') as FormArray).push(this.assignmentsDetailsFormGroup());
+  }
+
+  public removeAssignments(idx: number): void {
+    (this.leaseAssignmentForm.get('leaseAssignment') as FormArray).removeAt(idx);
   }
 
 }
