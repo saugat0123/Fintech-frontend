37fa3cd7434dffe3995878fd48004be2013bbb67
sujesh07 <sujesh@sbsolutionsnepal.com>
2022-05-12 11:34:30 +0545

    Remove unnecesary files

--- src/app/feature/loan-information-template/security/view-security-table/view-security-table.component.ts
+++ src/app/feature/loan-information-template/security/view-security-table/view-security-table.component.ts
@@ -3,34 +3,35 @@ import {CustomerInfoData} from '../../../loan/model/customerInfoData';
 import {Security} from '../../../loan/model/security';
 
 @Component({
-  selector: 'app-view-security-table',
-  templateUrl: './view-security-table.component.html',
-  styleUrls: ['./view-security-table.component.scss']
+    selector: 'app-view-security-table',
+    templateUrl: './view-security-table.component.html',
+    styleUrls: ['./view-security-table.component.scss']
 })
 export class ViewSecurityTableComponent implements OnInit {
-  @Input() customerInfo: CustomerInfoData;
-  securities: Array<Security> = new Array<Security>();
-  regex = /_/g;
-  @Output() security: EventEmitter<Object> = new EventEmitter<Object>();
-  securityData = {
-    security: null,
-    securityType: null,
-    isEdit: null
-  };
+    @Input() customerInfo: CustomerInfoData;
+    securities: Array<Security> = new Array<Security>();
+    regex = /_/g;
+    @Output() security: EventEmitter<Object> = new EventEmitter<Object>();
+    securityData = {
+        security: null,
+        securityType: null,
+        isEdit: null
+    };
 
-  constructor() { }
-
-  ngOnInit() {
-    if (this.customerInfo.securities.length > 0) {
-      this.securities = this.customerInfo.securities;
+    constructor() {
     }
-  }
 
-  public onEdit(security: Security): void {
-    this.securityData.security = security;
-    this.securityData.securityType = security.securityType;
-    this.securityData.isEdit = true;
-    this.security.emit(this.securityData);
-  }
+    ngOnInit() {
+        if (this.customerInfo.securities.length > 0) {
+            this.securities = this.customerInfo.securities;
+        }
+    }
+
+    public onEdit(security: Security): void {
+        this.securityData.security = security;
+        this.securityData.securityType = security.securityType;
+        this.securityData.isEdit = true;
+        this.security.emit(this.securityData);
+    }
 
 }
