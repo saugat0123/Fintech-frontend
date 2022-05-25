c3aba89f434a6973848d9ce91d6fdecdb49677f6
Binit0 <binita.ghimire@sbsolutionsnepal.com>
2022-05-25 12:29:07 +0545

    removed other risk type in risk component and viewed risk analysis in detail view of individual

--- src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-risk-analysis/above-risk-analysis.component.ts
+++ src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-risk-analysis/above-risk-analysis.component.ts
@@ -11,6 +11,14 @@ export class AboveRiskAnalysisComponent implements OnInit {
   tempRiskData;
   riskData;
   riskDataFree: boolean;
+  industryVisible = false;
+  businessVisible = false;
+  financialVisible = false;
+  managementVisible = false;
+  successionVisible = false;
+  environmentVisible = false;
+  adverseVisible = false;
+  politicalVisible = false;
 
   constructor() { }
 
@@ -21,6 +29,48 @@ export class AboveRiskAnalysisComponent implements OnInit {
         this.riskData = this.tempRiskData;
         if (!ObjectUtil.isEmpty(this.tempRiskData.riskAnalysisFreeText)) {
           this.riskDataFree = true;
+        }
+      }
+      if (!ObjectUtil.isEmpty(this.riskData)) {
+        if (!ObjectUtil.isEmpty(this.riskData.industryRisk) || !ObjectUtil.isEmpty(this.riskData.industryFactor)) {
+          this.industryVisible = true;
+        } else {
+          this.industryVisible = false;
+        }
+        if (!ObjectUtil.isEmpty(this.riskData.businessRisk) || !ObjectUtil.isEmpty(this.riskData.businessFactor)) {
+          this.businessVisible = true;
+        } else {
+          this.businessVisible = false;
+        }
+        if (!ObjectUtil.isEmpty(this.riskData.financialRisk) || !ObjectUtil.isEmpty(this.riskData.financialFactor)) {
+          this.financialVisible = true;
+        } else {
+          this.financialVisible = false;
+        }
+        if (!ObjectUtil.isEmpty(this.riskData.managementRisk) || !ObjectUtil.isEmpty(this.riskData.managementFactor)) {
+          this.managementVisible = true;
+        } else {
+          this.managementVisible = false;
+        }
+        if (!ObjectUtil.isEmpty(this.riskData.successionRisk) || !ObjectUtil.isEmpty(this.riskData.successionFactor)) {
+          this.successionVisible = true;
+        } else {
+          this.successionVisible = false;
+        }
+        if (!ObjectUtil.isEmpty(this.riskData.environmentalIssues) || !ObjectUtil.isEmpty(this.riskData.environmentalIssuesFactor)) {
+          this.environmentVisible = true;
+        } else {
+          this.environmentVisible = false;
+        }
+        if (!ObjectUtil.isEmpty(this.riskData.adverseFeatures) || !ObjectUtil.isEmpty(this.riskData.adverseFeaturesFactor)) {
+          this.adverseVisible = true;
+        } else {
+          this.adverseVisible = false;
+        }
+        if (!ObjectUtil.isEmpty(this.riskData.politicalRisk) || !ObjectUtil.isEmpty(this.riskData.politicalFactor)) {
+          this.politicalVisible = true;
+        } else {
+          this.politicalVisible = false;
         }
       }
     }
