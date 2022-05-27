01eb4f0012c54ffc364393f056ae9dcf96482a9e
Unish Bhattarai <unish.bhattaral@sbsolutionsnepal.com>
2022-05-27 14:43:12 +0545

    Retail-UAT-Template-Fixed

--- src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/retail-combined-offer-letter/retail-combined-offer-letter-sections/section1-customer-offer-letter-type/section1-customer-offer-letter-type.component.ts
+++ src/app/feature/credit-administration/cad-document-template/nabil/nabil-sme/retail-combined-offer-letter/retail-combined-offer-letter-sections/section1-customer-offer-letter-type/section1-customer-offer-letter-type.component.ts
@@ -16,6 +16,7 @@ export class Section1CustomerOfferLetterTypeComponent implements OnInit {
   loanOption;
   loanData;
   loanName: Array<any> = new Array<any>();
+  loanNepaliName: Array<any> = new Array<any>();
   NCELL: boolean;
   reqDate;
   prevDate;
@@ -48,6 +49,7 @@ export class Section1CustomerOfferLetterTypeComponent implements OnInit {
     if (!ObjectUtil.isEmpty(this.cadData.assignedLoan)) {
       this.cadData.assignedLoan.forEach(val => {
         this.loanName.push(val.loan.name);
+        this.loanNepaliName.push(val.loan.nepaliName);
       });
       this.loanName.forEach(value => {
         if (value === 'HOME LOAN COMBINED') {
