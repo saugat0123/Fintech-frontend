53b823afafb28d763f269db4f5ccf6e6a9f4c2e1
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2022-06-08 16:30:39 +0545

    shown crg Gamma data at detail view page

--- src/app/feature/loan-information-view/crg-gamma-detail-view/crg-gamma-detail-view.component.ts
+++ src/app/feature/loan-information-view/crg-gamma-detail-view/crg-gamma-detail-view.component.ts
@@ -3,45 +3,56 @@ import {ObjectUtil} from '../../../@core/utils/ObjectUtil';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-crg-gamma-detail-view',
-  templateUrl: './crg-gamma-detail-view.component.html',
-  styleUrls: ['./crg-gamma-detail-view.component.scss']
+    selector: 'app-crg-gamma-detail-view',
+    templateUrl: './crg-gamma-detail-view.component.html',
+    styleUrls: ['./crg-gamma-detail-view.component.scss']
 })
 export class CrgGammaDetailViewComponent implements OnInit {
 
-  @Input() formData: any;
-  crgGammaList = [];
-  crgGammaData;
-  spinner = false;
+    @Input() formData: any;
+    crgGammaList = [];
+    crgGammaData;
+    spinner = false;
 
-  constructor(
-      private modalService: NgbModal,
-  ) { }
-
-  ngOnInit() {
-    console.log('formData', this.formData);
-    if (!ObjectUtil.isEmpty(this.formData) && this.formData) {
-      this.crgGammaData = this.formData.data ? JSON.parse(this.formData.data) : this.formData.data;
-      console.log('crgGammaData', this.crgGammaData);
-      for (const [key, value] of Object.entries(this.crgGammaData)) {
-        const ansModel = {
-          question: null,
-          answer: null,
-          score: null,
-        };
-        if (!(key.includes(`Parameter`) || key === `totalPoint` || key === `grade`)) {
-          ansModel.question = key;
-          ansModel.answer = this.crgGammaData[`${key}Parameter`];
-          ansModel.score = value;
-
-          this.crgGammaList.push(ansModel);
-        }
-      }
+    constructor(
+        private modalService: NgbModal,
+    ) {
     }
-    console.log('crgGammaList', this.crgGammaList);
-  }
 
-  onClose() {
-    this.modalService.dismissAll();
-  }
+    ngOnInit() {
+        if (!ObjectUtil.isEmpty(this.formData) && this.formData) {
+            this.crgGammaData = this.formData.data ? JSON.parse(this.formData.data) : this.formData.data;
+            console.log('crgGammaData', this.crgGammaData);
+            this.crgGammaData.groupObject.forEach(obj => {
+                const crgData = {
+                    groupName: null,
+                    ansModel: null,
+                    groupTotal: null
+                };
+                const dataWithGroup = [];
+                let ansModel;
+                for (const [key, value] of Object.entries(obj.gammaQuestionAnswer)) {
+                    ansModel = {
+                        question: null,
+                        answer: null,
+                        score: null,
+                    };
+                    if (!(key.includes(`Parameter`))) {
+                        ansModel.question = key;
+                        ansModel.answer = obj.gammaQuestionAnswer[`${key}Parameter`];
+                        ansModel.score = value;
+                        dataWithGroup.push(ansModel);
+                    }
+                    crgData.groupName = obj.groupName;
+                    crgData.groupTotal = obj.groupTotal;
+                    crgData.ansModel = dataWithGroup;
+                }
+                this.crgGammaList.push(crgData);
+            });
+        }
+    }
+
+    onClose() {
+        this.modalService.dismissAll();
+    }
 }
