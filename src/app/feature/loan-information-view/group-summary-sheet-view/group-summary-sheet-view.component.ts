e3ce1da749614f696f994a8c92c5950216b0f527
RameshBhandari <ramesh.bhandari@sbsolutionsnepal.com>
2022-06-09 16:37:47 +0545

    Created group summary sheet view component and patched data

---
+++ src/app/feature/loan-information-view/group-summary-sheet-view/group-summary-sheet-view.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ObjectUtil} from '../../../@core/utils/ObjectUtil';
+
+@Component({
+  selector: 'app-group-summary-sheet-view',
+  templateUrl: './group-summary-sheet-view.component.html',
+  styleUrls: ['./group-summary-sheet-view.component.scss']
+})
+export class GroupSummarySheetViewComponent implements OnInit {
+  @Input() loanDataHolder;
+  gssData;
+  constructor() { }
+
+  ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.loanDataHolder)) {
+      this.gssData = JSON.parse(this.loanDataHolder.gssData);
+    }
+  }
+
+}
