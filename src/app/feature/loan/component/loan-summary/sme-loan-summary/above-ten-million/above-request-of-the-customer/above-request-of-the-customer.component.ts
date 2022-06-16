298ac4afeb642c7b69ed38bbc5a5a51662c46817
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2022-06-16 14:58:57 +0545

    Handle null at detail view page

--- src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-request-of-the-customer/above-request-of-the-customer.component.ts
+++ src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-request-of-the-customer/above-request-of-the-customer.component.ts
@@ -2,13 +2,18 @@ import {Component, Input, OnInit} from '@angular/core';
 import {LoanDataHolder} from '../../../../../model/loanData';
 
 @Component({
-  selector: "app-above-request-of-the-customer",
-  templateUrl: "./above-request-of-the-customer.component.html",
-  styleUrls: ["./above-request-of-the-customer.component.scss"],
+    selector: 'app-above-request-of-the-customer',
+    templateUrl: './above-request-of-the-customer.component.html',
+    styleUrls: ['./above-request-of-the-customer.component.scss'],
 })
 export class AboveRequestOfTheCustomerComponent implements OnInit {
-  @Input() customerAllLoanList: LoanDataHolder[];
-  constructor() {}
-  creditFacilities = ['.....', ".....", ".....", "....."];
-  ngOnInit() {}
+    @Input() customerAllLoanList: LoanDataHolder[];
+
+    constructor() {
+    }
+
+    creditFacilities = ['.....', '.....', '.....', '.....'];
+
+    ngOnInit() {
+    }
 }
