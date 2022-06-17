82f4c364fcff1b9e11525a99563e0386639620b0
Subodh Bhandari <subodh.bhandari@sbsolutionsnepal.com>
2022-06-17 15:53:46 +0545

    Modifying SwapCharge (Three digit after decimal )

---
+++ src/app/@core/directive/three-digit-decimal.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ThreeDigitDecimalDirective } from './three-digit-decimal.directive';
+
+describe('ThreeDigitDecimalDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ThreeDigitDecimalDirective();
+    expect(directive).toBeTruthy();
+  });
+});
