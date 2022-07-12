b9a199448c6e688ec75b0e96b87807a4106cd383
Ritesh Shrestha <ritesh@sbsolutionsnepal.com>
2022-07-12 12:37:03 +0545

    Refactor format of code in cicl component.

--- src/app/feature/loan-information-template/cicl/cicl.component.ts
+++ src/app/feature/loan-information-template/cicl/cicl.component.ts
@@ -23,248 +23,246 @@ export class CiclComponent implements OnInit {
     @Input() customerInfoId: number;
     // @Input() calendarType: CalendarType;
 
-    @Input() fromProfile: boolean;
-    calendarType = CalendarType.AD;
+  @Input() fromProfile: boolean;
+  calendarType = CalendarType.AD;
 
-    ciclForm: FormGroup;
+  ciclForm: FormGroup;
 
-    ciclList: Array<Cicl> = new Array<Cicl>();
-    @Output() ciclDataEmitter = new EventEmitter();
+  ciclList: Array<Cicl> = new Array<Cicl>();
+  @Output() ciclDataEmitter = new EventEmitter();
 
-    submitted = false;
-    ckeConfig = Editor.CK_CONFIG;
+  submitted = false;
+  ckeConfig = Editor.CK_CONFIG;
 
-    relationshipList: RelationshipList = new RelationshipList();
-    relationlist;
-    ciclRelation = CiclRelationListEnum.pair();
-    ciclHistory = false;
-    variableChecked = false;
-    bankingRelationChecked;
-    bankingRelationshipList = BankingRelationship.enumObject();
-    bankingRelationship;
+  relationshipList: RelationshipList = new RelationshipList();
+  relationlist;
+  ciclRelation = CiclRelationListEnum.pair();
+  ciclHistory = false;
+  variableChecked = false;
+  bankingRelationChecked;
+  bankingRelationshipList = BankingRelationship.enumObject();
+  bankingRelationship;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private toastService: ToastService,
-        private el: ElementRef,
-        private customerInfoService: CustomerInfoService
-    ) {
+  constructor(
+      private formBuilder: FormBuilder,
+      private toastService: ToastService,
+      private el: ElementRef,
+      private customerInfoService: CustomerInfoService
+  ) {
+  }
+
+
+  get ciclRemarks() {
+    return this.ciclForm.get('ciclRemarks');
+  }
+
+  get ciclArray() {
+    return this.ciclForm.get('ciclArray');
+  }
+
+  get ciclFormControls() {
+    return this.ciclForm.controls;
+  }
+
+
+  ngOnInit() {
+    if (!ObjectUtil.isEmpty(this.ciclValue)) {
+      this.ciclList = JSON.parse(this.ciclValue.data);
+    } else {
+      this.ciclValue = new CiclArray();
     }
-
-
-    get ciclRemarks() {
-        return this.ciclForm.get('ciclRemarks');
+    const ciclList = this.ciclList[0];
+    if (ciclList === undefined) {
+        this.ciclHistory = false;
+    } else if (ciclList.nameOfBorrower !== null) {
+        this.ciclHistory = true;
     }
-
-    get ciclArray() {
-        return this.ciclForm.get('ciclArray');
-    }
-
-    get ciclFormControls() {
-        return this.ciclForm.controls;
-    }
-
-
-    ngOnInit() {
-        if (!ObjectUtil.isEmpty(this.ciclValue)) {
-            this.ciclList = JSON.parse(this.ciclValue.data);
-        } else {
-            this.ciclValue = new CiclArray();
+    if (!ObjectUtil.isEmpty(this.ciclValue.cibRemark)) {
+        this.variableChecked = JSON.parse(this.ciclValue.cibRemark).checked;
+        if (!ObjectUtil.isEmpty(JSON.parse(this.ciclValue.cibRemark).value)) {
+            this.ciclValue.cibCharge = JSON.parse(this.ciclValue.cibRemark).value;
         }
-        const ciclList = this.ciclList[0];
-        if (ciclList === undefined) {
-            this.ciclHistory = false;
-        } else if (ciclList.nameOfBorrower !== null) {
-            this.ciclHistory = true;
-        }
-        if (!ObjectUtil.isEmpty(this.ciclValue.cibRemark)) {
-            this.variableChecked = JSON.parse(this.ciclValue.cibRemark).checked;
-            if (!ObjectUtil.isEmpty(JSON.parse(this.ciclValue.cibRemark).value)) {
-                this.ciclValue.cibCharge = JSON.parse(this.ciclValue.cibRemark).value;
-            }
-            this.bankingRelationship = JSON.parse(this.ciclValue.cibRemark).bankingRelationship;
-            this.bankingRelationChecked = JSON.parse(this.ciclValue.cibRemark).bankingRelationChecked;
-        }
-        this.buildCiclForm();
-        this.relationlist = this.relationshipList.relation;
+        this.bankingRelationship = JSON.parse(this.ciclValue.cibRemark).bankingRelationship;
+        this.bankingRelationChecked = JSON.parse(this.ciclValue.cibRemark).bankingRelationChecked;
     }
-
-    buildCiclForm() {
-        this.ciclForm = this.formBuilder.group({
-            ciclArray: this.formBuilder.array([]),
-            ciclRemarks: [ObjectUtil.isEmpty(this.ciclValue) ? '' : this.ciclValue.remarks],
-            cibCharge: [ObjectUtil.isEmpty(this.ciclValue) ? undefined : this.ciclValue.cibCharge],
-            cibRemark: [ObjectUtil.isEmpty(this.ciclValue) ? undefined : this.ciclValue.cibRemark],
-        });
-        if (!ObjectUtil.isEmpty(this.ciclList)) {
-            if ((this.ciclList.length > 0)) {
-                this.patchCiclFormGroup(this.ciclList);
-            } else {
-                this.addCiclFormGroup();
-            }
-        } else {
-            this.addCiclFormGroup();
-        }
+    this.buildCiclForm();
+    this.relationlist = this.relationshipList.relation;
+  }
+  buildCiclForm() {
+    this.ciclForm = this.formBuilder.group({
+      ciclArray: this.formBuilder.array([]),
+      ciclRemarks: [ObjectUtil.isEmpty(this.ciclValue) ? '' : this.ciclValue.remarks],
+      cibCharge: [ObjectUtil.isEmpty(this.ciclValue) ? undefined : this.ciclValue.cibCharge],
+      cibRemark: [ObjectUtil.isEmpty(this.ciclValue) ? undefined : this.ciclValue.cibRemark],
+    });
+    if (!ObjectUtil.isEmpty(this.ciclList)) {
+      if ((this.ciclList.length > 0)) {
+        this.patchCiclFormGroup(this.ciclList);
+      } else {
+        this.addCiclFormGroup();
+      }
+    } else {
+      this.addCiclFormGroup();
     }
+  }
 
-    addCiclFormGroup() {
-        const controls = this.ciclForm.controls.ciclArray as FormArray;
-        controls.push(
-            this.formBuilder.group({
-                nameOfBorrower: [undefined, Validators.required],
-                fiName: [undefined, Validators.required],
-                facilityName: [undefined, Validators.required],
-                overdueAmount: [undefined, Validators.required],
-                outstandingAmount: [undefined, Validators.required],
-                ciclStatus: [undefined, Validators.required],
-                obtaineddate: [undefined, Validators.required],
-                loanamount: [undefined, Validators.required],
-                overdue: [undefined],
-                ciclRelation: [undefined],
-                probabilityDefer: [undefined],
-                riskScore: [undefined]
-            }));
+  addCiclFormGroup() {
+    const controls = this.ciclForm.controls.ciclArray as FormArray;
+    controls.push(
+        this.formBuilder.group({
+          nameOfBorrower: [undefined, Validators.required],
+          fiName: [undefined, Validators.required],
+          facilityName: [undefined, Validators.required],
+          overdueAmount: [undefined, Validators.required],
+          outstandingAmount: [undefined, Validators.required],
+          ciclStatus: [undefined, Validators.required],
+          obtaineddate: [undefined, Validators.required],
+          loanamount: [undefined, Validators.required],
+          overdue: [undefined],
+          ciclRelation: [undefined],
+            probabilityDefer: [undefined],
+            riskScore: [undefined]
+        }));
+  }
+
+  addCicl() {
+    const controls = this.ciclForm.controls.ciclArray as FormArray;
+    if (FormUtils.checkEmptyProperties(controls)) {
+      this.toastService.show(new Alert(AlertType.INFO, 'Please Fill All CICL Detail To Add More'));
+      return;
     }
+    controls.push(
+        this.formBuilder.group({
+          nameOfBorrower: [undefined, Validators.required],
+          fiName: [undefined, Validators.required],
+          facilityName: [undefined, Validators.required],
+          overdueAmount: [undefined, Validators.required],
+          outstandingAmount: [undefined, Validators.required],
+          ciclStatus: [undefined, Validators.required],
+          obtaineddate: [undefined, Validators.required],
+          loanamount: [undefined, Validators.required],
+          overdue: [undefined],
+          ciclRelation: [undefined],
+            probabilityDefer: [undefined],
+            riskScore: [undefined]
 
-    addCicl() {
-        const controls = this.ciclForm.controls.ciclArray as FormArray;
-        if (FormUtils.checkEmptyProperties(controls)) {
-            this.toastService.show(new Alert(AlertType.INFO, 'Please Fill All CICL Detail To Add More'));
+        }));
+
+  }
+
+  removeCICL(index: number) {
+    (this.ciclArray as FormArray).removeAt(index);
+  }
+
+  patchCiclFormGroup(ciclList: Array<Cicl>) {
+    const controls = this.ciclForm.controls.ciclArray as FormArray;
+    ciclList.forEach(cicl => {
+      controls.push(
+          this.formBuilder.group({
+            nameOfBorrower: [cicl.nameOfBorrower, Validators.required],
+            fiName: [cicl.nameOfFI, Validators.required],
+            facilityName: [cicl.facility, Validators.required],
+            overdueAmount: [cicl.overdueAmount, Validators.required],
+            outstandingAmount: [cicl.outstandingAmount, Validators.required],
+            ciclStatus: [cicl.status, Validators.required],
+            obtaineddate: [new Date(cicl.obtaineddate), Validators.required],
+            loanamount: [cicl.loanamount, Validators.required],
+            overdue: [cicl.overdue],
+            ciclRelation: [cicl.ciclRelation],
+              probabilityDefer: [cicl.probabilityDefer],
+              riskScore: [cicl.riskScore],
+
+          }));
+    });
+  }
+  scrollToFirstInvalidControl() {
+    const firstInvalidControl: HTMLElement = this.el.nativeElement.querySelector(
+        'form .ng-invalid'
+    );
+    window.scroll({
+      top: this.getTopOffset(firstInvalidControl),
+      left: 0,
+      behavior: 'smooth'
+    });
+    firstInvalidControl.focus();
+  }
+
+  private getTopOffset(controlEl: HTMLElement): number {
+    const labelOffset = 50;
+    return controlEl.getBoundingClientRect().top + window.scrollY - labelOffset;
+  }
+
+
+  onSubmit() {
+  this.submitted = true;
+  if (this.ciclHistory === false) {
+      const controls = this.ciclForm.controls.ciclArray as FormArray;
+      controls.clearAsyncValidators();
+      controls.clear();
+  }
+    if (this.ciclHistory === true) {
+        if (this.ciclForm.invalid) {
+            this.scrollToFirstInvalidControl();
             return;
         }
-        controls.push(
-            this.formBuilder.group({
-                nameOfBorrower: [undefined, Validators.required],
-                fiName: [undefined, Validators.required],
-                facilityName: [undefined, Validators.required],
-                overdueAmount: [undefined, Validators.required],
-                outstandingAmount: [undefined, Validators.required],
-                ciclStatus: [undefined, Validators.required],
-                obtaineddate: [undefined, Validators.required],
-                loanamount: [undefined, Validators.required],
-                overdue: [undefined],
-                ciclRelation: [undefined],
-                probabilityDefer: [undefined],
-                riskScore: [undefined]
-
-            }));
-
     }
 
-    removeCICL(index: number) {
-        (this.ciclArray as FormArray).removeAt(index);
+    // CICL
+
+    this.ciclList = new Array<Cicl>();
+    const ciclControls = this.ciclArray as FormArray;
+    for (const arrayControl of ciclControls.controls) {
+      const controls = (arrayControl as FormGroup).controls;
+      const cicl: Cicl = new Cicl();
+      cicl.nameOfBorrower = controls.nameOfBorrower.value;
+      cicl.nameOfFI = controls.fiName.value;
+      cicl.facility = controls.facilityName.value;
+      cicl.overdueAmount = controls.overdueAmount.value;
+      cicl.outstandingAmount = controls.outstandingAmount.value;
+      cicl.status = controls.ciclStatus.value;
+      cicl.obtaineddate = controls.obtaineddate.value;
+      cicl.loanamount = controls.loanamount.value;
+      cicl.overdue = controls.overdue.value;
+      cicl.ciclRelation = controls.ciclRelation.value;
+      cicl.probabilityDefer = controls.probabilityDefer.value;
+      cicl.riskScore = controls.riskScore.value;
+
+      this.ciclList.push(cicl);
+
     }
+    // uncomment if value is need
+    this.ciclValue.remarks = this.ciclForm.get('ciclRemarks').value === undefined ? '' : this.ciclForm.get('ciclRemarks').value;
+      if (!this.variableChecked) {
+          this.ciclValue.cibCharge = this.ciclForm.get('cibCharge').value === undefined ? '' : this.ciclForm.get('cibCharge').value;
+          this.ciclValue.cibRemark = null;
+          const ciclRemark = {
+              value: '',
+              checked: false,
+              bankingRelationship: this.bankingRelationship,
+              bankingRelationChecked: this.bankingRelationChecked
+          };
+          this.ciclValue.cibRemark = JSON.stringify(ciclRemark);
+      } else {
+          const ciclRemark = {
+              value: this.ciclForm.get('cibCharge').value === undefined ? '' : this.ciclForm.get('cibCharge').value,
+              checked: true,
+              bankingRelationship: this.bankingRelationship,
+              bankingRelationChecked: this.bankingRelationChecked
+          };
+          this.ciclValue.cibRemark = JSON.stringify(ciclRemark);
+          this.ciclValue.cibCharge = null;
+      }
+    this.ciclValue.data = JSON.stringify(this.ciclList);
+    this.ciclDataEmitter.emit(this.ciclValue);
+  }
 
-    patchCiclFormGroup(ciclList: Array<Cicl>) {
-        const controls = this.ciclForm.controls.ciclArray as FormArray;
-        ciclList.forEach(cicl => {
-            controls.push(
-                this.formBuilder.group({
-                    nameOfBorrower: [cicl.nameOfBorrower, Validators.required],
-                    fiName: [cicl.nameOfFI, Validators.required],
-                    facilityName: [cicl.facility, Validators.required],
-                    overdueAmount: [cicl.overdueAmount, Validators.required],
-                    outstandingAmount: [cicl.outstandingAmount, Validators.required],
-                    ciclStatus: [cicl.status, Validators.required],
-                    obtaineddate: [new Date(cicl.obtaineddate), Validators.required],
-                    loanamount: [cicl.loanamount, Validators.required],
-                    overdue: [cicl.overdue],
-                    ciclRelation: [cicl.ciclRelation],
-                    probabilityDefer: [cicl.probabilityDefer],
-                    riskScore: [cicl.riskScore],
-
-                }));
-        });
-    }
-
-    scrollToFirstInvalidControl() {
-        const firstInvalidControl: HTMLElement = this.el.nativeElement.querySelector(
-            'form .ng-invalid'
-        );
-        window.scroll({
-            top: this.getTopOffset(firstInvalidControl),
-            left: 0,
-            behavior: 'smooth'
-        });
-        firstInvalidControl.focus();
-    }
-
-    private getTopOffset(controlEl: HTMLElement): number {
-        const labelOffset = 50;
-        return controlEl.getBoundingClientRect().top + window.scrollY - labelOffset;
-    }
-
-
-    onSubmit() {
-        this.submitted = true;
-        if (this.ciclHistory === false) {
-            const controls = this.ciclForm.controls.ciclArray as FormArray;
-            controls.clearAsyncValidators();
-            controls.clear();
-        }
-        if (this.ciclHistory === true) {
-            if (this.ciclForm.invalid) {
-                this.scrollToFirstInvalidControl();
-                return;
-            }
-        }
-
-        // CICL
-
-        this.ciclList = new Array<Cicl>();
-        const ciclControls = this.ciclArray as FormArray;
-        for (const arrayControl of ciclControls.controls) {
-            const controls = (arrayControl as FormGroup).controls;
-            const cicl: Cicl = new Cicl();
-            cicl.nameOfBorrower = controls.nameOfBorrower.value;
-            cicl.nameOfFI = controls.fiName.value;
-            cicl.facility = controls.facilityName.value;
-            cicl.overdueAmount = controls.overdueAmount.value;
-            cicl.outstandingAmount = controls.outstandingAmount.value;
-            cicl.status = controls.ciclStatus.value;
-            cicl.obtaineddate = controls.obtaineddate.value;
-            cicl.loanamount = controls.loanamount.value;
-            cicl.overdue = controls.overdue.value;
-            cicl.ciclRelation = controls.ciclRelation.value;
-            cicl.probabilityDefer = controls.probabilityDefer.value;
-            cicl.riskScore = controls.riskScore.value;
-
-            this.ciclList.push(cicl);
-
-        }
-        // uncomment if value is need
-        this.ciclValue.remarks = this.ciclForm.get('ciclRemarks').value === undefined ? '' : this.ciclForm.get('ciclRemarks').value;
-        if (!this.variableChecked) {
-            this.ciclValue.cibCharge = this.ciclForm.get('cibCharge').value === undefined ? '' : this.ciclForm.get('cibCharge').value;
-            this.ciclValue.cibRemark = null;
-            const ciclRemark = {
-                value: '',
-                checked: false,
-                bankingRelationship: this.bankingRelationship,
-                bankingRelationChecked: this.bankingRelationChecked
-            };
-            this.ciclValue.cibRemark = JSON.stringify(ciclRemark);
-        } else {
-            const ciclRemark = {
-                value: this.ciclForm.get('cibCharge').value === undefined ? '' : this.ciclForm.get('cibCharge').value,
-                checked: true,
-                bankingRelationship: this.bankingRelationship,
-                bankingRelationChecked: this.bankingRelationChecked
-            };
-            this.ciclValue.cibRemark = JSON.stringify(ciclRemark);
-            this.ciclValue.cibCharge = null;
-        }
-        this.ciclValue.data = JSON.stringify(this.ciclList);
-        this.ciclDataEmitter.emit(this.ciclValue);
-    }
-
-    public ciclHistoryFound(ciclChk) {
-        if (!ciclChk) {
-            this.ciclHistory = false;
-        } else {
-            this.ciclHistory = true;
-        }
-    }
+  public ciclHistoryFound(ciclChk) {
+      if (!ciclChk) {
+          this.ciclHistory = false;
+      } else {
+          this.ciclHistory = true;
+      }
+  }
 
     variableCheck(event, type) {
         if (type === 'variable') {
