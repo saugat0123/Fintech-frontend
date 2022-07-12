ff7bd9dfbde6795601f28d16619d21aa2f7e06b1
Binit0 <binita.ghimire@sbsolutionsnepal.com>
2022-07-12 10:50:21 +0545

    added new component for guarantor acknowledgement

--- src/app/feature/credit-administration/cad-document-template/laxmi/laxmi-offer-letter/guarantor-acknowledgement/guarantor-acknowledgement.component.ts
+++ src/app/feature/credit-administration/cad-document-template/laxmi/laxmi-offer-letter/guarantor-acknowledgement/guarantor-acknowledgement.component.ts
@@ -1,6 +1,15 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {CadCheckListTemplateEnum} from '../../../../../admin/modal/cadCheckListTemplateEnum';
+import {ObjectUtil} from '../../../../../../@core/utils/ObjectUtil';
+import {CadFile} from '../../../../model/CadFile';
+import {Document} from '../../../../../admin/modal/document';
+import {Alert, AlertType} from '../../../../../../@theme/model/Alert';
+import {CreditAdministrationService} from '../../../../service/credit-administration.service';
+import {ToastService} from '../../../../../../@core/utils';
+import {NbDialogRef} from '@nebular/theme';
+import {CadOfferLetterModalComponent} from '../../../../cad-offerletter-profile/cad-offer-letter-modal/cad-offer-letter-modal.component';
+import {RouterUtilsService} from '../../../../utils/router-utils.service';
 
 @Component({
   selector: 'app-guarantor-acknowledgement',
@@ -18,10 +27,24 @@ export class GuarantorAcknowledgementComponent implements OnInit {
 
   constructor(
       private formBuilder: FormBuilder,
+      private administrationService: CreditAdministrationService,
+      private toastService: ToastService,
+      private dialogRef: NbDialogRef<CadOfferLetterModalComponent>,
+      private routerUtilsService: RouterUtilsService,
   ) { }
 
   ngOnInit() {
     this.buildForm();
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
+          this.initialInfoPrint = singleCadFile.initialInformation;
+        }
+      });
+    }
+    if (!ObjectUtil.isEmpty(this.initialInfoPrint)) {
+      this.form.patchValue(JSON.parse(this.initialInfoPrint));
+    }
   }
   private buildForm() {
     this.form = this.formBuilder.group({
@@ -37,6 +60,43 @@ export class GuarantorAcknowledgementComponent implements OnInit {
     });
   }
   submit() {
-
+    this.spinner = true;
+    let flag = true;
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
+          flag = false;
+          singleCadFile.initialInformation = JSON.stringify(this.form.value);
+        }
+      });
+      if (flag) {
+        const cadFile = new CadFile();
+        const document = new Document();
+        cadFile.initialInformation = JSON.stringify(this.form.value);
+        document.id = this.documentId;
+        cadFile.cadDocument = document;
+        cadFile.customerLoanId = this.customerLoanId;
+        this.cadData.cadFileList.push(cadFile);
+      }
+    } else {
+      const cadFile = new CadFile();
+      const document = new Document();
+      cadFile.initialInformation = JSON.stringify(this.form.value);
+      document.id = this.documentId;
+      cadFile.cadDocument = document;
+      cadFile.customerLoanId = this.customerLoanId;
+      this.cadData.cadFileList.push(cadFile);
+    }
+    this.administrationService.saveCadDocumentBulk(this.cadData).subscribe(() => {
+      this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved '));
+      this.dialogRef.close();
+      this.spinner = false;
+      this.routerUtilsService.reloadCadProfileRoute(this.cadData.id);
+    }, error => {
+      console.error(error);
+      this.spinner = false;
+      this.toastService.show(new Alert(AlertType.ERROR, 'Failed to save '));
+      this.dialogRef.close();
+    });
   }
 }
