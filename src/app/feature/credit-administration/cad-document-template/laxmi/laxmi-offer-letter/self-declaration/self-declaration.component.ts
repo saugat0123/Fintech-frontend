7ee4e092023dfc308e458187d3d9e2bce6857dd1
Binit0 <binita.ghimire@sbsolutionsnepal.com>
2022-07-24 15:16:21 +0545

    mapped self declaration legal document

--- src/app/feature/credit-administration/cad-document-template/laxmi/laxmi-offer-letter/self-declaration/self-declaration.component.ts
+++ src/app/feature/credit-administration/cad-document-template/laxmi/laxmi-offer-letter/self-declaration/self-declaration.component.ts
@@ -57,31 +57,14 @@ export class SelfDeclarationComponent implements OnInit {
     if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
       this.cadData.cadFileList.forEach(singleCadFile => {
         if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
-          this.initialInfoPrint = singleCadFile.initialInformation;
-          this.form.patchValue(JSON.parse(singleCadFile.initialInformation));
-          if (!ObjectUtil.isEmpty(JSON.parse(singleCadFile.initialInformation).amount)) {
-            this.amount = JSON.parse(singleCadFile.initialInformation).amount;
-          }
+          this.initialInfoPrint = JSON.parse(singleCadFile.supportedInformation);
         }
       });
     }
     if (!ObjectUtil.isEmpty(this.cadData.loanHolder.nepData)) {
       this.nepaliData = JSON.parse(this.cadData.loanHolder.nepData);
     }
-    if (!ObjectUtil.isEmpty(this.initialInfoPrint)) {
-      this.form.patchValue(JSON.parse(this.initialInfoPrint));
-      this.form.patchValue({
-        amountInWords: this.nepaliCurrencyWordPipe.transform(this.nepaliToEnglishPipe.transform(this.amount)),
-        branch: this.cadData.assignedLoan[0].branch.nepaliName
-      });
-    } else {
-      this.fillNepaliData();
-      this.form.patchValue({
-        amount: this.nepaliNumber.transform(this.amount, 'preeti'),
-        amountInWords: this.nepaliCurrencyWordPipe.transform(this.nepaliToEnglishPipe.transform(this.amount)),
-        branch: this.cadData.assignedLoan[0].branch.nepaliName
-      });
-    }
+    this.fillForm();
   }
   buildForm() {
     this.form = this.formBuilder.group({
@@ -95,18 +78,64 @@ export class SelfDeclarationComponent implements OnInit {
       branch: [undefined],
       nameOfIndividualDeclaration: [undefined],
       permanentAddressOfIndividualDeclaration: [undefined],
+      individualPerMunicipality: [undefined],
+      individualPerWard: [undefined],
+      individualPerTole: [undefined],
       temporaryAddressOfIndividualDeclaration: [undefined],
+      individualTempMunicipality: [undefined],
+      individualTempWard: [undefined],
+      individualTempTole: [undefined],
       nameOfInstitutionDeclaration: [undefined],
       signatureOfProprietor: [undefined],
       permanentAddressOfInstitutionDeclaration: [undefined],
+      institutionPerMunicipality: [undefined],
+      institutionPerWard: [undefined],
+      institutionPerTole: [undefined],
       temporaryAddressOfInstitutionDeclaration: [undefined],
+      institutionTempMunicipality: [undefined],
+      institutionTempWard: [undefined],
+      institutionTempTole: [undefined]
     });
   }
-  fillNepaliData() {
-    if (!ObjectUtil.isEmpty(this.nepaliData)) {
+  setFreeText() {
+    const freeText = {
+      year: this.form.get('year') ? this.form.get('year').value : '',
+      month: this.form.get('month') ? this.form.get('month').value : '',
+      day: this.form.get('day') ? this.form.get('day').value : ''
+    };
+    return JSON.stringify(freeText);
+  }
+  fillForm() {
+    if  (!ObjectUtil.isEmpty(this.nepaliData)) {
       this.form.patchValue({
-        name: this.nepaliData.name,
-        branch: this.cadData.loanHolder.branch.nepaliName
+        year: this.initialInfoPrint ? this.initialInfoPrint.year : '',
+        month: this.initialInfoPrint ? this.initialInfoPrint.month : '',
+        day: this.initialInfoPrint ? this.initialInfoPrint.day : '',
+        name: [undefined],
+        // address: [undefined],
+        amount: this.nepaliData.miscellaneousDetail ? this.nepaliData.miscellaneousDetail.loanAmountInFig : '',
+        amountInWords: this.nepaliData.miscellaneousDetail ? this.nepaliData.miscellaneousDetail.loanAmountInWord : '',
+        branch: this.nepaliData.branchDetail ? this.nepaliData.branchDetail.branchNameInNepali : '',
+        nameOfIndividualDeclaration: this.nepaliData.nepaliName ? this.nepaliData.nepaliName : '',
+        permanentAddressOfIndividualDeclaration: this.nepaliData.customerPermanentAddress ? this.nepaliData.customerPermanentAddress.district : '',
+        individualPerMunicipality: this.nepaliData.customerPermanentAddress ? this.nepaliData.customerPermanentAddress.municipality : '',
+        individualPerWard: this.nepaliData.customerPermanentAddress ? this.nepaliData.customerPermanentAddress.wardNo : '',
+        individualPerTole: this.nepaliData.customerPermanentAddress ? this.nepaliData.customerPermanentAddress.tole : '',
+        temporaryAddressOfIndividualDeclaration: this.nepaliData.customerTemporaryAddress ? this.nepaliData.customerTemporaryAddress.district : '',
+        individualTempMunicipality: this.nepaliData.customerTemporaryAddress ? this.nepaliData.customerTemporaryAddress.municipality : '',
+        individualTempWard: this.nepaliData.customerTemporaryAddress ? this.nepaliData.customerTemporaryAddress.wardNo : '',
+        individualTempTole: this.nepaliData.customerTemporaryAddress ? this.nepaliData.customerTemporaryAddress.tole : '',
+        nameOfInstitutionDeclaration: this.nepaliData.nepaliName ? this.nepaliData.nepaliName : '',
+        signatureOfProprietor: [undefined],
+        permanentAddressOfInstitutionDeclaration: this.nepaliData.institutionRegisteredAddress ? this.nepaliData.institutionRegisteredAddress.district : '',
+        institutionPerMunicipality: this.nepaliData.institutionRegisteredAddress ? this.nepaliData.institutionRegisteredAddress.municipality : '',
+        institutionPerWard: this.nepaliData.institutionRegisteredAddress ? this.nepaliData.institutionRegisteredAddress.wardNo : '',
+        institutionPerTole: this.nepaliData.institutionRegisteredAddress ? this.nepaliData.customerPermanentAddress.tole : '',
+        temporaryAddressOfInstitutionDeclaration: this.nepaliData.institutionCurrentAddress ? this.nepaliData.institutionCurrentAddress.district : '',
+        institutionTempMunicipality: this.nepaliData.institutionCurrentAddress ? this.nepaliData.institutionCurrentAddress.municipality : '',
+        institutionTempWard: this.nepaliData.institutionCurrentAddress ? this.nepaliData.institutionCurrentAddress.wardNo : '',
+        institutionTempTole: this.nepaliData.institutionCurrentAddress ? this.nepaliData.institutionCurrentAddress.tole : '',
+
       });
     }
   }
@@ -117,13 +146,13 @@ export class SelfDeclarationComponent implements OnInit {
       this.cadData.cadFileList.forEach(singleCadFile => {
         if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
           flag = false;
-          singleCadFile.initialInformation = JSON.stringify(this.form.value);
+          singleCadFile.supportedInformation = this.setFreeText();
         }
       });
       if (flag) {
         const cadFile = new CadFile();
         const document = new Document();
-        cadFile.initialInformation = JSON.stringify(this.form.value);
+        cadFile.supportedInformation = this.setFreeText();
         document.id = this.documentId;
         cadFile.cadDocument = document;
         cadFile.customerLoanId = this.customerLoanId;
@@ -132,7 +161,6 @@ export class SelfDeclarationComponent implements OnInit {
     } else {
       const cadFile = new CadFile();
       const document = new Document();
-      cadFile.initialInformation = JSON.stringify(this.form.value);
       document.id = this.documentId;
       cadFile.cadDocument = document;
       cadFile.customerLoanId = this.customerLoanId;
@@ -150,11 +178,10 @@ export class SelfDeclarationComponent implements OnInit {
       this.dialogRef.close();
       this.spinner = false;
     });
-    console.log(this.form.value);
   }
-  amountChange(e) {
-      this.form.patchValue({
-        amountInWords: this.nepaliCurrencyWordPipe.transform(this.nepaliToEnglishPipe.transform(e.target.value))
-      });
-  }
+  // amountChange(e) {
+  //     this.form.patchValue({
+  //       amountInWords: this.nepaliCurrencyWordPipe.transform(this.nepaliToEnglishPipe.transform(e.target.value))
+  //     });
+  // }
 }
