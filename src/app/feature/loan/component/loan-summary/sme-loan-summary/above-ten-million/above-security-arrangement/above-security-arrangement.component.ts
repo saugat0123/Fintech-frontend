080fd966cdfb5fde8bd1ad06aeb0e3a309312bec
KarmaGhalan2 <karma@sbsolutionsnepal.com>
2022-08-17 15:46:03 +0545

    security view for above 10 million customer

--- src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-security-arrangement/above-security-arrangement.component.ts
+++ src/app/feature/loan/component/loan-summary/sme-loan-summary/above-ten-million/above-security-arrangement/above-security-arrangement.component.ts
@@ -26,6 +26,8 @@ export class AboveSecurityArrangementComponent implements OnInit {
   landBuilding = false;
 
   proposedSecurity1: {
+    securityName: string,
+    purpose: string;
     owner: string,
     location: string,
     locationDetail: [{
@@ -33,7 +35,7 @@ export class AboveSecurityArrangementComponent implements OnInit {
       areaFormat: string,
       area: string,
     }],
-    considerValue: number,
+    fairMarketValue: number,
     marketValue: number,
     distressValue: number,
     typeOfProperty: string,
@@ -61,6 +63,20 @@ export class AboveSecurityArrangementComponent implements OnInit {
     },
     valuatorName: string,
     valuationDate: string;
+    landBuilding: {
+      fairMarketValue: number,
+      marketValue: number,
+      distressValue: number
+    }
+    totalFMV: number,
+    totalDV: number,
+    totalMV: number
+    building: {
+      fairMarketValue: number,
+      marketValue: number,
+      distressValue: number
+      area: string
+    }
   } [] = [];
   existingSecurity1: {
     owner: string, location: string, plot: string, area: string,
@@ -95,22 +111,32 @@ export class AboveSecurityArrangementComponent implements OnInit {
         const landDetail = this.formData['initialForm']['landDetails'];
         const landBuildingDetail = this.formData['initialForm']['landBuilding'];
         const apartmentDetail = this.formData['initialForm']['buildingDetails'];
+        const plantDetail = this.formData['initialForm']['plantDetails'];
         this.checkSecurityInSelectedArray('LandSecurity');
-        this.checkSecurityInSelectedArray('Land and Building Security');
-        this.checkSecurityInSelectedArray('ApartmentSecurity');
         if (this.landSelected) {
           landDetail.forEach((d) => {
-           this.getSecurityAndFixedCollateralData(d.uuid, d, 'Land Security');
+            this.getSecurityAndFixedCollateralData(d.uuid, d, 'Land Security');
           });
-
         }
-        // const landBuildings = this.formData['initialForm']['landBuilding'];
+        this.checkSecurityInSelectedArray('Land and Building Security');
         if (this.landBuilding) {
           landBuildingDetail.forEach((d) => {
             this.getSecurityAndFixedCollateralData(d.uuid, d, 'Land and Building Security');
           });
         }
-        console.log('proposedSecurity1', this.proposedSecurity1);
+        this.checkSecurityInSelectedArray('ApartmentSecurity');
+        this.checkSecurityInSelectedArray('PlantSecurity');
+
+        if (this.apartmentSelected) {
+          apartmentDetail.forEach((d) => {
+            this.getSecurityAndFixedCollateralData(d.uuid, d, 'Building Security');
+          });
+        }
+        if (this.plantSelected) {
+          plantDetail.forEach((d) => {
+            this.getSecurityAndFixedCollateralData(d.uuid, d, 'Plant and Machinery Security');
+          });
+        }
       }
     }
   }
@@ -136,69 +162,46 @@ export class AboveSecurityArrangementComponent implements OnInit {
     });
   }
 
-  calcPlantDV(data) {
-    const finalDv = (0.8 * Number(data));
-    console.log('before return', finalDv);
-    return finalDv;
-  }
-
   getSecurityAndFixedCollateralData(uuid: string, d: any, securityName: string) {
-    if (securityName === 'Plant') {
-      this.proposedSecurity1.push({
-        owner: null,
-        location: d.location,
-        locationDetail: null,
-        considerValue: null,
-        marketValue: null,
-        distressValue: null,
-        typeOfProperty: null,
-        plantAndMachinery: true,
-        plantMachineryData: {
-          modelNo: null,
-          amountMV: d.quotationMV,
-          amountDV: d.quotationDv,
-          amountFMV: d.quotation,
-        },
-        collateralData: {
-          zoningType: null,
-          accessRoad: null,
-          setBack: null,
-          visitedBy: null,
-          visitDate: null,
-          coordinate: {
-            longitude: null,
-            latitude: null,
+    let collateralData = null;
+    let DV = 0;
+    let FMV = 0;
+    let MV = 0;
+    switch (securityName) {
+      case 'Land Security':
+        this.collateralSiteVisitService.getCollateralByLatestDateOfVisit(this.securityId, uuid).subscribe((res: any) => {
+          if (!ObjectUtil.isEmpty(res.detail)) {
+            collateralData = JSON.parse(res.detail.siteVisitJsonData);
           }
-        },
-        valuatorName: d.landValuator,
-        valuationDate: d.landValuatorDate
-      });
-    } else {
-      let collateralData = null;
-      this.collateralSiteVisitService.getCollateralByLatestDateOfVisit(this.securityId, uuid).subscribe((res: any) => {
-        if (!ObjectUtil.isEmpty(res.detail)) {
-          collateralData = JSON.parse(res.detail.siteVisitJsonData);
-          console.log('collateralData', collateralData);
-        }
-        if (d.forProposed) {
-          this.totalMV += Number(d.landConsideredValue);
-          this.totalFMV += Number(d.marketValue);
-          this.totalDV += Number(d.distressValue);
+          if (d.plantMachineryChecked) {
+            MV = (ObjectUtil.isEmpty(d.landConsideredValue) ? 0 : Number(d.landConsideredValue)) +
+                (ObjectUtil.isEmpty(d.plantMachineryMV) ? 0 : Number(d.plantMachineryMV));
+            FMV = (ObjectUtil.isEmpty(d.marketValue) ? 0 : Number(d.marketValue)) +
+                (ObjectUtil.isEmpty(d.plantMachineryFMV) ? 0 : Number(d.plantMachineryFMV));
+            DV = (ObjectUtil.isEmpty(d.distressValue) ? 0 : Number(d.distressValue)) +
+                (ObjectUtil.isEmpty(d.plantMachineryDV) ? 0 : Number(d.plantMachineryDV));
+          } else {
+            MV = ObjectUtil.isEmpty(d.landConsideredValue) ? 0 : Number(d.landConsideredValue);
+            FMV = ObjectUtil.isEmpty(d.marketValue) ? 0 : Number(d.marketValue);
+            DV = ObjectUtil.isEmpty(d.distressValue) ? 0 : Number(d.distressValue);
+          }
 
           this.proposedSecurity1.push({
+            securityName: securityName,
+            purpose: ObjectUtil.isEmpty(d.forProposed) ? false : d.forProposed,
             owner: d.owner,
             location: d.location,
             locationDetail: d.locationDetail,
-            considerValue: d.landConsideredValue,
-            marketValue: d.marketValue,
-            distressValue: d.distressValue,
+            fairMarketValue: ObjectUtil.isEmpty(d.marketValue) ? 0 : d.marketValue,
+            marketValue: ObjectUtil.isEmpty(d.landConsideredValue) ? 0 : d.landConsideredValue,
+            distressValue: ObjectUtil.isEmpty(d.distressValue) ? 0 : d.distressValue,
             typeOfProperty: d.typeOfProperty,
             plantAndMachinery: d.plantMachineryChecked,
             plantMachineryData: {
               modelNo: d.plantMachineryModel,
-              amountMV: d.plantMachineryMV,
-              amountFMV: d.plantMachineryFMV,
-              amountDV: d.plantMachineryDV
+              amountMV: ObjectUtil.isEmpty(d.plantMachineryMV) ? 0 : d.plantMachineryMV,
+              amountFMV: ObjectUtil.isEmpty(d.plantMachineryFMV) ? 0 : d.plantMachineryFMV,
+              amountDV: ObjectUtil.isEmpty(d.plantMachineryDV) ? 0 : d.plantMachineryDV,
             },
             collateralData: {
               zoningType: ObjectUtil.isEmpty(collateralData) ? null : collateralData.typeOfProperty,
@@ -216,28 +219,207 @@ export class AboveSecurityArrangementComponent implements OnInit {
               }
             },
             valuatorName: d.landValuator,
-            valuationDate: d.landValuatorDate
+            valuationDate: d.landValuatorDate,
+            landBuilding: {
+              fairMarketValue: null,
+              marketValue: null,
+              distressValue: null,
+            },
+            totalFMV: FMV,
+            totalDV: DV,
+            totalMV: MV,
+            building: {
+              fairMarketValue: null,
+              marketValue: null,
+              distressValue: null,
+              area: null,
+            }
           });
+        });
+        break;
+      case 'Land and Building Security':
+        this.collateralSiteVisitService.getCollateralByLatestDateOfVisit(this.securityId, uuid).subscribe((res: any) => {
+          if (!ObjectUtil.isEmpty(res.detail)) {
+            collateralData = JSON.parse(res.detail.siteVisitJsonData);
+          }
+          if (d.plantMachineryChecked) {
+            MV = (ObjectUtil.isEmpty(d.landConsideredValue) ? 0 : Number(d.landConsideredValue)) +
+                (ObjectUtil.isEmpty(d.plantMachineryMV) ? 0 : Number(d.plantMachineryMV)) +
+                (ObjectUtil.isEmpty(d.considerValue) ? 0 : Number(d.considerValue));
+            FMV = (ObjectUtil.isEmpty(d.marketValue) ? 0 : Number(d.marketValue)) +
+                (ObjectUtil.isEmpty(d.plantMachineryFMV) ? 0 : Number(d.plantMachineryFMV)) +
+                (ObjectUtil.isEmpty(d.totalCost) ? 0 : Number(d.totalCost));
+            DV = (ObjectUtil.isEmpty(d.distressValue) ? 0 : Number(d.distressValue)) +
+                (ObjectUtil.isEmpty(d.plantMachineryDV) ? 0 : Number(d.plantMachineryDV)) +
+                (ObjectUtil.isEmpty(d.buildingDistressValue) ? 0 : Number(d.buildingDistressValue));
+          } else {
+            MV = (ObjectUtil.isEmpty(d.landConsideredValue) ? 0 : Number(d.landConsideredValue)) +
+                (ObjectUtil.isEmpty(d.considerValue) ? 0 : Number(d.considerValue));
+            FMV = (ObjectUtil.isEmpty(d.marketValue) ? 0 : Number(d.marketValue)) +
+                (ObjectUtil.isEmpty(d.totalCost) ? 0 : Number(d.totalCost));
+            DV = (ObjectUtil.isEmpty(d.distressValue) ? 0 : Number(d.distressValue)) +
+                (ObjectUtil.isEmpty(d.buildingDistressValue) ? 0 : Number(d.buildingDistressValue));
+          }
 
-        }
-
-
-        if (d.forExisting) {
-          this.totalMVEx += Number(d.landConsideredValue);
-          this.totalFMVEx += Number(d.marketValue);
-          this.totalDVEx += Number(d.distressValue);
-          this.existingSecurity1.push({
+          this.proposedSecurity1.push({
+            securityName: securityName,
+            purpose: ObjectUtil.isEmpty(d.forProposed) ? false : d.forProposed,
             owner: d.owner,
             location: d.location,
-            plot: d.plotNumber,
-            area: d.areaFormat,
-            considerValue: d.landConsideredValue,
-            marketValue: d.marketValue,
-            distressValue: d.distressValue,
+            locationDetail: d.locationDetail,
+            fairMarketValue: ObjectUtil.isEmpty(d.marketValue) ? 0 : d.marketValue,
+            marketValue: ObjectUtil.isEmpty(d.landConsideredValue) ? 0 : d.landConsideredValue,
+            distressValue: ObjectUtil.isEmpty(d.distressValue) ? 0 : d.distressValue,
+            typeOfProperty: d.typeOfProperty,
+            plantAndMachinery: d.plantMachineryChecked,
+            plantMachineryData: {
+              modelNo: d.plantMachineryModel,
+              amountMV: ObjectUtil.isEmpty(d.plantMachineryMV) ? 0 : d.plantMachineryMV,
+              amountFMV: ObjectUtil.isEmpty(d.plantMachineryFMV) ? 0 : d.plantMachineryFMV,
+              amountDV: ObjectUtil.isEmpty(d.plantMachineryDV) ? 0 : d.plantMachineryDV,
+            },
+            collateralData: {
+              zoningType: ObjectUtil.isEmpty(collateralData) ? null : collateralData.typeOfProperty,
+              accessRoad: ObjectUtil.isEmpty(collateralData) ? null : collateralData.roadAccessFrom,
+              setBack: {
+                road: ObjectUtil.isEmpty(collateralData) ? null : collateralData.roadSetbacks,
+                river: ObjectUtil.isEmpty(collateralData) ? null : collateralData.riverOrCanalSetbacks,
+                highTension: ObjectUtil.isEmpty(collateralData) ? null : collateralData.highTensionSetbacks
+              },
+              visitedBy: ObjectUtil.isEmpty(collateralData) ? null : collateralData.personContacted,
+              visitDate: ObjectUtil.isEmpty(collateralData) ? null : collateralData.date,
+              coordinate: {
+                longitude: ObjectUtil.isEmpty(collateralData) ? null : collateralData.fixedAssetsLongitude,
+                latitude: ObjectUtil.isEmpty(collateralData) ? null : collateralData.fixedAssetsLatitude,
+              }
+            },
+            valuatorName: d.landValuator,
+            valuationDate: d.landValuatorDate,
+            landBuilding: {
+              fairMarketValue: ObjectUtil.isEmpty(d.totalCost) ? 0 : Number(d.totalCost),
+              marketValue: ObjectUtil.isEmpty(d.considerValue) ? 0 : Number(d.considerValue),
+              distressValue: ObjectUtil.isEmpty(d.buildingDistressValue) ? 0 : Number(d.buildingDistressValue),
+            },
+            totalFMV: FMV,
+            totalDV: DV,
+            totalMV: MV,
+            building: {
+              fairMarketValue: null,
+              marketValue: null,
+              distressValue: null,
+              area: null,
+            }
           });
+        });
+        break;
+      case 'Building Security':
+        this.collateralSiteVisitService.getCollateralByLatestDateOfVisit(this.securityId, uuid).subscribe((res: any) => {
+          if (!ObjectUtil.isEmpty(res.detail)) {
+            collateralData = JSON.parse(res.detail.siteVisitJsonData);
+          }
+          if (d.plantMachineryChecked) {
+            MV = (ObjectUtil.isEmpty(d.plantMachineryMV) ? 0 : Number(d.plantMachineryMV)) +
+                (ObjectUtil.isEmpty(d.totalCost) ? 0 : Number(d.totalCost));
+            FMV = (ObjectUtil.isEmpty(d.plantMachineryFMV) ? 0 : Number(d.plantMachineryFMV)) +
+                (ObjectUtil.isEmpty(d.buildingFairMarketValue) ? 0 : Number(d.buildingFairMarketValue));
+            DV = (ObjectUtil.isEmpty(d.plantMachineryDV) ? 0 : Number(d.plantMachineryDV)) +
+                (ObjectUtil.isEmpty(d.buildingDistressValue) ? 0 : Number(d.buildingDistressValue));
+          } else {
+            MV = ObjectUtil.isEmpty(d.totalCost) ? 0 : Number(d.totalCost);
+            FMV = ObjectUtil.isEmpty(d.buildingFairMarketValue) ? 0 : Number(d.buildingFairMarketValue);
+            DV = ObjectUtil.isEmpty(d.buildingDistressValue) ? 0 : d.buildingDistressValue;
+          }
 
-        }
-      });
+          this.proposedSecurity1.push({
+            purpose: ObjectUtil.isEmpty(d.forProposed) ? false : d.forProposed,
+            securityName: securityName,
+            owner: d.buildingName,
+            location: null,
+            locationDetail: null,
+            fairMarketValue: null,
+            marketValue: null,
+            distressValue: null,
+            typeOfProperty: d.typeOfProperty,
+            plantAndMachinery: d.plantMachineryChecked,
+            plantMachineryData: {
+              modelNo: d.plantMachineryModel,
+              amountMV: ObjectUtil.isEmpty(d.plantMachineryMV) ? 0 : d.plantMachineryMV,
+              amountFMV: ObjectUtil.isEmpty(d.plantMachineryFMV) ? 0 : d.plantMachineryFMV,
+              amountDV: ObjectUtil.isEmpty(d.plantMachineryDV) ? 0 : d.plantMachineryDV,
+            },
+            collateralData: {
+              zoningType: ObjectUtil.isEmpty(collateralData) ? null : collateralData.typeOfProperty,
+              accessRoad: ObjectUtil.isEmpty(collateralData) ? null : collateralData.roadAccessFrom,
+              setBack: {
+                road: ObjectUtil.isEmpty(collateralData) ? null : collateralData.roadSetbacks,
+                river: ObjectUtil.isEmpty(collateralData) ? null : collateralData.riverOrCanalSetbacks,
+                highTension: ObjectUtil.isEmpty(collateralData) ? null : collateralData.highTensionSetbacks
+              },
+              visitedBy: ObjectUtil.isEmpty(collateralData) ? null : collateralData.personContacted,
+              visitDate: ObjectUtil.isEmpty(collateralData) ? null : collateralData.date,
+              coordinate: {
+                longitude: ObjectUtil.isEmpty(collateralData) ? null : collateralData.fixedAssetsLongitude,
+                latitude: ObjectUtil.isEmpty(collateralData) ? null : collateralData.fixedAssetsLatitude,
+              }
+            },
+            valuatorName: d.landValuator,
+            valuationDate: d.landValuatorDate,
+            landBuilding: {
+              fairMarketValue: null,
+              marketValue: null,
+              distressValue: null,
+            },
+            totalFMV: FMV,
+            totalDV: DV,
+            totalMV: MV,
+            building: {
+              fairMarketValue: ObjectUtil.isEmpty(d.buildingFairMarketValue) ? 0 : d.buildingFairMarketValue,
+              marketValue: ObjectUtil.isEmpty(d.totalCost) ? 0 : d.totalCost,
+              distressValue: ObjectUtil.isEmpty(d.buildingDistressValue) ? 0 : d.buildingDistressValue,
+              area: d.buildingArea,
+            }
+          });
+        });
+
+        break;
+      case 'Plant and Machinery Security':
+        this.proposedSecurity1.push({
+          securityName: securityName,
+          purpose: d.forProposed,
+          owner: null,
+          location: d.location,
+          locationDetail: null,
+          fairMarketValue: null,
+          marketValue: null,
+          distressValue: null,
+          typeOfProperty: null,
+          plantAndMachinery: true,
+          plantMachineryData: {
+            modelNo: null,
+            amountMV: ObjectUtil.isEmpty(d.quotationMV) ? 0 : d.quotationMV,
+            amountDV: ObjectUtil.isEmpty(d.quotationDv) ? 0 : d.quotationDv,
+            amountFMV: ObjectUtil.isEmpty(d.quotation) ? 0 : d.quotation,
+          },
+          collateralData: {
+            zoningType: null,
+            accessRoad: null,
+            setBack: null,
+            visitedBy: null,
+            visitDate: null,
+            coordinate: {
+              longitude: null,
+              latitude: null,
+            }
+          },
+          valuatorName: d.landValuator,
+          valuationDate: d.landValuatorDate,
+          landBuilding: null,
+          totalMV: ObjectUtil.isEmpty(d.quotationMV) ? 0 : d.quotationMV,
+          totalDV: ObjectUtil.isEmpty(d.quotationDv) ? 0 : d.quotationDv,
+          totalFMV: ObjectUtil.isEmpty(d.quotation) ? 0 : d.quotation,
+          building: null,
+        });
+        break;
     }
   }
 }
