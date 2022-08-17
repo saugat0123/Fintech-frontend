96509733776ec0f451df9135945c08f1fd56891a
nirajand1234 <loadbasic1>
2022-08-17 11:04:31 +0545

    kalo suchi borrower pg company component legal document template complete.

--- src/app/feature/credit-administration/cad-document-template/mega/mega.module.ts
+++ src/app/feature/credit-administration/cad-document-template/mega/mega.module.ts
@@ -18,6 +18,7 @@ import {PromissoryNoteCompanyComponent} from './promissory-note-company/promisso
 import {PromissoryNoteJointComponent} from './promissory-note-joint/promissory-note-joint.component';
 import {KaloSuchiBorrowerCompanyComponent} from './kalo-suchi-borrower-company/kalo-suchi-borrower-company.component';
 import {NgxPrintModule} from 'ngx-print';
+import {KaloSuchiBorrowerPgCompanyComponent} from './kalo-suchi-borrower-pg-company/kalo-suchi-borrower-pg-company.component';
 
 @NgModule({
   declarations: [LoanDeedCompanyComponent,
@@ -29,13 +30,14 @@ import {NgxPrintModule} from 'ngx-print';
     PromissoryNoteSingleBorrowerComponent,
     AssignmentOfReceivableComponent,
     KaloSuchiBorrowerCompanyComponent,
+    KaloSuchiBorrowerPgCompanyComponent,
     DeedHypoOfMachineryComponent,
     HypoOfStockComponent,
     LoanDeedMultipleComponent,
     LoanDeedIndividualComponent,
     PromissoryNoteCompanyComponent,
     PromissoryNoteJointComponent,
-    KaloSuchiBorrowerCompanyComponent],
+    ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -58,7 +60,8 @@ import {NgxPrintModule} from 'ngx-print';
     LoanDeedIndividualComponent,
     PromissoryNoteCompanyComponent,
     PromissoryNoteJointComponent,
-    KaloSuchiBorrowerCompanyComponent
+    KaloSuchiBorrowerCompanyComponent,
+    KaloSuchiBorrowerPgCompanyComponent
   ],
   entryComponents: [DeedHypoOfMachineryComponent
   ]
