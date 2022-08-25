8e3a2952754fd29089b6f3ccd19eb3122dd0f6d2
Karma Lama <karma@sbsolutionsnepal.com>
2022-08-25 10:12:07 +0000

    Consent letter legal document template recovered and template is completed

---
+++ src/app/feature/credit-administration/cad-document-template/mega/consent-letter-company/consent-letter-company.component.ts
@@ -0,0 +1,123 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CustomerApprovedLoanCadDocumentation} from '../../../model/customerApprovedLoanCadDocumentation';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {CreditAdministrationService} from '../../../service/credit-administration.service';
+import {ToastService} from '../../../../../@core/utils';
+import {NbDialogRef} from '@nebular/theme';
+import {CadOfferLetterModalComponent} from '../../../cad-offerletter-profile/cad-offer-letter-modal/cad-offer-letter-modal.component';
+import {RouterUtilsService} from '../../../utils/router-utils.service';
+import {ObjectUtil} from '../../../../../@core/utils/ObjectUtil';
+import {CadFile} from '../../../model/CadFile';
+import {Document} from '../../../../admin/modal/document';
+import {Alert, AlertType} from '../../../../../@theme/model/Alert';
+
+@Component({
+  selector: 'app-consent-letter-company',
+  templateUrl: './consent-letter-company.component.html',
+  styleUrls: ['./consent-letter-company.component.scss']
+})
+export class ConsentLetterCompanyComponent implements OnInit {
+
+
+  @Input() cadData: CustomerApprovedLoanCadDocumentation;
+  @Input() documentId: number;
+  @Input() customerLoanId: number;
+  consentLetterCompany: FormGroup;
+  nepData;
+
+  constructor(private formBuilder: FormBuilder,
+              private administrationService: CreditAdministrationService,
+              private toastService: ToastService,
+              private dialogRef: NbDialogRef<CadOfferLetterModalComponent>,
+              private routerUtilsService: RouterUtilsService
+  ) {
+  }
+
+  ngOnInit() {
+    this.buildForm();
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
+          this.consentLetterCompany.patchValue(JSON.parse(singleCadFile.initialInformation));
+        }
+      });
+    }
+    if (!ObjectUtil.isEmpty(this.cadData.loanHolder.nepData)) {
+      this.nepData = JSON.parse(this.cadData.loanHolder.nepData);
+    }
+  }
+
+  buildForm() {
+    this.consentLetterCompany = this.formBuilder.group({
+      companyAct: [undefined],
+      landOwnerMunicipality: [undefined],
+      branchOffice: [undefined],
+      ministryOffice: [undefined],
+      CompanyRegistrationDate: [undefined],
+      registrationNum: [undefined],
+      institutionRegisteredDistrict: [undefined],
+      institutionRegisteredVdcOrMunicipality: [undefined],
+      institutionRegisteredWard: [undefined],
+      borrowerName: [undefined],
+      loanAmount: [undefined],
+      amountInWords: [undefined],
+      gurantorName: [undefined],
+      landOwnerName: [undefined],
+      landOwnerDistrict: [undefined],
+      landOwnerWard: [undefined],
+      landOwnerTole: [undefined],
+      landOwnerName1: [undefined],
+      landArea: [undefined],
+      fullYear: [undefined],
+      monthOfYear: [undefined],
+      dayOfMonth: [undefined],
+      timeOfDay: [undefined],
+      witnessOne: [undefined],
+      witnessTwo: [undefined],
+      agreementPerson1: [undefined],
+      relationWithBorrower: [undefined],
+      agreementPerson2: [undefined],
+      relationWithBorrower1: [undefined],
+    });
+  }
+
+  submit() {
+    let flag = true;
+    if (!ObjectUtil.isEmpty(this.cadData) && !ObjectUtil.isEmpty(this.cadData.cadFileList)) {
+      this.cadData.cadFileList.forEach(singleCadFile => {
+        if (singleCadFile.customerLoanId === this.customerLoanId && singleCadFile.cadDocument.id === this.documentId) {
+          flag = false;
+          singleCadFile.initialInformation = JSON.stringify(this.consentLetterCompany.value);
+        }
+      });
+      if (flag) {
+        const cadFile = new CadFile();
+        const document = new Document();
+        cadFile.initialInformation = JSON.stringify(this.consentLetterCompany.value);
+        document.id = this.documentId;
+        cadFile.cadDocument = document;
+        cadFile.customerLoanId = this.customerLoanId;
+        this.cadData.cadFileList.push(cadFile);
+      }
+    } else {
+      const cadFile = new CadFile();
+      const document = new Document();
+      cadFile.initialInformation = JSON.stringify(this.consentLetterCompany.value);
+      document.id = this.documentId;
+      cadFile.cadDocument = document;
+      cadFile.customerLoanId = this.customerLoanId;
+      this.cadData.cadFileList.push(cadFile);
+    }
+
+    this.administrationService.saveCadDocumentBulk(this.cadData).subscribe(() => {
+      this.toastService.show(new Alert(AlertType.SUCCESS, 'Successfully saved Offer Letter'));
+      this.dialogRef.close();
+      this.routerUtilsService.reloadCadProfileRoute(this.cadData.id);
+    }, error => {
+      console.error(error);
+      this.toastService.show(new Alert(AlertType.ERROR, 'Failed to save Offer Letter'));
+      this.dialogRef.close();
+    });
+  }
+
+}
