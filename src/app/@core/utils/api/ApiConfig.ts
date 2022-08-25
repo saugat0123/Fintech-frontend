1b964d0e53ce95968998c6887594a9fe1d8d7b92
RameshBhandari <ramesh.bhandari@sbsolutionsnepal.com>
2022-08-25 17:11:11 +0545

    Multiple collateral in one-form

--- src/app/@core/utils/api/ApiConfig.ts
+++ src/app/@core/utils/api/ApiConfig.ts
@@ -1,13 +1,23 @@
 export class ApiConfig {
     public static TOKEN = 'http://localhost:8086/oauth/token';
     public static URL = 'http://localhost:8086';
+    //
+    // public static TOKEN = 'http://3.17.141.198:8080/LAS/oauth/token';
+    // public static URL = 'http://3.17.141.198:8080/LAS';
 
-    // Laxmi Micro Finance
-    public static MICRO_URL = 'http://103.198.9.175:8080/laxmi_mf';
+    // United Finance server info
+    // public static TOKEN = 'http://192.168.1.217:8080/oauth/token';
+    // public static URL = 'http://192.168.1.217:8080';
 
-    // Remit App UAT
-    public static REMIT_WEB = 'http://103.94.159.180/api';
+    // Mahalaxmi Bank Limited
+    // public static TOKEN = 'http://192.168.0.90:8086/oauth/token';
+    // public static URL = 'http://192.168.0.90:8086';
 
-    // Remit App Live
-    // public static REMIT_WEB ='https://remit.laxmibank.com/api';
+    // Tinau Mission server info
+    // public static TOKEN = 'http://192.168.1.119:8080/oauth/token';
+    // public static URL = 'http://192.168.1.119:8080';
+
+    // Shine Resunga server info
+    // public static TOKEN = 'http://192.168.0.33:8080/oauth/token';
+    // public static URL = 'http://192.168.0.33:8080';
 }
