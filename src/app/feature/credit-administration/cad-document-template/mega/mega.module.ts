72dcc270e2fa2787a653d7e014f4ad650f8c0dfc
Sulav Shrestha <sulavstha2629@gmail.com>
2022-08-28 17:42:46 +0545

    Kalo suchi borrower proprietorship template completed.

--- src/app/feature/credit-administration/cad-document-template/mega/mega.module.ts
+++ src/app/feature/credit-administration/cad-document-template/mega/mega.module.ts
@@ -60,10 +60,13 @@ import {ConsentLetterCompanyComponent} from './consent-letter-company/consent-le
 import {VehicleLoanDeedCompanyComponent} from './vehicle-loan-deed-company/vehicle-loan-deed-company.component';
 import {ConsentLetterIndividualComponent} from './consent-letter-individual/consent-letter-individual.component';
 import {KaloSuchiBorrowerIndividualComponent} from './kalo-suchi-borrower-individual/kalo-suchi-borrower-individual.component';
-import { VehicleLoanDeedIndividualComponent } from './vehicle-loan-deed-individual/vehicle-loan-deed-individual.component';
-import { KaloSuchiBorrowerPartnershipComponent } from './kalo-suchi-borrower-partnership/kalo-suchi-borrower-partnership.component';
+import {VehicleLoanDeedIndividualComponent} from './vehicle-loan-deed-individual/vehicle-loan-deed-individual.component';
+import {KaloSuchiBorrowerPartnershipComponent} from './kalo-suchi-borrower-partnership/kalo-suchi-borrower-partnership.component';
+import {
+    GeneralLetterOfHypothecationCompanyComponent
+} from './general-letter-of-hypothecation-company/general-letter-of-hypothecation-company.component';
+import {KaloSuchiBorrowerProprietorshipComponent} from './kalo-suchi-borrower-proprietorship/kalo-suchi-borrower-proprietorship.component';
 
-import {GeneralLetterOfHypothecationCompanyComponent} from './general-letter-of-hypothecation-company/general-letter-of-hypothecation-company.component';
 @NgModule({
     declarations: [PersonalGuaranteeJointBorrowerComponent,
         PersonalGuaranteeCompanyComponent,
@@ -109,6 +112,7 @@ import {GeneralLetterOfHypothecationCompanyComponent} from './general-letter-of-
         AssignmentOfReceivablesProprietorshipComponent,
         VehicleLoanDeedIndividualComponent,
         KaloSuchiBorrowerPartnershipComponent,
+        KaloSuchiBorrowerProprietorshipComponent,
     ],
     imports: [
         CommonModule,
@@ -161,9 +165,11 @@ import {GeneralLetterOfHypothecationCompanyComponent} from './general-letter-of-
         KaloSuchiBorrowerIndividualComponent,
         GeneralLetterOfHypothecationCompanyComponent,
         VehicleLoanDeedIndividualComponent,
-        KaloSuchiBorrowerPartnershipComponent
+        KaloSuchiBorrowerPartnershipComponent,
+        KaloSuchiBorrowerProprietorshipComponent,
     ],
 })
 
-export class MegaModule { }
+export class MegaModule {
+}
 
