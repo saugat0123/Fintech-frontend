e411d39fc1cfa392af2493afb0d910009ab39142
ramesh.mandal <ramesh.mandal@sbsolutionsnepal.com>
2022-09-11 15:36:28 +0545

    Rename agriDSL without Collateral to agriDSL upto 0.5 million

--- src/app/feature/customer/model/customerCategory.ts
+++ src/app/feature/customer/model/customerCategory.ts
@@ -4,8 +4,8 @@ export enum CustomerCategory {
     SME_ABOVE_TEN_MILLION = 'SME above NPR 10 Million',
     AGRICULTURE_UPTO_TWO_MILLION = 'AgriDSL up to 2 Million',
     AGRICULTURE_TWO_TO_TEN_MILLION = 'AgriDSL above 2 million to 10 Million',
+    AGRICULTURE_UPTO_ZERO_POINT_FIVE_MILLION = 'AgriDSL up to 0.5 Million',
     AGRICULTURE_ABOVE_TEN_MILLION = 'AgriDSL above 10 Million',
-    AGRICULTURE_UPTO_ZERO_POINT_FIVE_MILLION = 'AgriDSL upto 0.5 Million',
     DSL_WHOLE_SALE = 'DSL Whole Sale',
 }
 
