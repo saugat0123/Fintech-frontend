c3c75ad265a7bc8aad94e15ae6de74199be7faab
i_am_saugatdahal <saughat.dahal@sbsolutionsnepal.com>
2022-09-14 10:03:44 +0545

    changed font style of documents obtained.

--- src/app/feature/loan/model/documentsObtainedTable.ts
+++ src/app/feature/loan/model/documentsObtainedTable.ts
@@ -20,63 +20,63 @@ export class DocumentsObtainedTable {
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">1.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Loan Application</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Loan Application</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">2.</td>\n' +
-            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Board Minute/ Special AGM Minute</span></span></td>\n' +
+            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Board Minute/ Special AGM Minute</span></span></td>\n' +
             '\t\t\t<td>&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">3.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Quarterly Report</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Quarterly Report</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">4.</td>\n' +
-            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Company Registration</span></span></td>\n' +
+            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Company Registration</span></span></td>\n' +
             '\t\t\t<td>&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">5.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">PAN Certificate</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">PAN Certificate</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">6.</td>\n' +
-            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">NRB Registration License</span></span></td>\n' +
+            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">NRB Registration License</span></span></td>\n' +
             '\t\t\t<td>&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">7.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Letter of Setoff</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Letter of Setoff</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">8.</td>\n' +
-            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Latest tax clearance certificate</span></span></td>\n' +
+            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Latest tax clearance certificate</span></span></td>\n' +
             '\t\t\t<td>&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">9.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">AOA and MOA of the company</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">AOA and MOA of the company</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">10.</td>\n' +
-            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Offer Letter</span></span></td>\n' +
+            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Offer Letter</span></span></td>\n' +
             '\t\t\t<td>&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">11.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Loan Deed</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Loan Deed</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">12.</td>\n' +
-            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Declaration of loan utilization</span></span></td>\n' +
+            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Declaration of loan utilization</span></span></td>\n' +
             '\t\t\t<td>&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">13.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Promissory Note</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Promissory Note</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">14.</td>\n' +
-            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">CIC Reports</span></span></td>\n' +
+            '\t\t\t<td style="width:536px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">CIC Reports</span></span></td>\n' +
             '\t\t\t<td>&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:45px">15.</td>\n' +
-            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Other if required..</span></span></td>\n' +
+            '\t\t\t<td style="width:505px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Other if required..</span></span></td>\n' +
             '\t\t\t<td style="width:192px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:51px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:536px">&nbsp;</td>\n' +
@@ -107,10 +107,10 @@ export class DocumentsObtainedTable {
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td>1.</td>\n' +
-            '\t\t\t<td style="white-space:nowrap; width:517px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Hypothecation over entire assets of the Company Incuding Deprived Sector Lending</span></span></td>\n' +
+            '\t\t\t<td style="white-space:nowrap; width:517px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Hypothecation over entire assets of the Company Incuding Deprived Sector Lending</span></span></td>\n' +
             '\t\t\t<td style="width:195px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:48px">2.</td>\n' +
-            '\t\t\t<td style="width:541px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Assignment of Loan Accounts of the company</span></span></td>\n' +
+            '\t\t\t<td style="width:541px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Assignment of Loan Accounts of the company</span></span></td>\n' +
             '\t\t\t<td style="width:219px">&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t</tbody>\n' +
@@ -138,10 +138,10 @@ export class DocumentsObtainedTable {
             '\t\t</tr>\n' +
             '\t\t<tr>\n' +
             '\t\t\t<td style="width:32px">1.</td>\n' +
-            '\t\t\t<td style="width:521px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Corporate/ personal guarantee of</span></span></td>\n' +
+            '\t\t\t<td style="width:521px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Corporate/ personal guarantee of</span></span></td>\n' +
             '\t\t\t<td style="width:198px">&nbsp;</td>\n' +
             '\t\t\t<td style="width:44px">2.</td>\n' +
-            '\t\t\t<td style="width:545px"><span style="font-size:11.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Authority to sign on Corporate Guarantee</span></span></td>\n' +
+            '\t\t\t<td style="width:545px"><span style="font-size:11.0pt"><span style="font-family:&quot;Arial&quot;,serif">Authority to sign on Corporate Guarantee</span></span></td>\n' +
             '\t\t\t<td style="width:214px">&nbsp;</td>\n' +
             '\t\t</tr>\n' +
             '\t</tbody>\n' +
